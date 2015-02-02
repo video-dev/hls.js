f95d720070c80b08b4151165279561533b5730dc
mangui <g.du.pontavice@gmail.com>
2015-02-02 10:51:14 +0100

    move readSequenceParameterSet() to exp-golomb.js

--- src/transmuxer.js
+++ src/transmuxer.js
@@ -621,7 +621,8 @@ H264Stream = function() {
       break;
     case 0x07:
       event.nalUnitType = 'seq_parameter_set_rbsp';
-      event.config = readSequenceParameterSet(data.subarray(1));
+      var expGolombDecoder = new hls.ExpGolomb(data.subarray(1));
+      event.config = expGolombDecoder.readSequenceParameterSet();
       break;
     case 0x08:
       event.nalUnitType = 'pic_parameter_set_rbsp';
@@ -638,136 +639,6 @@ H264Stream = function() {
 
   this.end = function() {
     nalByteStream.end();
-  };
-
-  /**
-   * Advance the ExpGolomb decoder past a scaling list. The scaling
-   * list is optionally transmitted as part of a sequence parameter
-   * set and is not relevant to transmuxing.
-   * @param count {number} the number of entries in this scaling list
-   * @param expGolombDecoder {object} an ExpGolomb pointed to the
-   * start of a scaling list
-   * @see Recommendation ITU-T H.264, Section 7.3.2.1.1.1
-   */
-  skipScalingList = function(count, expGolombDecoder) {
-    var
-      lastScale = 8,
-      nextScale = 8,
-      j,
-      deltaScale;
-
-    for (j = 0; j < count; j++) {
-      if (nextScale !== 0) {
-        deltaScale = expGolombDecoder.readExpGolomb();
-        nextScale = (lastScale + deltaScale + 256) % 256;
-      }
-
-      lastScale = (nextScale === 0) ? lastScale : nextScale;
-    }
-  };
-
-  /**
-   * Read a sequence parameter set and return some interesting video
-   * properties. A sequence parameter set is the H264 metadata that
-   * describes the properties of upcoming video frames.
-   * @param data {Uint8Array} the bytes of a sequence parameter set
-   * @return {object} an object with configuration parsed from the
-   * sequence parameter set, including the dimensions of the
-   * associated video frames.
-   */
-  readSequenceParameterSet = function(data) {
-    var
-      frameCropLeftOffset = 0,
-      frameCropRightOffset = 0,
-      frameCropTopOffset = 0,
-      frameCropBottomOffset = 0,
-      expGolombDecoder, profileIdc, levelIdc, profileCompatibility,
-      chromaFormatIdc, picOrderCntType,
-      numRefFramesInPicOrderCntCycle, picWidthInMbsMinus1,
-      picHeightInMapUnitsMinus1,
-      frameMbsOnlyFlag,
-      scalingListCount,
-      i;
-
-    expGolombDecoder = new hls.ExpGolomb(data);
-    profileIdc = expGolombDecoder.readUnsignedByte(); // profile_idc
-    profileCompatibility = expGolombDecoder.readBits(5); // constraint_set[0-5]_flag
-    expGolombDecoder.skipBits(3); //  u(1), reserved_zero_2bits u(2)
-    levelIdc = expGolombDecoder.readUnsignedByte(); // level_idc u(8)
-    expGolombDecoder.skipUnsignedExpGolomb(); // seq_parameter_set_id
-
-    // some profiles have more optional data we don't need
-    if (profileIdc === 100 ||
-        profileIdc === 110 ||
-        profileIdc === 122 ||
-        profileIdc === 244 ||
-        profileIdc === 44 ||
-        profileIdc === 83 ||
-        profileIdc === 86 ||
-        profileIdc === 118 ||
-        profileIdc === 128) {
-      chromaFormatIdc = expGolombDecoder.readUnsignedExpGolomb();
-      if (chromaFormatIdc === 3) {
-        expGolombDecoder.skipBits(1); // separate_colour_plane_flag
-      }
-      expGolombDecoder.skipUnsignedExpGolomb(); // bit_depth_luma_minus8
-      expGolombDecoder.skipUnsignedExpGolomb(); // bit_depth_chroma_minus8
-      expGolombDecoder.skipBits(1); // qpprime_y_zero_transform_bypass_flag
-      if (expGolombDecoder.readBoolean()) { // seq_scaling_matrix_present_flag
-        scalingListCount = (chromaFormatIdc !== 3) ? 8 : 12;
-        for (i = 0; i < scalingListCount; i++) {
-          if (expGolombDecoder.readBoolean()) { // seq_scaling_list_present_flag[ i ]
-            if (i < 6) {
-              skipScalingList(16, expGolombDecoder);
-            } else {
-              skipScalingList(64, expGolombDecoder);
-            }
-          }
-        }
-      }
-    }
-
-    expGolombDecoder.skipUnsignedExpGolomb(); // log2_max_frame_num_minus4
-    picOrderCntType = expGolombDecoder.readUnsignedExpGolomb();
-
-    if (picOrderCntType === 0) {
-      expGolombDecoder.readUnsignedExpGolomb(); //log2_max_pic_order_cnt_lsb_minus4
-    } else if (picOrderCntType === 1) {
-      expGolombDecoder.skipBits(1); // delta_pic_order_always_zero_flag
-      expGolombDecoder.skipExpGolomb(); // offset_for_non_ref_pic
-      expGolombDecoder.skipExpGolomb(); // offset_for_top_to_bottom_field
-      numRefFramesInPicOrderCntCycle = expGolombDecoder.readUnsignedExpGolomb();
-      for(i = 0; i < numRefFramesInPicOrderCntCycle; i++) {
-        expGolombDecoder.skipExpGolomb(); // offset_for_ref_frame[ i ]
-      }
-    }
-
-    expGolombDecoder.skipUnsignedExpGolomb(); // max_num_ref_frames
-    expGolombDecoder.skipBits(1); // gaps_in_frame_num_value_allowed_flag
-
-    picWidthInMbsMinus1 = expGolombDecoder.readUnsignedExpGolomb();
-    picHeightInMapUnitsMinus1 = expGolombDecoder.readUnsignedExpGolomb();
-
-    frameMbsOnlyFlag = expGolombDecoder.readBits(1);
-    if (frameMbsOnlyFlag === 0) {
-      expGolombDecoder.skipBits(1); // mb_adaptive_frame_field_flag
-    }
-
-    expGolombDecoder.skipBits(1); // direct_8x8_inference_flag
-    if (expGolombDecoder.readBoolean()) { // frame_cropping_flag
-      frameCropLeftOffset = expGolombDecoder.readUnsignedExpGolomb();
-      frameCropRightOffset = expGolombDecoder.readUnsignedExpGolomb();
-      frameCropTopOffset = expGolombDecoder.readUnsignedExpGolomb();
-      frameCropBottomOffset = expGolombDecoder.readUnsignedExpGolomb();
-    }
-
-    return {
-      profileIdc: profileIdc,
-      levelIdc: levelIdc,
-      profileCompatibility: profileCompatibility,
-      width: ((picWidthInMbsMinus1 + 1) * 16) - frameCropLeftOffset * 2 - frameCropRightOffset * 2,
-      height: ((2 - frameMbsOnlyFlag) * (picHeightInMapUnitsMinus1 + 1) * 16) - (frameCropTopOffset * 2) - (frameCropBottomOffset * 2)
-    };
   };
 
 };
