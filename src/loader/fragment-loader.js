8e5802dbc803adbe574d44e1fadf4fc5efc6ce06
mangui <g.du.pontavice@gmail.com>
2015-02-02 18:07:07 +0100

    fire basic stats from fragmentLoader and playlistLoader

--- src/loader/fragment-loader.js
+++ src/loader/fragment-loader.js
@@ -1,5 +1,5 @@
 /*
- * playlist loader
+ * fragment loader
  *
  */
 
@@ -10,29 +10,43 @@
     fragmentLoader = function() {
         var url;
         var self = this;
+        var trequest;
+        var tfirst;
 
         fragmentLoader.prototype.init.call(this);
 
         this.load = function(url) {
             this.url = url;
+            trequest = Date.now();
+            tfirst = null;
             var xhr = new XMLHttpRequest();
             xhr.onload = loadsuccess;
             xhr.onerror = loaderror;
+            xhr.onprogress = loadprogress;
             xhr.responseType = 'arraybuffer';
-            xhr.open('GET', url, true);
+            xhr.open('GET', url + '?' + Date.now(), true);
             xhr.send();
         };
 
         function loadsuccess(event) {
-            console.log(
-                'received fragment, length:' +
-                    event.currentTarget.response.byteLength
-            );
+            self.trigger('stats', {
+                trequest: trequest,
+                tfirst: tfirst,
+                tend: Date.now(),
+                length: event.currentTarget.response.byteLength,
+                url: self.url
+            });
             self.trigger('data', event.currentTarget.response);
         }
 
         function loaderror(event) {
             console.log('error loading ' + self.url);
+        }
+
+        function loadprogress(event) {
+            if (tfirst === null) {
+                tfirst = Date.now();
+            }
         }
     };
     fragmentLoader.prototype = new hls.Stream();
