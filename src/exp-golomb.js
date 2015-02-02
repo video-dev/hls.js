5af79668c044b52b5b1c3b6377150dadec49aef6
mangui <g.du.pontavice@gmail.com>
2015-02-02 11:07:10 +0100

    exp-golomb.js : only expose readSequenceParameterSet()

--- src/exp-golomb.js
+++ src/exp-golomb.js
@@ -4,7 +4,7 @@
  */
 (function() {
     'use strict';
-    window.hls.ExpGolomb = function(workingData) {
+    hls.ExpGolomb = function(workingData) {
         var // the number of bytes left to examine in workingData
             workingBytesAvailable = workingData.byteLength,
             // the current word being examined
@@ -13,17 +13,17 @@
             workingBitsAvailable = 0; // :uint;
 
         // ():uint
-        this.length = function() {
+        function length() {
             return 8 * workingBytesAvailable;
-        };
+        }
 
         // ():uint
-        this.bitsAvailable = function() {
+        function bitsAvailable() {
             return 8 * workingBytesAvailable + workingBitsAvailable;
-        };
+        }
 
         // ():void
-        this.loadWord = function() {
+        function loadWord() {
             var position = workingData.byteLength - workingBytesAvailable,
                 workingBytes = new Uint8Array(4),
                 availableBytes = Math.min(4, workingBytesAvailable);
@@ -40,10 +40,10 @@
             // track the amount of workingData that has been processed
             workingBitsAvailable = availableBytes * 8;
             workingBytesAvailable -= availableBytes;
-        };
+        }
 
         // (count:int):void
-        this.skipBits = function(count) {
+        function skipBits(count) {
             var skipBytes; // :int
             if (workingBitsAvailable > count) {
                 workingWord <<= count;
@@ -55,15 +55,15 @@
                 count -= skipBytes * 8;
                 workingBytesAvailable -= skipBytes;
 
-                this.loadWord();
+                loadWord();
 
                 workingWord <<= count;
                 workingBitsAvailable -= count;
             }
-        };
+        }
 
         // (size:int):uint
-        this.readBits = function(size) {
+        function readBits(size) {
             var bits = Math.min(workingBitsAvailable, size), // :uint
                 valu = workingWord >>> (32 - bits); // :uint
 
@@ -76,19 +76,19 @@
             if (workingBitsAvailable > 0) {
                 workingWord <<= bits;
             } else if (workingBytesAvailable > 0) {
-                this.loadWord();
+                loadWord();
             }
 
             bits = size - bits;
             if (bits > 0) {
-                return (valu << bits) | this.readBits(bits);
+                return (valu << bits) | readBits(bits);
             } else {
                 return valu;
             }
-        };
+        }
 
         // ():uint
-        this.skipLeadingZeros = function() {
+        function skipLeadingZeros() {
             var leadingZeroCount; // :uint
             for (
                 leadingZeroCount = 0;
@@ -104,47 +104,47 @@
             }
 
             // we exhausted workingWord and still have not found a 1
-            this.loadWord();
-            return leadingZeroCount + this.skipLeadingZeros();
-        };
+            loadWord();
+            return leadingZeroCount + skipLeadingZeros();
+        }
 
         // ():void
-        this.skipUnsignedExpGolomb = function() {
-            this.skipBits(1 + this.skipLeadingZeros());
-        };
+        function skipUnsignedExpGolomb() {
+            skipBits(1 + skipLeadingZeros());
+        }
 
         // ():void
-        this.skipExpGolomb = function() {
-            this.skipBits(1 + this.skipLeadingZeros());
-        };
+        function skipExpGolomb() {
+            skipBits(1 + skipLeadingZeros());
+        }
 
         // ():uint
-        this.readUnsignedExpGolomb = function() {
-            var clz = this.skipLeadingZeros(); // :uint
-            return this.readBits(clz + 1) - 1;
-        };
+        function readUnsignedExpGolomb() {
+            var clz = skipLeadingZeros(); // :uint
+            return readBits(clz + 1) - 1;
+        }
 
         // ():int
-        this.readExpGolomb = function() {
-            var valu = this.readUnsignedExpGolomb(); // :int
+        function readExpGolomb() {
+            var valu = readUnsignedExpGolomb(); // :int
             if (0x01 & valu) {
                 // the number is odd if the low order bit is set
                 return (1 + valu) >>> 1; // add 1 to make it even, and divide by 2
             } else {
                 return -1 * (valu >>> 1); // divide by two then make it negative
             }
-        };
+        }
 
         // Some convenience functions
         // :Boolean
-        this.readBoolean = function() {
-            return 1 === this.readBits(1);
-        };
+        function readBoolean() {
+            return 1 === readBits(1);
+        }
 
         // ():int
-        this.readUnsignedByte = function() {
-            return this.readBits(8);
-        };
+        function readUnsignedByte() {
+            return readBits(8);
+        }
 
         /**
          * Advance the ExpGolomb decoder past a scaling list. The scaling
@@ -153,7 +153,7 @@
          * @param count {number} the number of entries in this scaling list
          * @see Recommendation ITU-T H.264, Section 7.3.2.1.1.1
          */
-        this.skipScalingList = function(count) {
+        function skipScalingList(count) {
             var lastScale = 8,
                 nextScale = 8,
                 j,
@@ -161,13 +161,13 @@
 
             for (j = 0; j < count; j++) {
                 if (nextScale !== 0) {
-                    deltaScale = this.readExpGolomb();
+                    deltaScale = readExpGolomb();
                     nextScale = (lastScale + deltaScale + 256) % 256;
                 }
 
                 lastScale = nextScale === 0 ? lastScale : nextScale;
             }
-        };
+        }
 
         /**
          * Read a sequence parameter set and return some interesting video
@@ -195,11 +195,11 @@
                 scalingListCount,
                 i;
 
-            profileIdc = this.readUnsignedByte(); // profile_idc
-            profileCompatibility = this.readBits(5); // constraint_set[0-5]_flag
-            this.skipBits(3); //  u(1), reserved_zero_2bits u(2)
-            levelIdc = this.readUnsignedByte(); // level_idc u(8)
-            this.skipUnsignedExpGolomb(); // seq_parameter_set_id
+            profileIdc = readUnsignedByte(); // profile_idc
+            profileCompatibility = readBits(5); // constraint_set[0-5]_flag
+            skipBits(3); //  u(1), reserved_zero_2bits u(2)
+            levelIdc = readUnsignedByte(); // level_idc u(8)
+            skipUnsignedExpGolomb(); // seq_parameter_set_id
 
             // some profiles have more optional data we don't need
             if (
@@ -213,18 +213,18 @@
                 profileIdc === 118 ||
                 profileIdc === 128
             ) {
-                chromaFormatIdc = this.readUnsignedExpGolomb();
+                chromaFormatIdc = readUnsignedExpGolomb();
                 if (chromaFormatIdc === 3) {
-                    this.skipBits(1); // separate_colour_plane_flag
+                    skipBits(1); // separate_colour_plane_flag
                 }
-                this.skipUnsignedExpGolomb(); // bit_depth_luma_minus8
-                this.skipUnsignedExpGolomb(); // bit_depth_chroma_minus8
-                this.skipBits(1); // qpprime_y_zero_transform_bypass_flag
-                if (this.readBoolean()) {
+                skipUnsignedExpGolomb(); // bit_depth_luma_minus8
+                skipUnsignedExpGolomb(); // bit_depth_chroma_minus8
+                skipBits(1); // qpprime_y_zero_transform_bypass_flag
+                if (readBoolean()) {
                     // seq_scaling_matrix_present_flag
                     scalingListCount = chromaFormatIdc !== 3 ? 8 : 12;
                     for (i = 0; i < scalingListCount; i++) {
-                        if (this.readBoolean()) {
+                        if (readBoolean()) {
                             // seq_scaling_list_present_flag[ i ]
                             if (i < 6) {
                                 skipScalingList(16);
@@ -236,39 +236,39 @@
                 }
             }
 
-            this.skipUnsignedExpGolomb(); // log2_max_frame_num_minus4
-            picOrderCntType = this.readUnsignedExpGolomb();
+            skipUnsignedExpGolomb(); // log2_max_frame_num_minus4
+            picOrderCntType = readUnsignedExpGolomb();
 
             if (picOrderCntType === 0) {
-                this.readUnsignedExpGolomb(); //log2_max_pic_order_cnt_lsb_minus4
+                readUnsignedExpGolomb(); //log2_max_pic_order_cnt_lsb_minus4
             } else if (picOrderCntType === 1) {
-                this.skipBits(1); // delta_pic_order_always_zero_flag
-                this.skipExpGolomb(); // offset_for_non_ref_pic
-                this.skipExpGolomb(); // offset_for_top_to_bottom_field
-                numRefFramesInPicOrderCntCycle = this.readUnsignedExpGolomb();
+                skipBits(1); // delta_pic_order_always_zero_flag
+                skipExpGolomb(); // offset_for_non_ref_pic
+                skipExpGolomb(); // offset_for_top_to_bottom_field
+                numRefFramesInPicOrderCntCycle = readUnsignedExpGolomb();
                 for (i = 0; i < numRefFramesInPicOrderCntCycle; i++) {
-                    this.skipExpGolomb(); // offset_for_ref_frame[ i ]
+                    skipExpGolomb(); // offset_for_ref_frame[ i ]
                 }
             }
 
-            this.skipUnsignedExpGolomb(); // max_num_ref_frames
-            this.skipBits(1); // gaps_in_frame_num_value_allowed_flag
+            skipUnsignedExpGolomb(); // max_num_ref_frames
+            skipBits(1); // gaps_in_frame_num_value_allowed_flag
 
-            picWidthInMbsMinus1 = this.readUnsignedExpGolomb();
-            picHeightInMapUnitsMinus1 = this.readUnsignedExpGolomb();
+            picWidthInMbsMinus1 = readUnsignedExpGolomb();
+            picHeightInMapUnitsMinus1 = readUnsignedExpGolomb();
 
-            frameMbsOnlyFlag = this.readBits(1);
+            frameMbsOnlyFlag = readBits(1);
             if (frameMbsOnlyFlag === 0) {
-                this.skipBits(1); // mb_adaptive_frame_field_flag
+                skipBits(1); // mb_adaptive_frame_field_flag
             }
 
-            this.skipBits(1); // direct_8x8_inference_flag
-            if (this.readBoolean()) {
+            skipBits(1); // direct_8x8_inference_flag
+            if (readBoolean()) {
                 // frame_cropping_flag
-                frameCropLeftOffset = this.readUnsignedExpGolomb();
-                frameCropRightOffset = this.readUnsignedExpGolomb();
-                frameCropTopOffset = this.readUnsignedExpGolomb();
-                frameCropBottomOffset = this.readUnsignedExpGolomb();
+                frameCropLeftOffset = readUnsignedExpGolomb();
+                frameCropRightOffset = readUnsignedExpGolomb();
+                frameCropTopOffset = readUnsignedExpGolomb();
+                frameCropBottomOffset = readUnsignedExpGolomb();
             }
 
             return {
@@ -287,7 +287,5 @@
                     frameCropBottomOffset * 2
             };
         };
-
-        this.loadWord();
     };
 })(this);
