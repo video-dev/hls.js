239df6010656123c53f4ed1d24c13fb43087e159
mangui <g.du.pontavice@gmail.com>
2015-02-16 13:15:56 +0100

    opti

--- src/remux/mp4-generator.js
+++ src/remux/mp4-generator.js
@@ -258,11 +258,7 @@ class MP4 {
             size += payload[i].byteLength;
         }
         result = new Uint8Array(size + 8);
-        view = new DataView(
-            result.buffer,
-            result.byteOffset,
-            result.byteLength
-        );
+        view = new DataView(result.buffer);
         view.setUint32(0, result.byteLength);
         result.set(type, 4);
 
