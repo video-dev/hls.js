d0d7f7ba0a6dac96d69db6a7f835cfa0bc141e24
mangui <g.du.pontavice@gmail.com>
2015-02-16 13:02:31 +0100

    remove logs on fragment load

--- src/hls.js
+++ src/hls.js
@@ -213,16 +213,7 @@ class Hls {
         rtt = stats.tfirst - stats.trequest;
         loadtime = stats.tend - stats.trequest;
         bw = stats.length * 8 / (1000 * loadtime);
-        logger.log(
-            data.url +
-                ' loaded, RTT(ms)/load(ms)/bitrate:' +
-                rtt +
-                '/' +
-                loadtime +
-                '/' +
-                bw.toFixed(3) +
-                ' Mb/s'
-        );
+        //logger.log(data.url + ' loaded, RTT(ms)/load(ms)/bitrate:' + rtt + '/' + loadtime + '/' + bw.toFixed(3) + ' Mb/s');
     }
 
     onFragmentParsed(event, data) {
