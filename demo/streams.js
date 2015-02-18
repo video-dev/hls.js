0ab33381b6fba7cec1ec9245c9a48cf34770ad3d
mangui <g.du.pontavice@gmail.com>
2015-02-18 14:00:45 +0100

    add test stream lists and input field to load custom stream

---
+++ demo/streams.js
@@ -0,0 +1,26 @@
+var teststreams = [{
+  file:'http://sublimevideo-media.s3.amazonaws.com/v/dm/hls/xwr14q/55008890_mp4_h264_aac_hq.m3u8',
+  title: 'sublimevideo xwr14q'
+},{
+  file:'http://www.flashls.org/playlists/test_001/stream.m3u8',
+  title: 'VOD (6 levels) - ffmpeg internal segmenter x264+aac'
+},{
+  file:'http://www.flashls.org/playlists/issue_002/test.m3u8',
+  title: 'issues/2'
+},{
+  file:'http://www.flashls.org/playlists/issue_006/sample.m3u8',
+  title: 'issues/6 - TS parsing robustness'
+},{
+  file:'http://www.flashls.org/playlists/issue_010/list.m3u8',
+  title: 'issues/10 - drift between segment and playlist duration'
+},{
+  file:'http://www.flashls.org/playlists/issue_012/playlist.m3u8',
+  title: 'issues/12 - PTS/seqnum not synchronized accross adaptive playlists'
+},{
+  file:'http://www.flashls.org/playlists/issue_020/new/test.m3u8',
+  title: 'issues/20 - AVC NAL unit parsing issue'
+},{
+  file:'http://www.flashls.org/playlists/issue_066/prog_index.m3u8',
+  title: 'issues/66 - accurate seeking - multiple keyframes per segment - playback artifacts'
+}
+];
