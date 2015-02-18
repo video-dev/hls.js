85f3714de3c0e62f7c524f9454d52a72452a0580
mangui <g.du.pontavice@gmail.com>
2015-02-18 15:45:53 +0100

    force level 0 loading in case master playlist references only 1 level

--- src/hls.js
+++ src/hls.js
@@ -163,7 +163,7 @@ class Hls {
                 '/' +
                 (stats.tend - stats.trequest)
         );
-        if (this.levels.length > 1) {
+        if (this.levels.length > 1 || this.levels[0].fragments === undefined) {
             // set level, it will trigger a playlist loading request
             this.playlistLoader.level = this.levels.length - 1;
         }
