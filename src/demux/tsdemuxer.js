3d4ae2feec0de026c80a707412e96617b3c15d1e
mangui <g.du.pontavice@gmail.com>
2015-02-20 15:48:04 +0100

    fix demuxing errors when no audio or video samples found

--- src/demux/tsdemuxer.js
+++ src/demux/tsdemuxer.js
@@ -681,6 +681,10 @@ class VideoSegmentStream extends Stream {
             sample,
             startdts;
 
+        if (this.nalUnitsLength === 0) {
+            return;
+        }
+
         // concatenate the video data and construct the mdat
         // first, we have to build the index from byte locations to
         // samples (that is, frames) in the video data
@@ -803,6 +807,11 @@ class AudioSegmentStream extends Stream {
             mdat,
             moof,
             boxes;
+
+        if (this.aacUnitsLength === 0) {
+            return;
+        }
+
         // // concatenate the audio data and construct the mdat
         // // first, we have to build the index from byte locations to
         // // samples (that is, frames) in the audio data
