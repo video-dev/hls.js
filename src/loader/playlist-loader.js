634c26c274a78496e4e8b677dae4684a9764ef01
mangui <g.du.pontavice@gmail.com>
2015-03-18 22:36:42 +0100

    disable audio on firefox

--- src/loader/playlist-loader.js
+++ src/loader/playlist-loader.js
@@ -89,6 +89,14 @@ class PlaylistLoader {
                             codec = codecs.shift();
                             if (codec.indexOf('avc1') !== -1) {
                                 codec = this.avc1toavcoti(codec);
+                            } else {
+                                if (
+                                    navigator.userAgent
+                                        .toLowerCase()
+                                        .indexOf('firefox') === -1
+                                ) {
+                                    continue;
+                                }
                             }
                             level.codecs += codec;
                         }
