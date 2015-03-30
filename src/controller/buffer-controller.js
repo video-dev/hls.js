video-dev/hls.js

commit 7d5ecbf537556cccbeed75acf0821e0672790e29

Firefox:fix playback issue and buffer discontinuities use 2 distinct sourcebuffer for audio and video firefox does not support a unique one correctly

src/controller/buffer-controller.js
@@ -57,10 +57,18 @@
     this.mp4segments = [];
     var sb = this.sourceBuffer;
     if(sb) {
-      //detach sourcebuffer from Media Source
-      this.mediaSource.removeSourceBuffer(sb);
-      sb.removeEventListener('updateend', this.onsbue);
-      sb.removeEventListener('error', this.onsbe);
+      if(sb.audio) {
+          //detach sourcebuffer from Media Source
+          this.mediaSource.removeSourceBuffer(sb.audio);
+          sb.audio.removeEventListener('updateend', this.onsbue);
+          sb.audio.removeEventListener('error', this.onsbe);
+      }
+      if(sb.video) {
+          //detach sourcebuffer from Media Source
+          this.mediaSource.removeSourceBuffer(sb.video);
+          sb.video.removeEventListener('updateend', this.onsbue);
+          sb.video.removeEventListener('error', this.onsbe);
+      }
       this.sourceBuffer = null;
     }
     observer.removeListener(Event.FRAMEWORK_READY, this.onfr);
@@ -208,11 +216,12 @@
       case APPENDING:
         if (this.sourceBuffer) {
           // if MP4 segment appending in progress nothing to do
-          if(this.sourceBuffer.updating) {
+          if(this.sourceBuffer.audio.updating || this.sourceBuffer.video.updating) {
             //logger.log('sb append in progress');
         // check if any MP4 segments left to append
           } else if(this.mp4segments.length) {
-            this.sourceBuffer.appendBuffer(this.mp4segments.shift());
+            var segment = this.mp4segments.shift();
+            this.sourceBuffer[segment.type].appendBuffer(segment.data);
             this.state = APPENDING;
           }
         }
@@ -358,26 +367,44 @@
   onInitSegment(event,data) {
     // check if codecs have been explicitely defined in the master playlist for this level;
     // if yes use these ones instead of the ones parsed from the demux
-    var codec = this.levels[this.level].codecs;
+    var audioCodec = this.levels[this.level].audioCodec, videoCodec = this.levels[this.level].videoCodec,sb;
+    //logger.log('playlist level A/V codecs:' + audioCodec + ',' + videoCodec);
     //logger.log('playlist codecs:' + codec);
     // if playlist does not specify codecs, use codecs found while parsing fragment
-    if(codec === undefined) {
-      codec = data.codec;
+    if(audioCodec === undefined) {
+      audioCodec = data.audioCodec;
     }
+    if(videoCodec === undefined) {
+      videoCodec = data.videoCodec;
+    }
+
     // codec="mp4a.40.5,avc1.420016";
     // in case several audio codecs might be used, force HE-AAC for audio (some browsers don't support audio codec switch)
     //don't do it for mono streams ...
     if(this.audiocodecswitch && data.audioChannelCount === 2 && navigator.userAgent.toLowerCase().indexOf('android') === -1 && navigator.userAgent.toLowerCase().indexOf('firefox') === -1) {
-      codec = codec.replace('mp4a.40.2','mp4a.40.5');
+      audioCodec = 'mp4a.40.5';
     }
-    logger.log('playlist/choosed codecs:' + this.levels[this.level].codecs + '/' + codec);
     if(!this.sourceBuffer) {
+      this.sourceBuffer = {};
+      logger.log('selected A/V codecs for sourceBuffers:' + audioCodec + ',' + videoCodec);
       // create source Buffer and link them to MediaSource
-      var sb = this.sourceBuffer = this.mediaSource.addSourceBuffer('video/mp4;codecs=' + codec);
-      sb.addEventListener('updateend', this.onsbue);
-      sb.addEventListener('error', this.onsbe);
+      if(audioCodec) {
+        sb = this.sourceBuffer.audio = this.mediaSource.addSourceBuffer('video/mp4;codecs=' + audioCodec);
+        sb.addEventListener('updateend', this.onsbue);
+        sb.addEventListener('error', this.onsbe);
+      }
+      if(videoCodec) {
+        sb = this.sourceBuffer.video = this.mediaSource.addSourceBuffer('video/mp4;codecs=' + videoCodec);
+        sb.addEventListener('updateend', this.onsbue);
+        sb.addEventListener('error', this.onsbe);
+      }
     }
-    this.mp4segments.push(data.moov);
+    if(audioCodec) {
+      this.mp4segments.push({ type : 'audio', data : data.audioMoov});
+    }
+    if(videoCodec) {
+      this.mp4segments.push({ type : 'video', data : data.videoMoov});
+    }
     //trigger handler right now
     this.tick();
   }
@@ -389,8 +416,8 @@
       level.details.sliding = data.startPTS - this.frag.start;
     }
     logger.log('      parsed data, type/startPTS/endPTS/startDTS/endDTS/sliding:' + data.type + '/' + data.startPTS.toFixed(3) + '/' + data.endPTS.toFixed(3) + '/' + data.startDTS.toFixed(3) + '/' + data.endDTS.toFixed(3) + '/' + level.details.sliding.toFixed(3));
-    this.mp4segments.push(data.moof);
-    this.mp4segments.push(data.mdat);
+    this.mp4segments.push({ type : data.type, data : data.moof});
+    this.mp4segments.push({ type : data.type, data : data.mdat});
     this.nextLoadPosition = data.endPTS;
     //trigger handler right now
     this.tick();

src/demux/tsdemuxerworker.js
@@ -28,9 +28,11 @@ class TSDemuxerWorker {
 
     // listen to events triggered by TS Demuxer
     observer.on(Event.FRAG_PARSING_INIT_SEGMENT, function(ev,data) {
-      var objData = { event : ev, codec : data.codec , moov : data.moov.buffer, width : data.width, height : data.height, audioChannelCount : data.audioChannelCount};
+      var objData = { event : ev,
+                      audioCodec : data.audioCodec , audioMoov : data.audioMoov.buffer, audioChannelCount : data.audioChannelCount,
+                      videoCodec : data.videoCodec , videoMoov : data.videoMoov.buffer, videoWidth : data.videoWidth, videoHeight : data.videoHeight};
       // pass moov as transferable object (no copy)
-      self.postMessage(objData,[objData.moov]);
+      self.postMessage(objData,[objData.audioMoov, objData.videoMoov]);
     });
     observer.on(Event.FRAG_PARSING_DATA, function(ev,data) {
       var objData = { event : ev , type : data.type, startPTS : data.startPTS, endPTS : data.endPTS , startDTS : data.startDTS, endDTS : data.endDTS ,moof : data.moof.buffer, mdat : data.mdat.buffer};

src/demux/demuxer.js
@@ -65,11 +65,13 @@ class Demuxer {
     switch(ev.data.event) {
       case Event.FRAG_PARSING_INIT_SEGMENT:
         observer.trigger(Event.FRAG_PARSING_INIT_SEGMENT,{
-          moov: new Uint8Array(ev.data.moov),
-          codec : ev.data.codec,
-          width : ev.data.width,
-          height : ev.data.height,
-          audioChannelCount : ev.data.audioChannelCount
+          audioMoov: new Uint8Array(ev.data.audioMoov),
+          audioCodec : ev.data.audioCodec,
+          audioChannelCount : ev.data.audioChannelCount,
+          videoMoov: new Uint8Array(ev.data.videoMoov),
+          videoCodec : ev.data.videoCodec,
+          videoWidth : ev.data.videoWidth,
+          videoHeight : ev.data.videoHeight
         });
       break;
       case Event.FRAG_PARSING_DATA:

src/demux/tsdemuxer.js
@@ -664,8 +664,8 @@
       //audio only
       if(this._aacTrack.config) {
          observer.trigger(Event.FRAG_PARSING_INIT_SEGMENT,{
-          moov: MP4.initSegment([this._aacTrack]),
-          codec : this._aacTrack.codec,
+          audioMoov: MP4.initSegment([this._aacTrack]),
+          audioCodec : this._aacTrack.codec,
           audioChannelCount : this._aacTrack.channelCount
         });
         this._initSegGenerated = true;
@@ -680,10 +680,10 @@
       //video only
       if(this._avcTrack.sps && this._avcTrack.pps) {
          observer.trigger(Event.FRAG_PARSING_INIT_SEGMENT,{
-          moov: MP4.initSegment([this._avcTrack]),
-          codec : this._avcTrack.codec,
-          width : this._avcTrack.width,
-          height : this._avcTrack.height
+          videoMoov: MP4.initSegment([this._avcTrack]),
+          videoCodec : this._avcTrack.codec,
+          videoWidth : this._avcTrack.width,
+          videoHeight : this._avcTrack.height
         });
         this._initSegGenerated = true;
         if(this._initPTS === undefined) {
@@ -696,11 +696,13 @@
       //audio and video
       if(this._aacTrack.config && this._avcTrack.sps && this._avcTrack.pps) {
          observer.trigger(Event.FRAG_PARSING_INIT_SEGMENT,{
-          moov: MP4.initSegment([this._avcTrack, this._aacTrack]),
-          codec : this._avcTrack.codec + ',' + this._aacTrack.codec,
-          width : this._avcTrack.width,
-          height : this._avcTrack.height,
-          audioChannelCount : this._aacTrack.channelCount
+          audioMoov: MP4.initSegment([this._aacTrack]),
+          audioCodec : this._aacTrack.codec,
+          audioChannelCount : this._aacTrack.channelCount,
+          videoMoov: MP4.initSegment([this._avcTrack]),
+          videoCodec : this._avcTrack.codec,
+          videoWidth : this._avcTrack.width,
+          videoHeight : this._avcTrack.height
         });
         this._initSegGenerated = true;
         if(this._initPTS === undefined) {

src/loader/playlist-loader.js
@@ -73,16 +73,12 @@ import observer             from '../observer';
           case 'CODECS':
             codecs = result.shift().split(',');
             while(codecs.length > 0) {
-              if (level.codecs === undefined) {
-                level.codecs = '';
-              } else {
-                level.codecs += ',';
-              }
               codec = codecs.shift();
               if(codec.indexOf('avc1') !== -1) {
-                codec = this.avc1toavcoti(codec);
+                level.videoCodec = this.avc1toavcoti(codec);
+              } else {
+                level.audioCodec = codec;
               }
-              level.codecs += codec;
             }
             break;
           default:

commit 3f5dc832596c5335f922dd7056fa5fa8a6377343

fix audio-only / video-only stream not working properly regression introduced by https://github.com/dailymotion/hls.js/commit/7d5ecbf537556cccbeed75acf0821e0672790e29

src/controller/buffer-controller.js
@@ -250,8 +250,10 @@ class BufferController {
                 if (this.sourceBuffer) {
                     // if MP4 segment appending in progress nothing to do
                     if (
-                        this.sourceBuffer.audio.updating ||
-                        this.sourceBuffer.video.updating
+                        (this.sourceBuffer.audio &&
+                            this.sourceBuffer.audio.updating) ||
+                        (this.sourceBuffer.video &&
+                            this.sourceBuffer.video.updating)
                     ) {
                         //logger.log('sb append in progress');
                         // check if any MP4 segments left to append

src/demux/tsdemuxerworker.js
@@ -31,18 +31,23 @@ class TSDemuxerWorker {
 
         // listen to events triggered by TS Demuxer
         observer.on(Event.FRAG_PARSING_INIT_SEGMENT, function(ev, data) {
-            var objData = {
-                event: ev,
-                audioCodec: data.audioCodec,
-                audioMoov: data.audioMoov.buffer,
-                audioChannelCount: data.audioChannelCount,
-                videoCodec: data.videoCodec,
-                videoMoov: data.videoMoov.buffer,
-                videoWidth: data.videoWidth,
-                videoHeight: data.videoHeight
-            };
+            var objData = { event: ev };
+            var objTransferable = [];
+            if (data.audioCodec) {
+                objData.audioCodec = data.audioCodec;
+                objData.audioMoov = data.audioMoov.buffer;
+                objData.audioChannelCount = data.audioChannelCount;
+                objTransferable.push(objData.audioMoov);
+            }
+            if (data.videoCodec) {
+                objData.videoCodec = data.videoCodec;
+                objData.videoMoov = data.videoMoov.buffer;
+                objData.videoWidth = data.videoWidth;
+                objData.videoHeight = data.videoHeight;
+                objTransferable.push(objData.videoMoov);
+            }
             // pass moov as transferable object (no copy)
-            self.postMessage(objData, [objData.audioMoov, objData.videoMoov]);
+            self.postMessage(objData, objTransferable);
         });
         observer.on(Event.FRAG_PARSING_DATA, function(ev, data) {
             var objData = {