1676e36326221a00dbcf227c130e77806756ec0f
mangui <g.du.pontavice@gmail.com>
2015-05-11 16:33:04 +0200

    dispatch LOAD_ERROR on empty manifest

--- src/loader/playlist-loader.js
+++ src/loader/playlist-loader.js
@@ -161,7 +161,8 @@ class PlaylistLoader {
         var data,
             string = event.currentTarget.responseText,
             url = event.currentTarget.responseURL,
-            id = this.id;
+            id = this.id,
+            levels;
         // responseURL not supported on some browsers (it is used to detect URL redirection)
         if (url === undefined) {
             // fallback to initial URL
@@ -190,13 +191,21 @@ class PlaylistLoader {
                     stats: this.stats
                 });
             } else {
+                levels = this.parseMasterPlaylist(string, url);
                 // multi level playlist, parse level info
-                observer.trigger(Event.MANIFEST_LOADED, {
-                    levels: this.parseMasterPlaylist(string, url),
-                    url: url,
-                    id: id,
-                    stats: this.stats
-                });
+                if (levels.length) {
+                    observer.trigger(Event.MANIFEST_LOADED, {
+                        levels: levels,
+                        url: url,
+                        id: id,
+                        stats: this.stats
+                    });
+                } else {
+                    observer.trigger(Event.LOAD_ERROR, {
+                        url: url,
+                        response: 'no level found in manifest'
+                    });
+                }
             }
         } else {
             observer.trigger(Event.LOAD_ERROR, {
