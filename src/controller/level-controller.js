ab51dc191021386bffc00035d93ed9e536aca0be
mangui <g.du.pontavice@gmail.com>
2015-07-06 12:28:07 +0200

    level-controller : trigger an emergency switch-down on fragment loading timeout

--- src/controller/level-controller.js
+++ src/controller/level-controller.js
@@ -207,6 +207,13 @@ class LevelController {
                     this.lastbw = 0;
                     this.lastfetchduration = 0;
                     break;
+                case ErrorDetails.FRAG_LOAD_TIMEOUT:
+                    logger.log(
+                        `level controller,frag load timeout: emergency switch-down for next fragment`
+                    );
+                    this.lastbw = 0;
+                    this.lastfetchduration = 0;
+                    break;
                 case ErrorDetails.LEVEL_LOAD_ERROR:
                     logger.log(
                         `level controller,level load error: try to reload same level`
