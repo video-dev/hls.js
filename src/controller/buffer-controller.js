315d349e69da5f898e7a921df2e96067e6cbc7bf
mangui <g.du.pontavice@gmail.com>
2015-09-17 10:35:35 +0200

    disable fragment drift computing with live playlists as well

--- src/controller/buffer-controller.js
+++ src/controller/buffer-controller.js
@@ -906,9 +906,9 @@
       logger.log(`      parsed data, type/startPTS/endPTS/startDTS/endDTS/nb:${data.type}/${data.startPTS.toFixed(3)}/${data.endPTS.toFixed(3)}/${data.startDTS.toFixed(3)}/${data.endDTS.toFixed(3)}/${data.nb}`);
       //this.frag.drift=data.startPTS-this.frag.start;
       this.frag.drift=0;
-      if(level.details.sliding) {
-        this.frag.drift-=level.details.sliding;
-      }
+      // if(level.details.sliding) {
+      //   this.frag.drift-=level.details.sliding;
+      // }
       //logger.log(`      drift:${this.frag.drift.toFixed(3)}`);
       this.mp4segments.push({ type : data.type, data : data.moof});
       this.mp4segments.push({ type : data.type, data : data.mdat});
