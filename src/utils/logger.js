22e4b2a42beb4807409e24e6c759eb31ac8b72dc
Cristián Meléndez Sáez <cristian.melendez@me.com>
2015-09-27 21:08:57 -0300

    Fixes missing property

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -3,35 +3,41 @@
 function noop() {}
 
 let fakeLogger = {
-  log: noop,
-  warn: noop,
-  info: noop,
-  error: noop
+    log: noop,
+    warn: noop,
+    info: noop,
+    error: noop
 };
 
 let exportedLogger = fakeLogger;
 
 export var enableLogs = function(debug) {
-  if (debug === true || typeof debug === 'object') {
-    exportedLogger.log = debug.log ? debug.log.bind(debug) : console.log.bind(console);
-    exportedLogger.info = debug.info ? debug.info.bind(debug) : console.info.bind(console);
-    exportedLogger.error = debug.error ? debug.error.bind(debug) : console.error.bind(console);
-    exportedLogger.warn = debug.warn ? debug.warn.bind(debug) : console.warn.bind(console);
-    // Some browsers don't allow to use bind on console object anyway
-    // fallback to default if needed
-    try {
-     exportedLogger.log();
+    if (debug === true || typeof debug === 'object') {
+        exportedLogger.log = debug.log
+            ? debug.log.bind(debug)
+            : console.log.bind(console);
+        exportedLogger.info = debug.info
+            ? debug.info.bind(debug)
+            : console.info.bind(console);
+        exportedLogger.error = debug.error
+            ? debug.error.bind(debug)
+            : console.error.bind(console);
+        exportedLogger.warn = debug.warn
+            ? debug.warn.bind(debug)
+            : console.warn.bind(console);
+        // Some browsers don't allow to use bind on console object anyway
+        // fallback to default if needed
+        try {
+            exportedLogger.log();
+        } catch (e) {
+            exportedLogger.log = noop;
+            exportedLogger.info = noop;
+            exportedLogger.error = noop;
+            exportedLogger.warn = noop;
+        }
+    } else {
+        exportedLogger = fakeLogger;
     }
-    catch (e) {
-      exportedLogger.log = noop;
-      exportedLogger.info = noop;
-      exportedLogger.error = noop;
-      exportedLogger.   = noop;
-    }
-  }
-  else {
-    exportedLogger = fakeLogger;
-  }
 };
 
 export var logger = exportedLogger;
