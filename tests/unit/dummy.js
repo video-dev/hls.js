8e929583d726341975d76022b510643d29735a5a
mangui <g.du.pontavice@gmail.com>
2015-10-05 18:00:58 +0200

    reorganize test folder

---
+++ tests/unit/dummy.js
@@ -0,0 +1,8 @@
+var assert = require("assert");
+describe('dummy test', function() {
+  describe('#1', function () {
+    it('always passing', function () {
+      assert.equal(true,true);
+    });
+  });
+});
