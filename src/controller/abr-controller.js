85c9a0731d3b95c2d82000bcc4a47ca7776eedb6
mangui <g.du.pontavice@gmail.com>
2015-10-06 15:43:53 +0200

    split AbrController from levelController allow custom ABR controller implementation

---
+++ src/controller/abr-controller.js
@@ -0,0 +1,84 @@
+/*
+ * simple ABR Controller
+*/
+
+import Event from '../events';
+import observer from '../observer';
+
+class AbrController {
+    constructor(hls) {
+        this.hls = hls;
+        this.lastfetchlevel = 0;
+        this._autoLevelCapping = -1;
+        this._nextAutoLevel = -1;
+        this.onflp = this.onFragmentLoadProgress.bind(this);
+        observer.on(Event.FRAG_LOAD_PROGRESS, this.onflp);
+    }
+
+    destroy() {
+        observer.off(Event.FRAG_LOAD_PROGRESS, this.onflp);
+    }
+
+    onFragmentLoadProgress(event, data) {
+        var stats = data.stats;
+        if (stats.aborted === undefined) {
+            this.lastfetchduration = (new Date() - stats.trequest) / 1000;
+            this.lastfetchlevel = data.frag.level;
+            this.lastbw = stats.loaded * 8 / this.lastfetchduration;
+            //console.log('fetchDuration:${this.lastfetchduration},bw:${(this.lastbw/1000).toFixed(0)}/${stats.aborted}');
+            // unset forced auto level
+            this._nextAutoLevel = -1;
+        }
+    }
+
+    /** Return the capping/max level value that could be used by automatic level selection algorithm **/
+    get autoLevelCapping() {
+        return this._autoLevelCapping;
+    }
+
+    /** set the capping/max level value that could be used by automatic level selection algorithm **/
+    set autoLevelCapping(newLevel) {
+        this._autoLevelCapping = newLevel;
+    }
+
+    get nextAutoLevel() {
+        var lastbw = this.lastbw,
+            hls = this.hls,
+            adjustedbw,
+            i,
+            maxAutoLevel;
+        if (this._autoLevelCapping === -1) {
+            maxAutoLevel = hls.levels.length - 1;
+        } else {
+            maxAutoLevel = this._autoLevelCapping;
+        }
+
+        if (this._nextAutoLevel != -1) {
+            return Math.min(this._nextAutoLevel, maxAutoLevel);
+        }
+
+        // follow algorithm captured from stagefright :
+        // https://android.googlesource.com/platform/frameworks/av/+/master/media/libstagefright/httplive/LiveSession.cpp
+        // Pick the highest bandwidth stream below or equal to estimated bandwidth.
+        for (i = 0; i <= maxAutoLevel; i++) {
+            // consider only 80% of the available bandwidth, but if we are switching up,
+            // be even more conservative (70%) to avoid overestimating and immediately
+            // switching back.
+            if (i <= this.lastfetchlevel) {
+                adjustedbw = 0.8 * lastbw;
+            } else {
+                adjustedbw = 0.7 * lastbw;
+            }
+            if (adjustedbw < hls.levels[i].bitrate) {
+                return Math.max(0, i - 1);
+            }
+        }
+        return i - 1;
+    }
+
+    set nextAutoLevel(nextLevel) {
+        this._nextAutoLevel = nextLevel;
+    }
+}
+
+export default AbrController;
