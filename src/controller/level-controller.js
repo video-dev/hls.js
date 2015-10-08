video-dev/hls.js

commit 7a5cd8aeb88df43774ca43304cb79aa85221b1bd

level-controller: discard audio-only level if playlists also contains audio+video levels related to https://github.com/dailymotion/hls.js/issues/23

src/controller/level-controller.js
@@ -32,18 +32,32 @@ class LevelController {
   }
 
   onManifestLoaded(event, data) {
-    var levels = [], bitrateStart, i, bitrateSet = {};
+    var levels0 = [], levels = [], bitrateStart, i, bitrateSet = {}, videoCodecFound = false, audioCodecFound = false;
+
+    // regroup redundant level together
     data.levels.forEach(level => {
+      if(level.videoCodec) videoCodecFound = true;
+      if(level.audioCodec) audioCodecFound = true;
       var redundantLevelId = bitrateSet[level.bitrate];
       if (redundantLevelId === undefined) {
         bitrateSet[level.bitrate] = levels.length;
         level.url = [level.url];
         level.urlId = 0;
-        levels.push(level);
+        levels0.push(level);
       } else {
-        levels[redundantLevelId].url.push(level.url);
+        levels0[redundantLevelId].url.push(level.url);
       }
     });
+
+    // remove audio-only level if we also have levels with audio+video codecs signalled
+    if(videoCodecFound && audioCodecFound) {
+      levels0.forEach(level => {
+        if(level.videoCodec) {
+          levels.push(level);
+        }
+      });
+    }
+
     // start bitrate is the first bitrate of the manifest
     bitrateStart = levels[0].bitrate;
     // sort level on bitrate

commit aaf0e917dc7c7d23412c449957c208ca2fbfbd6d

level-controller: fix exception with streams not including CODECS definition regression introduced by https://github.com/dailymotion/hls.js/commit/7a5cd8aeb88df43774ca43304cb79aa85221b1bd

src/controller/level-controller.js
@@ -60,6 +60,8 @@ class LevelController {
           levels.push(level);
         }
       });
+    } else {
+      levels = levels0;
     }
 
     // start bitrate is the first bitrate of the manifest