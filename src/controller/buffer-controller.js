fe5d09397b8bfdbe58b07b7d4b9490fb71fde0cb
mangui <g.du.pontavice@gmail.com>
2015-10-14 11:20:19 +0200

    buffer-controller : remove broken drift implementation

--- src/controller/buffer-controller.js
+++ src/controller/buffer-controller.js
@@ -213,8 +213,7 @@ class BufferController {
                     let fragments = levelDetails.fragments,
                         frag,
                         sliding = levelDetails.sliding,
-                        start = fragments[0].start + sliding,
-                        drift = 0;
+                        start = fragments[0].start + sliding;
                     // check if requested position is within seekable boundaries :
                     // in case of live playlist we need to ensure that requested position is not located before playlist start
                     //logger.log(`start/pos/bufEnd/seeking:${start.toFixed(3)}/${pos.toFixed(3)}/${bufferEnd.toFixed(3)}/${this.video.seeking}`);
@@ -270,11 +269,7 @@ class BufferController {
                         ) {
                             frag = fragments[fragIdx];
                             start = frag.start + sliding;
-                            if (frag.drift) {
-                                drift = frag.drift;
-                            }
-                            start += drift;
-                            //logger.log('level/sn/sliding/drift/start/end/bufEnd:${level}/${frag.sn}/${sliding.toFixed(3)}/${drift.toFixed(3)}/${start.toFixed(3)}/${(start+frag.duration).toFixed(3)}/${bufferEnd.toFixed(3)}');
+                            //logger.log('level/sn/sliding/start/end/bufEnd:${level}/${frag.sn}/${sliding.toFixed(3)}/${start.toFixed(3)}/${(start+frag.duration).toFixed(3)}/${bufferEnd.toFixed(3)}');
                             // offset should be within fragment boundary
                             if (
                                 start <= bufferEnd &&
@@ -308,7 +303,6 @@ class BufferController {
                         )}`
                     );
                     //logger.log('      loading frag ' + i +',pos/bufEnd:' + pos.toFixed(3) + '/' + bufferEnd.toFixed(3));
-                    frag.drift = drift;
                     frag.autoLevel = this.hls.autoLevelEnabled;
                     if (this.levels.length > 1) {
                         frag.expectedLen = Math.round(
@@ -1024,9 +1018,6 @@ class BufferController {
                     duration += details.sliding;
                     start += details.sliding;
                 }
-                if (this.frag.drift) {
-                    start += this.frag.drift;
-                }
                 logger.log(
                     `Demuxing ${this.frag.sn} of [${details.startSN} ,${
                         details.endSN
@@ -1128,12 +1119,6 @@ class BufferController {
                     data.nb
                 }`
             );
-            //this.frag.drift=data.startPTS-this.frag.start;
-            this.frag.drift = 0;
-            // if(level.details.sliding) {
-            //   this.frag.drift-=level.details.sliding;
-            // }
-            //logger.log('      drift:${this.frag.drift.toFixed(3)}');
             this.mp4segments.push({ type: data.type, data: data.moof });
             this.mp4segments.push({ type: data.type, data: data.mdat });
             this.nextLoadPosition = data.endPTS;
@@ -1143,9 +1128,6 @@ class BufferController {
                 end: data.endPTS,
                 frag: this.frag
             });
-            // if(data.type === 'video') {
-            //   this.frag.fpsExpected = (data.nb-1) / (data.endPTS - data.startPTS);
-            // }
             //trigger handler right now
             this.tick();
         } else {
