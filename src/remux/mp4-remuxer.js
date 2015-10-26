a64c51dc1baa087db8270c19b873f97d1464e88c
mangui <g.du.pontavice@gmail.com>
2015-10-26 12:52:11 +0100

    mp4-remuxer : handle overlapping AAC frames
    in case of timestamp overlap between AAC frames, adjust start time of next AAC frame
    related to https://github.com/dailymotion/hls.js/issues/42

--- src/remux/mp4-remuxer.js
+++ src/remux/mp4-remuxer.js
@@ -261,7 +261,7 @@ class MP4Remuxer {
         pts, dts, ptsnorm, dtsnorm,
         samples = [];
     /* concatenate the audio data and construct the mdat in place
-      (need 8 more bytes to fill length and mpdat type) */
+      (need 8 more bytes to fill length and mdat type) */
     mdat = new Uint8Array(track.len + 8);
     view = new DataView(mdat.buffer);
     view.setUint32(0, mdat.byteLength);
@@ -295,12 +295,13 @@ class MP4Remuxer {
             if (delta > 0) {
               logger.log(`AAC:${delta} ms hole between fragments detected,filling it`);
               // set PTS to next PTS, and ensure PTS is greater or equal than last DTS
-              ptsnorm = Math.max(this.nextAacPts, this.lastAacDts);
-              dtsnorm = ptsnorm;
               //logger.log('Audio/PTS/DTS adjusted:' + aacSample.pts + '/' + aacSample.dts);
             } else {
               logger.log(`AAC:${(-delta)} ms overlapping between fragments detected`);
             }
+            // set DTS to next DTS
+            ptsnorm = dtsnorm = this.nextAacPts;
+            logger.log('Audio/PTS/DTS adjusted:' + ptsnorm + '/' + dtsnorm);
           }
           else if (absdelta) {
             // not contiguous timestamp, check if PTS is within acceptable range
@@ -343,7 +344,6 @@ class MP4Remuxer {
     if (samples.length >= 2) {
       mp4Sample.duration = samples[samples.length - 2].duration;
     }
-    this.lastAacDts = dtsnorm;
     // next aac sample PTS should be equal to last sample PTS + duration
     this.nextAacPts = ptsnorm + pes2mp4ScaleFactor * mp4Sample.duration;
     //logger.log('Audio/PTS/PTSend:' + aacSample.pts.toFixed(0) + '/' + this.nextAacDts.toFixed(0));
