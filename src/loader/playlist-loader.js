fe5916ac8cec11ec73ce0aeb4552f203688d7044
Grzegorz Blaszczyk <grzegorz.blaszczyk@screen9.com>
2015-11-06 12:27:25 +0100

    Byte-Range Support for Segments

--- src/loader/playlist-loader.js
+++ src/loader/playlist-loader.js
@@ -113,8 +113,8 @@ class PlaylistLoader {
   }
 
   parseLevelPlaylist(string, baseurl, id) {
-    var currentSN = 0, totalduration = 0, level = {url: baseurl, fragments: [], live: true, startSN: 0}, result, regexp, cc = 0;
-    regexp = /(?:#EXT-X-(MEDIA-SEQUENCE):(\d+))|(?:#EXT-X-(TARGETDURATION):(\d+))|(?:#EXT(INF):([\d\.]+)[^\r\n]*[\r\n]+([^\r\n]+)|(?:#EXT-X-(ENDLIST))|(?:#EXT-X-(DIS)CONTINUITY))/g;
+    var currentSN = 0, totalduration = 0, level = {url: baseurl, fragments: [], live: true, startSN: 0}, result, regexp, cc = 0, frag, byterange_end_offset, byterange_start_offset;
+    regexp = /(?:#EXT-X-(MEDIA-SEQUENCE):(\d+))|(?:#EXT-X-(TARGETDURATION):(\d+))|(?:#EXT(INF):([\d\.]+)[^\r\n]*[\r\n]+#EXT-X-(BYTERANGE):([\d]+[@[\d]*)]*[\r\n]+([^\r\n]+))|(?:#EXT(INF):([\d\.]+)[^\r\n]*[\r\n]+([^\r\n]+)|(?:#EXT-X-(ENDLIST))|(?:#EXT-X-(DIS)CONTINUITY))/g;
     while ((result = regexp.exec(string)) !== null) {
       result.shift();
       result = result.filter(function(n) { return (n !== undefined); });
@@ -134,7 +134,20 @@ class PlaylistLoader {
         case 'INF':
           var duration = parseFloat(result[1]);
           if (!isNaN(duration)) {
-            level.fragments.push({url: this.resolve(result[2], baseurl), duration: duration, start: totalduration, sn: currentSN++, level: id, cc: cc});
+            frag = {url: this.resolve(result[2], baseurl), duration: duration, start: totalduration, sn: currentSN++, level: id, cc: cc};
+            if (result[2] === 'BYTERANGE') {
+              var params = result[3].split('@');
+              if (params.length === 1) {
+                byterange_start_offset = byterange_end_offset;   
+              } else {
+                byterange_start_offset = parseInt(params[1]);
+              }
+              byterange_end_offset = parseInt(params[0]) + byterange_start_offset;             
+              frag.byterange_start_offset = byterange_start_offset;
+              frag.byterange_end_offset = byterange_end_offset;  
+              frag.url = this.resolve(result[4], baseurl);                
+            }
+            level.fragments.push(frag);
             totalduration += duration;
           }
           break;
