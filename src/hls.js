8b2c6ffe1b81af470aa2f2e9a66073b90fed7090
mangui <g.du.pontavice@gmail.com>
2015-11-10 16:23:45 +0100

    remove dummy line

--- src/hls.js
+++ src/hls.js
@@ -125,7 +125,6 @@ class Hls {
         ms.addEventListener('sourceclose', this.onmsc);
         // link video and media Source
         video.src = URL.createObjectURL(ms);
-        video.addEventListener('error', this.onverror);
     }
 
     detachVideo() {
