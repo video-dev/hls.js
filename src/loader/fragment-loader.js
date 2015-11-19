4712a013d586993a2e2c259a35712e8373e74569
Kaj <kaj@kaj.de>
2015-11-19 20:36:31 +0100

    Enabled direct access to fragment & playlist loader

--- src/loader/fragment-loader.js
+++ src/loader/fragment-loader.js
@@ -25,7 +25,10 @@ class FragmentLoader {
         this.frag = frag;
         this.frag.loaded = 0;
         var config = this.hls.config;
-        frag.loader = this.loader = new config.loader(config);
+        frag.loader = this.loader =
+            typeof config.fLoader != 'undefined'
+                ? new config.fLoader(config)
+                : new config.loader(config);
         this.loader.load(
             frag.url,
             'arraybuffer',
