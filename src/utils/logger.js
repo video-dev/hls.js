4c158ab0b11c12aa32de3bd0c64a310514e7811a
Stephan Hesse <stephan@soundcloud.com>
2015-11-22 16:57:13 +0100

    Improved logger: Add time-diff at end of output to easily catch chronology of events + debug level enabled by default but trace hard-disabled

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -11,6 +11,8 @@ const fakeLogger = {
 
 let exportedLogger = fakeLogger;
 
+let lastCallTime;
+
 function noop() {}
 
 function consolePrintFn(type) {
@@ -18,7 +20,10 @@ function consolePrintFn(type) {
   if (func) {
     return function(...args) {
       if(args[0]) {
-        args[0] = (new Date()).toISOString() + ' | [' +  type + '] > ' + args[0];
+        const now = Date.now();
+        const diff = lastCallTime ? '+' + (now - lastCallTime) : '0';
+        args[0] = (new Date(now)).toISOString() + ' | [' +  type + '] > ' + args[0] + ' ( ' + diff + ' ms )';
+        lastCallTime = now;
       }
       func.apply(window.console, args);
     };
@@ -36,8 +41,8 @@ export var enableLogs = function(debugConfig) {
   if (debugConfig === true || typeof debugConfig === 'object') {
     exportLoggerFunctions(debugConfig,
       // Remove out from list here to hard-disable a log-level
-      'trace',
-      //'debug',
+      //'trace',
+      'debug',
       'log',
       'info',
       'warn',
