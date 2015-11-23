b0df4ce1cb986294b9dd6a7b3844af91c8f83c2c
jiri.salaj <jiri.salaj@artin.cz>
2015-11-23 23:19:59 +0100

    Key loading as new machine state

--- src/loader/key-loader.js
+++ src/loader/key-loader.js
@@ -38,7 +38,7 @@ class KeyLoader {
       this.decryptkey = frag.decryptdata.key;
     }
     this.frag.decryptdata.key = this.decryptkey;
-    this.hls.trigger(Event.FRAG_LOADING, {frag: this.frag});
+    this.hls.trigger(Event.KEY_LOADED, {frag: this.frag});
   }
   
   loadsuccess(event) {
@@ -46,7 +46,7 @@ class KeyLoader {
     this.decryptkey = this.frag.decryptdata.key;
     // detach fragment loader on load success
     this.frag.loader = undefined;
-    this.hls.trigger(Event.FRAG_LOADING, {frag: this.frag});
+    this.hls.trigger(Event.KEY_LOADED, {frag: this.frag});
   }
   
   loaderror(event) {
