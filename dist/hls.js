326ca047dd8dda12373b8eeac3f5476eaa180a4c
mangui <g.du.pontavice@gmail.com>
2015-11-24 15:39:13 +0100

    update dist related to https://github.com/dailymotion/hls.js/issues/83

--- dist/hls.js
+++ dist/hls.js
@@ -1319,19 +1319,6 @@ var MSEMediaController = (function () {
             this.fragPlaying = fragPlaying;
             this.hls.trigger(_events2['default'].FRAG_CHANGED, { frag: fragPlaying });
           }
-          // if stream is VOD (not live) and we reach End of Stream
-          var levelDetails = this.levels[this.level].details;
-          if (levelDetails && !levelDetails.live) {
-            // are we playing last fragment ?
-            if (fragPlaying.sn === levelDetails.endSN) {
-              var mediaSource = this.mediaSource;
-              if (mediaSource && mediaSource.readyState === 'open') {
-                _utilsLogger.logger.log('all media data available, signal endOfStream() to MediaSource');
-                //Notify the media element that it now has all of the media data
-                mediaSource.endOfStream();
-              }
-            }
-          }
         }
       }
     }
@@ -1833,6 +1820,20 @@ var MSEMediaController = (function () {
           this.fragLastKbps = Math.round(8 * stats.length / (stats.tbuffered - stats.tfirst));
           this.hls.trigger(_events2['default'].FRAG_BUFFERED, { stats: stats, frag: frag });
           _utilsLogger.logger.log('media buffered : ' + this.timeRangesToString(this.media.buffered));
+
+          // if stream is VOD (not live) and we reach End of Stream
+          var levelDetails = this.levels[this.level].details;
+          if (levelDetails && !levelDetails.live) {
+            // have we buffered last fragment ?
+            if (frag.sn === levelDetails.endSN) {
+              var mediaSource = this.mediaSource;
+              if (mediaSource && mediaSource.readyState === 'open') {
+                _utilsLogger.logger.log('all media data available, signal endOfStream() to MediaSource');
+                //Notify the media element that it now has all of the media data
+                mediaSource.endOfStream();
+              }
+            }
+          }
           this.state = State.IDLE;
         }
         var video = this.media;
