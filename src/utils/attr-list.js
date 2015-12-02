d7d94285becf231cf18cee4d22aab50573ca068c
Gil Pedersen <git@gpost.dk>
2015-12-02 19:10:41 +0100

    add and use generic playlist attribute list parser
    also exposes any stream info attribute for external processing

---
+++ src/utils/attr-list.js
@@ -0,0 +1,78 @@
+// adapted from https://github.com/kanongil/node-m3u8parse/blob/master/attrlist.js
+class AttrList {
+    constructor(attrs) {
+        if (typeof attrs === 'string') {
+            attrs = AttrList.parseAttrList(attrs);
+        }
+
+        Object.assign(this, attrs);
+    }
+
+    decimalInteger(/*attrName*/) {
+        throw new Error('Not supported');
+    }
+
+    hexadecimalInteger(attrName) {
+        let stringValue = (this[attrName] || '0x').slice(2);
+        stringValue = (stringValue.length & 1 ? '0' : '') + stringValue;
+
+        const value = new Uint8Array(stringValue.length / 2);
+        for (let i = 0; i < stringValue.length / 2; i++) {
+            value[i] = parseInt(stringValue.slice(i * 2, i * 2 + 2), 16);
+        }
+        return value;
+    }
+
+    decimalIntegerAsNumber(attrName) {
+        const intValue = parseInt(this[attrName], 10);
+        if (intValue >= Number.MAX_VALUE) {
+            throw new RangeError(
+                'Value is to large to represent without loss of precision'
+            );
+        }
+        return intValue;
+    }
+
+    hexadecimalIntegerAsNumber(attrName) {
+        const intValue = parseInt(this[attrName], 16);
+        if (intValue >= Number.MAX_VALUE) {
+            throw new RangeError(
+                'Value is to large to represent without loss of precision'
+            );
+        }
+        return intValue;
+    }
+
+    decimalFloatingPoint(attrName) {
+        return parseFloat(this[attrName]);
+    }
+
+    quotedString(attrName) {
+        const val = this[attrName];
+        return val ? val.slice(1, -1) : undefined;
+    }
+
+    enumeratedString(attrName) {
+        return this[attrName];
+    }
+
+    decimalResolution(attrName) {
+        const res = /(\d+)x(\d+)/.exec(this[attrName]);
+        return {
+            width: res ? parseInt(res[1], 10) : null,
+            height: res ? parseInt(res[2], 10) : null
+        };
+    }
+
+    static parseAttrList(input) {
+        const re = /(.+?)=((?:\".*?\")|.*?)(?:,|$)/g;
+        var match,
+            attrs = {};
+        while ((match = re.exec(input)) !== null) {
+            attrs[match[1]] = match[2];
+        }
+        return attrs;
+    }
+}
+
+export default AttrList;
