5731ddd6dc8b7bb772151c9f9cec64019cbdeb56
Gil Pedersen <git@gpost.dk>
2015-12-04 10:27:05 +0100

    don't try to use local crypto if the API is unavailable
    this also enabled usage in chrome web workers by accessing the crypto object directly

--- src/crypt/decrypter.js
+++ src/crypt/decrypter.js
@@ -9,7 +9,13 @@ import { logger } from '../utils/logger';
 class Decrypter {
     constructor(hls) {
         this.hls = hls;
-        this.disableWebCrypto = false;
+        try {
+            const browserCrypto = window ? window.crypto : crypto;
+            this.subtle = browserCrypto.subtle;
+            this.disableWebCrypto = !this.subtle;
+        } catch (e) {
+            this.disableWebCrypto = true;
+        }
     }
 
     destroy() {}
@@ -25,31 +31,24 @@ class Decrypter {
     decryptByWebCrypto(data, key, iv, callback) {
         logger.log('decrypting by WebCrypto API');
 
-        var localthis = this;
-        window.crypto.subtle
+        this.subtle
             .importKey('raw', key, { name: 'AES-CBC', length: 128 }, false, [
                 'decrypt'
             ])
-            .then(function(importedKey) {
-                window.crypto.subtle
+            .then(importedKey => {
+                this.subtle
                     .decrypt(
                         { name: 'AES-CBC', iv: iv.buffer },
                         importedKey,
                         data
                     )
                     .then(callback)
-                    .catch(function(err) {
-                        localthis.onWebCryptoError(
-                            err,
-                            data,
-                            key,
-                            iv,
-                            callback
-                        );
+                    .catch(err => {
+                        this.onWebCryptoError(err, data, key, iv, callback);
                     });
             })
-            .catch(function(err) {
-                localthis.onWebCryptoError(err, data, key, iv, callback);
+            .catch(err => {
+                this.onWebCryptoError(err, data, key, iv, callback);
             });
     }
 
