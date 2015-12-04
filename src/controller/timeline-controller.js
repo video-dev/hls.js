1a7fb349410d2375fedaa97663abdf7425fdcff6
Jeremy LaCivita <jeremy.lacivita@theplatform.com>
2015-12-03 19:30:42 -0800

    Removed timer-based rendering, added config option
    Previously the timeline manager had a tick() function that was checking
    if the media.currentTime was roughly close to a CEA-708 message PTS
    value, and feeding them into to CEA-708 interpreter, which mapped these
    back to media.currentTime.
    
    This had no value, and only wasted CPU, since the video tag allows
    creation of all the cues in advance, with the start/end times on them.
    
    Additionally, i added an enableCEA708Captions boolean in hls.config
    
    Lastly, updated the interpreter to support more than one caption in the
    buffer before displaying it (which is required by the spec, but not
    used by my sample media, currently)

--- src/controller/timeline-controller.js
+++ src/controller/timeline-controller.js
@@ -9,22 +9,20 @@ import CEA708Interpreter from '../utils/cea-708-interpreter';
 class TimelineController {
     constructor(hls) {
         this.hls = hls;
-        this.userDataQueue = [];
-        this.timer = setInterval(this.checkTracks.bind(this), 250);
-        this.onmediaatt0 = this.onMediaAttaching.bind(this);
-        this.onmediadet0 = this.onMediaDetaching.bind(this);
-        this.onud = this.onFragParsingUserData.bind(this);
-        this.index = 0;
-        hls.on(Event.MEDIA_ATTACHING, this.onmediaatt0);
-        hls.on(Event.MEDIA_DETACHING, this.onmediadet0);
-        hls.on(Hls.Events.FRAG_PARSING_USERDATA, this.onud);
-    }
+        this.config = hls.config;
 
-    destroy() {
-        if (this.timer) {
-            clearInterval(this.timer);
+        if (this.config.enableCEA708Captions) {
+            this.onmediaatt0 = this.onMediaAttaching.bind(this);
+            this.onmediadet0 = this.onMediaDetaching.bind(this);
+            this.onud = this.onFragParsingUserData.bind(this);
+            this.index = 0;
+            hls.on(Event.MEDIA_ATTACHING, this.onmediaatt0);
+            hls.on(Event.MEDIA_DETACHING, this.onmediadet0);
+            hls.on(Hls.Events.FRAG_PARSING_USERDATA, this.onud);
         }
     }
+
+    destroy() {}
 
     onMediaAttaching(event, data) {
         var media = (this.media = data.media);
@@ -34,18 +32,13 @@ class TimelineController {
     onMediaDetaching() {}
 
     onFragParsingUserData(event, data) {
+        // push all of the CEA-708 messages into the interpreter
+        // immediately. It will create the proper timestamps based on our PTS value
         for (var i = 0; i < data.samples.length; i++) {
-            this.userDataQueue.push(data.samples[i]);
-        }
-    }
-
-    checkTracks() {
-        var v = this.hls.media;
-        var u = this.userDataQueue;
-        if (v && u && u.length) {
-            while (v.currentTime >= u[this.index].pts) {
-                this.cea708Interpreter.push(u[this.index++].bytes);
-            }
+            this.cea708Interpreter.push(
+                data.samples[i].pts,
+                data.samples[i].bytes
+            );
         }
     }
 }
