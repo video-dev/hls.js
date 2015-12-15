1a758230a5a9f42cc6632820f356c487fe5f9d3d
mangui <g.du.pontavice@gmail.com>
2015-12-15 19:01:17 +0100

    lint

--- src/controller/level-controller.js
+++ src/controller/level-controller.js
@@ -66,11 +66,12 @@ class LevelController {
 
     // only keep level with supported audio/video codecs
     levels0 = levels0.filter(function(level) {
-      var checkSupported = function(codec) { return MediaSource.isTypeSupported(`video/mp4;codecs=${codec}`);}
+      var checkSupported = function(codec) { return MediaSource.isTypeSupported(`video/mp4;codecs=${codec}`);};
       var audioCodec = level.audioCodec, videoCodec = level.videoCodec;
-      return
-      ((audioCodec && checkSupported(audioCodec)) || !audioCodec) &&
-      ((videoCodec && checkSupported(videoCodec)) || !videoCodec);
+
+      return ((audioCodec && checkSupported(audioCodec)) || !audioCodec) &&
+             ((videoCodec && checkSupported(videoCodec)) || !videoCodec);
+
     });
 
     // start bitrate is the first bitrate of the manifest
