553ebf522378fd9e960a76ed9eb0b5a54f6f3baf
mangui <g.du.pontavice@gmail.com>
2015-12-18 12:29:32 +0100

    levelController : fix level filtering
    related to https://github.com/dailymotion/hls.js/commit/a2f5ddb4e2e7ad02ea36b1727778b4a455120723#commitcomment-15063051

--- src/controller/level-controller.js
+++ src/controller/level-controller.js
@@ -65,7 +65,7 @@ class LevelController {
     }
 
     // only keep level with supported audio/video codecs
-    levels0 = levels0.filter(function(level) {
+    levels = levels.filter(function(level) {
       var checkSupported = function(codec) { return MediaSource.isTypeSupported(`video/mp4;codecs=${codec}`);};
       var audioCodec = level.audioCodec, videoCodec = level.videoCodec;
 
