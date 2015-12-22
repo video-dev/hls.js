18d5a54d7fac6f56d384610ad26661b4517d490d
mangui <g.du.pontavice@gmail.com>
2015-12-22 15:26:38 +0100

    playlistLoader : unit test parseLevelPlaylist

--- tests/unit/loader/playlist-loader.js
+++ tests/unit/loader/playlist-loader.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const bufferIsEqual = require('arraybuffer-equal');
 
 import PlaylistLoader from '../../../src/loader/playlist-loader';
 
@@ -109,5 +110,99 @@ http://proxy-21.dailymotion.com/sec(2a991e17f08fcd94f95637a6dd718ddd)/video/107/
     assert.strictEqual(result[7]['bitrate'],2149280);
     assert.strictEqual(result[8]['bitrate'],6221600);
     assert.strictEqual(result[9]['bitrate'],6221600);
+  });
+
+  it('parses empty levels returns empty fragment array', () => {
+    var level = "";
+    var result = new PlaylistLoader({on : function() { }}).parseLevelPlaylist(level, 'http://proxy-62.dailymotion.com/sec(3ae40f708f79ca9471f52b86da76a3a8)/video/107/282/158282701_mp4_h264_aac_hq.m3u8#cell=core',0);
+    assert.strictEqual(result.fragments.length, 0);
+    assert.strictEqual(result.totalduration,0);
+  });
+
+  it('level with 0 frag returns empty fragment array', () => {
+    var level = `#EXTM3U
+#EXT-X-VERSION:3
+#EXT-X-PLAYLIST-TYPE:VOD
+#EXT-X-TARGETDURATION:14
+#EXTINF:11.360,`;
+    var result = new PlaylistLoader({on : function() { }}).parseLevelPlaylist(level, 'http://proxy-62.dailymotion.com/sec(3ae40f708f79ca9471f52b86da76a3a8)/video/107/282/158282701_mp4_h264_aac_hq.m3u8#cell=core',0);
+    assert.strictEqual(result.fragments.length, 0);
+    assert.strictEqual(result.totalduration,0);
+  });
+
+  it('parse level with several fragments', () => {
+    var level = `#EXTM3U
+#EXT-X-VERSION:3
+#EXT-X-PLAYLIST-TYPE:VOD
+#EXT-X-TARGETDURATION:14
+#EXTINF:11.360,
+/sec(3ae40f708f79ca9471f52b86da76a3a8)/frag(1)/video/107/282/158282701_mp4_h264_aac_hq.ts
+#EXTINF:11.320,
+/sec(3ae40f708f79ca9471f52b86da76a3a8)/frag(2)/video/107/282/158282701_mp4_h264_aac_hq.ts
+#EXTINF:13.480,
+/sec(3ae40f708f79ca9471f52b86da76a3a8)/frag(3)/video/107/282/158282701_mp4_h264_aac_hq.ts
+#EXTINF:11.200,
+/sec(3ae40f708f79ca9471f52b86da76a3a8)/frag(4)/video/107/282/158282701_mp4_h264_aac_hq.ts
+#EXTINF:3.880,
+/sec(3ae40f708f79ca9471f52b86da76a3a8)/frag(5)/video/107/282/158282701_mp4_h264_aac_hq.ts
+#EXT-X-ENDLIST`;
+    var result = new PlaylistLoader({on : function() { }}).parseLevelPlaylist(level, 'http://proxy-62.dailymotion.com/sec(3ae40f708f79ca9471f52b86da76a3a8)/video/107/282/158282701_mp4_h264_aac_hq.m3u8#cell=core',0);
+    assert.strictEqual(result.totalduration, 51.24);
+    assert.strictEqual(result.startSN, 0);
+    assert.strictEqual(result.targetduration, 14);
+    assert.strictEqual(result.live, false);
+    assert.strictEqual(result.fragments.length, 5);
+    assert.strictEqual(result.fragments[0].cc, 0);
+    assert.strictEqual(result.fragments[0].duration, 11.36);
+    assert.strictEqual(result.fragments[4].sn, 4);
+    assert.strictEqual(result.fragments[0].level, 0);
+    assert.strictEqual(result.fragments[4].cc, 0);
+    assert.strictEqual(result.fragments[4].sn, 4);
+    assert.strictEqual(result.fragments[4].start, 47.36);
+    assert.strictEqual(result.fragments[4].duration, 3.88);
+    assert.strictEqual(result.fragments[4].url, 'http://proxy-62.dailymotion.com/sec(3ae40f708f79ca9471f52b86da76a3a8)/frag(5)/video/107/282/158282701_mp4_h264_aac_hq.ts');
+  });
+
+
+  it('parse AES encrypted URLs, with implicit IV', () => {
+    var level = `#EXTM3U
+#EXT-X-VERSION:1
+## Created with Unified Streaming Platform(version=1.6.7)
+#EXT-X-MEDIA-SEQUENCE:1
+#EXT-X-ALLOW-CACHE:NO
+#EXT-X-TARGETDURATION:11
+#EXT-X-KEY:METHOD=AES-128,URI="oceans.key"
+#EXTINF:11, no desc
+oceans_aes-audio=65000-video=236000-1.ts
+#EXTINF:7, no desc
+oceans_aes-audio=65000-video=236000-2.ts
+#EXTINF:7, no desc
+oceans_aes-audio=65000-video=236000-3.ts
+#EXT-X-ENDLIST`;
+    var result = new PlaylistLoader({on : function() { }}).parseLevelPlaylist(level, 'http://foo.com/adaptive/oceans_aes/oceans_aes.m3u8',0);
+    assert.strictEqual(result.totalduration, 25);
+    assert.strictEqual(result.startSN, 1);
+    assert.strictEqual(result.targetduration, 11);
+    assert.strictEqual(result.live, false);
+    assert.strictEqual(result.fragments.length, 3);
+    assert.strictEqual(result.fragments[0].cc, 0);
+    assert.strictEqual(result.fragments[0].duration, 11);
+    assert.strictEqual(result.fragments[0].level, 0);
+    assert.strictEqual(result.fragments[0].url, 'http://foo.com/adaptive/oceans_aes/oceans_aes-audio=65000-video=236000-1.ts');
+    assert.strictEqual(result.fragments[0].decryptdata.uri, 'http://foo.com/adaptive/oceans_aes/oceans.key');
+    assert.strictEqual(result.fragments[0].decryptdata.method, 'AES-128');
+    var sn = 1;
+    var uint8View = new Uint8Array(16);
+    for (var i = 12; i < 16; i++) {
+      uint8View[i] = (sn >> 8*(15-i)) & 0xff;
+    }
+    assert(bufferIsEqual(result.fragments[0].decryptdata.iv.buffer, uint8View.buffer));
+
+    sn = 3;
+    uint8View = new Uint8Array(16);
+    for (var i = 12; i < 16; i++) {
+      uint8View[i] = (sn >> 8*(15-i)) & 0xff;
+    }
+    assert(bufferIsEqual(result.fragments[2].decryptdata.iv.buffer, uint8View.buffer));
   });
 });
