9c9f4238bb7a57050d5dd46f8bd2fa77ce608d67
mangui <g.du.pontavice@gmail.com>
2016-01-06 16:00:43 +0100

    level-helper: readable logs

--- src/helper/level-helper.js
+++ src/helper/level-helper.js
@@ -99,12 +99,12 @@ class LevelHelper {
       if (toIdx > fromIdx) {
         fragFrom.duration = fragToPTS-fragFrom.start;
         if(fragFrom.duration < 0) {
-          logger.error(`negative duration computed for ${fragFrom}, there should be some duration drift between playlist and fragment!`);
+          logger.error(`negative duration computed for frag ${fragFrom.sn},level ${fragFrom.level}, there should be some duration drift between playlist and fragment!`);
         }
       } else {
         fragTo.duration = fragFrom.start - fragToPTS;
         if(fragTo.duration < 0) {
-          logger.error(`negative duration computed for ${fragTo}, there should be some duration drift between playlist and fragment!`);
+          logger.error(`negative duration computed for frag ${fragTo.sn},level ${fragTo.level}, there should be some duration drift between playlist and fragment!`);
         }
       }
     } else {
