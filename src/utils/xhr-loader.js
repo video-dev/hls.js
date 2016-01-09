3111519a24cce6081b0dc2a432004bf9b884ee45
Leandro Moreira <leandromoreira@users.noreply.github.com>
2016-01-09 20:22:00 -0200

    adds missing semicolon

--- src/utils/xhr-loader.js
+++ src/utils/xhr-loader.js
@@ -52,7 +52,7 @@ class XhrLoader {
     var xhr = this.loader = new XMLHttpRequest();
     xhr.onreadystatechange = this.statechange.bind(this);
     xhr.onprogress = this.loadprogress.bind(this);
-    xhr.withCredentials = this.withCredentials
+    xhr.withCredentials = this.withCredentials;
 
     xhr.open('GET', this.url, true);
     if (this.byteRange) {
