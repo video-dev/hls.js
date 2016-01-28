fa14b69a021b793e6125349578000e6b8c4331cd
Grzegorz Blaszczyk <gjanblaszczyk@gmail.com>
2016-01-28 10:52:20 +0100

    remove unnecessary comma .

--- src/max-level-capping-mode.js
+++ src/max-level-capping-mode.js
@@ -5,5 +5,5 @@ module.exports = {
   /** max capped level should be the one with the dimensions equal or greater than the stage dimensions (so the video will be downscaled) **/
   DOWNSCALE: 'downscale',
   /** max capped level should be the one with the dimensions equal or lower than the stage dimensions (so the video will be upscaled) **/
-  UPSCALE: 'upscale',
+  UPSCALE: 'upscale'
 };
