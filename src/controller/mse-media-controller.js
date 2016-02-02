d1cd56f1f8ad087f8a8f4d29d9ab3f0027b91b08
mangui <g.du.pontavice@gmail.com>
2016-02-02 19:22:37 +0100

    mediaController: pace buffer flushing
    wait for SB update end to flush another buffer range if sourcebuffer is busy updating

--- src/controller/mse-media-controller.js
+++ src/controller/mse-media-controller.js
@@ -1284,6 +1284,8 @@ _checkBuffer() {
         this.flushRange.shift();
       } else {
         this._needsFlush = true;
+        // avoid looping, wait for SB update end to retrigger a flush
+        return;
       }
     }
     if (this.flushRange.length === 0) {
