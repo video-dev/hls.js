7d8f51afbb1a5ce66259e476ca36bb74ab49cbe5
Jeremy LaCivita <jeremy.lacivita@theplatform.com>
2016-02-17 14:49:41 -0800

    Reading SEI payloads via while loop
    as per https://github.com/mangui/flashls/issues/471

--- src/controller/timeline-controller.js
+++ src/controller/timeline-controller.js
@@ -56,7 +56,7 @@ class TimelineController extends EventHandler {
           }
         }
         cue = new VTTCue(startTime, endTime, text.trim());
-        cue.line = r + (navigator.userAgent.match(/Firefox\//) ? 3 : 0);
+        cue.line = r;
         cue.align = 'left';
         cue.position = 100 * (indent / 32) + (navigator.userAgent.match(/Firefox\//) ? 50 : 0);
         this.textTrack.addCue(cue);
@@ -66,9 +66,12 @@ class TimelineController extends EventHandler {
 
   clearCurrentCues()
   {
-    while (this.textTrack.cues.length > 0)
+    if (this.textTrack)
     {
-      this.textTrack.removeCue(this.textTrack.cues[0]);
+      while (this.textTrack.cues.length > 0)
+      {
+        this.textTrack.removeCue(this.textTrack.cues[0]);
+      }
     }
   }
 
