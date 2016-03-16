video-dev/hls.js

commit a84629dcc24e2926a031c0854d7087cc1debd35e

hls.config: introduce maxfragLookUpTolerance (default 0.2s)
this tolerance factor is used during fragment lookup.
instead of checking whether buffered.end is located within [start, end] range, frag lookup will be done by checking  within [start-maxfragLookUpTolerance, end-maxfragLookUpTolerance] range

this tolerance factor is used to cope with situations like
buffered.end = 9.991
frag[Ø] : [0,10]
frag[1] : [10,20]
=> buffered.end is within frag[0] range, but as we are close to frag[1], frag[1] should be choosen instead

this is useful when switching quality level, to directly load the correct fragment.
this is especially useful on emergency switch down, to avoid additional rebuffering delay
related to https://github.com/dailymotion/hls.js/issues/312

src/controller/stream-controller.js
@@ -234,12 +234,23 @@ class StreamController extends EventHandler {
             var foundFrag;
             if (bufferEnd < end) {
               foundFrag = BinarySearch.search(fragments, (candidate) => {
+                // offset should be within fragment boundary - config.maxfragLookUpTolerance
+                // this is to cope with situations like
+                // bufferEnd = 9.991
+                // frag[Ø] : [0,10]
+                // frag[1] : [10,20]
+                // bufferEnd is within frag[0] range ... although what we are expecting is to return frag[1] here
+                    //              frag start               frag start+duration
+                    //                  |-----------------------------|
+                    //              <--->                         <--->
+                    //  ...--------><-----------------------------><---------....
+                    // previous frag         matching fragment         next frag
+                    //  return -1             return 0                 return 1
                 //logger.log(`level/sn/start/end/bufEnd:${level}/${candidate.sn}/${candidate.start}/${(candidate.start+candidate.duration)}/${bufferEnd}`);
-                // offset should be within fragment boundary
-                if ((candidate.start + candidate.duration) <= bufferEnd) {
+                if ((candidate.start + candidate.duration - config.maxfragLookUpTolerance) <= bufferEnd) {
                   return 1;
                 }
-                else if (candidate.start > bufferEnd) {
+                else if (candidate.start - config.maxfragLookUpTolerance > bufferEnd) {
                   return -1;
                 }
                 return 0;

src/hls.js
@@ -45,6 +45,7 @@ class Hls {
           maxBufferSize: 60 * 1000 * 1000,
           maxBufferHole: 0.5,
           maxSeekHole: 2,
+          maxfragLookUpTolerance : 0.2,
           liveSyncDurationCount:3,
           liveMaxLatencyDurationCount: Infinity,
           liveSyncDuration: undefined,

commit 0982454097bb4a6de89a001cb6723542b74ec7ed

stream-controller: fix end of video not detected
regression introduced by https://github.com/dailymotion/hls.js/commit/a84629dcc24e2926a031c0854d7087cc1debd35e
related to https://github.com/dailymotion/hls.js/issues/314

src/controller/stream-controller.js
@@ -231,8 +231,12 @@ class StreamController extends EventHandler {
             }
           }
           if (!frag) {
-            var foundFrag;
+            let foundFrag;
+            let maxFragLookUpTolerance = config.maxFragLookUpTolerance;
             if (bufferEnd < end) {
+              if (bufferEnd > end - maxFragLookUpTolerance) {
+                maxFragLookUpTolerance = 0;
+              }
               foundFrag = BinarySearch.search(fragments, (candidate) => {
                 // offset should be within fragment boundary - config.maxFragLookUpTolerance
                 // this is to cope with situations like
@@ -247,10 +251,10 @@ class StreamController extends EventHandler {
                     // previous frag         matching fragment         next frag
                     //  return -1             return 0                 return 1
                 //logger.log(`level/sn/start/end/bufEnd:${level}/${candidate.sn}/${candidate.start}/${(candidate.start+candidate.duration)}/${bufferEnd}`);
-                if ((candidate.start + candidate.duration - config.maxFragLookUpTolerance) <= bufferEnd) {
+                if ((candidate.start + candidate.duration - maxFragLookUpTolerance) <= bufferEnd) {
                   return 1;
                 }
-                else if (candidate.start - config.maxFragLookUpTolerance > bufferEnd) {
+                else if (candidate.start - maxFragLookUpTolerance > bufferEnd) {
                   return -1;
                 }
                 return 0;