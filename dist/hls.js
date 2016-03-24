1a30dd54fa126e9d22339fec214ae0dff09bb31d
mangui <g.du.pontavice@gmail.com>
2016-03-24 12:26:57 +0100

    update dist
    related to #9

--- dist/hls.js
+++ dist/hls.js
@@ -7287,8 +7287,8 @@ var MP4Remuxer = function () {
           }
         });
       }
-      // next AVC sample DTS should be equal to last sample DTS + last sample duration
-      this.nextAvcDts = lastDTS + mp4SampleDuration;
+      // next AVC sample DTS should be equal to last sample DTS + last sample duration (in PES timescale)
+      this.nextAvcDts = lastDTS + mp4SampleDuration * pes2mp4ScaleFactor;
       track.len = 0;
       track.nbNalu = 0;
       if (outputSamples.length && navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
