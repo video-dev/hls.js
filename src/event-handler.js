59891f604f22b73e7f83cb3964fc0be6350a35df
mangui <g.du.pontavice@gmail.com>
2016-04-14 19:42:11 +0200

    event-handler : catch/report potential exception raised by hls.js internal event listeners

--- src/event-handler.js
+++ src/event-handler.js
@@ -4,7 +4,8 @@
 *
 */
 
-//import {logger} from './utils/logger';
+import { logger } from './utils/logger';
+import { ErrorTypes, ErrorDetails } from './errors';
 
 class EventHandler {
     constructor(hls, ...events) {
@@ -70,7 +71,22 @@ class EventHandler {
             }
             return this[funcName].bind(this, data);
         };
-        eventToFunction.call(this, event, data).call();
+        try {
+            eventToFunction.call(this, event, data).call();
+        } catch (err) {
+            logger.error(
+                `internal error happened while processing ${event}:${
+                    err.message
+                }`
+            );
+            this.hls.trigger(Event.ERROR, {
+                type: ErrorTypes.OTHER_ERROR,
+                details: ErrorDetails.INTERNAL_EXCEPTION,
+                fatal: false,
+                event: event,
+                err: err
+            });
+        }
     }
 }
 
