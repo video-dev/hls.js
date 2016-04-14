d22d0e320adcd8e728e79ee51c54d484c88b862b
Grzegorz Blaszczyk <gjanblaszczyk@gmail.com>
2016-04-14 11:37:23 +0200

    #359 improve manifest refresh logic.

--- src/controller/level-controller.js
+++ src/controller/level-controller.js
@@ -286,7 +286,7 @@ class LevelController extends EventHandler {
         // check if current playlist is a live playlist
         if (data.details.live) {
             // if live playlist we will have to reload it periodically
-            // set reload period to avarage of the frag duration, if avarage not set then use playlist target duration
+            // set reload period to average of the frag duration, if average not set then use playlist target duration
             let timerInterval = data.details.averagetargetduration
                 ? data.details.averagetargetduration
                 : data.details.targetduration;
--- src/loader/playlist-loader.js
+++ src/loader/playlist-loader.js
@@ -247,8 +247,7 @@ class PlaylistLoader extends EventHandler {
             totalduration -= frag.duration;
         }
         level.totalduration = totalduration;
-        level.averagetargetduration =
-            level.totalduration / level.fragments.length;
+        level.averagetargetduration = totalduration / level.fragments.length;
         level.endSN = currentSN - 1;
         return level;
     }
