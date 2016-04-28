7af8e5efeb83fb005fc2b4da362c185f28fc174e
mangui <g.du.pontavice@gmail.com>
2016-04-28 19:13:30 +0200

    fragment-loader: handle main/audio frag concurrent load
    related to https://github.com/dailymotion/hls.js/issues/30

--- src/loader/fragment-loader.js
+++ src/loader/fragment-loader.js
@@ -5,55 +5,72 @@
 import Event from '../events';
 import EventHandler from '../event-handler';
 import {ErrorTypes, ErrorDetails} from '../errors';
+import {logger} from '../utils/logger';
 
 class FragmentLoader extends EventHandler {
 
   constructor(hls) {
     super(hls, Event.FRAG_LOADING);
+    this.loaders = {};
   }
 
   destroy() {
-    if (this.loader) {
-      this.loader.destroy();
-      this.loader = null;
+    for (let loaderName in this.loaders) {
+      let loader = this.loaders[loaderName];
+      if (loader) {
+        loader.destroy();
+      }
     }
+    this.loaders = {};
     EventHandler.prototype.destroy.call(this);
   }
 
   onFragLoading(data) {
-    var frag = data.frag;
-    this.frag = frag;
-    this.frag.loaded = 0;
-    var config = this.hls.config;
-    frag.loader = this.loader = typeof(config.fLoader) !== 'undefined' ? new config.fLoader(config) : new config.loader(config);
-    this.loader.load(frag.url, null, 'arraybuffer', this.loadsuccess.bind(this), this.loaderror.bind(this), this.loadtimeout.bind(this), config.fragLoadingTimeOut, 1, 0, this.loadprogress.bind(this), frag);
+    let frag = this.frag = data.frag;
+    frag.loaded = 0;
+    let config = this.hls.config;
+    let context = { frag : frag };
+    let type = frag.type;
+    let loader = this.loaders[type];
+    if (loader) {
+      logger.warn(`abort previous fragment loader for type:${type}`);
+      loader.abort();
+    }
+    loader  = this.loaders[type] = frag.loader = typeof(config.fLoader) !== 'undefined' ? new config.fLoader(config) : new config.loader(config);
+    loader.load(frag.url, context, 'arraybuffer', this.loadsuccess.bind(this), this.loaderror.bind(this), this.loadtimeout.bind(this), config.fragLoadingTimeOut, 1, 0, this.loadprogress.bind(this), frag);
   }
 
-  loadsuccess(event, stats) {
-    var payload = event.currentTarget.response;
+  loadsuccess(event, stats, context) {
+    let payload = event.currentTarget.response, frag = context.frag;
     stats.length = payload.byteLength;
     // detach fragment loader on load success
-    this.frag.loader = undefined;
-    this.hls.trigger(Event.FRAG_LOADED, {payload: payload, frag: this.frag, stats: stats});
+    frag.loader = undefined;
+    this.loaders[frag.type] = undefined;
+    this.hls.trigger(Event.FRAG_LOADED, {payload: payload, frag: frag, stats: stats});
   }
 
-  loaderror(event) {
-    if (this.loader) {
-      this.loader.abort();
+  loaderror(event, context) {
+    let loader = context.loader;
+    if (loader) {
+      loader.abort();
     }
-    this.hls.trigger(Event.ERROR, {type: ErrorTypes.NETWORK_ERROR, details: ErrorDetails.FRAG_LOAD_ERROR, fatal: false, frag: this.frag, response: event});
+    this.loaders[context.type] = undefined;
+    this.hls.trigger(Event.ERROR, {type: ErrorTypes.NETWORK_ERROR, details: ErrorDetails.FRAG_LOAD_ERROR, fatal: false, frag: context.frag, response: event});
   }
 
   loadtimeout() {
-    if (this.loader) {
-      this.loader.abort();
+    let loader = context.loader;
+    if (loader) {
+      loader.abort();
     }
-    this.hls.trigger(Event.ERROR, {type: ErrorTypes.NETWORK_ERROR, details: ErrorDetails.FRAG_LOAD_TIMEOUT, fatal: false, frag: this.frag});
+    this.loaders[context.type] = undefined;
+    this.hls.trigger(Event.ERROR, {type: ErrorTypes.NETWORK_ERROR, details: ErrorDetails.FRAG_LOAD_TIMEOUT, fatal: false, frag: context.frag});
   }
 
-  loadprogress(stats) {
-    this.frag.loaded = stats.loaded;
-    this.hls.trigger(Event.FRAG_LOAD_PROGRESS, {frag: this.frag, stats: stats});
+  loadprogress(event, stats, context) {
+    let frag = context.frag;
+    frag.loaded = stats.loaded;
+    this.hls.trigger(Event.FRAG_LOAD_PROGRESS, {frag: frag, stats: stats});
   }
 }
 
