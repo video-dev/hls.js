ea4a91cb88a43b5c1e158dc3f4ff3c59f3e3dd12
mangui <g.du.pontavice@gmail.com>
2016-04-28 20:00:30 +0200

    key-loader: handle key concurrent load
    related to https://github.com/dailymotion/hls.js/issues/30

--- src/loader/key-loader.js
+++ src/loader/key-loader.js
@@ -10,29 +10,40 @@ class KeyLoader extends EventHandler {
 
   constructor(hls) {
     super(hls, Event.KEY_LOADING);
+    this.loaders = {};
     this.decryptkey = null;
     this.decrypturl = null;
   }
 
   destroy() {
-    if (this.loader) {
-      this.loader.destroy();
-      this.loader = null;
+    for (let loaderName in this.loaders) {
+      let loader = this.loaders[loaderName];
+      if (loader) {
+        loader.destroy();
+      }
     }
+    this.loaders = {};
     EventHandler.prototype.destroy.call(this);
   }
 
   onKeyLoading(data) {
-    var frag = this.frag = data.frag,
+    let frag = data.frag,
+        type = frag.type,
+        loader = this.loaders[type],
         decryptdata = frag.decryptdata,
         uri = decryptdata.uri;
         // if uri is different from previous one or if decrypt key not retrieved yet
       if (uri !== this.decrypturl || this.decryptkey === null) {
-        var config = this.hls.config;
-        frag.loader = this.loader = new config.loader(config);
+        let config = this.hls.config;
+
+        if (loader) {
+          logger.warn(`abort previous fragment loader for type:${type}`);
+          loader.abort();
+        }
+        frag.loader = this.loaders[type] = new config.loader(config);
         this.decrypturl = uri;
         this.decryptkey = null;
-        frag.loader.load(uri, 'arraybuffer', this.loadsuccess.bind(this), this.loaderror.bind(this), this.loadtimeout.bind(this), config.fragLoadingTimeOut, config.fragLoadingMaxRetry, config.fragLoadingRetryDelay, this.loadprogress.bind(this), frag);
+        frag.loader.load(uri, { frag : frag }, 'arraybuffer', this.loadsuccess.bind(this), this.loaderror.bind(this), this.loadtimeout.bind(this), config.fragLoadingTimeOut, config.fragLoadingMaxRetry, config.fragLoadingRetryDelay, this.loadprogress.bind(this), frag);
       } else if (this.decryptkey) {
         // we already loaded this key, return it
         decryptdata.key = this.decryptkey;
@@ -40,26 +51,33 @@ class KeyLoader extends EventHandler {
       }
   }
 
-  loadsuccess(event) {
-    var frag = this.frag;
+  loadsuccess(event, stats, context) {
+    let frag = context.frag;
     this.decryptkey = frag.decryptdata.key = new Uint8Array(event.currentTarget.response);
     // detach fragment loader on load success
     frag.loader = undefined;
+    this.loaders[context.type] = undefined;
     this.hls.trigger(Event.KEY_LOADED, {frag: frag});
   }
 
-  loaderror(event) {
-    if (this.loader) {
-      this.loader.abort();
+  loaderror(event, context) {
+    let frag = context.frag,
+        loader = frag.loader;
+    if (loader) {
+      loader.abort();
     }
-    this.hls.trigger(Event.ERROR, {type: ErrorTypes.NETWORK_ERROR, details: ErrorDetails.KEY_LOAD_ERROR, fatal: false, frag: this.frag, response: event});
+    this.loaders[context.type] = undefined;
+    this.hls.trigger(Event.ERROR, {type: ErrorTypes.NETWORK_ERROR, details: ErrorDetails.KEY_LOAD_ERROR, fatal: false, frag: frag, response: event});
   }
 
-  loadtimeout() {
-    if (this.loader) {
-      this.loader.abort();
+  loadtimeout(event, stats, context) {
+    let frag = context.frag,
+        loader = frag.loader;
+    if (loader) {
+      loader.abort();
     }
-    this.hls.trigger(Event.ERROR, {type: ErrorTypes.NETWORK_ERROR, details: ErrorDetails.KEY_LOAD_TIMEOUT, fatal: false, frag: this.frag});
+    this.loaders[context.type] = undefined;
+    this.hls.trigger(Event.ERROR, {type: ErrorTypes.NETWORK_ERROR, details: ErrorDetails.KEY_LOAD_TIMEOUT, fatal: false, frag: frag});
   }
 
   loadprogress() {
