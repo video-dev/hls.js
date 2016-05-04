96a3d7c50248f165dce43ed540052c3a9fad1671
mangui <g.du.pontavice@gmail.com>
2016-05-04 11:52:06 +0200

    update dist

--- dist/hls.js
+++ dist/hls.js
@@ -6464,6 +6464,17 @@ var PlaylistLoader = function (_EventHandler) {
           retry,
           timeout,
           retryDelay;
+
+      if (this.loading && this.loader) {
+        if (this.url === url && this.id === id1 && this.id2 === id2) {
+          // same request than last pending one, don't do anything
+          return;
+        } else {
+          // one playlist load request is pending, but with different params, abort it before loading new playlist
+          this.loader.abort();
+        }
+      }
+
       this.url = url;
       this.id = id1;
       this.id2 = id2;
@@ -6477,6 +6488,7 @@ var PlaylistLoader = function (_EventHandler) {
         retryDelay = config.levelLoadingRetryDelay;
       }
       this.loader = typeof config.pLoader !== 'undefined' ? new config.pLoader(config) : new config.loader(config);
+      this.loading = true;
       this.loader.load(url, '', this.loadsuccess.bind(this), this.loaderror.bind(this), this.loadtimeout.bind(this), timeout, retry, retryDelay);
     }
   }, {
@@ -6659,6 +6671,8 @@ var PlaylistLoader = function (_EventHandler) {
           id2 = this.id2,
           hls = this.hls,
           levels;
+
+      this.loading = false;
       // responseURL not supported on some browsers (it is used to detect URL redirection)
       // data-uri mode also not supported (but no need to detect redirection)
       if (url === undefined || url.indexOf('data:') === 0) {
@@ -6706,6 +6720,7 @@ var PlaylistLoader = function (_EventHandler) {
       if (this.loader) {
         this.loader.abort();
       }
+      this.loading = false;
       this.hls.trigger(_events2.default.ERROR, { type: _errors.ErrorTypes.NETWORK_ERROR, details: details, fatal: fatal, url: this.url, loader: this.loader, response: event.currentTarget, level: this.id, id: this.id2 });
     }
   }, {
@@ -6722,6 +6737,7 @@ var PlaylistLoader = function (_EventHandler) {
       if (this.loader) {
         this.loader.abort();
       }
+      this.loading = false;
       this.hls.trigger(_events2.default.ERROR, { type: _errors.ErrorTypes.NETWORK_ERROR, details: details, fatal: fatal, url: this.url, loader: this.loader, level: this.id, id: this.id2 });
     }
   }]);
