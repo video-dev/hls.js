cee7ee49ce7448e91f920c1e9607d22f30366e2f
mangui <g.du.pontavice@gmail.com>
2016-05-17 11:26:53 +0200

    tsdemuxer: don't push video frames until keyframe found on current/contiguous fragment

--- src/demux/tsdemuxer.js
+++ src/demux/tsdemuxer.js
@@ -472,8 +472,11 @@
     //build sample from PES
     // Annex B to MP4 conversion to be done
     if (units2.length) {
-      // only push AVC sample if keyframe already found. browsers expect a keyframe at first to start decoding
-      if (key === true || track.sps ) {
+      // only push AVC sample if keyframe already found in this fragment OR
+      // if keyframe found in last fragment (track.sps) AND fragment is contiguous
+      // browsers expect a keyframe at first to start decoding
+      if (key === true ||
+          (track.sps && this.contiguous)) {
         avcSample = {units: { units : units2, length : length}, pts: pes.pts, dts: pes.dts, key: key};
         samples.push(avcSample);
         track.len += length;
