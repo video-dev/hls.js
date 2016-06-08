7a565595f65afeb3d39f923644bde7ce5defca4a
mangui <g.du.pontavice@gmail.com>
2016-06-08 18:20:03 +0200

    stream-controller: allow seekOverHole in case of media is seeking and fragLoopLoadingError just occured
    fix fragLoopLoadingError looping in Firefox
    related to https://github.com/dailymotion/hls.js/issues/476

--- src/controller/stream-controller.js
+++ src/controller/stream-controller.js
@@ -641,6 +641,8 @@ class StreamController extends EventHandler {
   }
 
   onMediaSeeked() {
+    // reset flag, used to potentially recover fragLoopLoadingError on seeking
+    this.fragLoopLoadingErrorOnSeeking = false;
     // tick to speed up FRAGMENT_PLAYING triggering
     this.tick();
   }
@@ -968,6 +970,11 @@ class StreamController extends EventHandler {
         }
         break;
       case ErrorDetails.FRAG_LOOP_LOADING_ERROR:
+        let media = this.media;
+        if (media && media.seeking) {
+          this.fragLoopLoadingErrorOnSeeking = true;
+        }
+        /* falls through */
       case ErrorDetails.LEVEL_LOAD_ERROR:
       case ErrorDetails.LEVEL_LOAD_TIMEOUT:
       case ErrorDetails.KEY_LOAD_ERROR:
@@ -1024,7 +1031,10 @@ _checkBuffer() {
           logger.log(`target seek position:${targetSeekPosition}`);
         }
         var bufferInfo = BufferHelper.bufferInfo(media,currentTime,0),
-            expectedPlaying = !(media.paused || media.ended || media.seeking || media.buffered.length === 0),
+            expectedPlaying = !(media.paused || // not playing when media is paused
+                                media.ended  || // not playing when media is ended
+                                media.seeking && !this.fragLoopLoadingErrorOnSeeking || // not playing when media is seeking AND no fragLoopLoadingError on seeking
+                                media.buffered.length === 0), // not playing if nothing buffered
             jumpThreshold = 0.4, // tolerance needed as some browsers stalls playback before reaching buffered range end
             playheadMoving = currentTime > media.playbackRate*this.lastCurrentTime;
 
@@ -1058,7 +1068,7 @@ _checkBuffer() {
             if(nextBufferStart &&
                (delta < this.config.maxSeekHole) &&
                (delta > 0)  &&
-               !media.seeking) {
+               (!media.seeking || this.fragLoopLoadingErrorOnSeeking)) { // allow adjusting currentTime if we faced a fragLoopLoading error on seeking
               // next buffer is close ! adjust currentTime to nextBufferStart
               // this will ensure effective video decoding
               logger.log(`adjust currentTime from ${media.currentTime} to next buffered @ ${nextBufferStart} + nudge ${this.seekHoleNudgeDuration}`);
