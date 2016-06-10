85c6ca74520471f0e84015873bcb56b3a478dc05
mangui <g.du.pontavice@gmail.com>
2016-06-10 16:37:07 +0200

    buffer-controller: only update mediasource duration if its value increase
    avoids flushing buffered area when switching between non aligned quality levels OR when switching to a quality level with unknwon PTS

--- src/controller/buffer-controller.js
+++ src/controller/buffer-controller.js
@@ -26,7 +26,7 @@ class BufferController extends EventHandler {
     this._msDuration = null;
     // the value that we want to set mediaSource.duration to
     this._levelDuration = null;
-   
+
     // Source Buffer listeners
     this.onsbue = this.onSBUpdateEnd.bind(this);
     this.onsbe  = this.onSBUpdateError.bind(this);
@@ -253,7 +253,9 @@ class BufferController extends EventHandler {
     }
     // this._levelDuration was the last value we set.
     // not using mediaSource.duration as the browser may tweak this value
-    if (this._levelDuration !== this._msDuration) {
+    // only update mediasource duration if its value increase, this is to avoid
+    // flushing already buffered portion when switching between quality level, as they
+    if (this._levelDuration > this._msDuration) {
       logger.log(`Updating mediasource duration to ${this._levelDuration}`);
       mediaSource.duration = this._levelDuration;
       this._msDuration = this._levelDuration;
