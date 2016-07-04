854d2462a0593cc2121bf3efbb0f4673843a52df
mangui <g.du.pontavice@gmail.com>
2016-07-04 12:14:38 +0200

    lint

--- src/demux/demuxer-worker.js
+++ src/demux/demuxer-worker.js
@@ -3,65 +3,98 @@
  *  - provides MP4 Boxes back to main thread using [transferable objects](https://developers.google.com/web/updates/2011/12/Transferable-Objects-Lightning-Fast) in order to minimize message passing overhead.
  */
 
- import DemuxerInline from '../demux/demuxer-inline';
- import Event from '../events';
- import EventEmitter from 'events';
+import DemuxerInline from '../demux/demuxer-inline';
+import Event from '../events';
+import EventEmitter from 'events';
 
-var DemuxerWorker = function (self) {
-  // observer setup
-  var observer = new EventEmitter();
-  observer.trigger = function trigger (event, ...data) {
-    observer.emit(event, event, ...data);
-  };
+var DemuxerWorker = function(self) {
+    // observer setup
+    var observer = new EventEmitter();
+    observer.trigger = function trigger(event, ...data) {
+        observer.emit(event, event, ...data);
+    };
 
-  observer.off = function off (event, ...data) {
-    observer.removeListener(event, ...data);
-  };
-  self.addEventListener('message', function (ev) {
-    var data = ev.data;
-    //console.log('demuxer cmd:' + data.cmd);
-    switch (data.cmd) {
-      case 'init':
-        self.demuxer = new DemuxerInline(observer, data.id, data.typeSupported, JSON.parse(data.config));
-        break;
-      case 'demux':
-        self.demuxer.push(new Uint8Array(data.data), data.audioCodec, data.videoCodec, data.timeOffset, data.cc, data.level, data.sn, data.duration);
-        break;
-      default:
-        break;
-    }
-  });
+    observer.off = function off(event, ...data) {
+        observer.removeListener(event, ...data);
+    };
+    self.addEventListener('message', function(ev) {
+        var data = ev.data;
+        //console.log('demuxer cmd:' + data.cmd);
+        switch (data.cmd) {
+            case 'init':
+                self.demuxer = new DemuxerInline(
+                    observer,
+                    data.id,
+                    data.typeSupported,
+                    JSON.parse(data.config)
+                );
+                break;
+            case 'demux':
+                self.demuxer.push(
+                    new Uint8Array(data.data),
+                    data.audioCodec,
+                    data.videoCodec,
+                    data.timeOffset,
+                    data.cc,
+                    data.level,
+                    data.sn,
+                    data.duration
+                );
+                break;
+            default:
+                break;
+        }
+    });
 
-  // listen to events triggered by Demuxer
-  observer.on(Event.FRAG_PARSING_INIT_SEGMENT, function(ev, data) {
-    self.postMessage({event: ev, id : data.id, level : data.level, sn : data.sn, tracks : data.tracks, unique : data.unique });
-  });
+    // listen to events triggered by Demuxer
+    observer.on(Event.FRAG_PARSING_INIT_SEGMENT, function(ev, data) {
+        self.postMessage({
+            event: ev,
+            id: data.id,
+            level: data.level,
+            sn: data.sn,
+            tracks: data.tracks,
+            unique: data.unique
+        });
+    });
 
-  observer.on(Event.FRAG_PARSING_DATA, function(ev, data) {
-    var objData = {event: ev, id : data.id, level : data.level, sn : data.sn, type: data.type, startPTS: data.startPTS, endPTS: data.endPTS, startDTS: data.startDTS, endDTS: data.endDTS, data1: data.data1.buffer, data2: data.data2.buffer, nb: data.nb, , dropped : data.dropped};
-    // pass data1/data2 as transferable object (no copy)
-    self.postMessage(objData, [objData.data1, objData.data2]);
-  });
+    observer.on(Event.FRAG_PARSING_DATA, function(ev, data) {
+        var objData = {
+            event: ev,
+            id: data.id,
+            level: data.level,
+            sn: data.sn,
+            type: data.type,
+            startPTS: data.startPTS,
+            endPTS: data.endPTS,
+            startDTS: data.startDTS,
+            endDTS: data.endDTS,
+            data1: data.data1.buffer,
+            data2: data.data2.buffer,
+            nb: data.nb,
+            dropped: data.dropped
+        };
+        // pass data1/data2 as transferable object (no copy)
+        self.postMessage(objData, [objData.data1, objData.data2]);
+    });
 
-  observer.on(Event.FRAG_PARSED, function(event, data) {
-    self.postMessage({event: event, data : data});
-  });
+    observer.on(Event.FRAG_PARSED, function(event, data) {
+        self.postMessage({ event: event, data: data });
+    });
 
-  observer.on(Event.ERROR, function(event, data) {
-    self.postMessage({event: event, data: data });
-  });
+    observer.on(Event.ERROR, function(event, data) {
+        self.postMessage({ event: event, data: data });
+    });
 
-  observer.on(Event.FRAG_PARSING_METADATA, function(event, data) {
-    var objData = {event: event, id : data.id, samples: data.samples};
-    self.postMessage(objData);
-  });
+    observer.on(Event.FRAG_PARSING_METADATA, function(event, data) {
+        var objData = { event: event, id: data.id, samples: data.samples };
+        self.postMessage(objData);
+    });
 
-  observer.on(Event.FRAG_PARSING_USERDATA, function(event, data) {
-    var objData = {event: event, id : data.id, samples: data.samples};
-    self.postMessage(objData);
-  });
-
+    observer.on(Event.FRAG_PARSING_USERDATA, function(event, data) {
+        var objData = { event: event, id: data.id, samples: data.samples };
+        self.postMessage(objData);
+    });
 };
 
 export default DemuxerWorker;
-
