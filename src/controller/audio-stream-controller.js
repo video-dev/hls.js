de55d90f1d42b6e7630421ae9a370dc084cd15f1
mangui <g.du.pontavice@gmail.com>
2016-07-04 17:05:49 +0200

    audio-stream-controller: support configurable startPosition

--- src/controller/audio-stream-controller.js
+++ src/controller/audio-stream-controller.js
@@ -62,7 +62,7 @@ class AudioStreamController extends EventHandler {
         this.state = State.STOPPED;
     }
 
-    startLoad(startPosition = 0) {
+    startLoad(startPosition) {
         if (this.tracks) {
             var media = this.media,
                 lastCurrentTime = this.lastCurrentTime;
@@ -84,7 +84,7 @@ class AudioStreamController extends EventHandler {
             this.tick();
         } else {
             logger.warn('cannot start loading as audio tracks not parsed yet');
-            this.nextLoadPosition = startPosition;
+            this.startPosition = startPosition;
             this.state = State.STOPPED;
         }
     }
@@ -357,8 +357,9 @@ class AudioStreamController extends EventHandler {
         this.onvended = this.onMediaEnded.bind(this);
         media.addEventListener('seeking', this.onvseeking);
         media.addEventListener('ended', this.onvended);
-        if (this.tracks && this.config.autoStartLoad) {
-            this.startLoad();
+        let config = this.config;
+        if (this.tracks && config.autoStartLoad) {
+            this.startLoad(config.startPosition);
         }
     }
 
@@ -439,7 +440,20 @@ class AudioStreamController extends EventHandler {
         track.details = details;
 
         // compute start position
-        if (this.startFragRequested === false) {
+        if (!this.startFragRequested) {
+            // compute start position if set to -1. use it straight away if value is defined
+            if (this.startPosition === -1) {
+                // first, check if start time offset has been set in playlist, if yes, use this value
+                let startTimeOffset = details.startTimeOffset;
+                if (!isNaN(startTimeOffset)) {
+                    logger.log(
+                        `start time offset found in playlist, adjust startPosition to ${startTimeOffset}`
+                    );
+                    this.startPosition = startTimeOffset;
+                } else {
+                    this.startPosition = 0;
+                }
+            }
             this.nextLoadPosition = this.startPosition;
         }
         // only switch batck to IDLE state if we were waiting for track to start downloading a new fragment
@@ -596,8 +610,11 @@ class AudioStreamController extends EventHandler {
     }
 
     onBufferCreated(data) {
-        this.mediaBuffer = data.tracks.audio.buffer;
-        this.loadedmetadata = true;
+        let audioTrack = data.tracks.audio;
+        if (audioTrack) {
+            this.mediaBuffer = audioTrack.buffer;
+            this.loadedmetadata = true;
+        }
     }
 
     onBufferAppended(data) {
