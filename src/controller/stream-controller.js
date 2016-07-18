video-dev/hls.js

commit 582aab08657b6f92feb84de1bb992de7adf0f6c3

stream-controller: provide DTS instead of PTS as timeOffset to demuxer
demuxer use initDTS as reference
this fixes issues with different parsed PTS/DTS on same fragment

src/controller/stream-controller.js
@@ -764,7 +764,7 @@ class StreamController extends EventHandler {
         var currentLevel = this.levels[this.level],
             details = currentLevel.details,
             duration = details.totalduration,
-            start = fragCurrent.start,
+            start = fragCurrent.startDTS !== undefined ? fragCurrent.startDTS  : fragCurrent.start,
             level = fragCurrent.level,
             sn = fragCurrent.sn,
             audioCodec = currentLevel.audioCodec || this.config.defaultAudioCodec;
@@ -880,7 +880,7 @@ class StreamController extends EventHandler {
 
       logger.log(`parsed ${data.type},PTS:[${data.startPTS.toFixed(3)},${data.endPTS.toFixed(3)}],DTS:[${data.startDTS.toFixed(3)}/${data.endDTS.toFixed(3)}],nb:${data.nb}`);
 
-      var drift = LevelHelper.updateFragPTS(level.details,frag.sn,data.startPTS,data.endPTS),
+      var drift = LevelHelper.updateFragPTSDTS(level.details,frag.sn,data.startPTS,data.endPTS,data.startDTS,data.endDTS),
           hls = this.hls;
       hls.trigger(Event.LEVEL_PTS_UPDATED, {details: level.details, level: this.level, drift: drift});
 

src/helper/level-helper.js
@@ -42,7 +42,7 @@ class LevelHelper {
 
     // if at least one fragment contains PTS info, recompute PTS information for all fragments
     if(PTSFrag) {
-      LevelHelper.updateFragPTS(newDetails,PTSFrag.sn,PTSFrag.startPTS,PTSFrag.endPTS);
+      LevelHelper.updateFragPTSDTS(newDetails,PTSFrag.sn,PTSFrag.startPTS,PTSFrag.endPTS,PTSFrag.startDTS,PTSFrag.endDTS);
     } else {
       // ensure that delta is within oldfragments range
       // also adjust sliding in case delta is 0 (we could have old=[50-60] and new=old=[50-61])
@@ -61,7 +61,7 @@ class LevelHelper {
     return;
   }
 
-  static updateFragPTS(details,sn,startPTS,endPTS) {
+  static updateFragPTSDTS(details,sn,startPTS,endPTS,startDTS,endDTS) {
     var fragIdx, fragments, frag, i;
     // exit if sn out of range
     if (sn < details.startSN || sn > details.endSN) {
@@ -73,12 +73,16 @@ class LevelHelper {
     if(!isNaN(frag.startPTS)) {
       startPTS = Math.min(startPTS,frag.startPTS);
       endPTS = Math.max(endPTS, frag.endPTS);
+      startDTS = Math.min(startDTS,frag.startDTS);
+      endDTS = Math.max(endDTS, frag.endDTS);
     }
 
     var drift = startPTS - frag.start;
 
     frag.start = frag.startPTS = startPTS;
     frag.endPTS = endPTS;
+    frag.startDTS = startDTS;
+    frag.endDTS = endDTS;
     frag.duration = endPTS - startPTS;
     // adjust fragment PTS/duration from seqnum-1 to frag 0
     for(i = fragIdx ; i > 0 ; i--) {

commit 05a583ab930c34f2ff84b0497d61c005380cb1e0

stream-controller: Ignore startDTS when it is NaN in onFragLoaded
Prevents playback failure when calling stopLoad/startLoad repeatedly on live content

Fixes regression introduced by 582aab08657b6f92feb84de1bb992de7adf0f6c3

src/controller/stream-controller.js
@@ -795,7 +795,7 @@ class StreamController extends EventHandler {
         var currentLevel = this.levels[this.level],
             details = currentLevel.details,
             duration = details.totalduration,
-            start = fragCurrent.startDTS !== undefined ? fragCurrent.startDTS  : fragCurrent.start,
+            start = fragCurrent.startDTS !== undefined && !isNaN(fragCurrent.startDTS) ? fragCurrent.startDTS  : fragCurrent.start,
             level = fragCurrent.level,
             sn = fragCurrent.sn,
             audioCodec = currentLevel.audioCodec || this.config.defaultAudioCodec;