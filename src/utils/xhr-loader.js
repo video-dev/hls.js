1fd6ef237b7f4b1a0bee3979713285ccc4291b6a
mangui <g.du.pontavice@gmail.com>
2016-07-21 12:38:19 +0200

    xhr-loader: don't retry if HTTP status code between 400 and 499
    these errors are not recoverable. retrying is useless

--- src/utils/xhr-loader.js
+++ src/utils/xhr-loader.js
@@ -100,10 +100,19 @@ class XhrLoader {
                         : xhr.responseText
             };
             this.callbacks.onSuccess(response, stats, context);
-            // everything else is a failure
         } else {
-            // retry first
-            if (stats.retry < config.maxRetry) {
+            // if max nb of retries reached or if http status between 400 and 499 (such error cannot be recovered, retrying is useless), return error
+            if (
+                stats.retry >= config.maxRetry ||
+                (status >= 400 && status < 499)
+            ) {
+                logger.error(`${status} while loading ${context.url}`);
+                this.callbacks.onError(
+                    { code: status, text: xhr.statusText },
+                    context
+                );
+            } else {
+                // retry
                 logger.warn(
                     `${status} while loading ${context.url}, retrying in ${
                         this.retryDelay
@@ -119,13 +128,6 @@ class XhrLoader {
                 // set exponential backoff
                 this.retryDelay = Math.min(2 * this.retryDelay, 64000);
                 stats.retry++;
-                // permanent failure
-            } else {
-                logger.error(`${status} while loading ${context.url}`);
-                this.callbacks.onError(
-                    { code: status, text: xhr.statusText },
-                    context
-                );
             }
         }
     }
