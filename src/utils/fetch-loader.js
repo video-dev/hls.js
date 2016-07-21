0045735695280067b60104420101249b06284a5c
mangui <g.du.pontavice@gmail.com>
2016-07-21 12:21:51 +0200

    fetch-loader: support byte range

--- src/utils/fetch-loader.js
+++ src/utils/fetch-loader.js
@@ -24,6 +24,13 @@ class FetchLoader {
             credentials: 'same-origin'
         };
 
+        if (context.rangeEnd) {
+            initParams.headers = new Headers({
+                Range:
+                    'bytes=' + context.rangeStart + '-' + (context.rangeEnd - 1)
+            });
+        }
+
         let request = new Request(context.url, initParams),
             fetchPromise = fetch(request, initParams);
 
