7bee487b8ee5eeab5fd8c3457d001f93681cbb09
justin hartman <justin.hartman@gmail.com>
2016-08-03 11:15:11 -0400

    var -> let, let -> const, fix indentation, etc.

--- src/controller/abr-controller.js
+++ src/controller/abr-controller.js
@@ -37,7 +37,7 @@ class AbrController extends EventHandler {
   }
 
   onFragLoadProgress(data) {
-    var stats = data.stats;
+    const stats = data.stats;
     // only update stats if first frag loading
     // if same frag is loaded multiple times, it might be in browser cache, and loaded quickly
     // and leading to wrong bw estimation
@@ -54,7 +54,7 @@ class AbrController extends EventHandler {
       we compute expected time of arrival of the complete fragment.
       we compare it to expected time of buffer starvation
     */
-    let hls = this.hls, v = hls.media,frag = this.fragCurrent;
+    const hls = this.hls, v = hls.media,frag = this.fragCurrent;
 
     // if loader has been destroyed or loading has been aborted, stop timer and return
     if(!frag.loader || ( frag.loader.stats && frag.loader.stats.aborted)) {
@@ -65,17 +65,17 @@ class AbrController extends EventHandler {
     /* only monitor frag retrieval time if
     (video not paused OR first fragment being loaded(ready state === HAVE_NOTHING = 0)) AND autoswitching enabled AND not lowest level (=> means that we have several levels) */
     if (v && ((!v.paused && (v.playbackRate !== 0)) || !v.readyState) && frag.autoLevel && frag.level) {
-      let requestDelay = performance.now() - frag.trequest,
-          playbackRate = Math.abs(v.playbackRate);
+      const requestDelay = performance.now() - frag.trequest,
+            playbackRate = Math.abs(v.playbackRate);
       // monitor fragment load progress after half of expected fragment duration,to stabilize bitrate
       if (requestDelay > (500 * frag.duration / playbackRate)) {
-        let loadRate = Math.max(1,frag.loaded * 1000 / requestDelay); // byte/s; at least 1 byte/s to avoid division by zero
+        const loadRate = Math.max(1,frag.loaded * 1000 / requestDelay); // byte/s; at least 1 byte/s to avoid division by zero
         if (frag.expectedLen < frag.loaded) {
           frag.expectedLen = frag.loaded;
         }
-        let pos = v.currentTime;
-        let fragLoadedDelay = (frag.expectedLen - frag.loaded) / loadRate;
-        let bufferStarvationDelay = (BufferHelper.bufferInfo(v,pos,hls.config.maxBufferHole).end - pos) / playbackRate;
+        const pos = v.currentTime,
+              fragLoadedDelay = (frag.expectedLen - frag.loaded) / loadRate,
+              bufferStarvationDelay = (BufferHelper.bufferInfo(v,pos,hls.config.maxBufferHole).end - pos) / playbackRate;
         // consider emergency switch down only if we have less than 2 frag buffered AND
         // time to finish loading current fragment is bigger than buffer starvation delay
         // ie if we risk buffer starvation if bw does not increase quickly
@@ -152,8 +152,8 @@ class AbrController extends EventHandler {
   }
 
   get nextAutoLevel() {
-    let hls = this.hls;
-    var maxAutoLevel;
+    const hls = this.hls;
+    let maxAutoLevel;
     if (this._autoLevelCapping === -1 && hls.levels && hls.levels.length) {
       maxAutoLevel = hls.levels.length - 1;
     } else {
@@ -162,41 +162,39 @@ class AbrController extends EventHandler {
 
     // in case next auto level has been forced, return it straight-away (but capped)
     if (this._nextAutoLevel !== -1) {
-      return Math.min(this._nextAutoLevel,maxAutoLevel);
+      return Math.min(this._nextAutoLevel, maxAutoLevel);
     }
 
-    let v = hls.media,
+    const v = hls.media,
         currentLevel = this.fragCurrent.level,
         avgDuration = ((hls.levels && hls.levels.length && (currentLevel >= 0) && (currentLevel < hls.levels.length)) ? hls.levels[currentLevel].details.averagetargetduration : this.fragCurrent.duration),
         pos = (v ? v.currentTime : 0),
         lastbw = this.lastbw,
 
-    // playbackRate is the absolute value of the playback rate; if v.playbackRate is 0, we use 1 to load as
-    // if we're playing back at the normal rate.
-    playbackRate = ((v && (v.playbackRate !== 0)) ? Math.abs(v.playbackRate) : 1.0),
+        // playbackRate is the absolute value of the playback rate; if v.playbackRate is 0, we use 1 to load as
+        // if we're playing back at the normal rate.
+        playbackRate = ((v && (v.playbackRate !== 0)) ? Math.abs(v.playbackRate) : 1.0),
 
-    // bufferStarvationDelay is the wall-clock time left until the playback buffer is exhausted.
-    bufferStarvationDelay = (BufferHelper.bufferInfo(v, pos, hls.config.maxBufferHole).end - pos) / playbackRate,
+        // bufferStarvationDelay is the wall-clock time left until the playback buffer is exhausted.
+        bufferStarvationDelay = (BufferHelper.bufferInfo(v, pos, hls.config.maxBufferHole).end - pos) / playbackRate,
 
-    // targetMinBuffered is the wall-clock time of two segments' worth of media. We aim to maintain this
-    // much buffered data (minimum) while choosing the next level.
-    targetMinBuffered = 2 * avgDuration / playbackRate,
+        // targetMinBuffered is the wall-clock time of two segments' worth of media. We aim to maintain this
+        // much buffered data (minimum) while choosing the next level.
+        targetMinBuffered = 2 * avgDuration / playbackRate,
 
-    // availableFetchTime is how much "free time" we have to load the next segment in order to preserve
-    // the minimum amount of buffered data. This can be negative, meaning we're below our target minimum
-    // buffered threshold.
-    availableFetchTime = bufferStarvationDelay - targetMinBuffered;
-
-    var i;
+        // availableFetchTime is how much "free time" we have to load the next segment in order to preserve
+        // the minimum amount of buffered data. This can be negative, meaning we're below our target minimum
+        // buffered threshold.
+        availableFetchTime = bufferStarvationDelay - targetMinBuffered;
 
     logger.trace(`bufferStarvationDelay/targetMinBuffered/availableFetchTime: ${bufferStarvationDelay}/${targetMinBuffered}/${availableFetchTime}`);
 
     // If availableFetchTime is positive, we have a relatively easy choice to make -- find the highest level
     // that can (most likely) be fetched in availableFetchTime seconds.
     if (availableFetchTime > 0) {
-      for (i = maxAutoLevel; i >= 0 ; i--) {
-        let bitrate = hls.levels[i].bitrate,
-            fetchTime = bitrate * avgDuration / lastbw;
+      for (let i = maxAutoLevel; i >= 0 ; i--) {
+        const bitrate = hls.levels[i].bitrate,
+              fetchTime = bitrate * avgDuration / lastbw;
         logger.trace(`level/bitrate/lastbw/fetchTime/return: ${i}/${bitrate}/${lastbw}/${fetchTime}/${fetchTime < availableFetchTime}`);
         if (fetchTime < availableFetchTime) {
           return i;
@@ -207,13 +205,13 @@ class AbrController extends EventHandler {
     // If we get here, then availableFetchTime is either negative or so small that we couldn't expect to
     // fetch any of the levels in time. We don't necessarily have to switch down to zero, but should choose
     // a level that can be fetched faster than playback so we build our buffer back up to targetMinBuffered.
-    for (i = maxAutoLevel; i >= 0 ; i--) {
-      let bitrate = hls.levels[i].bitrate,
-          fetchTime = bitrate * avgDuration / lastbw,
+    for (let i = maxAutoLevel; i >= 0 ; i--) {
+      const bitrate = hls.levels[i].bitrate,
+            fetchTime = bitrate * avgDuration / lastbw,
 
-          // timeRecovered is the amount of buffered time that will be "recovered" assuming we're able to
-          // fetch the segment in the expected time.
-          timeRecovered = avgDuration - fetchTime;
+            // timeRecovered is the amount of buffered time that will be "recovered" assuming we're able to
+            // fetch the segment in the expected time.
+            timeRecovered = avgDuration - fetchTime;
 
       logger.trace(`level/bitrate/lastbw/fetchTime/timeRecovered/return: ${i}/${bitrate}/${lastbw}/${fetchTime}/${timeRecovered}/${availableFetchTime + timeRecovered > 0}`);
       if (availableFetchTime + timeRecovered > 0) {
