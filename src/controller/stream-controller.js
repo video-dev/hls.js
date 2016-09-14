51c333d25d8e0a320a94dd9d011a8e74e4e25f4c
mangui <g.du.pontavice@gmail.com>
2016-09-14 13:32:43 +0200

    stream-controller: in case first frag is already loaded, restart loading in IDLE state
    this is to avoid restarting in hls.startLevel (instead it will restart in hls.nextAutoLevel if auto switch is enabled

--- src/controller/stream-controller.js
+++ src/controller/stream-controller.js
@@ -77,11 +77,10 @@ class StreamController extends EventHandler {
           logger.log('resuming video');
           media.play();
         }
-        this.state = State.IDLE;
       } else {
         this.lastCurrentTime = this.startPosition ? this.startPosition : startPosition;
-        this.state = State.STARTING;
       }
+      this.state = this.startFragRequested ? State.IDLE : State.STARTING;
       this.nextLoadPosition = this.startPosition = this.lastCurrentTime;
       this.tick();
     } else {
