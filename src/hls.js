7012a6a0db2a24b9f910cb48f066fbdacd9f2a8b
woutd <woutd@users.noreply.github.com>
2016-10-03 12:21:52 +0200

    Update hls.js

--- src/hls.js
+++ src/hls.js
@@ -78,7 +78,7 @@ class Hls {
                 manifestLoadingMaxRetry: 1,
                 manifestLoadingRetryDelay: 1000,
                 manifestLoadingMaxRetryTimeout: 64000,
-                startLevel: 0,
+                startLevel: undefined,
                 levelLoadingTimeOut: 10000,
                 levelLoadingMaxRetry: 4,
                 levelLoadingRetryDelay: 1000,
