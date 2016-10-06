2fe9e8406f570cba5655d553b92bc91022bd8fc9
mangui <g.du.pontavice@gmail.com>
2016-10-06 10:04:33 +0200

    adts: audio codec config on Vivaldi should be similar to Chrome
    fix AV out of sync with Big Buck Bunny on Vivaldi
    related to #719

--- src/demux/adts.js
+++ src/demux/adts.js
@@ -32,8 +32,8 @@ import {ErrorTypes, ErrorDetails} from '../errors';
     // byte 3
     adtsChanelConfig |= ((data[offset + 3] & 0xC0) >>> 6);
     logger.log(`manifest codec:${audioCodec},ADTS data:type:${adtsObjectType},sampleingIndex:${adtsSampleingIndex}[${adtsSampleingRates[adtsSampleingIndex]}Hz],channelConfig:${adtsChanelConfig}`);
-    // firefox/Opera/Vivaldi: freq less than 24kHz = AAC SBR (HE-AAC)
-    if (/firefox|OPR|vivaldi/i.test(userAgent)) {
+    // firefox/Opera: freq less than 24kHz = AAC SBR (HE-AAC)
+    if (/firefox|OPR/i.test(userAgent)) {
       if (adtsSampleingIndex >= 6) {
         adtsObjectType = 5;
         config = new Array(4);
@@ -52,7 +52,7 @@ import {ErrorTypes, ErrorDetails} from '../errors';
       config = new Array(2);
       adtsExtensionSampleingIndex = adtsSampleingIndex;
     } else {
-      /*  for other browsers (chrome ...)
+      /*  for other browsers (Chrome/Vivaldi ...)
           always force audio type to be HE-AAC SBR, as some browsers do not support audio codec switch properly (like Chrome ...)
       */
       adtsObjectType = 5;
