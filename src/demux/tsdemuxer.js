79a4f8a74f11e8f28f58ce3f397616d076ac4fe2
mangui <g.du.pontavice@gmail.com>
2016-10-07 19:05:53 +0200

    tsdemuxer: handle Access Unit starting directly with IDR (missing AUD)
    fix internal exception happening in demuxerWorker
    related to #730

--- src/demux/tsdemuxer.js
+++ src/demux/tsdemuxer.js
@@ -554,7 +554,16 @@ class TSDemuxer {
                 //IDR
                 case 5:
                     push = true;
-                    if (debug && avcSample) {
+                    // handle PES not starting with AUD
+                    if (!avcSample) {
+                        avcSample = this.avcSample = this._createAVCSample(
+                            true,
+                            pes.pts,
+                            pes.dts,
+                            ''
+                        );
+                    }
+                    if (debug) {
                         avcSample.debug += 'IDR ';
                     }
                     avcSample.key = true;
@@ -699,13 +708,12 @@ class TSDemuxer {
                     if (avcSample) {
                         this.pushAccesUnit(avcSample, track);
                     }
-                    avcSample = this.avcSample = {
-                        key: false,
-                        pts: pes.pts,
-                        dts: pes.dts,
-                        units: { units: [], length: 0 },
-                        debug: debug ? 'AUD ' : ''
-                    };
+                    avcSample = this.avcSample = this._createAVCSample(
+                        false,
+                        pes.pts,
+                        pes.dts,
+                        debug ? 'AUD ' : ''
+                    );
                     break;
                 // Filler Data
                 case 12:
@@ -728,6 +736,16 @@ class TSDemuxer {
             this.pushAccesUnit(avcSample, track);
             this.avcSample = null;
         }
+    }
+
+    _createAVCSample(key, pts, dts, debug) {
+        return {
+            key: key,
+            pts: pts,
+            dts: dts,
+            units: { units: [], length: 0 },
+            debug: debug
+        };
     }
 
     _insertSampleInOrder(arr, data) {
