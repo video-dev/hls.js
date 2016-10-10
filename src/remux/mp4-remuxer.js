985b4a5826c01a5ac5ddd9ace1034b10fd2dbc1b
mangui <g.du.pontavice@gmail.com>
2016-10-10 12:08:45 +0200

    lint

--- src/remux/mp4-remuxer.js
+++ src/remux/mp4-remuxer.js
@@ -413,7 +413,7 @@ class MP4Remuxer {
 
     // only inject/drop audio frames in case time offset is accurate
     if (accurateTimeOffset) {
-      for (var i = 0; i < samples0.length; ) {
+      for (let i = 0; i < samples0.length; ) {
         // First, let's see how far off this frame is from where we expect it to be
         var sample = samples0[i],
             ptsNorm = this._PTSNormalize(sample.pts - this._initDTS, nextAacPts),
@@ -522,7 +522,7 @@ class MP4Remuxer {
           // no audio samples
           return;
         }
-        for (i = 0; i < numMissingFrames; i++) {
+        for (let i = 0; i < numMissingFrames; i++) {
           newStamp = ptsnorm - (numMissingFrames - i) * pesFrameDuration;
           fillFrame = AAC.getSilentFrame(track.channelCount);
           if (!fillFrame) {
