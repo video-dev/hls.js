8cb3b0ba342573f872fe4845284890dc663bc5b6
mangui <g.du.pontavice@gmail.com>
2016-10-13 11:26:50 +0200

    fragment-loader: detach this.loader on loadSuccess
    tiny optimization

--- src/loader/fragment-loader.js
+++ src/loader/fragment-loader.js
@@ -13,47 +13,51 @@ class FragmentLoader extends EventHandler {
   }
 
   destroy() {
-    if (this.loader) {
-      this.loader.destroy();
+    var loader = this.loader;
+    if (loader) {
+      loader.abort();
       this.loader = null;
     }
     EventHandler.prototype.destroy.call(this);
   }
 
   onFragLoading(data) {
-    var frag = data.frag;
-    this.frag = frag;
-    this.frag.loaded = 0;
-    var config = this.hls.config;
+    var frag = this.frag = data.frag,
+        config = this.hls.config;
+    frag.loaded = 0;
     frag.loader = this.loader = typeof(config.fLoader) !== 'undefined' ? new config.fLoader(config) : new config.loader(config);
-    this.loader.load(frag.url, 'arraybuffer', this.loadsuccess.bind(this), this.loaderror.bind(this), this.loadtimeout.bind(this), config.fragLoadingTimeOut, 0, 0, this.loadprogress.bind(this), frag);
+    frag.loader.load(frag.url, 'arraybuffer', this.loadsuccess.bind(this), this.loaderror.bind(this), this.loadtimeout.bind(this), config.fragLoadingTimeOut, 0, 0, this.loadprogress.bind(this), frag);
   }
 
   loadsuccess(event, stats) {
-    var payload = event.currentTarget.response;
+    var payload = event.currentTarget.response,
+        frag = this.frag;
     stats.length = payload.byteLength;
     // detach fragment loader on load success
-    this.frag.loader = undefined;
-    this.hls.trigger(Event.FRAG_LOADED, {payload: payload, frag: this.frag, stats: stats});
+    this.loader = frag.loader = undefined;
+    this.hls.trigger(Event.FRAG_LOADED, {payload: payload, frag: frag, stats: stats});
   }
 
   loaderror(event) {
-    if (this.loader) {
-      this.loader.abort();
+    var loader = this.loader;
+    if (loader) {
+      loader.abort();
     }
     this.hls.trigger(Event.ERROR, {type: ErrorTypes.NETWORK_ERROR, details: ErrorDetails.FRAG_LOAD_ERROR, fatal: false, frag: this.frag, response: event});
   }
 
   loadtimeout() {
-    if (this.loader) {
-      this.loader.abort();
+    var loader = this.loader;
+    if (loader) {
+      loader.abort();
     }
     this.hls.trigger(Event.ERROR, {type: ErrorTypes.NETWORK_ERROR, details: ErrorDetails.FRAG_LOAD_TIMEOUT, fatal: false, frag: this.frag});
   }
 
   loadprogress(event, stats) {
-    this.frag.loaded = stats.loaded;
-    this.hls.trigger(Event.FRAG_LOAD_PROGRESS, {frag: this.frag, stats: stats});
+    var frag = this.frag;
+    frag.loaded = stats.loaded;
+    this.hls.trigger(Event.FRAG_LOAD_PROGRESS, {frag: frag, stats: stats});
   }
 }
 
