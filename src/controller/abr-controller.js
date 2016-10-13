32bf40f9127a41b250163610b7014c77abce6c51
mangui <g.du.pontavice@gmail.com>
2016-10-13 11:39:55 +0200

    abr-controller: log fragment processing details

--- src/controller/abr-controller.js
+++ src/controller/abr-controller.js
@@ -144,6 +144,7 @@ class AbrController extends EventHandler {
     // and leading to wrong bw estimation
     if (stats.aborted !== true && frag.loadCounter === 1) {
       let fragLoadingProcessingMs = stats.tbuffered - stats.trequest;
+      logger.log(`latency/loading/parsing/append/kbps:${Math.round(stats.tfirst-stats.trequest)}/${Math.round(stats.tload-stats.tfirst)}/${Math.round(stats.tparsed-stats.tload)}/${Math.round(stats.tbuffered-stats.tparsed)}/${Math.round(8*stats.loaded/(stats.tbuffered-stats.trequest))}`);
       this.bwEstimator.sample(fragLoadingProcessingMs,stats.loaded);
       // if fragment has been loaded to perform a bitrate test, (hls.startLevel = -1), store bitrate test delay duration
       if (frag.bitrateTest) {
