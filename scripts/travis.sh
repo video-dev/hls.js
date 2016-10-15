a5404c4cbfe3236c7bb475de789b772829a4e1c0
Tom Jenkinson <tjcode@outlook.com>
2016-10-15 16:22:32 +0100

    Retry functional tests up to 3 times on failure

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -10,7 +10,19 @@ elif [ "${TRAVIS_MODE}" = "unitTests" ]; then
 	npm run test
 elif [ "${TRAVIS_MODE}" = "funcTests" ]; then
 	npm run builddist
-	npm run testfunc
+	n=0
+	maxRetries=3
+	until [ $n -ge ${maxRetries} ]
+	do
+		if [ $n -gt 0 ]; then
+			echo "Retrying... Attempt: ${n}"
+		fi
+		npm run testfunc && break
+		n=$[$n+1]
+	done
+	if [ ${n} = ${maxRetries} ]; then
+		exit 1
+	fi
 else
 	echo "Unknown travis mode: ${TRAVIS_MODE}" 1>&2
 	exit 1
