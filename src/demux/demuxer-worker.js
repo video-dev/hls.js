6ad868b676bddc9a99fd648fe42b28df1fb208dd
mangui <g.du.pontavice@gmail.com>
2016-10-27 14:05:01 +0200

    lint

--- src/demux/demuxer-worker.js
+++ src/demux/demuxer-worker.js
@@ -18,6 +18,11 @@ var DemuxerWorker = function(self) {
     observer.off = function off(event, ...data) {
         observer.removeListener(event, ...data);
     };
+
+    var forwardMessage = function(ev, data) {
+        self.postMessage({ event: ev, data: data });
+    };
+
     self.addEventListener('message', function(ev) {
         var data = ev.data;
         //console.log('demuxer cmd:' + data.cmd);
@@ -55,10 +60,6 @@ var DemuxerWorker = function(self) {
                 break;
         }
     });
-
-    var forwardMessage = function(ev, data) {
-        self.postMessage({ event: ev, data: data });
-    };
 
     // forward events to main thread
     observer.on(Event.FRAG_PARSING_INIT_SEGMENT, forwardMessage);
