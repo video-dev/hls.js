2a9844707ef8c8b0a02c5aa2cda6e14d01a3a3eb
mangui <g.du.pontavice@gmail.com>
2016-12-12 12:21:28 +0100

    decrypter: use browserCrypto.subtle || browserCrypto.webkitSubtle to access subtle API
    related to https://github.com/dailymotion/hls.js/issues/867

--- src/crypt/decrypter.js
+++ src/crypt/decrypter.js
@@ -30,17 +30,17 @@ class Decrypter {
             callback(this.decryptor.decrypt(data, 0, iv));
         } else {
             logger.log('decrypting by WebCrypto API');
-
+            const subtle = this.subtle;
             if (this.key !== key) {
                 this.key = key;
-                this.fastAesKey = new FastAESKey(key);
+                this.fastAesKey = new FastAESKey(subtle, key);
             }
 
             this.fastAesKey
                 .expandKey()
                 .then(aesKey => {
                     // decrypt using web crypto
-                    let crypto = new AESCrypto(iv);
+                    let crypto = new AESCrypto(subtle, iv);
                     crypto.decrypt(data, aesKey).then(result => {
                         callback(result);
                     });
@@ -52,13 +52,14 @@ class Decrypter {
     }
 
     onWebCryptoError(err, data, key, iv, callback) {
-        if (this.hls.config.enableSoftwareAES) {
+        let hls = this.hls;
+        if (hls.config.enableSoftwareAES) {
             logger.log('disabling to use WebCrypto API');
             this.disableWebCrypto = true;
             this.decrypt(data, key, iv, callback);
         } else {
             logger.error(`decrypting error : ${err.message}`);
-            this.hls.trigger(Event.ERROR, {
+            hls.trigger(Event.ERROR, {
                 type: ErrorTypes.MEDIA_ERROR,
                 details: ErrorDetails.FRAG_DECRYPT_ERROR,
                 fatal: true,
@@ -68,8 +69,9 @@ class Decrypter {
     }
 
     destroy() {
-        if (this.decryptor) {
-            this.decryptor.destroy();
+        let decryptor = this.decryptor;
+        if (decryptor) {
+            decryptor.destroy();
             this.decryptor = undefined;
         }
     }
