2b0f34b79e7c892924bd693c9f2a2d7a598ea215
mangui <g.du.pontavice@gmail.com>
2016-12-16 16:23:02 +0100

    level-controller: on recoverable network error, switch to current level - 1 instead of always switching to level 0
    merge from v0.5.x

--- src/controller/level-controller.js
+++ src/controller/level-controller.js
@@ -290,9 +290,12 @@ class LevelController extends EventHandler {
                 let recoverable = this._manualLevel === -1 && levelId;
                 if (recoverable) {
                     logger.warn(
-                        `level controller,${details}: emergency switch-down for next fragment`
+                        `level controller,${details}: switch-down for next fragment`
                     );
-                    abrController.nextAutoLevel = minAutoLevel;
+                    abrController.nextAutoLevel = Math.max(
+                        minAutoLevel,
+                        levelId - 1
+                    );
                 } else if (level && level.details && level.details.live) {
                     logger.warn(
                         `level controller,${details} on live stream, discard`
