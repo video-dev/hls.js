c4585caa770703bdefd09bee002d1d6e24432ace
Yvan Michel <yvan.michel@dailymotion.com>
2017-01-23 18:39:10 +0100

    Bundle the library with Webpack 2.2
    - Use webpack to bundle hls.js with the exact same config
    - Use webpack-dev-server to run the demo-page
    - Use webpack's publicPath in demo/index.html
    - Add __VERSION__ as global in .jshintrc
    - Replace __VERSION__ at bundling
    - Remove useless devDependencies
    - Remove private attr
    - Move url-toolkit in dependencies

--- src/hls.js
+++ src/hls.js
@@ -20,8 +20,7 @@ import {hlsDefaultConfig} from './config';
 class Hls {
 
   static get version() {
-    // replaced with browserify-versionify transform
-    return '__VERSION__';
+    return __VERSION__;
   }
 
   static isSupported() {
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+var pkgJson = require('./package.json')
+var path = require('path')
+var webpack = require('webpack')
+
+var env = process.env.NODE_ENV
+
+var config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/assets/',
+    library: 'Hls',
+    libraryTarget: 'umd'
+  },
+  devServer: {
+    compress: true,
+    contentBase: path.resolve(__dirname, 'demo')
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: [
+        path.resolve(__dirname, 'node_modules')
+      ],
+      loader: 'babel-loader',
+      options: {
+        presets: ['es2015']
+      }
+    }]
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      __VERSION__: JSON.stringify(pkgJson.version)
+    })
+  ]
+}
+
+if (env === 'production') {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        sequences: true,
+        dead_code: true,
+        conditionals: true,
+        booleans: true,
+        unused: true,
+        if_return: true,
+        join_vars: true,
+        drop_console: true
+      },
+      mangle: {
+        screw_ie8: true
+      },
+      output: {
+        screw_ie8: true
+      }
+    })
+  )
+}
+
+module.exports = config
