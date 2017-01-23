a99c9752ab45a5c63f3944f0c933172665e96015
mangui <g.du.pontavice@gmail.com>
2017-01-23 19:17:33 +0100

    playlist-loader: create new string on parsing playlist
    avoid sliced strings usage in Chrome Browser, leading to each fragment referencing the whole stream manifest, leading to excessive memory usage on live playlists (in which buffered fragments are retained by stream-controller).
    each buffered fragments was thus potentially holding a reference to a different stream manifest (which could be hundred of kBs ...)
    
    related to #939

--- src/loader/playlist-loader.js
+++ src/loader/playlist-loader.js
@@ -340,7 +340,8 @@ class PlaylistLoader extends EventHandler {
             if (duration) {
                 // INF
                 frag.duration = parseFloat(duration);
-                const title = result[2];
+                // avoid sliced strings    https://github.com/dailymotion/hls.js/issues/939
+                const title = (' ' + result[2]).slice(1);
                 frag.title = title ? title : null;
                 frag.tagList.push(
                     title ? ['INF', duration, title] : ['INF', duration]
@@ -356,7 +357,8 @@ class PlaylistLoader extends EventHandler {
                     frag.level = id;
                     frag.cc = cc;
                     frag.baseurl = baseurl;
-                    frag.relurl = result[3];
+                    // avoid sliced strings    https://github.com/dailymotion/hls.js/issues/939
+                    frag.relurl = (' ' + result[3]).slice(1);
 
                     level.fragments.push(frag);
                     prevFrag = frag;
@@ -367,7 +369,7 @@ class PlaylistLoader extends EventHandler {
                 }
             } else if (result[4]) {
                 // X-BYTERANGE
-                frag.rawByteRange = result[4];
+                frag.rawByteRange = (' ' + result[4]).slice(1);
                 if (prevFrag) {
                     const lastByteRangeEndOffset = prevFrag.byteRangeEndOffset;
                     if (lastByteRangeEndOffset) {
@@ -376,8 +378,12 @@ class PlaylistLoader extends EventHandler {
                 }
             } else if (result[5]) {
                 // PROGRAM-DATE-TIME
-                frag.rawProgramDateTime = result[5];
-                frag.tagList.push(['PROGRAM-DATE-TIME', result[5]]);
+                // avoid sliced strings    https://github.com/dailymotion/hls.js/issues/939
+                frag.rawProgramDateTime = (' ' + result[5]).slice(1);
+                frag.tagList.push([
+                    'PROGRAM-DATE-TIME',
+                    frag.rawProgramDateTime
+                ]);
             } else {
                 result = result[0].match(LEVEL_PLAYLIST_REGEX_SLOW);
                 for (i = 1; i < result.length; i++) {
@@ -386,8 +392,9 @@ class PlaylistLoader extends EventHandler {
                     }
                 }
 
-                const value1 = result[i + 1];
-                const value2 = result[i + 2];
+                // avoid sliced strings    https://github.com/dailymotion/hls.js/issues/939
+                const value1 = (' ' + result[i + 1]).slice(1);
+                const value2 = (' ' + result[i + 2]).slice(1);
 
                 switch (result[i]) {
                     case '#':
