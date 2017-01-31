be87156f9681dacfec67f0750b5c01ffa549abc6
mangui <g.du.pontavice@gmail.com>
2017-01-31 15:07:07 +0100

    functional tests: 3rd try to fold logs on travis

--- tests/functional/auto/hlsjs.js
+++ tests/functional/auto/hlsjs.js
@@ -125,9 +125,9 @@ describe('testing hls.js playback in the browser on "'+browserDescription+'"', f
           callback({ code : 'loadeddata', logs : logString});
         };
       }, url).then(function(result) {
-        console.log('$');
+        console.log('$ debug logs');
         console.log(result.logs);
-        console.log('$');
+        console.log('$ ');
         assert.strictEqual(result.code, 'loadeddata');
       });
     }
@@ -145,7 +145,8 @@ describe('testing hls.js playback in the browser on "'+browserDescription+'"', f
           callback({ code : video.readyState, logs : logString});
         }, 12000);
       }, url).then(function(result) {
-        console.log('$ ' + result.logs);
+        console.log('$ debug logs');
+        console.log(result.logs);
         console.log('$ ');
         assert.strictEqual(result.code, 4);
       });
@@ -164,7 +165,8 @@ describe('testing hls.js playback in the browser on "'+browserDescription+'"', f
           callback({ code : 'seeked', logs : logString});
         };
       }, url).then(function(result) {
-        console.log('$ ' + result.logs);
+        console.log('$ debug logs');
+        console.log(result.logs);
         console.log('$ ');
         assert.strictEqual(result.code, 'seeked');
       });
@@ -183,7 +185,8 @@ describe('testing hls.js playback in the browser on "'+browserDescription+'"', f
           callback({ code : 'ended', logs : logString});
         };
       }, url).then(function(result) {
-        console.log('$ ' + result.logs);
+        console.log('$ debug logs');
+        console.log(result.logs);
         console.log('$ ');
         assert.strictEqual(result.code, 'ended');
       });
