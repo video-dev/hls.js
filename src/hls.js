4fc731b32980c58a837837cb424aacc82f666f98
mangui <g.du.pontavice@gmail.com>
2017-02-24 18:25:39 +0100

    hls: instantiate stream-controller after abr-controller
    fix startLevel=-1 not working since v0.7.0
    FRAG_LOADED event needs to be handled first in abr-controller so that nextAutoLevel will return the correct value when stream-controller will pick it up
    relying on listener order is not satisfactory, we need to sync of a better way ...

--- src/hls.js
+++ src/hls.js
@@ -111,6 +111,17 @@ class Hls {
         this.off = observer.off.bind(observer);
         this.trigger = observer.trigger.bind(observer);
 
+        // core controllers and network loaders
+        const abrController = (this.abrController = new config.abrController(
+            this
+        ));
+        const bufferController = new config.bufferController(this);
+        const capLevelController = new config.capLevelController(this);
+        const fpsController = new config.fpsController(this);
+        const playListLoader = new PlaylistLoader(this);
+        const fragmentLoader = new FragmentLoader(this);
+        const keyLoader = new KeyLoader(this);
+
         // network controllers
         const levelController = (this.levelController = new LevelController(
             this
@@ -125,20 +136,7 @@ class Hls {
         if (Controller) {
             networkControllers.push(new Controller(this));
         }
-
         this.networkControllers = networkControllers;
-
-        // core controllers and network loaders
-        // hls.abrController is referenced in levelController, this would need to be fixed
-        const abrController = (this.abrController = new config.abrController(
-            this
-        ));
-        const bufferController = new config.bufferController(this);
-        const capLevelController = new config.capLevelController(this);
-        const fpsController = new config.fpsController(this);
-        const playListLoader = new PlaylistLoader(this);
-        const fragmentLoader = new FragmentLoader(this);
-        const keyLoader = new KeyLoader(this);
 
         let coreComponents = [
             playListLoader,
