5f100918f028cc871ef31b05d7502943ffe2f4fa
Yvan Michel <yvan.michel@dailymotion.com>
2017-03-23 16:20:22 +0100

    Save the last current-time outside checkFragmentChanged method
    Allowing to move out the fragment-changed logic outside the
    StreamController

--- src/controller/stream-controller.js
+++ src/controller/stream-controller.js
@@ -164,8 +164,28 @@ class StreamController extends BaseStreamController {
     }
     // check buffer
     this._checkBuffer();
+
+    // save the last current-time in case of media/detach or MEDIA_ERR_DECODE.
+    this._saveLastCurrentTime();
+
     // check/update current fragment
     this._checkFragmentChanged();
+  }
+
+  _saveLastCurrentTime() {
+    const video = this.media;
+
+    if (video && video.readyState && video.seeking === false) {
+      const currentTime = video.currentTime;
+      // if video element is in seeked state, currentTime can only increase.
+      // (assuming that playback rate is positive ...)
+      // As sometimes currentTime jumps back to zero after a
+      // media decode error, check this, to avoid seeking back to
+      // wrong position after a media decode error
+      if (currentTime > video.playbackRate * this.lastCurrentTime) {
+        this.lastCurrentTime = currentTime;
+      }
+    }
   }
 
   // Ironically the "idle" state is the on we do the most logic in it seems ....
@@ -568,16 +588,6 @@ class StreamController extends BaseStreamController {
   _checkFragmentChanged() {
     var fragPlayingCurrent, currentTime, video = this.media;
     if (video && video.readyState && video.seeking === false) {
-      currentTime = video.currentTime;
-      /* if video element is in seeked state, currentTime can only increase.
-        (assuming that playback rate is positive ...)
-        As sometimes currentTime jumps back to zero after a
-        media decode error, check this, to avoid seeking back to
-        wrong position after a media decode error
-      */
-      if(currentTime > video.playbackRate*this.lastCurrentTime) {
-        this.lastCurrentTime = currentTime;
-      }
       if (BufferHelper.isBuffered(video,currentTime)) {
         fragPlayingCurrent = this.getBufferedFrag(currentTime);
       } else if (BufferHelper.isBuffered(video,currentTime + 0.1)) {
