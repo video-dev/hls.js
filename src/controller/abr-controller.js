ac2795f69608b12837fa2d665e11e03a4e6e558f
Karthika Nair <karthika601@gmail.com>
2017-04-07 09:47:45 +0530

    replaced data.stats to stats

--- src/controller/abr-controller.js
+++ src/controller/abr-controller.js
@@ -246,7 +246,7 @@ class AbrController extends EventHandler {
                 )}`
             );
             this._bwEstimator.sample(fragLoadingProcessingMs, stats.loaded);
-            data.stats.bwEstimate = this._bwEstimator.getEstimate();
+            stats.bwEstimate = this._bwEstimator.getEstimate();
             // if fragment has been loaded to perform a bitrate test, (hls.startLevel = -1), store bitrate test delay duration
             if (frag.bitrateTest) {
                 this.bitrateTestDelay = fragLoadingProcessingMs / 1000;
