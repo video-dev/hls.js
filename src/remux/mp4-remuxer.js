7f5271b4792e8e00618319beec445f8188cddc62
Guillaume du Pontavice <g.du.pontavice@gmail.com>
2017-04-25 21:45:38 -0700

    mp4-remuxer: polish log traces

--- src/remux/mp4-remuxer.js
+++ src/remux/mp4-remuxer.js
@@ -456,7 +456,7 @@ class MP4Remuxer {
 
         // If we're overlapping by more than a duration, drop this sample
         if (delta <= -inputSampleDuration) {
-          logger.warn(`Dropping 1 audio frame @ ${(nextPtsNorm/inputTimeScale).toFixed(3)}s due to ${Math.abs(1000*delta/inputTimeScale)} ms overlap.`);
+          logger.warn(`Dropping audio frame @ ${(nextPtsNorm/inputTimeScale).toFixed(3)}s due to ${Math.round(Math.abs(1000*delta/inputTimeScale))} ms overlap.`);
           inputSamples.splice(i, 1);
           track.len -= sample.unit.length;
           // Don't touch nextPtsNorm or i
@@ -465,7 +465,7 @@ class MP4Remuxer {
         // also only inject silent audio frames if currentTime !== 0 (nextPtsNorm !== 0)
         else if (delta >= inputSampleDuration && nextPtsNorm) {
           var missing = Math.round(delta / inputSampleDuration);
-          logger.warn(`Injecting ${missing} audio frame @ ${(nextPtsNorm/inputTimeScale).toFixed(3)}s due to ${1000*delta/inputTimeScale} ms gap.`);
+          logger.warn(`Injecting ${missing} audio frame @ ${(nextPtsNorm/inputTimeScale).toFixed(3)}s due to ${Math.round(1000*delta/inputTimeScale)} ms gap.`);
           for (var j = 0; j < missing; j++) {
             newStamp = nextPtsNorm + initDTS;
             newStamp = Math.max(newStamp, initDTS);
