4d6b1a54c4d166018e720aff6f3a37dccdbd20d1
Kyle Aurand <kyle@jwplayer.com>
2017-05-01 11:51:53 -0400

    Adds an “s” to the event name enumeration to match conventions

--- src/events.js
+++ src/events.js
@@ -68,7 +68,7 @@ module.exports = {
     // fired when a subtitle fragment has been processed - data: { success : boolean, frag : the processed frag}
     SUBTITLE_FRAG_PROCESSED: 'hlsSubtitleFragProcessed',
     // fired when a set of VTTCues to be managed externally has been parsed - data: { type: string, track: string, cues: [ VTTCue ] }
-    CUES_PARSED: 'hlsCueParsed',
+    CUES_PARSED: 'hlsCuesParsed',
     // fired when a text track to be managed externally is found - data: { tracks: [ { label: string, kind: string, default: boolean } ] }
     NON_NATIVE_TEXT_TRACKS_FOUND: 'hlsNonNativeTextTracksFound',
     // fired when the first timestamp is found. - data: { id : demuxer id, initPTS: initPTS , frag : fragment object}
