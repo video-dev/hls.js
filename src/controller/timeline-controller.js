7a9b69cec844587685f9524b6665494b76d4103f
Guillaume du Pontavice <g.du.pontavice@gmail.com>
2017-05-02 22:46:44 -0700

    timeline-controller: factorize access to textTrack

--- src/controller/timeline-controller.js
+++ src/controller/timeline-controller.js
@@ -290,17 +290,14 @@ class TimelineController extends EventHandler {
                     vttCCs,
                     frag.cc,
                     function(cues) {
+                        const currentTrack = textTracks[frag.trackId];
                         // Add cues and trigger event with success true.
                         cues.forEach(cue => {
                             // Sometimes there are cue overlaps on segmented vtts so the same
                             // cue can appear more than once in different vtt files.
                             // This avoid showing duplicated cues with same timecode and text.
-                            if (
-                                !textTracks[frag.trackId].cues.getCueById(
-                                    cue.id
-                                )
-                            ) {
-                                textTracks[frag.trackId].addCue(cue);
+                            if (!currentTrack.cues.getCueById(cue.id)) {
+                                currentTrack.addCue(cue);
                             }
                         });
                         hls.trigger(Event.SUBTITLE_FRAG_PROCESSED, {
