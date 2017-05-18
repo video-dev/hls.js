827650d4b9257729c960fa7208c24333d878950e
Guillaume du Pontavice <g.du.pontavice@gmail.com>
2017-05-18 12:14:10 +0200

    stream-controller: increase tolerance for EOS detection
    up to one frag time diff tolerance
    this helps to EOS source for which last fragment audio/video are not aligned, for example
    
    Parsed audio,PTS:[60.000,62.717],DTS:[60.000/62.717],nb:117,dropped:0
    Parsed video,PTS:[60.059,67.326],DTS:[59.993/67.326],nb:220,dropped:0
    main buffered : [0.000,62.717]
    
    related to #1147

--- src/controller/stream-controller.js
+++ src/controller/stream-controller.js
@@ -248,10 +248,10 @@ class StreamController extends EventHandler {
         // real duration might be lower than initial duration if there are drifts between real frag duration and playlist signaling
         const duration = Math.min(media.duration,fragPrevious.start + fragPrevious.duration);
         // if everything (almost) til the end is buffered, let's signal eos
-        // we don't compare exactly media.duration === bufferInfo.end as there could be some subtle media duration difference
-        // using half frag duration should help cope with these cases.
+        // we don't compare exactly media.duration === bufferInfo.end as there could be some subtle media duration difference (audio/video offsets...)
+        // tolerate up to one frag duration to cope with these cases.
         // also cope with almost zero last frag duration (max last frag duration with 200ms) refer to https://github.com/video-dev/hls.js/pull/657
-        if (duration - Math.max(bufferInfo.end,fragPrevious.start) <= Math.max(0.2,fragPrevious.duration/2)) {
+        if (duration - Math.max(bufferInfo.end,fragPrevious.start) <= Math.max(0.2,fragPrevious.duration)) {
         // Finalize the media stream
         let data = {};
         if (this.altAudio) {
