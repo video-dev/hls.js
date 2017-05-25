c3578398aa03a327130f0c41f5b986fc18abd715
Guillaume du Pontavice <g.du.pontavice@gmail.com>
2017-05-25 01:13:58 -0700

    mp4-remuxer: rename var

--- src/remux/mp4-remuxer.js
+++ src/remux/mp4-remuxer.js
@@ -552,7 +552,7 @@ class MP4Remuxer {
               // if we have frame overlap, overlapping for more than half a frame duraion
             } else if (delta < -12) {
               // drop overlapping audio frames... browser will deal with it
-              logger.log(`drop overlapping AAC sample, expected/parsed/delta:${(nextAudioPts/inputTimeScale).toFixed(3)}s/${(ptsnorm/inputTimeScale).toFixed(3)}s/${(-delta)}ms`);
+              logger.log(`drop overlapping AAC sample, expected/parsed/delta:${(nextAudioPts/inputTimeScale).toFixed(3)}s/${(pts/inputTimeScale).toFixed(3)}s/${(-delta)}ms`);
               track.len -= unit.byteLength;
               continue;
             }
