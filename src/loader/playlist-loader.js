76f094e1d7e1a10124a5f2398d534706be45e144
Steve Mayhew <mayhew.steve@gmail.com>
2017-06-01 15:07:15 -0700

    Cleanup RegEx init (no commented code).  Try longer timeout on selenium

--- src/loader/playlist-loader.js
+++ src/loader/playlist-loader.js
@@ -12,20 +12,18 @@ import { logger } from '../utils/logger';
 // https://regex101.com is your friend
 const MASTER_PLAYLIST_REGEX = /#EXT-X-STREAM-INF:([^\n\r]*)[\r\n]+([^\r\n]+)/g;
 const MASTER_PLAYLIST_MEDIA_REGEX = /#EXT-X-MEDIA:(.*)/g;
-//const LEVEL_PLAYLIST_REGEX_FAST = /#EXTINF:(\d*(?:\.\d+)?)(?:,(.*)\s+)?|(?!#)(\S+)|#EXT-X-BYTERANGE:*(.+)|#EXT-X-PROGRAM-DATE-TIME:(.+)|#.*/g;
 
-const LEVEL_PLAYLIST_REGEX_PARTS = [
-    //'/',
-    /#EXTINF:(\d*(?:\.\d+)?)(?:,(.*)\s+)?/.source, // duration (#EXTINF:<duration>,<title>), group 1 => duration, group 2 => title
-    /|(?!#)(\S+)/.source, // segment URI, group 3 => the URI (note newline is not eaten)
-    /|#EXT-X-BYTERANGE:*(.+)/.source, // next segment's byterange, group 4 => range spec (x@y)
-    /|#EXT-X-PROGRAM-DATE-TIME:(.+)/.source, // next segment's program date/time group 5 => the datetime spec
-    /|#.*/.source // All other non-segment oriented tags will match with all groups empty
-];
 const LEVEL_PLAYLIST_REGEX_FAST = new RegExp(
-    LEVEL_PLAYLIST_REGEX_PARTS.join(''),
+    [
+        /#EXTINF:(\d*(?:\.\d+)?)(?:,(.*)\s+)?/.source, // duration (#EXTINF:<duration>,<title>), group 1 => duration, group 2 => title
+        /|(?!#)(\S+)/.source, // segment URI, group 3 => the URI (note newline is not eaten)
+        /|#EXT-X-BYTERANGE:*(.+)/.source, // next segment's byterange, group 4 => range spec (x@y)
+        /|#EXT-X-PROGRAM-DATE-TIME:(.+)/.source, // next segment's program date/time group 5 => the datetime spec
+        /|#.*/.source // All other non-segment oriented tags will match with all groups empty
+    ].join(''),
     'g'
 );
+
 const LEVEL_PLAYLIST_REGEX_SLOW = /(?:(?:#(EXTM3U))|(?:#EXT-X-(PLAYLIST-TYPE):(.+))|(?:#EXT-X-(MEDIA-SEQUENCE): *(\d+))|(?:#EXT-X-(TARGETDURATION): *(\d+))|(?:#EXT-X-(KEY):(.+))|(?:#EXT-X-(START):(.+))|(?:#EXT-X-(ENDLIST))|(?:#EXT-X-(DISCONTINUITY-SEQ)UENCE:(\d+))|(?:#EXT-X-(DIS)CONTINUITY))|(?:#EXT-X-(VERSION):(\d+))|(?:#EXT-X-(MAP):(.+))|(?:(#)(.*):(.*))|(?:(#)(.*))(?:.*)\r?\n?/;
 
 class LevelKey {
