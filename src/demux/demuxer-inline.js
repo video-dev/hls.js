5d5d44202952328953915e2bbf9149be276e3deb
radiantmediaplayer <arnaud@radiantmediaplayer.com>
2017-06-05 19:46:41 +0200

    audio-only-probe-demux-aac-before-mp3

--- src/demux/demuxer-inline.js
+++ src/demux/demuxer-inline.js
@@ -67,8 +67,8 @@ class DemuxerInline {
       const typeSupported = this.typeSupported;
       const config = this.config;
       const muxConfig = [ {demux : TSDemuxer,  remux : MP4Remuxer},
-                          {demux : MP3Demuxer, remux : MP4Remuxer},
                           {demux : AACDemuxer, remux : MP4Remuxer},
+                          {demux : MP3Demuxer, remux : MP4Remuxer},
                           {demux : MP4Demuxer, remux : PassThroughRemuxer}];
 
       // probe for content type
