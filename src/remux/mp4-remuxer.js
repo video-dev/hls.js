3d39b4493685828732f5dcd7b7338ab750fceb23
Guillaume du Pontavice <g.du.pontavice@gmail.com>
2017-06-23 09:43:24 -0700

    mp4-remuxer: don't recompute initPTS if no samples found

--- src/remux/mp4-remuxer.js
+++ src/remux/mp4-remuxer.js
@@ -81,20 +81,22 @@ class MP4Remuxer {
                         )
                     );
                 }
-                const initPTSDelta = refPTS - initPTS;
-                if (Math.abs(initPTSDelta) > 10 * timeScale) {
-                    logger.warn(
-                        `timestamp inconsistency, ${(
-                            initPTSDelta / timeScale
-                        ).toFixed(
-                            3
-                        )}s delta against expected value: missing discontinuity ? reset initPTS/initDTS`
-                    );
-                    this._initPTS = initPTS;
-                    this._initDTS = initDTS;
-                    this.observer.trigger(Event.INIT_PTS_FOUND, {
-                        initPTS: initPTS
-                    });
+                if (initPTS !== Infinity) {
+                    const initPTSDelta = refPTS - initPTS;
+                    if (Math.abs(initPTSDelta) > 10 * timeScale) {
+                        logger.warn(
+                            `timestamp inconsistency, ${(
+                                initPTSDelta / timeScale
+                            ).toFixed(
+                                3
+                            )}s delta against expected value: missing discontinuity ? reset initPTS/initDTS`
+                        );
+                        this._initPTS = initPTS;
+                        this._initDTS = initDTS;
+                        this.observer.trigger(Event.INIT_PTS_FOUND, {
+                            initPTS: initPTS
+                        });
+                    }
                 }
             }
         }
