225d7c95dee19d8e676ae1503505335e9efa994d
Guillaume du Pontavice <g.du.pontavice@gmail.com>
2017-07-18 22:38:42 -0700

    demuxer: allow minification of internal properties

--- src/demux/demuxer.js
+++ src/demux/demuxer.js
@@ -11,10 +11,10 @@ const MediaSource = getMediaSource();
 class Demuxer {
 
   constructor(hls, id) {
-    this.hls = hls;
-    this.id = id;
+    this._hls = hls;
+    this._id = id;
     // observer setup
-    const observer = this.observer = new EventEmitter();
+    const observer = this._observer = new EventEmitter();
     const config = hls.config;
     observer.trigger = function trigger (event, ...data) {
       observer.emit(event, event, ...data);
@@ -26,8 +26,8 @@ class Demuxer {
 
     var forwardMessage = function(ev,data) {
       data = data || {};
-      data.frag = this.frag;
-      data.id = this.id;
+      data.frag = this._frag;
+      data.id = this._id;
       hls.trigger(ev,data);
     }.bind(this);
 
@@ -53,9 +53,9 @@ class Demuxer {
         logger.log('demuxing in webworker');
         let w;
         try {
-          w = this.w = work(require.resolve('../demux/demuxer-worker.js'));
-          this.onwmsg = this.onWorkerMessage.bind(this);
-          w.addEventListener('message', this.onwmsg);
+          w = this._w = work(require.resolve('../demux/demuxer-worker.js'));
+          this._onwmsg = this._onWorkerMessage.bind(this);
+          w.addEventListener('message', this._onwmsg);
           w.onerror = function(event) { hls.trigger(Event.ERROR, {type: ErrorTypes.OTHER_ERROR, details: ErrorDetails.INTERNAL_EXCEPTION, fatal: true, event : 'demuxerWorker', err : { message : event.message + ' (' + event.filename + ':' + event.lineno + ')' }});};
           w.postMessage({cmd: 'init', typeSupported : typeSupported, vendor : vendor, id : id, config: JSON.stringify(config)});
         } catch(err) {
@@ -64,69 +64,69 @@ class Demuxer {
             // revoke the Object URL that was used to create demuxer worker, so as not to leak it
             URL.revokeObjectURL(w.objectURL);
           }
-          this.demuxer = new DemuxerInline(observer,typeSupported,config,vendor);
-          this.w = undefined;
+          this._demuxer = new DemuxerInline(observer,typeSupported,config,vendor);
+          this._w = undefined;
         }
       } else {
-        this.demuxer = new DemuxerInline(observer,typeSupported,config, vendor);
+        this._demuxer = new DemuxerInline(observer,typeSupported,config, vendor);
       }
   }
 
   destroy() {
-    let w = this.w;
+    let w = this._w;
     if (w) {
-      w.removeEventListener('message', this.onwmsg);
+      w.removeEventListener('message', this._onwmsg);
       w.terminate();
-      this.w = null;
+      this._w = null;
     } else {
-      let demuxer = this.demuxer;
+      let demuxer = this._demuxer;
       if (demuxer) {
         demuxer.destroy();
-        this.demuxer = null;
+        this._demuxer = null;
       }
     }
-    let observer = this.observer;
+    let observer = this._observer;
     if (observer) {
       observer.removeAllListeners();
-      this.observer = null;
+      this._observer = null;
     }
   }
 
   push(data, initSegment, audioCodec, videoCodec, frag, duration,accurateTimeOffset,defaultInitPTS) {
-    const w = this.w;
+    const w = this._w;
     const timeOffset = !isNaN(frag.startDTS) ? frag.startDTS  : frag.start;
     const decryptdata = frag.decryptdata;
-    const lastFrag = this.frag;
+    const lastFrag = this._frag;
     const discontinuity = !(lastFrag && (frag.cc === lastFrag.cc));
     const trackSwitch = !(lastFrag && (frag.level === lastFrag.level));
     const nextSN = lastFrag && (frag.sn === (lastFrag.sn+1));
     const contiguous = !trackSwitch && nextSN;
     if (discontinuity) {
-      logger.log(`${this.id}:discontinuity detected`);
+      logger.log(`${this._id}:discontinuity detected`);
     }
     if (trackSwitch) {
-      logger.log(`${this.id}:switch detected`);
+      logger.log(`${this._id}:switch detected`);
     }
-    this.frag = frag;
+    this._frag = frag;
     if (w) {
       // post fragment payload as transferable objects for ArrayBuffer (no copy)
       w.postMessage({cmd: 'demux', data, decryptdata, initSegment, audioCodec, videoCodec, timeOffset, discontinuity, trackSwitch, contiguous, duration, accurateTimeOffset,defaultInitPTS}, data instanceof ArrayBuffer ? [data] : []);
     } else {
-      let demuxer = this.demuxer;
+      let demuxer = this._demuxer;
       if (demuxer) {
         demuxer.push(data, decryptdata, initSegment, audioCodec, videoCodec, timeOffset, discontinuity, trackSwitch, contiguous, duration, accurateTimeOffset,defaultInitPTS);
       }
     }
   }
 
-  onWorkerMessage(ev) {
+  _onWorkerMessage(ev) {
     let data = ev.data,
-        hls = this.hls;
+        hls = this._hls;
     //console.log('onWorkerMessage:' + data.event);
     switch(data.event) {
       case 'init':
         // revoke the Object URL that was used to create demuxer worker, so as not to leak it
-        URL.revokeObjectURL(this.w.objectURL);
+        URL.revokeObjectURL(this._w.objectURL);
         break;
       // special case for FRAG_PARSING_DATA: data1 and data2 are transferable objects
       case Event.FRAG_PARSING_DATA:
@@ -137,8 +137,8 @@ class Demuxer {
         /* falls through */
       default:
         data.data = data.data || {};
-        data.data.frag = this.frag;
-        data.data.id = this.id;
+        data.data.frag = this._frag;
+        data.data.id = this._id;
         hls.trigger(data.event, data.data);
         break;
     }
