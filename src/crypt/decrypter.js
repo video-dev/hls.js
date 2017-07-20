e5784eccbaccd7de8a382e401ce6194277758628
Guillaume du Pontavice <g.du.pontavice@gmail.com>
2017-07-19 22:49:20 -0700

    decrypter: allow minification of internal properties

--- src/crypt/decrypter.js
+++ src/crypt/decrypter.js
@@ -9,80 +9,80 @@ import {logger} from '../utils/logger';
 
 class Decrypter {
   constructor(observer,config) {
-    this.observer = observer;
-    this.config = config;
-    this.logEnabled = true;
+    this._observer = observer;
+    this._config = config;
+    this._logEnabled = true;
     try {
       const browserCrypto = crypto ? crypto : self.crypto;
-      this.subtle = browserCrypto.subtle || browserCrypto.webkitSubtle;
+      this._subtle = browserCrypto.subtle || browserCrypto.webkitSubtle;
     } catch (e) {}
-    this.disableWebCrypto = !this.subtle;
+    this._disableWebCrypto = !this._subtle;
   }
 
   isSync() {
-    return (this.disableWebCrypto && this.config.enableSoftwareAES);
+    return (this._disableWebCrypto && this._config.enableSoftwareAES);
   }
 
   decrypt(data, key, iv, callback) {
-    if (this.disableWebCrypto && this.config.enableSoftwareAES) {
-      if (this.logEnabled) {
+    if (this._disableWebCrypto && this._config.enableSoftwareAES) {
+      if (this._logEnabled) {
         logger.log('JS AES decrypt');
-        this.logEnabled = false;
+        this._logEnabled = false;
       }
-      let decryptor = this.decryptor;
+      let decryptor = this._decryptor;
       if (!decryptor) {
-        this.decryptor = decryptor = new AESDecryptor();
+        this._decryptor = decryptor = new AESDecryptor();
       }
       decryptor.expandKey(key);
       callback(decryptor.decrypt(data, 0, iv));
     }
     else {
-      if (this.logEnabled) {
+      if (this._logEnabled) {
         logger.log('WebCrypto AES decrypt');
-        this.logEnabled = false;
+        this._logEnabled = false;
       }
-      const subtle = this.subtle;
-      if (this.key !== key) {
-        this.key = key;
-        this.fastAesKey = new FastAESKey(subtle,key);
+      const subtle = this._subtle;
+      if (this._key !== key) {
+        this._key = key;
+        this._fastAesKey = new FastAESKey(subtle,key);
       }
 
-      this.fastAesKey.expandKey().
+      this._fastAesKey.expandKey().
         then((aesKey) => {
           // decrypt using web crypto
           let crypto = new AESCrypto(subtle,iv);
           crypto.decrypt(data, aesKey).
           catch ((err) => {
-            this.onWebCryptoError(err, data, key, iv, callback);
+            this._onWebCryptoError(err, data, key, iv, callback);
           }).
           then((result) => {
             callback(result);
           });
         }).
         catch ((err) => {
-          this.onWebCryptoError(err, data, key, iv, callback);
+          this._onWebCryptoError(err, data, key, iv, callback);
         });
     }
   }
 
-  onWebCryptoError(err, data, key, iv, callback) {
-    if (this.config.enableSoftwareAES) {
+  _onWebCryptoError(err, data, key, iv, callback) {
+    if (this._config.enableSoftwareAES) {
       logger.log('WebCrypto Error, disable WebCrypto API');
-      this.disableWebCrypto = true;
-      this.logEnabled = true;
-      this.decrypt(data, key, iv, callback);
+      this._disableWebCrypto = true;
+      this._logEnabled = true;
+      this._decrypt(data, key, iv, callback);
     }
     else {
       logger.error(`decrypting error : ${err.message}`);
-      this.observer.trigger(Event.ERROR, {type : ErrorTypes.MEDIA_ERROR, details : ErrorDetails.FRAG_DECRYPT_ERROR, fatal : true, reason : err.message});
+      this._observer.trigger(Event.ERROR, {type : ErrorTypes.MEDIA_ERROR, details : ErrorDetails.FRAG_DECRYPT_ERROR, fatal : true, reason : err.message});
     }
   }
 
   destroy() {
-    let decryptor = this.decryptor;
+    let decryptor = this._decryptor;
     if (decryptor) {
       decryptor.destroy();
-      this.decryptor = undefined;
+      this._decryptor = undefined;
     }
   }
 }
