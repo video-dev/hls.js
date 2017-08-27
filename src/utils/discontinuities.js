2504e614662770d1d055f232eede711eb80c54a4
Guillaume du Pontavice <g.du.pontavice@gmail.com>
2017-08-27 14:49:53 -0700

    discontinuities: don't export internal methods

--- src/utils/discontinuities.js
+++ src/utils/discontinuities.js
@@ -1,7 +1,7 @@
 import BinarySearch from './binary-search';
 import {logger} from '../utils/logger';
 
-export function findFirstFragWithCC(fragments, cc) {
+function findFirstFragWithCC(fragments, cc) {
   let firstFrag = null;
 
   for (let i = 0; i < fragments.length; i += 1) {
@@ -27,7 +27,7 @@ export function findFragWithCC(fragments, CC) {
   });
 }
 
-export function shouldAlignOnDiscontinuities(lastFrag, lastLevel, details) {
+function shouldAlignOnDiscontinuities(lastFrag, lastLevel, details) {
   let shouldAlign = false;
   if (lastLevel && lastLevel.details && details) {
     if (details.endCC > details.startCC || (lastFrag && lastFrag.cc < details.startCC)) {
@@ -38,7 +38,7 @@ export function shouldAlignOnDiscontinuities(lastFrag, lastLevel, details) {
 }
 
 // Find the first frag in the previous level which matches the CC of the first frag of the new level
-export function findDiscontinuousReferenceFrag(prevDetails, curDetails) {
+function findDiscontinuousReferenceFrag(prevDetails, curDetails) {
   const prevFrags = prevDetails.fragments;
   const curFrags = curDetails.fragments;
 
@@ -57,7 +57,7 @@ export function findDiscontinuousReferenceFrag(prevDetails, curDetails) {
   return prevStartFrag;
 }
 
-export function adjustPtsByReferenceFrag(referenceFrag, details) {
+function adjustPtsByReferenceFrag(referenceFrag, details) {
   if (!referenceFrag) {
     return;
   }
