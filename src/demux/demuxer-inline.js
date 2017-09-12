d923e340478d9de6c0083d4f62391124f5ef0ee0
John Bartos <jbartos7@gmail.com>
2017-09-12 17:52:37 -0400

    Update demuxer-inline.js

--- src/demux/demuxer-inline.js
+++ src/demux/demuxer-inline.js
@@ -134,7 +134,7 @@ class DemuxerInline {
             const observer = this.observer;
             const typeSupported = this.typeSupported;
             const config = this.config;
-            // probing order is AAC/MP3/TS/MP4
+            // probing order is TS/AAC/MP3/MP4
             const muxConfig = [
                 { demux: TSDemuxer, remux: MP4Remuxer },
                 { demux: AACDemuxer, remux: MP4Remuxer },
