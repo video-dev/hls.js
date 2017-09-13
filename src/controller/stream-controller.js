ffb24f3dba7b527e739bd1e049a66b5482af46f1
Guillaume du Pontavice <g.du.pontavice@gmail.com>
2017-09-13 00:04:53 -0700

    stream-controller: don't trigger EOS if last fragment requires backtracking
    related to #1338

--- src/controller/stream-controller.js
+++ src/controller/stream-controller.js
@@ -278,13 +278,15 @@ class StreamController extends EventHandler {
             return;
         }
 
-        // we just got done loading the final fragment, and currentPos is buffered, and there is no other buffered range after ...
-        // rationale is that in case there are any buffered rangesafter, it means that there are unbuffered portion in between
-        // so we should not switch to ENDED in that case, to be able to buffer themx
+        // we just got done loading the final fragment and there is no other buffered range after ...
+        // rationale is that in case there are any buffered ranges after, it means that there are unbuffered portion in between
+        // so we should not switch to ENDED in that case, to be able to buffer them
+        // dont switch to ENDED if we need to backtrack last fragment
         let fragPrevious = this.fragPrevious;
         if (
             !levelDetails.live &&
             fragPrevious &&
+            !fragPrevious.backtracked &&
             fragPrevious.sn === levelDetails.endSN &&
             bufferLen &&
             !bufferInfo.nextStart
