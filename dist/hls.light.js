1cc05b32d46d7613658b415a6e63f25f6eee8e85
John Bartos <jbartos@jwplayer.com>
2017-09-20 17:06:26 -0400

    Reset dist

--- dist/hls.light.js
+++ dist/hls.light.js
@@ -5389,7 +5389,7 @@ var demuxer_inline_DemuxerInline = function () {
       var observer = this.observer;
       var typeSupported = this.typeSupported;
       var config = this.config;
-      // probing order is TS/AAC/MP3/MP4
+      // probing order is AAC/MP3/TS/MP4
       var muxConfig = [{ demux: tsdemuxer, remux: mp4_remuxer }, { demux: aacdemuxer, remux: mp4_remuxer }, { demux: mp3demuxer, remux: mp4_remuxer }, { demux: mp4demuxer, remux: passthrough_remuxer }];
 
       // probe for content type
@@ -5918,19 +5918,15 @@ var playlist_loader_PlaylistLoader = function (_EventHandler) {
   };
 
   PlaylistLoader.prototype.parseMasterPlaylistMedia = function parseMasterPlaylistMedia(string, baseurl, type) {
-    var _this2 = this;
-
-    var audioGroups = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : [];
+    var audioCodec = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : null;
 
     var result = void 0,
         medias = [],
         id = 0;
     MASTER_PLAYLIST_MEDIA_REGEX.lastIndex = 0;
-
-    var _loop = function _loop() {
+    while ((result = MASTER_PLAYLIST_MEDIA_REGEX.exec(string)) != null) {
       var media = {};
-      attrs = new attr_list(result[1]);
-
+      var attrs = new attr_list(result[1]);
       if (attrs.TYPE === type) {
         media.groupId = attrs['GROUP-ID'];
         media.instreamId = attrs['INSTREAM-ID'];
@@ -5940,27 +5936,18 @@ var playlist_loader_PlaylistLoader = function (_EventHandler) {
         media.autoselect = attrs.AUTOSELECT === 'YES';
         media.forced = attrs.FORCED === 'YES';
         if (attrs.URI) {
-          media.url = _this2.resolve(attrs.URI, baseurl);
+          media.url = this.resolve(attrs.URI, baseurl);
         }
         media.lang = attrs.LANGUAGE;
         if (!media.name) {
           media.name = media.lang;
         }
-        if (audioGroups.length) {
-          var groupCodec = audioGroups.find(function (x) {
-            return x.groupId === media.groupId;
-          });
-          media.audioCodec = groupCodec ? groupCodec.audioCodec : audioGroups[0].audioCodec;
+        if (audioCodec) {
+          media.audioCodec = audioCodec;
         }
         media.id = id++;
         medias.push(media);
       }
-    };
-
-    while ((result = MASTER_PLAYLIST_MEDIA_REGEX.exec(string)) != null) {
-      var attrs;
-
-      _loop();
     }
     return medias;
   };
@@ -6181,10 +6168,7 @@ var playlist_loader_PlaylistLoader = function (_EventHandler) {
         var levels = this.parseMasterPlaylist(string, url);
         // multi level playlist, parse level info
         if (levels.length) {
-          var audioGroups = levels.map(function (l) {
-            return { groupId: l.attrs.AUDIO, audioCodec: l.audioCodec };
-          });
-          var audioTracks = this.parseMasterPlaylistMedia(string, url, 'AUDIO', audioGroups);
+          var audioTracks = this.parseMasterPlaylistMedia(string, url, 'AUDIO', levels[0].audioCodec);
           var subtitles = this.parseMasterPlaylistMedia(string, url, 'SUBTITLES');
           var captions = this.parseMasterPlaylistMedia(string, url, 'CLOSED-CAPTIONS');
           if (audioTracks.length) {
@@ -8723,16 +8707,15 @@ var level_controller_LevelController = function (_EventHandler) {
   };
 
   LevelController.prototype.onManifestLoaded = function onManifestLoaded(data) {
-    var levels0 = [];
-    var levels = [];
-    var audioTracks = [];
-    var bitrateStart = void 0;
-    var bitrateSet = {};
-    var videoCodecFound = false;
-    var audioCodecFound = false;
-    var hls = this.hls;
-    var brokenmp4inmp3 = /chrome|firefox/.test(navigator.userAgent.toLowerCase());
-    var checkSupported = function checkSupported(type, codec) {
+    var levels0 = [],
+        levels = [],
+        bitrateStart,
+        bitrateSet = {},
+        videoCodecFound = false,
+        audioCodecFound = false,
+        hls = this.hls,
+        brokenmp4inmp3 = /chrome|firefox/.test(navigator.userAgent.toLowerCase()),
+        checkSupported = function checkSupported(type, codec) {
       return MediaSource.isTypeSupported(type + '/mp4;codecs=' + codec);
     };
 
@@ -8776,12 +8759,6 @@ var level_controller_LevelController = function (_EventHandler) {
       return (!audioCodec || checkSupported('audio', audioCodec)) && (!videoCodec || checkSupported('video', videoCodec));
     });
 
-    if (data.audioTracks) {
-      audioTracks = data.audioTracks.filter(function (track) {
-        return !track.audioCodec || checkSupported('audio', track.audioCodec);
-      });
-    }
-
     if (levels.length) {
       // start bitrate is the first bitrate of the manifest
       bitrateStart = levels[0].bitrate;
@@ -8798,7 +8775,7 @@ var level_controller_LevelController = function (_EventHandler) {
           break;
         }
       }
-      hls.trigger(events["a" /* default */].MANIFEST_PARSED, { levels: levels, audioTracks: audioTracks, firstLevel: this._firstLevel, stats: data.stats, audio: audioCodecFound, video: videoCodecFound, altAudio: data.audioTracks.length > 0 });
+      hls.trigger(events["a" /* default */].MANIFEST_PARSED, { levels: levels, firstLevel: this._firstLevel, stats: data.stats, audio: audioCodecFound, video: videoCodecFound, altAudio: data.audioTracks.length > 0 });
     } else {
       hls.trigger(events["a" /* default */].ERROR, { type: errors["b" /* ErrorTypes */].MEDIA_ERROR, details: errors["a" /* ErrorDetails */].MANIFEST_INCOMPATIBLE_CODECS_ERROR, fatal: true, url: hls.url, reason: 'no level with compatible codecs found in manifest' });
     }
