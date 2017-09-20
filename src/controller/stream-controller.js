video-dev/hls.js

commit 8db8ea3029b415ddea2c656a230ac0ce55e588a9

stream-controller: allow start level loading retry even if media not attached
# Conflicts:
#	src/controller/stream-controller.js

src/controller/stream-controller.js
@@ -190,8 +190,8 @@ class StreamController extends EventHandler {
     // if video not attached AND
     // start fragment already requested OR start frag prefetch disable
     // exit loop
-    // => if media not attached but start frag prefetch is enabled and start frag not requested yet, we will not exit loop
-    if (!media &&
+    // => if start level loaded and media not attached but start frag prefetch is enabled and start frag not requested yet, we will not exit loop
+    if (this.levelLastLoaded !== undefined && !media &&
       (this.startFragRequested || !config.startFragPrefetch)) {
       return true;
     }
@@ -204,12 +204,14 @@ class StreamController extends EventHandler {
       pos = this.nextLoadPosition;
     }
     // determine next load level
-    let level = hls.nextLoadLevel;
+    let level = hls.nextLoadLevel,
+        levelInfo = this.levels[level],
+        levelBitrate = levelInfo.bitrate,
+        maxBufLen;
 
     // compute max Buffer Length that we could get from this load level, based on level bitrate. don't buffer more than 60 MB and more than 30s
-    let maxBufLen;
-    if ((this.levels[level]).hasOwnProperty('bitrate')) {
-      maxBufLen = Math.max(8 * config.maxBufferSize / this.levels[level].bitrate, config.maxBufferLength);
+    if (levelBitrate) {
+      maxBufLen = Math.max(8 * config.maxBufferSize / levelBitrate, config.maxBufferLength);
     } else {
       maxBufLen = config.maxBufferLength;
     }
@@ -229,10 +231,9 @@ class StreamController extends EventHandler {
     logger.trace(`buffer length of ${bufferLen.toFixed(3)} is below max of ${maxBufLen.toFixed(3)}. checking for more payload ...`);
 
     // set next load level : this will trigger a playlist load if needed
-    hls.nextLoadLevel = level;
-    this.level = level;
+    this.level = hls.nextLoadLevel = level;
 
-    const levelDetails = this.levels[level].details;
+    const levelDetails = levelInfo.details;
     // if level info not retrieved yet, switch state and wait for level retrieval
     // if live playlist, ensure that new playlist has been refreshed to avoid loading/try to load
     // a useless and outdated fragment (that might even introduce load error if it is already out of the live playlist)

commit 617344c8207dccae8b32740506fa33305eb1818b

stream-controller: don't start loading first fragment if first LEVEL_LOADED not received yet
fix EXT-X-START not honored on single level playlist
related to #1353

regression introduced by https://github.com/video-dev/hls.js/commit/8db8ea3029b415ddea2c656a230ac0ce55e588a9

src/controller/stream-controller.js
@@ -182,12 +182,12 @@ class StreamController extends EventHandler {
           config = hls.config,
           media = this.media;
 
-    // if video not attached AND
-    // start fragment already requested OR start frag prefetch disable
-    // exit loop
-    // => if start level loaded and media not attached but start frag prefetch is enabled and start frag not requested yet, we will not exit loop
-    if (this.levelLastLoaded !== undefined && !media &&
-      (this.startFragRequested || !config.startFragPrefetch)) {
+
+    // if start level not parsed yet OR
+    // if video not attached AND start fragment already requested OR start frag prefetch disable
+    // exit loop, as we either need more info (level not parsed) or we need media to be attached to load new fragment
+    if (this.levelLastLoaded === undefined || (
+      !media && (this.startFragRequested || !config.startFragPrefetch))) {
       return;
     }
 
@@ -411,7 +411,7 @@ class StreamController extends EventHandler {
       //  return -1             return 0                 return 1
       //logger.log(`level/sn/start/end/bufEnd:${level}/${candidate.sn}/${candidate.start}/${(candidate.start+candidate.duration)}/${bufferEnd}`);
       // Set the lookup tolerance to be small enough to detect the current segment - ensures we don't skip over very small segments
-      let candidateLookupTolerance = Math.min(maxFragLookUpTolerance, candidate.duration);
+      let candidateLookupTolerance = Math.min(maxFragLookUpTolerance, candidate.duration + (candidate.deltaPTS ? candidate.deltaPTS : 0));
       if (candidate.start + candidate.duration - candidateLookupTolerance <= bufferEnd) {
         return 1;
       } // if maxFragLookUpTolerance will have negative value then don't return -1 for first element
@@ -892,7 +892,6 @@ class StreamController extends EventHandler {
       logger.log('both AAC/HE-AAC audio found in levels; declaring level codec as HE-AAC');
     }
     this.levels = data.levels;
-    this.startLevelLoaded = false;
     this.startFragRequested = false;
     let config = this.config;
     if (config.autoStartLoad || this.forceStartLoad) {