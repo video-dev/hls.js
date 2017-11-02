786eb1c39a4e20b6de6843e6afd09ba0f6fd74c5
DanFerrer <danielferrer212@gmail.com>
2017-11-02 19:16:20 -0400

    replace fromCharCode with utf8ArrayToStr

--- src/utils/webvtt-parser.js
+++ src/utils/webvtt-parser.js
@@ -61,9 +61,7 @@ const WebVTTParser = {
     parse: function(vttByteArray, syncPTS, vttCCs, cc, callBack, errorCallBack) {
         // Convert byteArray into string, replacing any somewhat exotic linefeeds with "\n", then split on that character.
         let re = /\r\n|\n\r|\n|\r/g;
-        let vttLines = new Uint8Array(vttByteArray)
-          .reduce((raw, vttByte) => raw + String.fromCharCode(vttByte), '')
-          .trim().replace(re, '\n').split('\n');
+        let vttLines = utf8ArrayToStr(new Uint8Array(vttByteArray)).trim().replace(re, '\n').split('\n');
 
         let cueTime = '00:00.000';
         let mpegTs = 0;
