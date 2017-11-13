86aaaa932a28808e431fbfc08d71bbadea118fde
dannyfinks <danny@jwplayer.com>
2017-11-13 11:51:36 -0500

    Update dist for hls.js to include 608 captions position fix (#127)

--- dist/hls.light.js
+++ dist/hls.light.js
@@ -844,7 +844,6 @@ function isUndefined(arg) {
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 // EXTERNAL MODULE: ./src/events.js
 var events = __webpack_require__(1);
