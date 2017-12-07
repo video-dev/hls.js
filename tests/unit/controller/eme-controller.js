e6621042c9c8aff95b10c78a0018bfee05388663
tchakabam <tchakabam@gmail.com>
2017-12-07 19:16:54 +0100

    EME controller unit test basics

--- tests/unit/controller/eme-controller.js
+++ tests/unit/controller/eme-controller.js
@@ -2,10 +2,79 @@ import EMEController from '../../../src/controller/eme-controller';
 
 import HlsMock from '../../mocks/hls.mock';
 
-describe.only('EMEController', () => {
-  it('should be constructable with an unconfigured Hls.js instance', () => {
+const MediaMock = function() {
+  return {
+    setMediaKeys: sinon.spy(),
+    addEventListener: sinon.spy()
+  }
+};
 
-    new EMEController(new HlsMock());
+const fakeLevels = [
+  {
+    audioCodec: 'audio/foo'
+  },
+  {
+    videoCoded: 'video/foo'
+  }
+];
+
+let emeController;
+let media;
+
+const setupEach = function(config) {
+  media = new MediaMock();
+
+  emeController = new EMEController(new HlsMock(config));
+}
+
+describe.only('EMEController', () => {
+
+  beforeEach(() => {
+    setupEach();
+  });
+
+  it('should be constructable with an unconfigured Hls.js instance', () => {});
+
+  it('should not do anything when `emeEnabled` is false (default)', () => {
+
+    let reqMediaKsAccessSpy = sinon.spy();
+
+    setupEach({
+      requestMediaKeySystemAccessFunc: reqMediaKsAccessSpy
+    });
+
+    emeController.onMediaAttached({media});
+    emeController.onManifestParsed({media});
+
+    media.setMediaKeys.callCount.should.be.equal(0);
+    reqMediaKsAccessSpy.callCount.should.be.equal(0);
+  });
+
+  it('should request keys when `emeEnabled` is true (but not set them)', (done) => {
+
+      let reqMediaKsAccessSpy = sinon.spy(() => {
+        return Promise.resolve({
+          // Media-keys mock
+        })
+      });
+
+      setupEach({
+        emeEnabled: true,
+        requestMediaKeySystemAccessFunc: reqMediaKsAccessSpy
+      });
+
+      emeController.onMediaAttached({media});
+
+      media.setMediaKeys.callCount.should.be.equal(0);
+      reqMediaKsAccessSpy.callCount.should.be.equal(0);
+
+      emeController.onManifestParsed({levels: fakeLevels});
+
+      setTimeout(() => {
+        media.setMediaKeys.callCount.should.be.equal(0);
+        reqMediaKsAccessSpy.callCount.should.be.equal(1);
+        done();
+      }, 0)
 
   });
 })
