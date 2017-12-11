4e24a4461b6aaf04ad4a58fc5f33051982ae623b
Flávio Ribeiro <flavio.ribeiro@nytimes.com>
2017-12-11 18:06:00 -0500

    fix docs

--- webpack.config.bundle-analyzer.js
+++ webpack.config.bundle-analyzer.js
@@ -7,7 +7,7 @@ const configs = clone(webpackConfigDev)();
 configs.forEach(function(config) {
   if (config.name === 'dist') {
     // filter out ModuleConcatenationPlugin
-    // https://github.com/webpack-contrib/webpack-bundle-analyzer/issues/115j
+    // https://github.com/webpack-contrib/webpack-bundle-analyzer/issues/115
     config.plugins = config.plugins.filter(plugin => plugin.constructor.name != 'ModuleConcatenationPlugin');
     
     config.plugins = config.plugins.concat([new BundleAnalyzerPlugin()]);
