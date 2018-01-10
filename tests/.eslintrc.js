59b899eea6789799d00b28de55165db1b9233061
tchakabam <tchakabam@gmail.com>
2018-01-11 00:21:35 +0100

    add eslintrc for tests code

---
+++ tests/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+    },
+    "globals": {
+        // Test globals
+        "after": false,
+        "afterEach": false,
+        "assert": false,
+        "before": false,
+        "beforeEach": false,
+        "describe": false,
+        "expect": false,
+        "it": false,
+        "sinon": false,
+        "xit": false,
+    },
+};
