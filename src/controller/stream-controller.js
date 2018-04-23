e6f3e90fc1f03ca9bff5fb533a4e467bf88b773d
John Bartos <jbartos@jwplayer.com>
2018-04-23 16:22:24 -0400

    Refactor bufferStall reporting to be on a 1 second timer; add tests

--- src/controller/stream-controller.js
+++ src/controller/stream-controller.js
@@ -1451,13 +1451,17 @@ class StreamController extends TaskLoop {
       } else if (expectedPlaying) {
         // The playhead isn't moving but it should be
         // Allow some slack time to for small stalls to resolve themselves
+        const stalledDuration = tnow - this.stalled;
+        console.log(tnow)
+        const bufferInfo = BufferHelper.bufferInfo(media, currentTime, config.maxBufferHole);
         if (!this.stalled) {
           this.stalled = tnow;
           return;
+        } else if (stalledDuration >= 1000) {
+          // Report stalling after trying to fix
+          this._reportStall(bufferInfo.len);
         }
 
-        const bufferInfo = BufferHelper.bufferInfo(media, currentTime, config.maxBufferHole);
-        const stalledDuration = tnow - this.stalled;
         this._tryFixBufferStall(bufferInfo, stalledDuration);
       }
     }
@@ -1522,10 +1526,6 @@ class StreamController extends TaskLoop {
       // Reset stalled so to rearm watchdog timer
       this.stalled = null;
       this._tryNudgeBuffer();
-    }
-
-    if (stalledDuration > 1000) {
-      this._reportStall(bufferInfo.len);
     }
   }
 
