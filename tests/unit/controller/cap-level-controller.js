783da89cbafa0555656e860fd32e677578d710a9
John Bartos <jbartos@jwplayer.com>
2018-04-27 16:03:05 -0400

    Activate level capping only when a video codec is found JW8-1523

--- tests/unit/controller/cap-level-controller.js
+++ tests/unit/controller/cap-level-controller.js
@@ -1,5 +1,7 @@
 const assert = require('assert');
 
+import sinon from 'sinon';
+import Hls from '../../../src/hls';
 import CapLevelController from '../../../src/controller/cap-level-controller';
 
 const levels = [
@@ -55,6 +57,77 @@ describe('CapLevelController', function () {
       const expected = -1;
       const actual = CapLevelController.getMaxLevelByMediaSize(undefined, 5000, 5000);
       assert.equal(expected, actual);
+    });
+  });
+
+  describe('initialization', function () {
+    let capLevelController;
+    let firstLevelSpy;
+    let startCappingSpy;
+    beforeEach(function () {
+      const hls = new Hls({ capLevelToPlayerSize: true });
+      firstLevelSpy = sinon.spy(hls, 'firstLevel', ['set']);
+      capLevelController = new CapLevelController(hls);
+      startCappingSpy = sinon.spy(capLevelController, '_startCapping');
+    });
+
+    describe('start and stop', function () {
+      it('immediately caps and sets a timer for monitoring size size', function () {
+        const detectPlayerSizeSpy = sinon.spy(capLevelController, 'detectPlayerSize');
+        capLevelController._startCapping();
+        assert(capLevelController.timer);
+        assert(firstLevelSpy.set.calledOnce);
+        assert(detectPlayerSizeSpy.calledOnce);
+      });
+
+      it('stops the capping timer and resets capping', function () {
+        capLevelController.autoLevelCapping = 4;
+        capLevelController.timer = 1;
+        capLevelController._stopCapping();
+
+        assert.strictEqual(capLevelController.autoLevelCapping, Number.POSITIVE_INFINITY);
+        assert.strictEqual(capLevelController.restrictedLevels.length, 0);
+        assert.strictEqual(capLevelController.firstLevel, null);
+        assert.strictEqual(capLevelController.timer, null);
+      });
+    });
+
+    it('constructs with no restrictions', function () {
+      assert.strictEqual(capLevelController.levels.length, 0);
+      assert.strictEqual(capLevelController.restrictedLevels.length, 0);
+      assert.strictEqual(capLevelController.timer, null);
+      assert.strictEqual(capLevelController.autoLevelCapping, Number.POSITIVE_INFINITY);
+      assert(firstLevelSpy.set.notCalled);
+    });
+
+    it('starts capping on BUFFER_CODECS only if video is found', function () {
+      capLevelController.onBufferCodecs({ video: {} });
+      assert(startCappingSpy.calledOnce);
+    });
+
+    it('does not start capping on BUFFER_CODECS if video is not found', function () {
+      capLevelController.onBufferCodecs({ audio: {} });
+      assert(startCappingSpy.notCalled);
+    });
+
+    it('starts capping if the video codec was found after the audio codec', function () {
+      capLevelController.onBufferCodecs({ audio: {} });
+      assert(startCappingSpy.notCalled);
+      capLevelController.onBufferCodecs({ video: {} });
+      assert(startCappingSpy.calledOnce);
+    });
+
+    it('receives level information from the MANIFEST_PARSED event', function () {
+      capLevelController.restrictedLevels = [1];
+      let data = {
+        levels: [{ foo: 'bar' }],
+        firstLevel: 0
+      };
+
+      capLevelController.onManifestParsed(data);
+      assert.strictEqual(capLevelController.levels, data.levels);
+      assert.strictEqual(capLevelController.firstLevel, data.firstLevel);
+      assert.strictEqual(capLevelController.restrictedLevels.length, 0);
     });
   });
 });
