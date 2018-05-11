76b10f05b142a0a6d2bb3d4b39d9d8138f58ec88
John Bartos <jbartos@jwplayer.com>
2018-05-11 10:39:00 -0400

    Update dist

--- dist/hls.light.js
+++ dist/hls.light.js
@@ -8957,6 +8957,7 @@ var stream_controller_StreamController = function (_TaskLoop) {
     this.fragmentTracker.removeAllFragments();
     this.stalled = false;
     this.startPosition = this.lastCurrentTime = 0;
+    this.fragPlaying = null;
   };
 
   StreamController.prototype.onManifestParsed = function onManifestParsed(data) {
