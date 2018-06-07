18be7d8aecc8b203166b544e7451594fe755d67b
Stephan Hesse <tchakabam@gmail.com>
2018-06-07 13:34:35 +0200

    decrypter: lint fix

--- src/crypt/decrypter.js
+++ src/crypt/decrypter.js
@@ -11,7 +11,7 @@ import { getSelfScope } from '../utils/get-self-scope';
 
 // see https://stackoverflow.com/a/11237259/589493
 /* eslint-disable-next-line no-undef */
-const window = getSelfScope();  // safeguard for code that might run both on worker and main thread
+const window = getSelfScope(); // safeguard for code that might run both on worker and main thread
 
 const { performance, crypto } = window;
 
