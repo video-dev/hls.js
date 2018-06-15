1879842568eeb4dbaa748e329f7c921dc46dfc39
John Bartos <jbartos@jwplayer.com>
2018-06-15 14:16:25 -0400

    Avoid more reassignments of window

--- src/demux/demuxer-inline.js
+++ src/demux/demuxer-inline.js
@@ -18,10 +18,8 @@ import PassThroughRemuxer from '../remux/passthrough-remuxer';
 import { getSelfScope } from '../utils/get-self-scope';
 
 // see https://stackoverflow.com/a/11237259/589493
-/* eslint-disable-next-line no-undef */
-const window = getSelfScope(); // safeguard for code that might run both on worker and main thread
-
-const { performance } = window;
+const global = getSelfScope(); // safeguard for code that might run both on worker and main thread
+const performance = global;
 
 class DemuxerInline {
   constructor (observer, typeSupported, config, vendor) {
--- src/demux/demuxer.js
+++ src/demux/demuxer.js
@@ -10,8 +10,7 @@ import { getSelfScope } from '../utils/get-self-scope';
 
 // see https://stackoverflow.com/a/11237259/589493
 /* eslint-disable-next-line no-undef */
-const window = getSelfScope(); // safeguard for code that might run both on worker and main thread
-
+const global = getSelfScope(); // safeguard for code that might run both on worker and main thread
 const MediaSource = getMediaSource();
 
 class Demuxer {
@@ -69,7 +68,7 @@ class Demuxer {
         logger.error('error while initializing DemuxerWorker, fallback on DemuxerInline');
         if (w) {
           // revoke the Object URL that was used to create demuxer worker, so as not to leak it
-          window.URL.revokeObjectURL(w.objectURL);
+          global.URL.revokeObjectURL(w.objectURL);
         }
         this.demuxer = new DemuxerInline(observer, typeSupported, config, vendor);
         this.w = undefined;
@@ -134,7 +133,7 @@ class Demuxer {
     switch (data.event) {
     case 'init':
       // revoke the Object URL that was used to create demuxer worker, so as not to leak it
-      window.URL.revokeObjectURL(this.w.objectURL);
+      global.URL.revokeObjectURL(this.w.objectURL);
       break;
       // special case for FRAG_PARSING_DATA: data1 and data2 are transferable objects
     case Event.FRAG_PARSING_DATA:
