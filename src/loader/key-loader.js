28492ac577418a64a5d370cfe1b871b75a0dd0a6
Sri <srisreed@gmail.com>
2018-07-17 15:52:07 -0700

    key-loader: set maxRetry to 0, differ to stream-controller to handle retry

--- src/loader/key-loader.js
+++ src/loader/key-loader.js
@@ -46,7 +46,7 @@ class KeyLoader extends EventHandler {
 
       let loaderContext, loaderConfig, loaderCallbacks;
       loaderContext = { url: uri, frag: frag, responseType: 'arraybuffer' };
-      loaderConfig = { timeout: config.fragLoadingTimeOut, maxRetry: config.fragLoadingMaxRetry, retryDelay: config.fragLoadingRetryDelay, maxRetryDelay: config.fragLoadingMaxRetryTimeout };
+      loaderConfig = { timeout: config.fragLoadingTimeOut, maxRetry: 0, retryDelay: config.fragLoadingRetryDelay, maxRetryDelay: config.fragLoadingMaxRetryTimeout };
       loaderCallbacks = { onSuccess: this.loadsuccess.bind(this), onError: this.loaderror.bind(this), onTimeout: this.loadtimeout.bind(this) };
       frag.loader.load(loaderContext, loaderConfig, loaderCallbacks);
     } else if (this.decryptkey) {
