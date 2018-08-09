b9cb8d883826af8ca37992f34fd007ffc4a7759f
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-08-10 08:24:26 +0900

    Use `global` as global object in Node.js

--- src/utils/get-self-scope.js
+++ src/utils/get-self-scope.js
@@ -2,7 +2,7 @@ export function getSelfScope () {
   // see https://stackoverflow.com/a/11237259/589493
   if (typeof window === 'undefined') {
     /* eslint-disable-next-line no-undef */
-    return self;
+    return global;
   } else {
     return window;
   }
