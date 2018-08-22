d93360479a6b69e5f69f2e3e4742f7eb139561ba
Stephan Hesse <tchakabam@gmail.com>
2018-08-22 21:41:16 +0200

    add toolchain files to linter (disabled)

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,4 +2,6 @@ echo "Linting files ..."
 
 ./node_modules/.bin/eslint src/ tests/ --ext .js --ext .ts $1
 
+#./node_modules/.bin/eslint *.js src/ tests/ --ext .js --ext .ts $1 # lints all js files of toolchain as well
+
 # ./node_modules/.bin/eslint demo/ --ext .js --ext .ts $1
