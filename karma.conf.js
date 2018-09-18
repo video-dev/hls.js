3996e7477b30e2c1b512e53c4efa8502abce8081
John Bartos <jbartos@jwplayer.com>
2018-09-18 10:45:54 -0400

    Add chai & sinon-chai, remove deprecated testing libs/files

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,7 @@ module.exports = function(config) {
 
 		// frameworks to use
 		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-		frameworks: ['mocha', 'sinon'],
+		frameworks: ['mocha', 'sinon-chai'],
 
 		// list of files / patterns to load in the browser
 		files: [
@@ -25,7 +25,7 @@ module.exports = function(config) {
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
-		  '**/*.js': ['webpack', 'sourcemap']
+		  'tests/**/*.js': ['webpack', 'sourcemap']
 		},
 
 		// test results reporter to use
