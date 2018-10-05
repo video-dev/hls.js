2e5c44228b918fcdcbd6ccd88f00115c351a4927
Stephan Hesse <tchakabam@gmail.com>
2018-10-05 19:38:46 +0200

    subtitle stream ctrl unit tests

--- tests/unit/controller/subtitle-stream-controller.js
+++ tests/unit/controller/subtitle-stream-controller.js
@@ -1,5 +1,6 @@
 import assert from 'assert';
 import sinon from 'sinon';
+
 import Hls from '../../../src/hls';
 import Event from '../../../src/events';
 import { FragmentTracker, FragmentState } from '../../../src/controller/fragment-tracker';
@@ -10,42 +11,106 @@ import Fragment from '../../../src/loader/fragment';
 
 const State = SubtitleStreamControllerState;
 
-describe.only('SubtitleStreamController', function () {
+const mediaMock = {
+  currentTime: 0
+};
+
+const tracksMock = [
+  { id: 0, details: {} },
+  { id: 1 }
+];
+
+describe('SubtitleStreamController', function () {
   let hls;
   let fragmentTracker;
   let streamController;
 
-  beforeEach(function () {
+  beforeEach(() => {
     hls = new Hls({});
     fragmentTracker = new FragmentTracker(hls);
     streamController = new SubtitleStreamController(hls, fragmentTracker);
-    streamController.startFragRequested = true;
+
+    streamController.onMediaAttached(mediaMock);
   });
 
-  /**
-   * Assert: streamController should be started
-   * @param {StreamController} streamController
-   */
-  const assertStreamControllerStarted = (streamController) => {
-    assert.equal(streamController.hasInterval(), true, 'StreamController should start interval');
-    assert.notDeepEqual(streamController.state, State.STOPPED, 'StreamController\'s state should not be STOPPED');
-  };
-
-  /**
-   * Assert: streamController should be stopped
-   * @param {StreamController} streamController
-   */
-  const assertStreamControllerStopped = (streamController) => {
-    assert.equal(streamController.hasInterval(), false, 'StreamController should stop interval');
-    assert.equal(streamController.state, State.STOPPED, 'StreamController\'s state should be STOPPED');
-  };
-
-  it('should be STOPPED when it is initialized', function () {
-    assertStreamControllerStopped(streamController);
+  afterEach(() => {
+    streamController.onMediaDetaching(mediaMock);
   });
 
-  it('should not start without tracks data', function () {
-    streamController.startLoad(1);
-    assertStreamControllerStopped(streamController);
+  describe('onSubtitleTracksUpdate', () => {
+    beforeEach(() => {
+      hls.trigger(Event.SUBTITLE_TRACKS_UPDATED, {
+        subtitleTracks: tracksMock
+      });
+    });
+
+    it('should update tracks list', () => {
+      assert(streamController.tracks === tracksMock);
+    });
+  });
+
+  describe('onSubtitleTrackSwitch', () => {
+    beforeEach(() => {
+      streamController.tracks = tracksMock;
+      streamController.clearInterval = sinon.spy();
+      streamController.setInterval = sinon.spy();
+
+      hls.trigger(Event.SUBTITLE_TRACK_SWITCH, {
+        id: 0
+      });
+    });
+
+    it('should call setInterval if details available', () => {
+      assert(streamController.setInterval.calledOnce);
+    });
+
+    it('should call clearInterval if no tracks present', () => {
+      streamController.tracks = null;
+      hls.trigger(Event.SUBTITLE_TRACK_SWITCH, {
+        id: 0
+      });
+      assert(streamController.clearInterval.calledOnce);
+    });
+
+    it('should call clearInterval if new track id === -1', () => {
+      hls.trigger(Event.SUBTITLE_TRACK_SWITCH, {
+        id: -1
+      });
+      assert(streamController.clearInterval.calledOnce);
+    });
+  });
+
+  describe('onSubtitleTrackLoaded', () => {
+    let detailsMock = {};
+
+    beforeEach(() => {
+      streamController.setInterval = sinon.spy();
+      streamController.tracks = tracksMock;
+      hls.trigger(Event.SUBTITLE_TRACK_LOADED, {
+        id: 1, details: detailsMock
+      });
+    });
+
+    it('should add details to track object in list', () => {
+      assert(streamController.tracks[1].details === detailsMock);
+    });
+
+    it('should call setInterval', () => {
+      assert(streamController.setInterval.calledOnce);
+    });
+
+    it('should not crash when no tracks present', () => {
+      streamController.tracks = null;
+      hls.trigger(Event.SUBTITLE_TRACK_LOADED, {
+        id: 0, details: {}
+      });
+    });
+
+    it('should not crash when no track id does not exist', () => {
+      streamController.tracks = null;
+      hls.trigger(Event.SUBTITLE_TRACK_LOADED, {
+        id: 5, details: {}
+      });
+    });
   });
 });
