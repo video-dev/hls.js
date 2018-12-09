63c9d13a3e1500cd5a1d1222eab400459cd24edc
John Bartos <jbartos@jwplayer.com>
2018-12-09 00:26:03 -0500

    Dont preprocess non-test files

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function(config) {
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
-		  '**/*.js': ['webpack', 'sourcemap']
+		  'tests/unit/**/*.js': ['webpack', 'sourcemap']
 		},
 
 		// test results reporter to use
