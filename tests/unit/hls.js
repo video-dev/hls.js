4af7ed99134ea5e91afdf6480a5f91a969527920
beraliv <beraliv@yandex-team.ru>
2019-01-22 10:21:15 +0300

    test: add mocked _bwEstimator

--- tests/unit/hls.js
+++ tests/unit/hls.js
@@ -3,8 +3,19 @@ import Hls from '../../src/hls';
 const assert = require('assert');
 
 describe('Hls', () => {
-  it('should return bandwidth estimate', () => {
-    let hls = new Hls();
+  it('should return a bandwidth estimate if the estimator exists', () => {
+    const MOCKED_ESTIMATE = 2000;
+    const hls = new Hls();
+    hls.abrController = {
+      _bwEstimator: {
+        getEstimate: () => MOCKED_ESTIMATE
+      }
+    };
+    assert.strictEqual(hls.bandwidthEstimate, MOCKED_ESTIMATE);
+  });
+
+  it('should return NaN if the estimator does not exist', () => {
+    const hls = new Hls();
     assert.strictEqual(isNaN(hls.bandwidthEstimate), true);
   });
 });
