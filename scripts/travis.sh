d93d4539557ad9c1ff78bd0a1c357cd163e38d3c
Tom Jenkinson <tom.jenkinson@soundcloud.com>
2019-01-26 17:57:06 +0100

    configure netlify for PR's
    this follows the same process that runs for canary releases

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -30,34 +30,40 @@ elif [ "${TRAVIS_MODE}" = "funcTests" ]; then
 	if [ ${n} = ${maxRetries} ]; then
 		exit 1
 	fi
-elif [ "${TRAVIS_MODE}" = "release" ] || [ "${TRAVIS_MODE}" = "releaseCanary" ]; then
+elif [ "${TRAVIS_MODE}" = "release" ] || [ "${TRAVIS_MODE}" = "releaseCanary" ] || [ "${TRAVIS_MODE}" = "netlifyPr" ]; then
   # update the version
   # make sure everything is fetched https://github.com/travis-ci/travis-ci/issues/3412
   git fetch --unshallow
   node ./scripts/set-package-version.js
   npm run lint
   npm run build
-  npm run test:unit
-  if [[ $(node ./scripts/check-already-published.js) = "not published" ]]; then
-    # write the token to config
-    # see https://docs.npmjs.com/private-modules/ci-server-config
-    echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> .npmrc
-    if [ "${TRAVIS_MODE}" = "releaseCanary" ]; then
-      npm publish --tag canary
-      echo "Published canary."
-      curl https://purge.jsdelivr.net/npm/hls.js@canary
-      curl https://purge.jsdelivr.net/npm/hls.js@canary/dist/hls-demo.js
-      echo "Cleared jsdelivr cache."
-    elif [ "${TRAVIS_MODE}" = "release" ]; then
-      npm publish
-      curl https://purge.jsdelivr.net/npm/hls.js@latest
-      echo "Published."
+  if [ "${TRAVIS_MODE}" != "netlifyPr" ]; then
+    npm run test:unit
+    if [[ $(node ./scripts/check-already-published.js) = "not published" ]]; then
+      # write the token to config
+      # see https://docs.npmjs.com/private-modules/ci-server-config
+      echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> .npmrc
+      if [ "${TRAVIS_MODE}" = "releaseCanary" ]; then
+        npm publish --tag canary
+        echo "Published canary."
+        curl https://purge.jsdelivr.net/npm/hls.js@canary
+        curl https://purge.jsdelivr.net/npm/hls.js@canary/dist/hls-demo.js
+        echo "Cleared jsdelivr cache."
+      elif [ "${TRAVIS_MODE}" = "release" ]; then
+        npm publish
+        curl https://purge.jsdelivr.net/npm/hls.js@latest
+        echo "Published."
+      fi
+    else
+      echo "Already published."
     fi
-  else
-    echo "Already published."
   fi
   npm run docs
-  ./scripts/build-deploy-netlify.sh
+
+  ./scripts/build-netlify.sh
+  if [ "${TRAVIS_MODE}" != "netlifyPr" ]; then
+    ./scripts/deploy-netlify.sh
+  fi
 else
 	echo "Unknown travis mode: ${TRAVIS_MODE}" 1>&2
 	exit 1
