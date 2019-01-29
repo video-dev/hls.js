2a80ff24006cb55bb5e815a90487166919a1b524
Jamie Stackhouse <contin673@gmail.com>
2019-01-29 01:26:27 -0400

    Add tests for parsing byte range as seperate call.

--- tests/unit/loader/fragment.js
+++ tests/unit/loader/fragment.js
@@ -1,12 +1,37 @@
 import Fragment from '../../../src/loader/fragment';
 
 describe('Fragment class tests', function () {
+  /**
+   * @type {Fragment}
+   */
   let frag;
-  describe('endProgramDateTime getter', function () {
-    beforeEach(function () {
-      frag = new Fragment();
+  beforeEach(function () {
+    frag = new Fragment();
+  });
+
+  describe('parseByteRange', function () {
+    it('parses byte range string with length@offset', function () {
+      frag.parseByteRange('1000@10000');
+      expect(frag.byteRangeStartOffset).to.equal(10000);
+      expect(frag.byteRangeEndOffset).to.equal(11000);
     });
 
+    it('parses byte range with no offset and uses 0 as offset', function () {
+      frag.parseByteRange('5000');
+      expect(frag.byteRangeStartOffset).to.equal(0);
+      expect(frag.byteRangeEndOffset).to.equal(5000);
+    });
+
+    it('parses byte range with no offset and uses 0 as offset', function () {
+      const prevFrag = new Fragment();
+      prevFrag.parseByteRange('1000@10000');
+      frag.parseByteRange('5000', prevFrag);
+      expect(frag.byteRangeStartOffset).to.equal(11000);
+      expect(frag.byteRangeEndOffset).to.equal(16000);
+    });
+  });
+
+  describe('endProgramDateTime getter', function () {
     it('computes endPdt when pdt and duration are valid', function () {
       frag.programDateTime = 1000;
       frag.duration = 1;
