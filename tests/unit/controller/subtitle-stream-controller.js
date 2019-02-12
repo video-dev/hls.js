fba8fe3d73055479ab997b3e05b42ba6fd22dd9e
John Bartos <jbartos7@gmail.com>
2019-02-12 16:52:20 -0500

    Implement live VTT subtitle loading
    Always clear fragPrevious on seek
    
    Update src/controller/subtitle-stream-controller.js
    
    Fix test failures & add test
    
    Fix lint error

--- tests/unit/controller/subtitle-stream-controller.js
+++ tests/unit/controller/subtitle-stream-controller.js
@@ -1,18 +1,14 @@
-import assert from 'assert';
 import sinon from 'sinon';
 
 import Hls from '../../../src/hls';
 import Event from '../../../src/events';
-import { FragmentTracker, FragmentState } from '../../../src/controller/fragment-tracker';
-import { SubtitleStreamController, SubtitleStreamControllerState } from '../../../src/controller/subtitle-stream-controller';
-import M3U8Parser from '../../../src/loader/m3u8-parser';
-import { mockFragments } from '../../mocks/data';
-import Fragment from '../../../src/loader/fragment';
-
-const State = SubtitleStreamControllerState;
+import { FragmentTracker } from '../../../src/controller/fragment-tracker';
+import { SubtitleStreamController } from '../../../src/controller/subtitle-stream-controller';
 
 const mediaMock = {
-  currentTime: 0
+  currentTime: 0,
+  addEventListener () {},
+  removeEventListener () {}
 };
 
 const tracksMock = [
@@ -23,94 +19,121 @@ const tracksMock = [
 describe('SubtitleStreamController', function () {
   let hls;
   let fragmentTracker;
-  let streamController;
+  let subtitleStreamController;
 
-  beforeEach(() => {
+  beforeEach(function () {
     hls = new Hls({});
     fragmentTracker = new FragmentTracker(hls);
-    streamController = new SubtitleStreamController(hls, fragmentTracker);
+    subtitleStreamController = new SubtitleStreamController(hls, fragmentTracker);
 
-    streamController.onMediaAttached(mediaMock);
+    subtitleStreamController.onMediaAttached({ media: mediaMock });
   });
 
-  afterEach(() => {
-    streamController.onMediaDetaching(mediaMock);
+  afterEach(function () {
+    subtitleStreamController.onMediaDetaching({ media: mediaMock });
   });
 
-  describe('onSubtitleTracksUpdate', () => {
-    beforeEach(() => {
+  describe('onSubtitleTracksUpdate', function () {
+    beforeEach(function () {
       hls.trigger(Event.SUBTITLE_TRACKS_UPDATED, {
         subtitleTracks: tracksMock
       });
     });
 
-    it('should update tracks list', () => {
-      assert(streamController.tracks === tracksMock);
+    it('should update tracks list', function () {
+      expect(subtitleStreamController.tracks).to.equal(tracksMock);
     });
   });
 
-  describe('onSubtitleTrackSwitch', () => {
-    beforeEach(() => {
-      streamController.tracks = tracksMock;
-      streamController.clearInterval = sinon.spy();
-      streamController.setInterval = sinon.spy();
+  describe('onSubtitleTrackSwitch', function () {
+    beforeEach(function () {
+      subtitleStreamController.tracks = tracksMock;
+      subtitleStreamController.clearInterval = sinon.spy();
+      subtitleStreamController.setInterval = sinon.spy();
 
       hls.trigger(Event.SUBTITLE_TRACK_SWITCH, {
         id: 0
       });
     });
 
-    it('should call setInterval if details available', () => {
-      assert(streamController.setInterval.calledOnce);
+    it('should call setInterval if details available', function () {
+      expect(subtitleStreamController.setInterval).to.have.been.calledOnce;
     });
 
-    it('should call clearInterval if no tracks present', () => {
-      streamController.tracks = null;
+    it('should call clearInterval if no tracks present', function () {
+      subtitleStreamController.tracks = null;
       hls.trigger(Event.SUBTITLE_TRACK_SWITCH, {
         id: 0
       });
-      assert(streamController.clearInterval.calledOnce);
+      expect(subtitleStreamController.clearInterval).to.have.been.calledOnce;
     });
 
-    it('should call clearInterval if new track id === -1', () => {
+    it('should call clearInterval if new track id === -1', function () {
       hls.trigger(Event.SUBTITLE_TRACK_SWITCH, {
         id: -1
       });
-      assert(streamController.clearInterval.calledOnce);
+      expect(subtitleStreamController.clearInterval).to.have.been.calledOnce;
     });
   });
 
-  describe('onSubtitleTrackLoaded', () => {
-    let detailsMock = {};
+  describe('onSubtitleTrackLoaded', function () {
+    beforeEach(function () {
+      subtitleStreamController.setInterval = sinon.spy();
+      subtitleStreamController.tracks = tracksMock;
+    });
 
-    beforeEach(() => {
-      streamController.setInterval = sinon.spy();
-      streamController.tracks = tracksMock;
+    // Details are in subtitle-track-controller.js' onSubtitleTrackLoaded handler
+    it('should handle the event if the data matches the current track', function () {
+      const details = { foo: 'bar' };
+      subtitleStreamController.currentTrackId = 1;
       hls.trigger(Event.SUBTITLE_TRACK_LOADED, {
-        id: 1, details: detailsMock
+        id: 1, details
       });
+      expect(subtitleStreamController.tracks[1].details).to.equal(details);
+      expect(subtitleStreamController.setInterval).to.have.been.calledOnce;
     });
 
-    it('should add details to track object in list', () => {
-      assert(streamController.tracks[1].details === detailsMock);
-    });
-
-    it('should call setInterval', () => {
-      assert(streamController.setInterval.calledOnce);
-    });
-
-    it('should not crash when no tracks present', () => {
-      streamController.tracks = null;
+    it('should ignore the event if the data does not match the current track', function () {
+      const details = { foo: 'bar' };
+      subtitleStreamController.currentTrackId = 0;
       hls.trigger(Event.SUBTITLE_TRACK_LOADED, {
-        id: 0, details: {}
+        id: 1, details
       });
+      expect(subtitleStreamController.tracks[0].details).to.not.equal(details);
+      expect(subtitleStreamController.setInterval).to.not.have.been.called;
     });
 
-    it('should not crash when no track id does not exist', () => {
-      streamController.tracks = null;
+    it('should ignore the event if there are no tracks, or the id is not within the tracks array', function () {
+      subtitleStreamController.tracks = [];
+      subtitleStreamController.trackId = 0;
+      const details = { foo: 'bar' };
       hls.trigger(Event.SUBTITLE_TRACK_LOADED, {
-        id: 5, details: {}
+        id: 0, details
       });
+      expect(subtitleStreamController.tracks[0]).to.not.exist;
+      expect(subtitleStreamController.setInterval).to.not.have.been.called;
+    });
+  });
+
+  describe('onLevelLoaded', function () {
+    it('records the start time of the last known A/V track', function () {
+      hls.trigger(Event.LEVEL_UPDATED, {
+        details: { fragments: [{ start: 5 }] }
+      });
+      expect(subtitleStreamController.lastAVStart).to.equal(5);
+
+      hls.trigger(Event.LEVEL_UPDATED, {
+        details: { fragments: [] }
+      });
+      expect(subtitleStreamController.lastAVStart).to.equal(0);
+    });
+  });
+
+  describe('onMediaSeeking', function () {
+    it('nulls fragPrevious', function () {
+      subtitleStreamController.fragPrevious = {};
+      subtitleStreamController.onMediaSeeking();
+      expect(subtitleStreamController.fragPrevious).to.not.exist;
     });
   });
 });
