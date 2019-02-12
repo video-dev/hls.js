acbe9c02b7d93175f65f7400c1bbef93aecd7c98
John Bartos <jbartos7@gmail.com>
2019-02-12 16:52:20 -0500

    Implement live VTT subtitle loading

--- src/hls.js
+++ src/hls.js
@@ -194,7 +194,7 @@ export default class Hls extends Observer {
        * @member {SubtitleTrackController} subtitleTrackController
        */
       this.subtitleTrackController = subtitleTrackController;
-      coreComponents.push(subtitleTrackController);
+      networkControllers.push(subtitleTrackController);
     }
 
     Controller = config.emeController;
@@ -211,7 +211,7 @@ export default class Hls extends Observer {
     // optional subtitle controllers
     Controller = config.subtitleStreamController;
     if (Controller) {
-      coreComponents.push(new Controller(this, fragmentTracker));
+      networkControllers.push(new Controller(this, fragmentTracker));
     }
     Controller = config.timelineController;
     if (Controller) {
