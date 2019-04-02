d54b9c5f9366dc23f288eef1dde1056e386ce0b3
Jamie Stackhouse <contin673@gmail.com>
2019-04-01 23:27:45 -0300

    Draft usage of strict event emitter.

--- src/observer.ts
+++ src/observer.ts
@@ -1,15 +1,40 @@
 import { EventEmitter } from 'eventemitter3';
+import HlsEvents from './events';
+
+type Arguments < T > = [T] extends [ (...args: infer U) => any ]
+  ? U
+  : [T] extends [void] ? [] : [T];
+
+interface TypedEventEmitter<Events> {
+  addListener<E extends keyof Events> (event: E, listener: Events[E]): this
+  on<E extends keyof Events> (event: E, listener: Events[E]): this
+  once<E extends keyof Events> (event: E, listener: Events[E]): this
+
+  removeAllListeners<E extends keyof Events> (event: E): this
+  removeListener<E extends keyof Events> (event: E, listener: Events[E]): this
+
+  emit<E extends keyof Events> (event: E, ...args: Arguments<Events[E]>): boolean
+  listeners<E extends keyof Events> (event: E): Function[]
+  listenerCount<E extends keyof Events> (event: E): number
+
+  trigger<E extends keyof Events> (event: E, ...args: Arguments<Events[E]>): void
+}
+
+interface MyEvents {
+  [HlsEvents.MEDIA_ATTACHING]: (media: HTMLMediaElement) => void
+  [HlsEvents.MEDIA_ATTACHED]: (media: HTMLMediaElement) => void
+}
 
 /**
  * Simple adapter sub-class of Nodejs-like EventEmitter.
  */
-export class Observer extends EventEmitter {
+export class Observer extends EventEmitter implements TypedEventEmitter<MyEvents> {
   /**
    * We simply want to pass along the event-name itself
    * in every call to a handler, which is the purpose of our `trigger` method
    * extending the standard API.
    */
-  trigger (event: string, ...data: Array<any>): void {
+  trigger<E extends keyof MyEvents> (event: E, ...data: Arguments<MyEvents[E]>): void {
     this.emit(event, event, ...data);
   }
 }
