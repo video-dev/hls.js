0ca292f1fb1ec3d2481150ec1c2251a1e5a2eae4
Jamie Stackhouse <contin673@gmail.com>
2019-04-01 22:10:08 -0300

    Add types to TextTrack utils.

--- src/utils/texttrack-utils.ts
+++ src/utils/texttrack-utils.ts
@@ -1,18 +1,18 @@
 
-export function sendAddTrackEvent (track, videoEl) {
-  let event = null;
+export function sendAddTrackEvent (track: TextTrack, videoEl: HTMLMediaElement) {
+  let event: Event;
   try {
-    event = new window.Event('addtrack');
+    event = new Event('addtrack');
   } catch (err) {
     // for IE11
     event = document.createEvent('Event');
     event.initEvent('addtrack', false, false);
   }
-  event.track = track;
+  (event as any).track = track;
   videoEl.dispatchEvent(event);
 }
 
-export function clearCurrentCues (track) {
+export function clearCurrentCues (track: TextTrack) {
   if (track && track.cues) {
     while (track.cues.length > 0) {
       track.removeCue(track.cues[0]);
