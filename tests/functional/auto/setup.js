9e11a1932609abd1acb0482e6f91f4e08eb5f071
Jamie Stackhouse <contin673@gmail.com>
2019-08-25 22:18:34 -0300

    Extended debugging.

--- tests/functional/auto/setup.js
+++ tests/functional/auto/setup.js
@@ -239,6 +239,7 @@ describe(`testing hls.js playback in the browser on "${browserDescription}"`, fu
       capabilities.username = process.env.SAUCE_USERNAME;
       capabilities.accessKey = process.env.SAUCE_ACCESS_KEY;
       capabilities.avoidProxy = true;
+      capabilities.extendedDebugging = true;
       browser = browser.usingServer(`http://${process.env.SAUCE_USERNAME}:${process.env.SAUCE_ACCESS_KEY}@ondemand.saucelabs.com:80/wd/hub`);
     }
 
@@ -273,6 +274,7 @@ describe(`testing hls.js playback in the browser on "${browserDescription}"`, fu
           }
         } catch (e) {
           const source = await browser.getPageSource();
+          console.log('Failed to load test page, source of other page below.');
           console.log(source);
           throw e;
         }
