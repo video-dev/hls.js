21349d845b3ce0dd80c16ee6716ab1fab7fb2d4d
Stephan Hesse <tchakabam@gmail.com>
2019-08-27 23:46:20 +0200

    timescale-conversion: improve generic conversion function by adding rounding option instead of having that only in toMsFromMpegTsClock and add return-type for toMpegTsClockFromTimescale

--- src/utils/timescale-conversion.ts
+++ src/utils/timescale-conversion.ts
@@ -1,14 +1,14 @@
 const MPEG_TS_CLOCK_FREQ_HZ = 90000;
 
-export function toTimescaleFromBase (value, destScale: number, srcBase: number = 1): number {
-  return value * destScale * srcBase; // equivalent to `(value * scale) / (1 / base)`
-}
-
-export function toMsFromMpegTsClock (value: number, round: boolean = true): number {
-  const result = toTimescaleFromBase(value, 1000, 1 / MPEG_TS_CLOCK_FREQ_HZ);
+export function toTimescaleFromBase (value, destScale: number, srcBase: number = 1, round: boolean = false): number {
+  const result = value * destScale * srcBase; // equivalent to `(value * scale) / (1 / base)`
   return round ? Math.round(result) : result;
 }
 
-export function toMpegTsClockFromTimescale (value: number, srcScale: number = 1) {
+export function toMsFromMpegTsClock (value: number, round: boolean = true): number {
+  return toTimescaleFromBase(value, 1000, 1 / MPEG_TS_CLOCK_FREQ_HZ, round);
+}
+
+export function toMpegTsClockFromTimescale (value: number, srcScale: number = 1): number {
   return toTimescaleFromBase(value, MPEG_TS_CLOCK_FREQ_HZ, 1 / srcScale);
 }
