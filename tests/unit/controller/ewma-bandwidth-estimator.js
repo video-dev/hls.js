9600c882d1d170c0562d5345beb972f07783af47
Rob Walch <rob@jwplayer.com>
2019-09-11 19:13:09 -0400

    Handle rounding errors in bandwidth estimator tests

--- tests/unit/controller/ewma-bandwidth-estimator.js
+++ tests/unit/controller/ewma-bandwidth-estimator.js
@@ -25,9 +25,9 @@ describe('EwmaBandWidthEstimator', function () {
     bwEstimator.sample(8000, 1000000);
     expect(bwEstimator.getEstimate()).to.equal(1000000);
     bwEstimator.sample(4000, 1000000);
-    expect(bwEstimator.getEstimate()).to.equal(1396480.1544736226);
+    expect(bwEstimator.getEstimate()).to.closeTo(1396480.1544736226, 0.000000001);
     bwEstimator.sample(1000, 1000000);
-    expect(bwEstimator.getEstimate()).to.equal(2056826.9489827948);
+    expect(bwEstimator.getEstimate()).to.closeTo(2056826.9489827948, 0.000000001);
   });
 
   it('returns correct value bitrate is slow=9,fast=5', function () {
@@ -36,8 +36,8 @@ describe('EwmaBandWidthEstimator', function () {
     bwEstimator.sample(8000, 1000000);
     expect(bwEstimator.getEstimate()).to.equal(1000000);
     bwEstimator.sample(4000, 1000000);
-    expect(bwEstimator.getEstimate()).to.equal(1439580.319105247);
+    expect(bwEstimator.getEstimate()).to.closeTo(1439580.319105247, 0.000000001);
     bwEstimator.sample(1000, 1000000);
-    expect(bwEstimator.getEstimate()).to.equal(2208342.324322311);
+    expect(bwEstimator.getEstimate()).to.closeTo(2208342.324322311, 0.000000001);
   });
 });
