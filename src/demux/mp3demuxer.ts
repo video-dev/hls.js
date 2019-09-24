9c6e05b69fdcb66ba20b8d693019c6d8631edeb6
Rob Walch <rob@jwplayer.com>
2019-09-23 20:28:49 -0400

    Fix mp3 and aac demuxer streaming

--- src/demux/mp3demuxer.ts
+++ src/demux/mp3demuxer.ts
@@ -9,12 +9,19 @@ import { dummyTrack } from './dummy-demuxed-track';
 import { appendUint8Array } from '../utils/mp4-tools';
 
 class MP3Demuxer implements Demuxer {
+  private observer: any;
+  private config: any;
   private _audioTrack!: any;
   private _id3Track!: DemuxedTrack;
-  private cachedData: Uint8Array = new Uint8Array();
   private frameIndex: number = 0;
+  private cachedData: Uint8Array = new Uint8Array();
   private initPTS: number | null = null;
   static readonly minProbeByteLength: number = 4;
+
+  constructor (observer, config) {
+    this.observer = observer;
+    this.config = config;
+  }
 
   resetInitSegment (audioCodec, videoCodec, duration) {
     this._audioTrack = { container: 'audio/mpeg', type: 'audio', id: -1, sequenceNumber: 0, isAAC: false, samples: [], len: 0, manifestCodec: audioCodec, duration: duration, inputTimeScale: 90000 };
@@ -36,25 +43,28 @@ class MP3Demuxer implements Demuxer {
   }
 
   static probe (data) {
+    if (!data) {
+      return false;
+    }
+
     // check if data contains ID3 timestamp and MPEG sync word
-    let offset, length;
-    const id3Data = ID3.getID3Data(data, 0);
-    if (id3Data && ID3.getTimeStamp(id3Data) !== undefined) {
-      // Look for MPEG header | 1111 1111 | 111X XYZX | where X can be either 0 or 1 and Y or Z should be 1
-      // Layer bits (position 14 and 15) in header should be always different from 0 (Layer I or Layer II or Layer III)
-      // More info http://www.mp3-tech.org/programmer/frame_header.html
-      for (offset = id3Data.length, length = Math.min(data.length - 1, offset + 100); offset < length; offset++) {
-        if (MpegAudio.probe(data, offset)) {
-          logger.log('MPEG Audio sync word found !');
-          return true;
-        }
+    // Look for MPEG header | 1111 1111 | 111X XYZX | where X can be either 0 or 1 and Y or Z should be 1
+    // Layer bits (position 14 and 15) in header should be always different from 0 (Layer I or Layer II or Layer III)
+    // More info http://www.mp3-tech.org/programmer/frame_header.html
+    const id3Data = ID3.getID3Data(data, 0) || [];
+    let offset = id3Data.length;
+
+    for (let length = data.length; offset < length; offset++) {
+      if (MpegAudio.probe(data, offset)) {
+        logger.log('MPEG Audio sync word found !');
+        return true;
       }
     }
     return false;
   }
 
   // feed incoming data to the front of the parsing pipeline
-  demux (data, timeOffset) {
+  demux (data, timeOffset): DemuxerResult {
     if (this.cachedData.length) {
       data = appendUint8Array(this.cachedData, data);
       this.cachedData = new Uint8Array();
@@ -70,26 +80,25 @@ class MP3Demuxer implements Demuxer {
     const length = data.length;
 
     if (this.initPTS === null) {
-      this.initPTS = timestamp ? 90 * timestamp : timeOffset * 90000;
+      this.initPTS = Number.isFinite(timestamp) ? timestamp * 90 : timeOffset * 90000;
     }
 
     if (id3Data.length) {
       id3Track.samples.push({ pts: this.initPTS, dts: this.initPTS, data: id3Data });
     }
 
+    pts = this.initPTS;
+
     while (offset < length) {
       if (MpegAudio.canParse(data, offset)) {
         const frame = MpegAudio.appendFrame(track, data, offset, this.initPTS, this.frameIndex);
         if (frame) {
-          offset += frame.length;
-          pts = frame.sample.pts;
           this.frameIndex++;
+          pts = frame.sample.pts;
+          offset += frame.length;
           lastDataIndex = offset;
         } else {
-          const partialData = data.slice(offset);
-
-          this.cachedData = appendUint8Array(this.cachedData, partialData);
-          offset += partialData.length;
+          offset = length;
         }
       } else if (ID3.canParse(data, offset)) {
         id3Data = ID3.getID3Data(data, offset);
@@ -97,10 +106,7 @@ class MP3Demuxer implements Demuxer {
         offset += id3Data.length;
         lastDataIndex = offset;
       } else {
-        const partialData = data.slice(offset);
-
-        this.cachedData = appendUint8Array(this.cachedData, partialData);
-        offset += partialData.length;
+        offset++;
       }
       if (offset === length && lastDataIndex !== length) {
         const partialData = data.slice(lastDataIndex);
