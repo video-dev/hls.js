e063f6c83f7de467675b07a16d273ef58fc89550
Tom Jenkinson <tom@tjenkinson.me>
2019-09-28 10:49:35 +0100

    enable 'array-bracket-spacing' rule

--- tests/unit/controller/abr-controller.js
+++ tests/unit/controller/abr-controller.js
@@ -12,11 +12,11 @@ describe('AbrController', function () {
     const hls = new Hls({ maxStarvationDelay: 4 });
     hls.levelController._levels = [
       { bitrate: 105000, name: '144', details: { totalduration: 4, fragments: [{}] } },
-      { bitrate: 246440, name: '240', details: { totalduration: 10, fragments: [ {} ] } },
-      { bitrate: 460560, name: '380', details: { totalduration: 10, fragments: [ {} ] } },
-      { bitrate: 836280, name: '480', details: { totalduration: 10, fragments: [ {} ] } },
-      { bitrate: 2149280, name: '720', details: { totalduration: 10, fragments: [ {} ] } },
-      { bitrate: 6221600, name: '1080', details: { totalduration: 10, fragments: [ {} ] } }
+      { bitrate: 246440, name: '240', details: { totalduration: 10, fragments: [{}] } },
+      { bitrate: 460560, name: '380', details: { totalduration: 10, fragments: [{}] } },
+      { bitrate: 836280, name: '480', details: { totalduration: 10, fragments: [{}] } },
+      { bitrate: 2149280, name: '720', details: { totalduration: 10, fragments: [{}] } },
+      { bitrate: 6221600, name: '1080', details: { totalduration: 10, fragments: [{}] } }
     ];
     const abrController = new AbrController(hls);
     abrController.bwEstimator = new EwmaBandWidthEstimator(hls, 15, 4, 5e5);
