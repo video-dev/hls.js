dfc34b3475045fc49e0026813dfb60d220fc4ef4
Rob Walch <rob@jwplayer.com>
2019-09-29 19:00:58 -0400

    Update qa.jwplayer.com test stream urls

--- tests/test-streams.js
+++ tests/test-streams.js
@@ -151,7 +151,7 @@ module.exports = {
   }
   ),
   audioOnlyMultipleLevels: {
-    'url': 'https://s3.amazonaws.com/bob.jwplayer.com/~alex/121628/new_master.m3u8',
+    'url': 'https://s3.amazonaws.com/qa.jwplayer.com/~alex/121628/new_master.m3u8',
     'description': 'Multiple non-alternate audio levels',
     'live': false,
     'abr': false
@@ -173,7 +173,7 @@ module.exports = {
     description: 'One PDT, no discontinuities'
   },
   noTrackIntersection: {
-    url: 'https://s3.amazonaws.com/bob.jwplayer.com/%7Ealex/123633/new_master.m3u8',
+    url: 'https://s3.amazonaws.com/qa.jwplayer.com/~alex/123633/new_master.m3u8',
     description: 'Audio/video track PTS values do not intersect; 10 second start gap'
   },
   // altAudioNoVideoCodecSignaled: {
