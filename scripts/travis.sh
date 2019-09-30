4953002ab25b61d6ad1f8beb78a315207ea740a0
Tom Jenkinson <tom@tjenkinson.me>
2019-09-30 21:28:13 +0100

    run type check and types build on CI

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -11,7 +11,9 @@ if [ "${TRAVIS_MODE}" = "build" ]; then
   npm run lint
   echo "travis_fold:end:lint"
   echo "travis_fold:start:build"
+  npm run type-check
   npm run build
+  npm run build:types
   echo "travis_fold:end:build"
   # check that hls.js doesn't error if requiring in node
   # see https://github.com/video-dev/hls.js/pull/1642
@@ -44,7 +46,10 @@ elif [ "${TRAVIS_MODE}" = "release" ] || [ "${TRAVIS_MODE}" = "releaseCanary" ] 
   fi
   node ./scripts/set-package-version.js
   npm run lint
+  npm run type-check
   npm run build
+  npm run build:types
+  
   if [ "${TRAVIS_MODE}" != "netlifyPr" ]; then
     npm run test:unit
     if [[ $(node ./scripts/check-already-published.js) = "not published" ]]; then
