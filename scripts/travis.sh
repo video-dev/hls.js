8d40a9c49be2f337c3b3ff1461681dd7463f77ff
Tom Jenkinson <tom@tjenkinson.me>
2019-12-13 19:52:39 +0000

    Update travis.sh

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -47,6 +47,7 @@ elif [ "${TRAVIS_MODE}" = "release" ] || [ "${TRAVIS_MODE}" = "releaseCanary" ] 
     # make sure everything is fetched https://github.com/travis-ci/travis-ci/issues/3412
     git fetch --unshallow
   fi
+  git log
   git fetch --tags --all --verbose
   git tag --sort=-v:refname
   git tag
