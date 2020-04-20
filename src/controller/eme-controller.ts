ba421268d1bcd21cbb351704ada03ea35b985aaa
springuper <springuper@gmail.com>
2020-04-20 17:25:16 +0800

    Close media key sessions and clear media keys when media detached

--- src/controller/eme-controller.ts
+++ src/controller/eme-controller.ts
@@ -522,10 +522,32 @@ class EMEController extends EventHandler {
   }
 
   onMediaDetached () {
-    if (this._media) {
-      this._media.removeEventListener('encrypted', this._onMediaEncrypted);
+    const media = this._media;
+    if (!media) return;
+
+    media.removeEventListener('encrypted', this._onMediaEncrypted);
+
+    // Close all sessions and remove media keys from the video element.
+    Promise.all(this._mediaKeysList.map((mediaKeysListItem) => {
+      if (mediaKeysListItem.mediaKeysSession) {
+        try {
+          return mediaKeysListItem.mediaKeysSession.close();
+        } catch (ex) {
+          // Ignore errors when closing the sessions. Closing a session that
+          // generated no key requests will throw an error.
+        }
+      }
+    })).then(() => {
+      this._mediaKeysList = [];
+
+      try {
+        return media.setMediaKeys(null);
+      } catch (ex) {
+        // Ignore any failures while removing media keys from the video element.
+      }
+    }).then(() => {
       this._media = null; // release reference
-    }
+    });
   }
 
   // TODO: Use manifest types here when they are defined
