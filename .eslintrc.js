993ef82e35d8bdd0b470d31df09787a421bb6668
Tom Jenkinson <tom@tjenkinson.me>
2020-05-09 10:43:46 +0100

    feat: Format code by Prettier
    Co-Authored-By: Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,61 +1,27 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es6': true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
   },
-  'globals': {
+  globals: {
     // Allowed globals
-    'console': true,
+    console: true,
 
     // Compile-time defines
-    '__VERSION__': true,
-    '__USE_SUBTITLES__': true,
-    '__USE_ALT_AUDIO__': true,
-    '__USE_EME_DRM__': true
+    __VERSION__: true,
+    __USE_SUBTITLES__: true,
+    __USE_ALT_AUDIO__: true,
+    __USE_EME_DRM__: true,
   },
-  // see https://standardjs.com/
-  // see https://github.com/standard/eslint-config-standard
-  'extends': [
-    'eslint:recommended',
-    'standard'
-  ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'sourceType': 'module'
+  // see https://standardjs.com/, https://github.com/prettier/eslint-config-prettier, https://github.com/prettier/eslint-plugin-prettier
+  extends: ['eslint-config-prettier', 'standard', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
   },
-  'plugins': [
-    '@typescript-eslint'
-  ],
-  'rules': {
-    // our basic style rules
-    'semi': [
-      'error',
-      'always'
-    ],
-    'indent': [
-      'error',
-      2
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    // spacing
-    'space-infix-ops': 2,
-    'space-unary-ops': [2, { 'words': true, 'nonwords': false }],
-    'space-in-parens': ['error', 'never'],
-    'keyword-spacing': [2, { 'before': true, 'after': true }],
-    // enforce litteral objects on multiple lines
-    'block-spacing': 'error',
-    'curly': 2,
-    'object-curly-spacing': ['error', 'always'],
-    'brace-style': ['error', '1tbs', { 'allowSingleLine': false }],
-
+  plugins: ['@typescript-eslint'],
+  rules: {
     // limit code block and line length
     /*
     "max-len": 1,
@@ -73,28 +39,30 @@ module.exports = {
     // forbid "one var" style, enforce one declaration per variable
     'one-var': 2,
 
-    'no-restricted-globals': [2,
+    'no-restricted-globals': [
+      2,
       {
-        'name': 'window',
-        'message': 'Use `self` instead of `window` to access the global context everywhere (including workers).'
+        name: 'window',
+        message: 'Use `self` instead of `window` to access the global context everywhere (including workers).',
       },
       {
-        'name': 'SourceBuffer',
-        'message': 'Use `self.SourceBuffer`'
+        name: 'SourceBuffer',
+        message: 'Use `self.SourceBuffer`',
       },
       {
-        'name': 'setTimeout',
-        'message': 'Use `self.setTimeout`'
+        name: 'setTimeout',
+        message: 'Use `self.setTimeout`',
       },
       {
-        'name': 'setInterval',
-        'message': 'Use `self.setInterval`'
-      }
+        name: 'setInterval',
+        message: 'Use `self.setInterval`',
+      },
     ],
 
-    'no-restricted-properties': [2,
-      { 'property': 'findIndex' },  // Intended to block usage of Array.prototype.findIndex
-      { 'property': 'find' }        // Intended to block usage of Array.prototype.find
+    'no-restricted-properties': [
+      2,
+      { property: 'findIndex' }, // Intended to block usage of Array.prototype.findIndex
+      { property: 'find' }, // Intended to block usage of Array.prototype.find
     ],
 
     'standard/no-callback-literal': 1,
@@ -112,28 +80,25 @@ module.exports = {
     'no-undefined': 1,
     'no-global-assign': 2,
     'prefer-const': 2,
-    'dot-notation': 2,
-    'array-bracket-spacing': 2,
-    'quote-props': 2,
     'no-void': 0,
     'no-useless-catch': 2,
-    'lines-between-class-members': 2,
-    'no-prototype-builtins': 0
+    'no-prototype-builtins': 0,
   },
-  'overrides': [
+  overrides: [
     {
-      'files': ['*.ts'],
-      'rules': {
+      files: ['*.ts'],
+      rules: {
         'no-unused-vars': 0,
         '@typescript-eslint/no-unused-vars': 1,
         '@typescript-eslint/prefer-optional-chain': 2,
-        '@typescript-eslint/consistent-type-assertions': [ 2,
+        '@typescript-eslint/consistent-type-assertions': [
+          2,
           {
-            'assertionStyle': 'as',
-            'objectLiteralTypeAssertions': 'never'
-          }
-        ]
-      }
-    }
-  ]
+            assertionStyle: 'as',
+            objectLiteralTypeAssertions: 'never',
+          },
+        ],
+      },
+    },
+  ],
 };
