387dc59ec50651665fbd3923109b14e7d8de4a7d
Tom Jenkinson <tom@tjenkinson.me>
2020-05-11 18:25:29 +0100

    remove workaround for eventemitter3 v4.0.1
    it was fixed in 4.0.3

--- src/observer.ts
+++ src/observer.ts
@@ -3,7 +3,7 @@ import { EventEmitter } from 'eventemitter3';
 /**
  * Simple adapter sub-class of Nodejs-like EventEmitter.
  */
-export class Observer extends EventEmitter<any> {
+export class Observer extends EventEmitter {
   /**
    * We simply want to pass along the event-name itself
    * in every call to a handler, which is the purpose of our `trigger` method
