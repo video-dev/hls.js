55178302712d04f5c7913e25710ebd86084e3fc8
Rob Walch <rob@jwplayer.com>
2020-06-21 10:44:27 -0400

    Add semver meta tags to latest (pre-)release version for deployments

--- scripts/version-parser.js
+++ scripts/version-parser.js
@@ -4,10 +4,10 @@ const VALID_VERSION_REGEX = /^v\d+\.\d+\.\d+(?:-([a-zA-Z][0-9a-zA-Z-]*))?/;
 const STABLE_VERSION_REGEX = /^v\d+\.\d+\.\d+$/;
 
 module.exports = {
-  isValidVersion: function(version) {
+  isValidVersion: function (version) {
     return VALID_VERSION_REGEX.test(version);
   },
-  isValidStableVersion: function(version) {
+  isValidStableVersion: function (version) {
     return STABLE_VERSION_REGEX.test(version);
   },
   // extract what we should use as the npm dist-tag (https://docs.npmjs.com/cli/dist-tag)
@@ -15,11 +15,11 @@ module.exports = {
   // v1.2.3-beta => beta
   // v1.2.3-beta.1 => beta
   // v1.2.3 => latest
-  getVersionTag: function(version) {
+  getVersionTag: function (version) {
     const match = VALID_VERSION_REGEX.exec(version);
     if (!match) {
       throw new Error('Invalid version.');
     }
     return match[1] || 'latest';
   }
-}
+};
