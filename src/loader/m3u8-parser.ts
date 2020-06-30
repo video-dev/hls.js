3d6be17a7e463cabbf7b25df1fef2aa4e5db3678
Rob Walch <rob@jwplayer.com>
2020-06-30 19:13:03 -0400

    Cleanup manifest and playlist RegExp

--- src/loader/m3u8-parser.ts
+++ src/loader/m3u8-parser.ts
@@ -17,18 +17,34 @@ import { LevelAttributes } from '../types/level';
  */
 
 // https://regex101.com is your friend
-const MASTER_PLAYLIST_REGEX = /(?:#EXT-X-STREAM-INF:([^\n\r]*)[\r\n]+([^\r\n]+)|#EXT-X-SESSION-DATA:([^\n\r]*)[\r\n]+)/g;
+const MASTER_PLAYLIST_REGEX = /#EXT-X-STREAM-INF:([^\n\r]*)[\r\n]+([^\r\n]+)|#EXT-X-SESSION-DATA:([^\n\r]*)[\r\n]+/g;
 const MASTER_PLAYLIST_MEDIA_REGEX = /#EXT-X-MEDIA:(.*)/g;
 
 const LEVEL_PLAYLIST_REGEX_FAST = new RegExp([
   /#EXTINF:\s*(\d*(?:\.\d+)?)(?:,(.*)\s+)?/.source, // duration (#EXTINF:<duration>,<title>), group 1 => duration, group 2 => title
-  /|(?!#)([\S+ ?]+)/.source, // segment URI, group 3 => the URI (note newline is not eaten)
-  /|#EXT-X-BYTERANGE:*(.+)/.source, // next segment's byterange, group 4 => range spec (x@y)
-  /|#EXT-X-PROGRAM-DATE-TIME:(.+)/.source, // next segment's program date/time group 5 => the datetime spec
-  /|#.*/.source // All other non-segment oriented tags will match with all groups empty
-].join(''), 'g');
+  /(?!#)([\S+ ?]+)/.source, // segment URI, group 3 => the URI (note newline is not eaten)
+  /#EXT-X-BYTERANGE:*(.+)/.source, // next segment's byterange, group 4 => range spec (x@y)
+  /#EXT-X-PROGRAM-DATE-TIME:(.+)/.source, // next segment's program date/time group 5 => the datetime spec
+  /#.*/.source // All other non-segment oriented tags will match with all groups empty
+].join('|'), 'g');
 
-const LEVEL_PLAYLIST_REGEX_SLOW = /(?:(?:#(EXTM3U))|(?:#EXT-X-(PLAYLIST-TYPE):(.+))|(?:#EXT-X-(MEDIA-SEQUENCE): *(\d+))|(?:#EXT-X-(TARGETDURATION): *(\d+))|(?:#EXT-X-(KEY):(.+))|(?:#EXT-X-(START):(.+))|(?:#EXT-X-(ENDLIST))|(?:#EXT-X-(DISCONTINUITY-SEQ)UENCE: *(\d+))|(?:#EXT-X-(DIS)CONTINUITY))|(?:#EXT-X-(PREFETCH-DIS)CONTINUITY)|(?:#EXT-X-(PREFETCH):(.+))|(?:#EXT-X-(VERSION):(\d+))|(?:#EXT-X-(MAP):(.+))|(?:(#)([^:]*):(.*))|(?:(#)(.*))(?:.*)\r?\n?/;
+const LEVEL_PLAYLIST_REGEX_SLOW = new RegExp([
+  /#(EXTM3U)/.source,
+  /#EXT-X-(PLAYLIST-TYPE):(.+)/.source,
+  /#EXT-X-(MEDIA-SEQUENCE): *(\d+)/.source,
+  /#EXT-X-(TARGETDURATION): *(\d+)/.source,
+  /#EXT-X-(KEY):(.+)/.source,
+  /#EXT-X-(START):(.+)/.source,
+  /#EXT-X-(ENDLIST)/.source,
+  /#EXT-X-(DISCONTINUITY-SEQ)UENCE: *(\d+)/.source,
+  /#EXT-X-(DIS)CONTINUITY/.source,
+  /#EXT-X-(PREFETCH-DIS)CONTINUITY/.source, // TODO: deprecate LHLS
+  /#EXT-X-(PREFETCH):(.+)/.source, // TODO: deprecate LHLS
+  /#EXT-X-(VERSION):(\d+)/.source,
+  /#EXT-X-(MAP):(.+)/.source,
+  /(#)([^:]*):(.*)/.source,
+  /(#)(.*)(?:.*)\r?\n?/.source
+].join('|'));
 
 const MP4_REGEX_SUFFIX = /\.(mp4|m4s|m4v|m4a)$/i;
 
