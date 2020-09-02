b88cbb1f054a6bc0a01b2f9afd86d32543201363
Victor Chao <cchao@twitter.com>
2020-09-02 22:54:43 +0800

    Add CMAF closed captions support

---
+++ src/demux/dummy-demuxed-track.js
@@ -0,0 +1,11 @@
+export function dummyTrack () {
+  return {
+    type: '',
+    id: -1,
+    pid: -1,
+    inputTimeScale: 90000,
+    sequenceNumber: -1,
+    samples: [],
+    dropped: 0
+  };
+}
--- src/remux/passthrough-remuxer.js
+++ src/remux/passthrough-remuxer.js
@@ -38,8 +38,36 @@ class PassThroughRemuxer {
       nb: 1,
       dropped: 0
     });
+
+    if (textTrack && textTrack.samples && textTrack.samples.length) {
+      this.remuxText(textTrack);
+    }
+
     // notify end of parsing
     observer.trigger(Event.FRAG_PARSED);
+  }
+
+  remuxText (track) {
+    track.samples.sort(function (a, b) {
+      return (a.pts - b.pts);
+    });
+
+    const length = track.samples.length; let sample;
+    const inputTimeScale = track.timescale;
+    const initPTS = track.initPTS;
+    // consume samples
+    if (length && initPTS) {
+      for (let index = 0; index < length; index++) {
+        sample = track.samples[index];
+        // setting text pts, dts to relative time
+        // using this._initPTS and this._initDTS to calculate relative time
+        sample.pts = ((sample.pts - initPTS) / inputTimeScale);
+      }
+    }
+
+    this.observer.trigger(Event.FRAG_PARSING_USERDATA, {
+      samples: track.samples
+    });
   }
 }
 
