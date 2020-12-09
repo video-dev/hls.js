5275331ce1b8fb56a5f2032027650a2621a89ab0
Rob Walch <rob@jwplayer.com>
2020-12-08 19:16:04 -0500

    Copy workflow files into patch branch

--- scripts/set-package-version.js
+++ scripts/set-package-version.js
@@ -5,34 +5,33 @@ const versionParser = require('./version-parser.js');
 const packageJson = require('../package.json');
 const { isValidStableVersion, incrementPatch } = require('./version-parser.js');
 
-const TRAVIS_MODE = process.env.TRAVIS_MODE;
 const latestVersion = getLatestVersionTag();
 let newVersion = '';
 
 try {
-  if (TRAVIS_MODE === 'release') {
+  if (process.env.TAG) {
     // write the version field in the package json to the version in the git tag
-    const tag = process.env.TRAVIS_TAG;
+    const tag = process.env.TAG;
     if (!versionParser.isValidVersion(tag)) {
       throw new Error(`Unsupported tag for release: "${tag}"`);
     }
     // remove v
     newVersion = tag.substring(1);
-    if (!versionParser.isDefinitelyGreaterThanAlphas(newVersion)) {
-      // 1.2.3-0.alpha.500
-      // 1.2.3-0.alpha.501
-      // 1.2.3-0.aaalpha.custom => bad
-      // 1.2.3-0.aaalpha.custom.0.alpha.503 => now lower than 1.2.3-0.alpha.501
-      throw new Error(`It's possible that "${newVersion}" has a lower precedense than an existing alpha version which is not allowed.`);
+    if (!versionParser.isDefinitelyGreaterThanCanaries(newVersion)) {
+      // 1.2.3-0.canary.500
+      // 1.2.3-0.canary.501
+      // 1.2.3-0.caaanary.custom => bad
+      // 1.2.3-0.caaanary.custom.0.canary.503 => now lower than 1.2.3-0.canary.501
+      throw new Error(`It's possible that "${newVersion}" has a lower precedense than an existing canary version which is not allowed.`);
     }
-  } else if (TRAVIS_MODE === 'releaseAlpha' || TRAVIS_MODE === 'netlifyPr' || TRAVIS_MODE === 'netlifyBranch') {
+  } else {
     // bump patch in version from latest git tag
     let intermediateVersion = latestVersion;
     const isStable = isValidStableVersion(intermediateVersion);
 
     // if last git tagged version is a prerelease we should append `.0.<type>.<commit num>`
     // if the last git tagged version is a stable version then we should append `-0.<type>.<commit num>` and increment the patch
-    // `type` can be `pr`, `branch`, or `alpha`
+    // `type` can be `pr`, `branch`, or `canary`
     if (isStable) {
       intermediateVersion = incrementPatch(intermediateVersion);
     }
@@ -40,15 +39,13 @@ try {
     // remove v
     intermediateVersion = intermediateVersion.substring(1);
 
-    const suffix = TRAVIS_MODE === 'netlifyPr'
+    const suffix = process.env.NETLIFY && process.env.CONTEXT === 'deploy-preview'
       ? `pr.${process.env.REVIEW_ID/* set by netlify */}.${getCommitHash().substr(0, 8)}`
-      : TRAVIS_MODE === 'netlifyBranch'
+      : process.env.NETLIFY && process.env.CONTEXT === 'branch-deploy'
         ? `branch.${process.env.BRANCH/* set by netlify */.replace(/[^a-zA-Z0-9]/g, '-')}.${getCommitHash().substr(0, 8)}`
-        : `0.alpha.${getCommitNum()}`;
+        : `0.canary.${getCommitNum()}`;
 
     newVersion = `${intermediateVersion}${isStable ? '-' : '.'}${suffix}`;
-  } else {
-    throw new Error('Unsupported travis mode: ' + TRAVIS_MODE);
   }
 
   if (!versionParser.isGreaterOrEqual(newVersion, latestVersion)) {
