eff877cee0e7848f2c58c45b7d60643991b0007c
Rob Walch <rob@jwplayer.com>
2020-12-20 23:36:03 -0500

    Improve remuxing of non-independent segments and backtracking Fixes #2808

--- src/types/fragment-tracker.ts
+++ src/types/fragment-tracker.ts
@@ -1,11 +1,17 @@
-import Fragment, { Part } from '../loader/fragment';
+// eslint-disable-next-line import/no-duplicates
+import type Fragment from '../loader/fragment';
+// eslint-disable-next-line import/no-duplicates
+import type { Part } from '../loader/fragment';
 import type { SourceBufferName } from './buffer';
+import type { FragLoadedData } from './events';
 
 export interface FragmentEntity {
   body: Fragment,
-  part: Part | null,
+  part: Part | null
+  loaded: FragLoadedData | null,
+  backtrack: FragLoadedData | null,
+  buffered: boolean,
   range: { [key in SourceBufferName]: FragmentBufferedRange }
-  buffered: boolean
 }
 
 export interface FragmentTimeRange {
