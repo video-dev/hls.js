76e11d5fa28f7622b0470d2f266028304426dcef
matamatanot <matamatanot@gmail.com>
2020-12-31 08:09:57 +0900

    chore: replace

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     es6: true,
     mocha: true
   },
-  plugins: ['mocha'],
+  plugins: ['mocha', 'node'],
   globals: {
     // Test globals
     after: false,
@@ -22,12 +22,12 @@ module.exports = {
     'one-var': 0,
     'no-undefined': 0,
     'no-unused-expressions': 0,
-    'no-restricted-modules': ['error', 'assert'],
     'no-restricted-properties': [2,
       { property: 'findIndex' }, // Intended to block usage of Array.prototype.findIndex
       { property: 'find' }, // Intended to block usage of Array.prototype.find
       { property: 'only' } // Intended to block usage of it.only in commits
     ],
+    'node/no-restricted-require': ['error', ['assert']],
     'mocha/no-mocha-arrows': 2
   }
 };
