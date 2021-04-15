84ff5560536579bb773258db31882d61cfbc4757
Rob Walch <rob@jwplayer.com>
2021-04-15 13:44:44 -0400

    Fix AES-128 in streams with alt-audio tracks Fixes #3772 (v1.0 regression)

--- src/controller/audio-stream-controller.ts
+++ src/controller/audio-stream-controller.ts
@@ -344,11 +344,7 @@ class AudioStreamController
     }
 
     if (frag.decryptdata?.keyFormat === 'identity' && !frag.decryptdata?.key) {
-      this.log(
-        `Loading key for ${frag.sn} of [${trackDetails.startSN} ,${trackDetails.endSN}],track ${trackId}`
-      );
-      this.state = State.KEY_LOADING;
-      hls.trigger(Events.KEY_LOADING, { frag });
+      this.loadKey(frag, trackDetails);
     } else {
       this.loadFragment(frag, trackDetails, targetBufferTime);
     }
