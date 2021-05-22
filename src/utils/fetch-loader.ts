f6f67ea52a7b628551644e4b72ccbcc7ee69057b
softworkz <softworkz@hotmail.com>
2021-05-22 17:34:12 +0200

    Fix exception in fetch-loader error handler

--- src/utils/fetch-loader.ts
+++ src/utils/fetch-loader.ts
@@ -152,12 +152,10 @@ class FetchLoader implements Loader<LoaderContext> {
           return;
         }
         // CORS errors result in an undefined code. Set it to 0 here to align with XHR's behavior
-        const code = error.code || 0;
-        callbacks.onError(
-          { code, text: error.message },
-          context,
-          error.details
-        );
+        // when destroying, 'error' itself can be undefined
+        const code: number = !error ? 0 : error.code || 0;
+        const text: string = !error ? null : error.message;
+        callbacks.onError({ code, text }, context, error.details);
       });
   }
 
