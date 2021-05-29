2a9afc0f3c4b17f0c626cee0ae41e76d90ae9e34
Rob Walch <rob@jwplayer.com>
2021-05-29 11:25:00 -0400

    Improve subtitle fragment finder for gaps in live playlists

--- src/controller/fragment-finders.ts
+++ src/controller/fragment-finders.ts
@@ -63,7 +63,9 @@ export function findFragmentByPTS(
   let fragNext: Fragment | null = null;
   if (fragPrevious) {
     fragNext =
-      fragments[(fragPrevious.sn as number) - (fragments[0].sn as number) + 1];
+      fragments[
+        (fragPrevious.sn as number) - (fragments[0].sn as number) + 1
+      ] || null;
   } else if (bufferEnd === 0 && fragments[0].start === 0) {
     fragNext = fragments[0];
   }
