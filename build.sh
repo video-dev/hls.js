b6ba5b6819bf559d91135376e386f188e8e51956
PooleCool <101667390+5THforLIFE@users.noreply.github.com>
2022-06-28 02:40:01 -0400

    5th
    ok2

---
+++ build.sh
@@ -0,0 +1,3 @@
+/openpkg/bin/gcc -o mtools/SunOS-5.10-i386/msend -lsocket -lnsl msend.c
+/openpkg/bin/gcc -o mtools/SunOS-5.10-i386/mdump -lsocket -lnsl mdump.c
+/openpkg/bin/gcc -o mtools/SunOS-5.10-i386/mpong -lsocket -lnsl -lm mpong.c
