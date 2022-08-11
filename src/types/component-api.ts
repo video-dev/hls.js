9d628fc6b51d0b94ffc9e974c97f8065859849f9
Luke Pommersheim <lpommers@gmail.com>
2022-08-10 21:33:37 -0400

    create an interface for abr controllers

--- src/types/component-api.ts
+++ src/types/component-api.ts
@@ -1,5 +1,13 @@
+import EwmaBandWidthEstimator from '../utils/ewma-bandwidth-estimator';
+
 export interface ComponentAPI {
   destroy(): void;
+}
+
+export interface AbrComponentAPI extends ComponentAPI {
+  nextAutoLevel: Number;
+  clearTimer(): void;
+  readonly bwEstimator?: EwmaBandWidthEstimator;
 }
 
 export interface NetworkComponentAPI extends ComponentAPI {
