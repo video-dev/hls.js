video-dev/hls.js

commit 89ffc7132c537222105ac75a2f1da76062b4736a

Load segments in order even when first playlist segment duration is incorrect or is greater than media segment duration (#4864)
#4828

src/controller/base-stream-controller.ts
@@ -502,6 +502,16 @@ export default class BaseStreamController
       }`
     );
     this.state = State.IDLE;
+    if (!media) {
+      return;
+    }
+    if (
+      !this.loadedmetadata &&
+      media.buffered.length &&
+      this.fragCurrent === this.fragPrevious
+    ) {
+      this.loadedmetadata = true;
+    }
     this.tick();
   }
 
@@ -1015,12 +1025,12 @@ export default class BaseStreamController
 
     if (frag) {
       const curSNIdx = frag.sn - levelDetails.startSN;
-      const sameLevel = fragPrevious && frag.level === fragPrevious.level;
-      const nextFrag = fragments[curSNIdx + 1];
       if (fragPrevious && frag.sn === fragPrevious.sn && !loadingParts) {
         // Force the next fragment to load if the previous one was already selected. This can occasionally happen with
         // non-uniform fragment durations
+        const sameLevel = fragPrevious && frag.level === fragPrevious.level;
         if (sameLevel) {
+          const nextFrag = fragments[curSNIdx + 1];
           if (
             frag.sn < endSN &&
             this.fragmentTracker.getState(nextFrag) !== FragmentState.OK

src/remux/passthrough-remuxer.ts
@@ -32,17 +32,17 @@ class PassThroughRemuxer implements Remuxer {
   private initData?: InitData;
   private initPTS?: number;
   private initTracks?: TrackSet;
-  private lastEndDTS: number | null = null;
+  private lastEndTime: number | null = null;
 
   public destroy() {}
 
   public resetTimeStamp(defaultInitPTS) {
     this.initPTS = defaultInitPTS;
-    this.lastEndDTS = null;
+    this.lastEndTime = null;
   }
 
   public resetNextTimestamp() {
-    this.lastEndDTS = null;
+    this.lastEndTime = null;
   }
 
   public resetInitSegment(
@@ -117,7 +117,7 @@ class PassThroughRemuxer implements Remuxer {
     textTrack: DemuxedUserdataTrack,
     timeOffset: number
   ): RemuxerResult {
-    let { initPTS, lastEndDTS } = this;
+    let { initPTS, lastEndTime } = this;
     const result: RemuxerResult = {
       audio: undefined,
       video: undefined,
@@ -129,8 +129,8 @@ class PassThroughRemuxer implements Remuxer {
     // If we haven't yet set a lastEndDTS, or it was reset, set it to the provided timeOffset. We want to use the
     // lastEndDTS over timeOffset whenever possible; during progressive playback, the media source will not update
     // the media duration (which is what timeOffset is provided as) before we need to process the next chunk.
-    if (!Number.isFinite(lastEndDTS!)) {
-      lastEndDTS = this.lastEndDTS = timeOffset || 0;
+    if (!Number.isFinite(lastEndTime!)) {
+      lastEndTime = this.lastEndTime = timeOffset || 0;
     }
 
     // The binary segment data is added to the videoTrack in the mp4demuxer. We don't check to see if the data is only
@@ -159,20 +159,20 @@ class PassThroughRemuxer implements Remuxer {
       this.emitInitSegment = false;
     }
 
+    const startDTS = getStartDTS(initData, data);
     if (!Number.isFinite(initPTS!)) {
-      this.initPTS =
-        initSegment.initPTS =
-        initPTS =
-          computeInitPTS(initData, data, lastEndDTS);
+      this.initPTS = initSegment.initPTS = initPTS = startDTS - timeOffset;
     }
 
     const duration = getDuration(data, initData);
-    const startDTS = lastEndDTS as number;
-    const endDTS = duration + startDTS;
+    const startTime = audioTrack
+      ? startDTS - (initPTS as number)
+      : (lastEndTime as number);
+    const endTime = startTime + duration;
     offsetStartDTS(initData, data, initPTS as number);
 
     if (duration > 0) {
-      this.lastEndDTS = endDTS;
+      this.lastEndTime = endTime;
     } else {
       logger.warn('Duration parsed from mp4 should be greater than zero');
       this.resetNextTimestamp();
@@ -192,10 +192,10 @@ class PassThroughRemuxer implements Remuxer {
 
     const track: RemuxedTrack = {
       data1: data,
-      startPTS: startDTS,
-      startDTS,
-      endPTS: endDTS,
-      endDTS,
+      startPTS: startTime,
+      startDTS: startTime,
+      endPTS: endTime,
+      endDTS: endTime,
       type,
       hasAudio,
       hasVideo,
@@ -225,9 +225,6 @@ class PassThroughRemuxer implements Remuxer {
     return result;
   }
 }
-
-const computeInitPTS = (initData, data, timeOffset) =>
-  getStartDTS(initData, data) - timeOffset;
 
 function getParsedTrackCodec(
   track: InitDataTrack | undefined,

commit 1023132fb0eb8edb5787c808f3a6f8ef79253a8d

Fix seek to live start regression introduced in #4864

src/controller/base-stream-controller.ts
@@ -524,9 +524,12 @@ export default class BaseStreamController
       this.fragCurrent === this.fragPrevious
     ) {
       this.loadedmetadata = true;
+      this.seekToStartPos();
     }
     this.tick();
   }
+
+  protected seekToStartPos() {}
 
   protected _handleFragmentLoadComplete(fragLoadedEndData: PartsLoadedData) {
     const { transmuxer } = this;

src/controller/stream-controller.ts
@@ -917,13 +917,7 @@ export default class StreamController
       return;
     }
 
-    // Check combined buffer
-    const buffered = BufferHelper.getBuffered(media);
-
-    if (!this.loadedmetadata && buffered.length) {
-      this.loadedmetadata = true;
-      this.seekToStartPos();
-    } else {
+    if (this.loadedmetadata || !BufferHelper.getBuffered(media).length) {
       // Resolve gaps using the main buffer, whose ranges are the intersections of the A/V sourcebuffers
       const activeFrag = this.state !== State.IDLE ? this.fragCurrent : null;
       gapController.poll(this.lastCurrentTime, activeFrag);
@@ -972,9 +966,8 @@ export default class StreamController
 
   /**
    * Seeks to the set startPosition if not equal to the mediaElement's current time.
-   * @private
    */
-  private seekToStartPos() {
+  protected seekToStartPos() {
     const { media } = this;
     if (!media) {
       return;

tests/unit/controller/stream-controller.ts
@@ -377,11 +377,19 @@ describe('StreamController', function () {
       streamController['checkBuffer']();
     });
 
-    it('should seek to start pos when metadata has not yet been loaded', function () {
+    it('should seek to start pos when data is first loaded', function () {
+      const firstFrag = new Fragment(PlaylistLevelType.MAIN, '');
+      firstFrag.duration = 5.0;
+      firstFrag.level = 1;
+      firstFrag.start = 0;
+      firstFrag.sn = 1;
+      firstFrag.cc = 0;
       // @ts-ignore
       const seekStub = sandbox.stub(streamController, 'seekToStartPos');
       streamController['loadedmetadata'] = false;
-      streamController['checkBuffer']();
+      streamController['fragCurrent'] = streamController['fragPrevious'] =
+        firstFrag;
+      streamController['fragBufferedComplete'](firstFrag, null);
       expect(seekStub).to.have.been.calledOnce;
       expect(streamController['loadedmetadata']).to.be.true;
     });