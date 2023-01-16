01c48d6ca5c6405c4c001a5e10c6f1584f02256f
Rob Walch <rob@jwplayer.com>
2023-01-15 21:47:54 -0800

    Revert "Documentation - Add interface for AbrController (#4842)"
    This reverts commit 617d78bcb7776250ede8f7f2f0ea23830d4953a7.

--- src/types/component-api.ts
+++ src/types/component-api.ts
@@ -1,12 +1,5 @@
-import EwmaBandWidthEstimator from '../utils/ewma-bandwidth-estimator';
-
 export interface ComponentAPI {
   destroy(): void;
-}
-
-export interface AbrComponentAPI extends ComponentAPI {
-  nextAutoLevel: Number;
-  readonly bwEstimator?: EwmaBandWidthEstimator;
 }
 
 export interface NetworkComponentAPI extends ComponentAPI {
