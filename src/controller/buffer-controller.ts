7733355c5dcaf450e6f1a0e5971434db0ca07c55
Rob Walch <robwalch@users.noreply.github.com>
2023-01-16 21:04:00 -0800

    Light build optimizations and fixes (#5160)

--- src/controller/buffer-controller.ts
+++ src/controller/buffer-controller.ts
@@ -134,7 +134,7 @@ export default class BufferController implements ComponentAPI {
     // in case alt audio is not used, only one BUFFER_CODEC event will be fired from main stream controller
     // it will contain the expected nb of source buffers, no need to compute it
     let codecEvents: number = 2;
-    if ((data.audio && !data.video) || !data.altAudio) {
+    if ((data.audio && !data.video) || !data.altAudio || !__USE_ALT_AUDIO__) {
       codecEvents = 1;
     }
     this.bufferCodecEventsExpected = this._bufferCodecEventsTotal = codecEvents;
