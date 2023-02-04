26b4f2c9179e3b81ee114ae6f1e77b1e51e1f17f
Rob Walch <rwalch@apple.com>
2023-02-03 21:22:10 -0800

    Cleanup HLS attribute RegEx

--- src/utils/attr-list.ts
+++ src/utils/attr-list.ts
@@ -1,5 +1,5 @@
-const DECIMAL_RESOLUTION_REGEX = /^(\d+)x(\d+)$/; // eslint-disable-line no-useless-escape
-const ATTR_LIST_REGEX = /\s*(.+?)\s*=((?:\".*?\")|.*?)(?:,|$)/g; // eslint-disable-line no-useless-escape
+const DECIMAL_RESOLUTION_REGEX = /^(\d+)x(\d+)$/;
+const ATTR_LIST_REGEX = /(.+?)=(".*?"|.*?)(?:,|$)/g;
 
 // adapted from https://github.com/kanongil/node-m3u8parse/blob/master/attrlist.js
 export class AttrList {
@@ -103,8 +103,8 @@ export class AttrList {
       ) {
         value = value.slice(1, -1);
       }
-
-      attrs[match[1]] = value;
+      const name = match[1].trim();
+      attrs[name] = value;
     }
     return attrs;
   }
