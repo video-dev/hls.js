video-dev/hls.js

commit 08fa93599c7cd5fec31533dd6c2c470be14b1e80

Fix currentLevel and FRAG_CHANGED reporting when seeking back from edge in Low-Latency streams (#5102)
Related to #5085 and #5077

src/controller/fragment-tracker.ts
@@ -23,7 +23,7 @@ export enum FragmentState {
 }
 
 export class FragmentTracker implements ComponentAPI {
-  private activeFragment: Fragment | null = null;
+  private mainFragEntity: FragmentEntity | null = null;
   private activeParts: Part[] | null = null;
   private endListFragments: { [key in PlaylistLevelType]?: FragmentEntity } =
     Object.create(null);
@@ -65,7 +65,7 @@ export class FragmentTracker implements ComponentAPI {
       // @ts-ignore
       this.endListFragments =
       this.timeRanges =
-      this.activeFragment =
+      this.mainFragEntity =
       this.activeParts =
         null;
   }
@@ -79,34 +79,33 @@ export class FragmentTracker implements ComponentAPI {
     levelType: PlaylistLevelType
   ): Fragment | Part | null {
     if (levelType === PlaylistLevelType.MAIN) {
-      const { activeFragment, activeParts } = this;
-      if (!activeFragment) {
-        return null;
-      }
-      if (activeParts) {
-        for (let i = activeParts.length; i--; ) {
-          const activePart = activeParts[i];
-          const appendedPTS = activePart
-            ? activePart.end
-            : activeFragment.appendedPTS;
-          if (
-            activePart.start <= position &&
-            appendedPTS !== undefined &&
-            position <= appendedPTS
-          ) {
-            // 9 is a magic number. remove parts from lookup after a match but keep some short seeks back.
-            if (i > 9) {
-              this.activeParts = activeParts.slice(i - 9);
+      const { mainFragEntity, activeParts } = this;
+      if (mainFragEntity) {
+        if (mainFragEntity && activeParts) {
+          for (let i = activeParts.length; i--; ) {
+            const activePart = activeParts[i];
+            const appendedPTS = activePart
+              ? activePart.end
+              : mainFragEntity.appendedPTS;
+            if (
+              activePart.start <= position &&
+              appendedPTS !== null &&
+              position <= appendedPTS
+            ) {
+              // 9 is a magic number. remove parts from lookup after a match but keep some short seeks back.
+              if (i > 9) {
+                this.activeParts = activeParts.slice(i - 9);
+              }
+              return activePart;
             }
-            return activePart;
           }
+        } else if (
+          mainFragEntity.body.start <= position &&
+          mainFragEntity.appendedPTS !== null &&
+          position <= mainFragEntity.appendedPTS
+        ) {
+          return mainFragEntity.body;
         }
-      } else if (
-        activeFragment.start <= position &&
-        activeFragment.appendedPTS !== undefined &&
-        position <= activeFragment.appendedPTS
-      ) {
-        return activeFragment;
       }
     }
     return this.getBufferedFrag(position, levelType);
@@ -362,6 +361,7 @@ export class FragmentTracker implements ComponentAPI {
     const fragKey = getFragmentKey(frag);
     this.fragments[fragKey] = {
       body: frag,
+      appendedPTS: null,
       loaded: data,
       buffered: false,
       range: Object.create(null),
@@ -373,10 +373,23 @@ export class FragmentTracker implements ComponentAPI {
     data: BufferAppendedData
   ) {
     const { frag, part, timeRanges } = data;
+    let mainFragEntity = this.mainFragEntity;
     if (frag.type === PlaylistLevelType.MAIN) {
-      if (this.activeFragment !== frag) {
-        this.activeFragment = frag;
-        frag.appendedPTS = undefined;
+      const lastMainFrag = mainFragEntity ? mainFragEntity.body : null;
+      if (lastMainFrag !== frag) {
+        if (mainFragEntity && lastMainFrag && lastMainFrag.sn !== frag.sn) {
+          // archive frag for getBufferedFrag()
+          mainFragEntity.buffered = true;
+          this.fragments[getFragmentKey(lastMainFrag)] = mainFragEntity;
+        }
+        const fragKey = getFragmentKey(frag);
+        mainFragEntity = this.mainFragEntity = this.fragments[fragKey] || {
+          body: frag,
+          appendedPTS: null,
+          loaded: null,
+          buffered: false,
+          range: Object.create(null),
+        };
       }
       if (part) {
         let activeParts = this.activeParts;
@@ -393,7 +406,7 @@ export class FragmentTracker implements ComponentAPI {
     Object.keys(timeRanges).forEach((elementaryStream: SourceBufferName) => {
       const timeRange = timeRanges[elementaryStream] as TimeRanges;
       this.detectEvictedFragments(elementaryStream, timeRange);
-      if (!part && frag.type === PlaylistLevelType.MAIN) {
+      if (!part && mainFragEntity) {
         const streamInfo = frag.elementaryStreams[elementaryStream];
         if (!streamInfo) {
           return;
@@ -401,9 +414,12 @@ export class FragmentTracker implements ComponentAPI {
         for (let i = 0; i < timeRange.length; i++) {
           const rangeEnd = timeRange.end(i);
           if (rangeEnd <= streamInfo.endPTS && rangeEnd > streamInfo.startPTS) {
-            frag.appendedPTS = Math.max(rangeEnd, frag.appendedPTS || 0);
+            mainFragEntity.appendedPTS = Math.max(
+              rangeEnd,
+              mainFragEntity.appendedPTS || 0
+            );
           } else {
-            frag.appendedPTS = streamInfo.endPTS;
+            mainFragEntity.appendedPTS = streamInfo.endPTS;
           }
         }
       }
@@ -446,7 +462,9 @@ export class FragmentTracker implements ComponentAPI {
     const fragKey = getFragmentKey(fragment);
     fragment.stats.loaded = 0;
     fragment.clearElementaryStreamInfo();
-    fragment.appendedPTS = undefined;
+    if (this.mainFragEntity === this.fragments[fragKey]) {
+      this.mainFragEntity = null;
+    }
     delete this.fragments[fragKey];
     if (fragment.endList) {
       delete this.endListFragments[fragment.type];
@@ -456,7 +474,7 @@ export class FragmentTracker implements ComponentAPI {
   public removeAllFragments() {
     this.fragments = Object.create(null);
     this.endListFragments = Object.create(null);
-    this.activeFragment = null;
+    this.mainFragEntity = null;
     this.activeParts = null;
   }
 }

src/types/fragment-tracker.ts
@@ -4,6 +4,9 @@ import type { FragLoadedData } from './events';
 
 export interface FragmentEntity {
   body: Fragment;
+  // appendedPTS is the latest buffered presentation time within the fragment's time range.
+  // It is used to determine: which fragment is appended at any given position, and hls.currentLevel.
+  appendedPTS: number | null;
   loaded: FragLoadedData | null;
   buffered: boolean;
   range: { [key in SourceBufferName]: FragmentBufferedRange };

src/controller/level-helper.ts
@@ -199,7 +199,6 @@ export function mergeDetails(
       ) {
         newFrag.start = newFrag.startPTS = oldFrag.startPTS as number;
         newFrag.startDTS = oldFrag.startDTS;
-        newFrag.appendedPTS = oldFrag.appendedPTS;
         newFrag.maxStartPTS = oldFrag.maxStartPTS;
 
         newFrag.endPTS = oldFrag.endPTS;

src/loader/fragment.ts
@@ -120,8 +120,6 @@ export class Fragment extends BaseSegment {
   public startPTS?: number;
   // The ending Presentation Time Stamp (PTS) of the fragment. Set after transmux complete.
   public endPTS?: number;
-  // The latest Presentation Time Stamp (PTS) appended to the buffer.
-  public appendedPTS?: number;
   // The starting Decode Time Stamp (DTS) of the fragment. Set after transmux complete.
   public startDTS!: number;
   // The ending Decode Time Stamp (DTS) of the fragment. Set after transmux complete.

commit 770b0aeadf05bb0ac034fec0b1dbca6cbc21c893

Patch/v1.4.4 doris (#82)
* reference v1 jsdelivr url in v1 api doc

people should be using that over latest so that things don't break when we release v2 with potentially breaking changes. Main readme already used v1 url

* chore(deps): update dependency @rollup/plugin-alias to v4.0.4

* chore(deps): update dependency @microsoft/api-documenter to v7.21.7

* chore(deps): update dependency @rollup/plugin-typescript to v11.1.0

* chore(deps): update dependency @rollup/plugin-node-resolve to v15.0.2

* chore(deps): update dependency wrangler to v2.15.0

* chore(deps): update dependency @rollup/plugin-alias to v5

* Fix readme logos & add Mux logo (#5390)

* docs: replace broken logo with Mux

* docs: fix TED logo

* docs: replace broken logos

* chore(deps): update dependency lint-staged to v13.2.1

* chore(deps): update dependency typescript to v5.0.4

* chore(deps): update dependency eslint to v8.38.0

* chore(deps): update dependency es-check to v7.1.1

* chore(deps): update dependency @rollup/plugin-terser to v0.4.1

* chore(deps): update typescript-eslint monorepo to v5.58.0

* chore(deps): update dependency semver to v7.4.0

* chore(deps): update dependency @rollup/plugin-commonjs to v24.1.0

* chore(deps): update dependency semver to v7.5.0

* chore(deps): update typescript-eslint monorepo to v5.59.0

* Setup npm provenance statements (#5406)

* give publish job permissions for npm provenance

* add `—provenance` flag to `npm publish`

* remove unneeded `contents` permission

* chore(deps): update dependency chromedriver to v112 (#5393)

Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
Co-authored-by: Tom Jenkinson <tjenkinson@users.noreply.github.com>

* set `internalChecksAsSuccess` renovate option

* chore(deps): update dependency wrangler to v2.15.1

* set renovate `prNotPendingHours` to 0

* chore(deps): update dependency wrangler to v2.16.0

* chore(deps): update dependency rollup to v3.20.3

* chore(deps): update dependency rollup to v3.20.7

* chore(deps): update dependency rollup to v3.21.0

* chore(deps): update tjenkinson/gh-action-auto-merge-dependency-updates digest to 01b22a9 (#5324)

Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

* tweak renovate options again

see https://github.com/renovatebot/renovate/discussions/21720

* reset remainderData in resetContiguity (#5410)

* docs: add Media Chrome for custom UI (#5391)

* Perform playlist retries before redundant failover and level switch (#5420)

(Pathway switch is still prioritized over retries when available)
Fixes #5419

* chore(deps): update dependency sinon to v15.0.4

* chore(deps): update dependency karma-chrome-launcher to v3.2.0

* chore(deps): update dependency selenium-webdriver to v4.9.0

* Update audio-track-controller.ts (#5431)

Sometimes STABLE-RENDITION_ID of tracks is undefined. Adding a guard for it, otherwise audio track doesn't switch.

* chore(deps): update dependency eslint to v8.39.0

* chore(deps): update dependency karma to v6.4.2

* chore(deps): update dependency prettier to v2.8.8

* Fix incorrect HEVC main profile codec id (#5438)

* fix: move `types` condition to the front (#5439)

* chore(deps): update dependency wrangler to v2.17.0

* chore(deps): update typescript-eslint monorepo to v5.59.1

* chore(deps): update typescript-eslint monorepo to v5.59.2

* Fix Low-Latency part and fragment tracking (#5423)

* Fix Low-Latency part and fragment tracking regression introduced in #5102

build-config.js
@@ -296,6 +296,11 @@ const configs = Object.entries({
     format: FORMAT.umd,
     minified: true,
   }),
+  lightEsm: buildRollupConfig({
+    type: BUILD_TYPE.light,
+    format: FORMAT.esm,
+    minified: false,
+  }),
   worker: {
     input: './src/demux/transmuxer-worker.ts',
     onwarn: (e) => {

src/controller/audio-stream-controller.ts
@@ -660,8 +660,12 @@ class AudioStreamController
     const { frag, part } = data;
     if (frag.type !== PlaylistLevelType.AUDIO) {
       if (!this.loadedmetadata && frag.type === PlaylistLevelType.MAIN) {
-        if ((this.videoBuffer || this.media)?.buffered.length) {
-          this.loadedmetadata = true;
+        const bufferable = this.videoBuffer || this.media;
+        if (bufferable) {
+          const bufferedTimeRanges = BufferHelper.getBuffered(bufferable);
+          if (bufferedTimeRanges.length) {
+            this.loadedmetadata = true;
+          }
         }
       }
       return;

src/controller/base-playlist-controller.ts
@@ -316,6 +316,9 @@ export default class BasePlaylistController implements NetworkComponentAPI {
           action === NetworkErrorAction.SendAlternateToPenaltyBox));
     if (retry) {
       this.requestScheduled = -1;
+      if (retryCount >= retryConfig.maxNumRetry) {
+        return false;
+      }
       if (isTimeout && errorEvent.context?.deliveryDirectives) {
         // The LL-HLS request already timed out so retry immediately
         this.warn(

src/controller/error-controller.ts
@@ -72,6 +72,7 @@ export default class ErrorController implements NetworkComponentAPI {
     const hls = this.hls;
     hls.on(Events.ERROR, this.onError, this);
     hls.on(Events.MANIFEST_LOADING, this.onManifestLoading, this);
+    hls.on(Events.LEVEL_UPDATED, this.onLevelUpdated, this);
   }
 
   private unregisterListeners() {
@@ -82,6 +83,7 @@ export default class ErrorController implements NetworkComponentAPI {
     hls.off(Events.ERROR, this.onError, this);
     hls.off(Events.ERROR, this.onErrorOut, this);
     hls.off(Events.MANIFEST_LOADING, this.onManifestLoading, this);
+    hls.off(Events.LEVEL_UPDATED, this.onLevelUpdated, this);
   }
 
   destroy() {
@@ -106,6 +108,10 @@ export default class ErrorController implements NetworkComponentAPI {
   private onManifestLoading() {
     this.playlistError = 0;
     this.penalizedRenditions = {};
+  }
+
+  private onLevelUpdated() {
+    this.playlistError = 0;
   }
 
   private onError(event: Events.ERROR, data: ErrorData) {
@@ -255,21 +261,12 @@ export default class ErrorController implements NetworkComponentAPI {
         retryCount,
       };
     }
-    // Do not perform level switch if an error occurred using delivery directives
-    // Allow reload without directives (handled in playlist-loader)
-    if (data.context?.deliveryDirectives) {
-      return {
-        action: NetworkErrorAction.DoNothing,
-        flags: ErrorActionFlags.None,
-        retryConfig: retryConfig || {
-          maxNumRetry: 0,
-          retryDelayMs: 0,
-          maxRetryDelayMs: 0,
-        },
-        retryCount,
-      };
+    const errorAction = this.getLevelSwitchAction(data, levelIndex);
+    if (retryConfig) {
+      errorAction.retryConfig = retryConfig;
+      errorAction.retryCount = retryCount;
     }
-    return this.getLevelSwitchAction(data, levelIndex);
+    return errorAction;
   }
 
   private getFragRetryOrSwitchAction(data: ErrorData): IErrorAction {
@@ -380,6 +377,7 @@ export default class ErrorController implements NetworkComponentAPI {
         }
         if (nextLevel > -1 && hls.loadLevel !== nextLevel) {
           data.levelRetry = true;
+          this.playlistError = 0;
           return {
             action: NetworkErrorAction.SendAlternateToPenaltyBox,
             flags: ErrorActionFlags.None,

src/controller/fragment-tracker.ts
@@ -138,7 +138,7 @@ export class FragmentTracker implements ComponentAPI {
     }
     // Check if any flagged fragments have been unloaded
     // excluding anything newer than appendedPartSn
-    const appendedPartSn = (appendedPart?.fragment.sn || 0) as number;
+    const appendedPartSn = (appendedPart?.fragment.sn || -1) as number;
     Object.keys(this.fragments).forEach((key) => {
       const fragmentEntity = this.fragments[key];
       if (!fragmentEntity) {
@@ -492,7 +492,8 @@ function isPartial(fragmentEntity: FragmentEntity): boolean {
     fragmentEntity.buffered &&
     (fragmentEntity.body.gap ||
       fragmentEntity.range.video?.partial ||
-      fragmentEntity.range.audio?.partial)
+      fragmentEntity.range.audio?.partial ||
+      fragmentEntity.range.audiovideo?.partial)
   );
 }
 

src/demux/tsdemuxer.ts
@@ -100,20 +100,43 @@ class TSDemuxer implements Demuxer {
 
   static syncOffset(data: Uint8Array): number {
     const length = data.length;
-    const scanwindow =
+    let scanwindow =
       Math.min(PACKET_LENGTH * 5, data.length - PACKET_LENGTH) + 1;
     let i = 0;
     while (i < scanwindow) {
       // a TS init segment should contain at least 2 TS packets: PAT and PMT, each starting with 0x47
       let foundPat = false;
+      let packetStart = -1;
+      let tsPackets = 0;
       for (let j = i; j < length; j += PACKET_LENGTH) {
         if (data[j] === 0x47) {
-          if (!foundPat && parsePID(data, j) === 0) {
-            foundPat = true;
+          tsPackets++;
+          if (packetStart === -1) {
+            packetStart = j;
+            // First sync word found at offset, increase scan length (#5251)
+            if (packetStart !== 0) {
+              scanwindow =
+                Math.min(
+                  packetStart + PACKET_LENGTH * 99,
+                  data.length - PACKET_LENGTH
+                ) + 1;
+            }
           }
-          if (foundPat && j + PACKET_LENGTH > scanwindow) {
-            return i;
+          if (!foundPat) {
+            foundPat = parsePID(data, j) === 0;
           }
+          // Sync word found at 0 with 3 packets, or found at offset least 2 packets up to scanwindow (#5501)
+          if (
+            foundPat &&
+            tsPackets > 1 &&
+            ((packetStart === 0 && tsPackets > 2) ||
+              j + PACKET_LENGTH > scanwindow)
+          ) {
+            return packetStart;
+          }
+        } else if (tsPackets) {
+          // Exit if sync word found, but does not contain contiguous packets (#5501)
+          return -1;
         } else {
           break;
         }

src/remux/mp4-remuxer.ts
@@ -498,7 +498,7 @@ export default class MP4Remuxer implements Remuxer {
             )} ms (${delta}dts) overlapping between fragments detected`
           );
         }
-        if (!foundOverlap || nextAvcDts > inputSamples[0].pts) {
+        if (!foundOverlap || nextAvcDts >= inputSamples[0].pts) {
           firstDTS = nextAvcDts;
           const firstPTS = inputSamples[0].pts - delta;
           inputSamples[0].dts = firstDTS;
@@ -1086,12 +1086,12 @@ export function flushTextTrackMetadataCueSamples(
     // using this._initPTS and this._initDTS to calculate relative time
     sample.pts =
       normalizePts(
-        sample.pts - (initPTS.baseTime * 90000) / initPTS.timescale,
+        sample.pts - (initPTS.baseTime * inputTimeScale) / initPTS.timescale,
         timeOffset * inputTimeScale
       ) / inputTimeScale;
     sample.dts =
       normalizePts(
-        sample.dts - (initDTS.baseTime * 90000) / initDTS.timescale,
+        sample.dts - (initDTS.baseTime * inputTimeScale) / initDTS.timescale,
         timeOffset * inputTimeScale
       ) / inputTimeScale;
   }

src/remux/passthrough-remuxer.ts
@@ -167,20 +167,25 @@ class PassThroughRemuxer implements Remuxer {
       this.emitInitSegment = false;
     }
 
+    const duration = getDuration(data, initData);
     const startDTS = getStartDTS(initData, data);
     const decodeTime = startDTS === null ? timeOffset : startDTS;
     if (
-      isInvalidInitPts(initPTS, decodeTime, timeOffset) ||
+      isInvalidInitPts(initPTS, decodeTime, timeOffset, duration) ||
       (initSegment.timescale !== initPTS.timescale && accurateTimeOffset)
     ) {
       initSegment.initPTS = decodeTime - timeOffset;
+      if (initPTS && initPTS.timescale === 1) {
+        logger.warn(
+          `Adjusting initPTS by ${initSegment.initPTS - initPTS.baseTime}`
+        );
+      }
       this.initPTS = initPTS = {
         baseTime: initSegment.initPTS,
         timescale: 1,
       };
     }
 
-    const duration = getDuration(data, initData);
     const startTime = audioTrack
       ? decodeTime - initPTS.baseTime / initPTS.timescale
       : (lastEndTime as number);
@@ -244,14 +249,16 @@ class PassThroughRemuxer implements Remuxer {
 function isInvalidInitPts(
   initPTS: RationalTimestamp | null,
   startDTS: number,
-  timeOffset: number
+  timeOffset: number,
+  duration: number
 ): initPTS is null {
   if (initPTS === null) {
     return true;
   }
-  // InitPTS is invalid when it would cause start time to be negative, or distance from time offset to be more than 1 second
+  // InitPTS is invalid when distance from program would be more than segment duration or a minimum of one second
+  const minDuration = Math.max(duration, 1);
   const startTime = startDTS - initPTS.baseTime / initPTS.timescale;
-  return startTime < 0 && Math.abs(startTime - timeOffset) > 1;
+  return Math.abs(startTime - timeOffset) > minDuration;
 }
 
 function getParsedTrackCodec(