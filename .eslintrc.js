03ca3af8a5a6fe0dd631908fb995eaf50b42b05d
Tom Jenkinson <tom@tjenkinson.me>
2023-10-08 15:10:54 +0100

    Run prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
-    project: './tsconfig.json'
+    project: './tsconfig.json',
   },
   plugins: ['@typescript-eslint', 'import'],
   rules: {
