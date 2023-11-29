487edba985a44336e927d533cbd6af6225299233
Rob Walch <robwalch@users.noreply.github.com>
2023-11-29 12:12:07 -0800

    Expand isSupported check to test other codecs Resolves #6004

--- src/is-supported.ts
+++ src/is-supported.ts
@@ -1,23 +1,17 @@
 import { getMediaSource } from './utils/mediasource-helper';
+import { getCodecCompatibleName, mimeTypeForCodec } from './utils/codecs';
 import type { ExtendedSourceBuffer } from './types/buffer';
 
 function getSourceBuffer(): typeof self.SourceBuffer {
   return self.SourceBuffer || (self as any).WebKitSourceBuffer;
 }
 
-/**
- * @ignore
- */
 export function isSupported(): boolean {
   const mediaSource = getMediaSource();
   if (!mediaSource) {
     return false;
   }
   const sourceBuffer = getSourceBuffer();
-  const isTypeSupported =
-    mediaSource &&
-    typeof mediaSource.isTypeSupported === 'function' &&
-    mediaSource.isTypeSupported('video/mp4; codecs="avc1.42E01E,mp4a.40.2"');
 
   // if SourceBuffer is exposed ensure its API is valid
   // Older browsers do not expose SourceBuffer globally so checking SourceBuffer.prototype is impossible
@@ -26,12 +20,40 @@ export function isSupported(): boolean {
     (sourceBuffer.prototype &&
       typeof sourceBuffer.prototype.appendBuffer === 'function' &&
       typeof sourceBuffer.prototype.remove === 'function');
-  return !!isTypeSupported && !!sourceBufferValidAPI;
+  if (!sourceBufferValidAPI) {
+    return false;
+  }
+
+  return (
+    !!mediaSource &&
+    typeof mediaSource.isTypeSupported === 'function' &&
+    ([
+      'avc1.42E01E,mp4a.40.2',
+      'hvc1.1.6.L123.B0',
+      'av01.0.01M.08',
+      'vp09.00.50.08',
+    ].some((codecsForVideoContainer) =>
+      isMimeTypeSupported(
+        mediaSource,
+        mimeTypeForCodec(codecsForVideoContainer, 'video'),
+      ),
+    ) ||
+      ['mp4a.40.2', 'fLaC'].some((codecForAudioContainer) =>
+        isMimeTypeSupported(
+          mediaSource,
+          mimeTypeForCodec(codecForAudioContainer, 'audio'),
+        ),
+      ))
+  );
 }
 
-/**
- * @ignore
- */
+function isMimeTypeSupported(
+  mediaSource: typeof MediaSource,
+  mimeType: string,
+): boolean {
+  return mediaSource.isTypeSupported(mimeType);
+}
+
 export function changeTypeSupported(): boolean {
   const sourceBuffer = getSourceBuffer();
   return (
