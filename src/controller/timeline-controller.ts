48f3bf1ee11deb9b5064c166abfa26f07b647d22
Rob Walch <robwalch@users.noreply.github.com>
2024-05-23 17:36:12 -0700

    Fix 608 parser handling of redundant special chars and cue timing after seek (#6439)
    * Fix 608 parser handling of redundant control codes
    Fixes #6427
    
    * Fix 608 caption TextTrack Cue seek/discontinuity timing regression introduced in v1.5.0 with #5557

--- src/controller/timeline-controller.ts
+++ src/controller/timeline-controller.ts
@@ -466,20 +466,17 @@ export class TimelineController implements ComponentAPI {
     // if this frag isn't contiguous, clear the parser so cues with bad start/end times aren't added to the textTrack
     if (this.enabled && data.frag.type === PlaylistLevelType.MAIN) {
       const { cea608Parser1, cea608Parser2, lastSn } = this;
-      if (!cea608Parser1 || !cea608Parser2) {
-        return;
-      }
       const { cc, sn } = data.frag;
       const partIndex = data.part?.index ?? -1;
-      if (
-        !(
-          sn === lastSn + 1 ||
-          (sn === lastSn && partIndex === this.lastPartIndex + 1) ||
-          cc === this.lastCc
-        )
-      ) {
-        cea608Parser1.reset();
-        cea608Parser2.reset();
+      if (cea608Parser1 && cea608Parser2) {
+        if (
+          sn !== lastSn + 1 ||
+          (sn === lastSn && partIndex !== this.lastPartIndex + 1) ||
+          cc !== this.lastCc
+        ) {
+          cea608Parser1.reset();
+          cea608Parser2.reset();
+        }
       }
       this.lastCc = cc as number;
       this.lastSn = sn as number;
