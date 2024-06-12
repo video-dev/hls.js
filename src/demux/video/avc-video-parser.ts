video-dev/hls.js

commit 0a497317de8e73e6ad0f35bc06c8b327e10c995f

Fix extraction of NALu overflowing PES packet (#6268)
Co-authored-by: vladimir.vyatkin <vladimir.vyatkin@portal59.ru>

src/demux/video/avc-video-parser.ts
@@ -17,7 +17,7 @@ class AvcVideoParser extends BaseVideoParser {
     last: boolean,
     duration: number,
   ) {
-    const units = this.parseNALu(track, pes.data);
+    const units = this.parseNALu(track, pes.data, last);
     const debug = false;
     let VideoSample = this.VideoSample;
     let push: boolean;

src/demux/video/base-video-parser.ts
@@ -29,22 +29,6 @@ abstract class BaseVideoParser {
     };
   }
 
-  protected getLastNalUnit(
-    samples: VideoSample[],
-  ): VideoSampleUnit | undefined {
-    let VideoSample = this.VideoSample;
-    let lastUnit: VideoSampleUnit | undefined;
-    // try to fallback to previous sample if current one is empty
-    if (!VideoSample || VideoSample.units.length === 0) {
-      VideoSample = samples[samples.length - 1];
-    }
-    if (VideoSample?.units) {
-      const units = VideoSample.units;
-      lastUnit = units[units.length - 1];
-    }
-    return lastUnit;
-  }
-
   protected pushAccessUnit(
     VideoSample: ParsedVideoSample,
     videoTrack: DemuxedVideoTrack,
@@ -86,6 +70,7 @@ abstract class BaseVideoParser {
   protected parseNALu(
     track: DemuxedVideoTrack,
     array: Uint8Array,
+    last: boolean,
   ): Array<{
     data: Uint8Array;
     type: number;
@@ -133,6 +118,10 @@ abstract class BaseVideoParser {
             data: array.subarray(lastUnitStart, overflow),
             type: lastUnitType,
           };
+          if (track.lastNalu) {
+            units.push(track.lastNalu);
+            track.lastNalu = null;
+          }
           // logger.log('pushing NALU, type/size:' + unit.type + '/' + unit.data.byteLength);
           units.push(unit);
         } else {
@@ -140,7 +129,7 @@ abstract class BaseVideoParser {
           // first check if start code delimiter is overlapping between 2 PES packets,
           // ie it started in last packet (lastState not zero)
           // and ended at the beginning of this PES packet (i <= 4 - lastState)
-          const lastUnit = this.getLastNalUnit(track.samples);
+          const lastUnit = track.lastNalu;
           if (lastUnit) {
             if (lastState && i <= 4 - lastState) {
               // start delimiter overlapping between PES packets
@@ -163,6 +152,8 @@ abstract class BaseVideoParser {
                 array.subarray(0, overflow),
               );
               lastUnit.state = 0;
+              units.push(lastUnit);
+              track.lastNalu = null;
             }
           }
         }
@@ -187,15 +178,21 @@ abstract class BaseVideoParser {
         type: lastUnitType,
         state: state,
       };
-      units.push(unit);
-      // logger.log('pushing NALU, type/size/state:' + unit.type + '/' + unit.data.byteLength + '/' + state);
-    }
-    // no NALu found
-    if (units.length === 0) {
+      if (!last) {
+        track.lastNalu = unit;
+        // logger.log('store NALu to push it on next PES');
+      } else {
+        units.push(unit);
+        // logger.log('pushing NALU, type/size/state:' + unit.type + '/' + unit.data.byteLength + '/' + state);
+      }
+    } else if (units.length === 0) {
+      // no NALu found
       // append pes.data to previous NAL unit
-      const lastUnit = this.getLastNalUnit(track.samples);
+      const lastUnit = track.lastNalu;
       if (lastUnit) {
         lastUnit.data = appendUint8Array(lastUnit.data, array);
+        units.push(lastUnit);
+        track.lastNalu = null;
       }
     }
     track.naluState = state;

src/demux/video/hevc-video-parser.ts
@@ -19,7 +19,7 @@ class HevcVideoParser extends BaseVideoParser {
     last: boolean,
     duration: number,
   ) {
-    const units = this.parseNALu(track, pes.data);
+    const units = this.parseNALu(track, pes.data, last);
     const debug = false;
     let VideoSample = this.VideoSample;
     let push: boolean;

src/types/demuxer.ts
@@ -77,6 +77,7 @@ export interface DemuxedVideoTrackBase extends DemuxedTrack {
   pps?: Uint8Array[];
   sps?: Uint8Array[];
   naluState?: number;
+  lastNalu?: VideoSampleUnit | null;
   segmentCodec?: string;
   manifestCodec?: string;
   samples: VideoSample[] | Uint8Array;

tests/test-streams.js
@@ -272,4 +272,11 @@ module.exports = {
     description: 'Advanced stream (HEVC Main 10, MPEG-TS segments)',
     skipFunctionalTests: true,
   },
+  mpegTsBitmovinHevc: {
+    url: 'https://bitmovin-a.akamaihd.net/content/dataset/multi-codec/hevc/v720p_ts.m3u8',
+    description:
+      'HLS M2TS by Bitmovin (HEVC Main, many NALUs overflowing PESes, video only)',
+    abr: false,
+    skipFunctionalTests: true,
+  },
 };

commit 03c570f396423f382b764165cd0a28e4043a0945

Fix decode error escape in dev (#6492)
Revert NALu overflow handling change introduced in #6268 that results in a decode error at 30s w/ https://playertest.longtailvideo.com/adaptive/elephants_dream_v4/media/b2962000-video.m3u8
`PIPELINE_ERROR_DECODE: Error Domain=NSOSStatusErrorDomain Code=-12909 "(null)" (-12909): VTDecompressionOutputCallback`

src/demux/video/avc-video-parser.ts
@@ -14,10 +14,10 @@ class AvcVideoParser extends BaseVideoParser {
     track: DemuxedVideoTrack,
     textTrack: DemuxedUserdataTrack,
     pes: PES,
-    last: boolean,
+    endOfSegment: boolean,
     duration: number,
   ) {
-    const units = this.parseNALu(track, pes.data, last);
+    const units = this.parseNALu(track, pes.data, endOfSegment);
     const debug = false;
     let VideoSample = this.VideoSample;
     let push: boolean;
@@ -206,7 +206,7 @@ class AvcVideoParser extends BaseVideoParser {
       }
     });
     // if last PES packet, push samples
-    if (last && VideoSample) {
+    if (endOfSegment && VideoSample) {
       this.pushAccessUnit(VideoSample, track);
       this.VideoSample = null;
     }

src/demux/video/base-video-parser.ts
@@ -29,6 +29,22 @@ abstract class BaseVideoParser {
     };
   }
 
+  protected getLastNalUnit(
+    samples: VideoSample[],
+  ): VideoSampleUnit | undefined {
+    let VideoSample = this.VideoSample;
+    let lastUnit: VideoSampleUnit | undefined;
+    // try to fallback to previous sample if current one is empty
+    if (!VideoSample || VideoSample.units.length === 0) {
+      VideoSample = samples[samples.length - 1];
+    }
+    if (VideoSample?.units) {
+      const units = VideoSample.units;
+      lastUnit = units[units.length - 1];
+    }
+    return lastUnit;
+  }
+
   protected pushAccessUnit(
     VideoSample: ParsedVideoSample,
     videoTrack: DemuxedVideoTrack,
@@ -70,7 +86,7 @@ abstract class BaseVideoParser {
   protected parseNALu(
     track: DemuxedVideoTrack,
     array: Uint8Array,
-    last: boolean,
+    endOfSegment: boolean,
   ): Array<{
     data: Uint8Array;
     type: number;
@@ -118,10 +134,6 @@ abstract class BaseVideoParser {
             data: array.subarray(lastUnitStart, overflow),
             type: lastUnitType,
           };
-          if (track.lastNalu) {
-            units.push(track.lastNalu);
-            track.lastNalu = null;
-          }
           // logger.log('pushing NALU, type/size:' + unit.type + '/' + unit.data.byteLength);
           units.push(unit);
         } else {
@@ -129,7 +141,7 @@ abstract class BaseVideoParser {
           // first check if start code delimiter is overlapping between 2 PES packets,
           // ie it started in last packet (lastState not zero)
           // and ended at the beginning of this PES packet (i <= 4 - lastState)
-          const lastUnit = track.lastNalu;
+          const lastUnit = this.getLastNalUnit(track.samples);
           if (lastUnit) {
             if (lastState && i <= 4 - lastState) {
               // start delimiter overlapping between PES packets
@@ -152,8 +164,6 @@ abstract class BaseVideoParser {
                 array.subarray(0, overflow),
               );
               lastUnit.state = 0;
-              units.push(lastUnit);
-              track.lastNalu = null;
             }
           }
         }
@@ -178,21 +188,15 @@ abstract class BaseVideoParser {
         type: lastUnitType,
         state: state,
       };
-      if (!last) {
-        track.lastNalu = unit;
-        // logger.log('store NALu to push it on next PES');
-      } else {
-        units.push(unit);
-        // logger.log('pushing NALU, type/size/state:' + unit.type + '/' + unit.data.byteLength + '/' + state);
-      }
-    } else if (units.length === 0) {
-      // no NALu found
+      units.push(unit);
+      // logger.log('pushing NALU, type/size/state:' + unit.type + '/' + unit.data.byteLength + '/' + state);
+    }
+    // no NALu found
+    if (units.length === 0) {
       // append pes.data to previous NAL unit
-      const lastUnit = track.lastNalu;
+      const lastUnit = this.getLastNalUnit(track.samples);
       if (lastUnit) {
         lastUnit.data = appendUint8Array(lastUnit.data, array);
-        units.push(lastUnit);
-        track.lastNalu = null;
       }
     }
     track.naluState = state;

src/demux/video/hevc-video-parser.ts
@@ -16,10 +16,10 @@ class HevcVideoParser extends BaseVideoParser {
     track: DemuxedVideoTrack,
     textTrack: DemuxedUserdataTrack,
     pes: PES,
-    last: boolean,
+    endOfSegment: boolean,
     duration: number,
   ) {
-    const units = this.parseNALu(track, pes.data, last);
+    const units = this.parseNALu(track, pes.data, endOfSegment);
     const debug = false;
     let VideoSample = this.VideoSample;
     let push: boolean;
@@ -244,7 +244,7 @@ class HevcVideoParser extends BaseVideoParser {
       }
     });
     // if last PES packet, push samples
-    if (last && VideoSample) {
+    if (endOfSegment && VideoSample) {
       this.pushAccessUnit(VideoSample, track);
       this.VideoSample = null;
     }

src/types/demuxer.ts
@@ -77,7 +77,6 @@ export interface DemuxedVideoTrackBase extends DemuxedTrack {
   pps?: Uint8Array[];
   sps?: Uint8Array[];
   naluState?: number;
-  lastNalu?: VideoSampleUnit | null;
   segmentCodec?: string;
   manifestCodec?: string;
   samples: VideoSample[] | Uint8Array;