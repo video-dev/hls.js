video-dev/hls.js

commit c4d9a929f36385139a5a6180baa6dd9f02cf4586

Reduce workers across instances (#6550)
* Share a single Web Worker for Transmuxers across instances

* Replace use of keyword `async`

* Use configured logger in remuxer and demuxer instances and static calls to probe
Related to #2461

src/demux/transmuxer.ts
@@ -9,7 +9,7 @@ import MP3Demuxer from './audio/mp3demuxer';
 import { AC3Demuxer } from './audio/ac3-demuxer';
 import MP4Remuxer from '../remux/mp4-remuxer';
 import PassThroughRemuxer from '../remux/passthrough-remuxer';
-import { logger } from '../utils/logger';
+import { PlaylistLevelType } from '../types/loader';
 import {
   isFullSegmentEncryption,
   getAesModeFromFullSegmentMethod,
@@ -19,18 +19,16 @@ import type { Remuxer } from '../types/remuxer';
 import type { TransmuxerResult, ChunkMetadata } from '../types/transmuxer';
 import type { HlsConfig } from '../config';
 import type { DecryptData } from '../loader/level-key';
-import type { PlaylistLevelType } from '../types/loader';
 import type { TypeSupported } from '../utils/codecs';
+import type { ILogger } from '../utils/logger';
 import type { RationalTimestamp } from '../utils/timescale-conversion';
-import { optionalSelf } from '../utils/global';
 
-let now;
+let now: () => number;
 // performance.now() not available on WebWorker, at least on Safari Desktop
 try {
   now = self.performance.now.bind(self.performance);
 } catch (err) {
-  logger.debug('Unable to use Performance API on this environment');
-  now = optionalSelf?.Date.now;
+  now = Date.now;
 }
 
 type MuxConfig =
@@ -52,7 +50,8 @@ if (__USE_M2TS_ADVANCED_CODECS__) {
 }
 
 export default class Transmuxer {
-  public async: boolean = false;
+  private asyncResult: boolean = false;
+  private logger: ILogger;
   private observer: HlsEventEmitter;
   private typeSupported: TypeSupported;
   private config: HlsConfig;
@@ -72,12 +71,14 @@ export default class Transmuxer {
     config: HlsConfig,
     vendor: string,
     id: PlaylistLevelType,
+    logger: ILogger,
   ) {
     this.observer = observer;
     this.typeSupported = typeSupported;
     this.config = config;
     this.vendor = vendor;
     this.id = id;
+    this.logger = logger;
   }
 
   configure(transmuxConfig: TransmuxConfig) {
@@ -144,6 +145,7 @@ export default class Transmuxer {
         }
         uintData = new Uint8Array(decryptedData);
       } else {
+        this.asyncResult = true;
         this.decryptionPromise = decrypter
           .webCryptoDecrypt(
             uintData,
@@ -162,7 +164,7 @@ export default class Transmuxer {
             this.decryptionPromise = null;
             return result;
           });
-        return this.decryptionPromise!;
+        return this.decryptionPromise;
       }
     }
 
@@ -170,7 +172,7 @@ export default class Transmuxer {
     if (resetMuxers) {
       const error = this.configureTransmuxer(uintData);
       if (error) {
-        logger.warn(`[transmuxer] ${error.message}`);
+        this.logger.warn(`[transmuxer] ${error.message}`);
         this.observer.emit(Events.ERROR, Events.ERROR, {
           type: ErrorTypes.MEDIA_ERROR,
           details: ErrorDetails.FRAG_PARSING_ERROR,
@@ -208,6 +210,8 @@ export default class Transmuxer {
       accurateTimeOffset,
       chunkMeta,
     );
+    this.asyncResult = isPromise(result);
+
     const currentState = this.currentTransmuxState;
 
     currentState.contiguous = true;
@@ -228,6 +232,7 @@ export default class Transmuxer {
     const { decrypter, currentTransmuxState, decryptionPromise } = this;
 
     if (decryptionPromise) {
+      this.asyncResult = true;
       // Upon resolution, the decryption promise calls push() and returns its TransmuxerResult up the stack. Therefore
       // only flushing is required for async decryption
       return decryptionPromise.then(() => {
@@ -254,11 +259,16 @@ export default class Transmuxer {
     if (!demuxer || !remuxer) {
       // If probing failed, then Hls.js has been given content its not able to handle
       stats.executeEnd = now();
-      return [emptyResult(chunkMeta)];
+      const emptyResults = [emptyResult(chunkMeta)];
+      if (this.asyncResult) {
+        return Promise.resolve(emptyResults);
+      }
+      return emptyResults;
     }
 
     const demuxResultOrPromise = demuxer.flush(timeOffset);
     if (isPromise(demuxResultOrPromise)) {
+      this.asyncResult = true;
       // Decrypt final SAMPLE-AES samples
       return demuxResultOrPromise.then((demuxResult) => {
         this.flushRemux(transmuxResults, demuxResult, chunkMeta);
@@ -267,6 +277,9 @@ export default class Transmuxer {
     }
 
     this.flushRemux(transmuxResults, demuxResultOrPromise, chunkMeta);
+    if (this.asyncResult) {
+      return Promise.resolve(transmuxResults);
+    }
     return transmuxResults;
   }
 
@@ -277,10 +290,10 @@ export default class Transmuxer {
   ) {
     const { audioTrack, videoTrack, id3Track, textTrack } = demuxResult;
     const { accurateTimeOffset, timeOffset } = this.currentTransmuxState;
-    logger.log(
-      `[transmuxer.ts]: Flushed fragment ${chunkMeta.sn}${
+    this.logger.log(
+      `[transmuxer.ts]: Flushed ${this.id} sn: ${chunkMeta.sn}${
         chunkMeta.part > -1 ? ' p: ' + chunkMeta.part : ''
-      } of level ${chunkMeta.level}`,
+      } of ${this.id === PlaylistLevelType.MAIN ? 'level' : 'track'} ${chunkMeta.level}`,
     );
     const remuxResult = this.remuxer!.remux(
       audioTrack,
@@ -438,7 +451,7 @@ export default class Transmuxer {
     // probe for content type
     let mux;
     for (let i = 0, len = muxConfig.length; i < len; i++) {
-      if (muxConfig[i].demux?.probe(data)) {
+      if (muxConfig[i].demux?.probe(data, this.logger)) {
         mux = muxConfig[i];
         break;
       }
@@ -452,10 +465,10 @@ export default class Transmuxer {
     const Remuxer: MuxConfig['remux'] = mux.remux;
     const Demuxer: MuxConfig['demux'] = mux.demux;
     if (!remuxer || !(remuxer instanceof Remuxer)) {
-      this.remuxer = new Remuxer(observer, config, typeSupported, vendor);
+      this.remuxer = new Remuxer(observer, config, typeSupported, this.logger);
     }
     if (!demuxer || !(demuxer instanceof Demuxer)) {
-      this.demuxer = new Demuxer(observer, config, typeSupported);
+      this.demuxer = new Demuxer(observer, config, typeSupported, this.logger);
       this.probe = Demuxer.probe;
     }
   }

src/demux/tsdemuxer.ts
@@ -18,11 +18,11 @@ import HevcVideoParser from './video/hevc-video-parser';
 import SampleAesDecrypter from './sample-aes';
 import { Events } from '../events';
 import { appendUint8Array, RemuxerTrackIdConfig } from '../utils/mp4-tools';
-import { logger } from '../utils/logger';
 import { ErrorTypes, ErrorDetails } from '../errors';
 import type { HlsConfig } from '../config';
 import type { HlsEventEmitter } from '../events';
 import type { TypeSupported } from '../utils/codecs';
+import type { ILogger } from '../utils/logger';
 import {
   MetadataSchema,
   type DemuxedVideoTrack,
@@ -54,9 +54,10 @@ export type ParsedVideoSample = ParsedTimestamp &
 const PACKET_LENGTH = 188;
 
 class TSDemuxer implements Demuxer {
+  private readonly logger: ILogger;
   private readonly observer: HlsEventEmitter;
   private readonly config: HlsConfig;
-  private typeSupported: TypeSupported;
+  private readonly typeSupported: TypeSupported;
 
   private sampleAes: SampleAesDecrypter | null = null;
   private pmtParsed: boolean = false;
@@ -77,14 +78,16 @@ class TSDemuxer implements Demuxer {
     observer: HlsEventEmitter,
     config: HlsConfig,
     typeSupported: TypeSupported,
+    logger: ILogger,
   ) {
     this.observer = observer;
     this.config = config;
     this.typeSupported = typeSupported;
+    this.logger = logger;
     this.videoParser = null;
   }
 
-  static probe(data: Uint8Array) {
+  static probe(data: Uint8Array, logger: ILogger) {
     const syncOffset = TSDemuxer.syncOffset(data);
     if (syncOffset > 0) {
       logger.warn(
@@ -288,7 +291,7 @@ class TSDemuxer implements Demuxer {
         switch (pid) {
           case videoPid:
             if (stt) {
-              if (videoData && (pes = parsePES(videoData))) {
+              if (videoData && (pes = parsePES(videoData, this.logger))) {
                 if (this.videoParser === null) {
                   switch (videoTrack.segmentCodec) {
                     case 'avc':
@@ -321,7 +324,7 @@ class TSDemuxer implements Demuxer {
             break;
           case audioPid:
             if (stt) {
-              if (audioData && (pes = parsePES(audioData))) {
+              if (audioData && (pes = parsePES(audioData, this.logger))) {
                 switch (audioTrack.segmentCodec) {
                   case 'aac':
                     this.parseAACPES(audioTrack, pes);
@@ -345,7 +348,7 @@ class TSDemuxer implements Demuxer {
             break;
           case id3Pid:
             if (stt) {
-              if (id3Data && (pes = parsePES(id3Data))) {
+              if (id3Data && (pes = parsePES(id3Data, this.logger))) {
                 this.parseID3PES(id3Track, pes);
               }
 
@@ -362,7 +365,7 @@ class TSDemuxer implements Demuxer {
             }
 
             pmtId = this._pmtId = parsePAT(data, offset);
-            // logger.log('PMT PID:'  + this._pmtId);
+            // this.logger.log('PMT PID:'  + this._pmtId);
             break;
           case pmtId: {
             if (stt) {
@@ -375,6 +378,7 @@ class TSDemuxer implements Demuxer {
               this.typeSupported,
               isSampleAes,
               this.observer,
+              this.logger,
             );
 
             // only update track id if track PID found while parsing PMT
@@ -400,7 +404,7 @@ class TSDemuxer implements Demuxer {
             }
 
             if (unknownPID !== null && !pmtParsed) {
-              logger.warn(
+              this.logger.warn(
                 `MPEG-TS PMT found at ${start} after unknown PID '${unknownPID}'. Backtracking to sync byte @${syncOffset} to parse all TS packets.`,
               );
               unknownPID = null;
@@ -428,6 +432,8 @@ class TSDemuxer implements Demuxer {
         new Error(
           `Found ${tsPacketErrors} TS packet/s that do not start with 0x47`,
         ),
+        undefined,
+        this.logger,
       );
     }
 
@@ -477,7 +483,7 @@ class TSDemuxer implements Demuxer {
     const id3Data = id3Track.pesData;
     // try to parse last PES packets
     let pes: PES | null;
-    if (videoData && (pes = parsePES(videoData))) {
+    if (videoData && (pes = parsePES(videoData, this.logger))) {
       if (this.videoParser === null) {
         switch (videoTrack.segmentCodec) {
           case 'avc':
@@ -505,7 +511,7 @@ class TSDemuxer implements Demuxer {
       videoTrack.pesData = videoData;
     }
 
-    if (audioData && (pes = parsePES(audioData))) {
+    if (audioData && (pes = parsePES(audioData, this.logger))) {
       switch (audioTrack.segmentCodec) {
         case 'aac':
           this.parseAACPES(audioTrack, pes);
@@ -522,7 +528,7 @@ class TSDemuxer implements Demuxer {
       audioTrack.pesData = null;
     } else {
       if (audioData?.size) {
-        logger.log(
+        this.logger.log(
           'last AAC PES packet truncated,might overlap between fragments',
         );
       }
@@ -531,7 +537,7 @@ class TSDemuxer implements Demuxer {
       audioTrack.pesData = audioData;
     }
 
-    if (id3Data && (pes = parsePES(id3Data))) {
+    if (id3Data && (pes = parsePES(id3Data, this.logger))) {
       this.parseID3PES(id3Track, pes);
       id3Track.pesData = null;
     } else {
@@ -625,7 +631,12 @@ class TSDemuxer implements Demuxer {
       } else {
         reason = 'No ADTS header found in AAC PES';
       }
-      emitParsingError(this.observer, new Error(reason), recoverable);
+      emitParsingError(
+        this.observer,
+        new Error(reason),
+        recoverable,
+        this.logger,
+      );
       if (!recoverable) {
         return;
       }
@@ -648,7 +659,7 @@ class TSDemuxer implements Demuxer {
       const frameDuration = ADTS.getFrameDuration(track.samplerate as number);
       pts = aacOverFlow.sample.pts + frameDuration;
     } else {
-      logger.warn('[tsdemuxer]: AAC PES unknown PTS');
+      this.logger.warn('[tsdemuxer]: AAC PES unknown PTS');
       return;
     }
 
@@ -679,7 +690,7 @@ class TSDemuxer implements Demuxer {
     let offset = 0;
     const pts = pes.pts;
     if (pts === undefined) {
-      logger.warn('[tsdemuxer]: MPEG PES unknown PTS');
+      this.logger.warn('[tsdemuxer]: MPEG PES unknown PTS');
       return;
     }
 
@@ -711,7 +722,7 @@ class TSDemuxer implements Demuxer {
       const data = pes.data;
       const pts = pes.pts;
       if (pts === undefined) {
-        logger.warn('[tsdemuxer]: AC3 PES unknown PTS');
+        this.logger.warn('[tsdemuxer]: AC3 PES unknown PTS');
         return;
       }
       const length = data.length;
@@ -730,7 +741,7 @@ class TSDemuxer implements Demuxer {
 
   private parseID3PES(id3Track: DemuxedMetadataTrack, pes: PES) {
     if (pes.pts === undefined) {
-      logger.warn('[tsdemuxer]: ID3 PES unknown PTS');
+      this.logger.warn('[tsdemuxer]: ID3 PES unknown PTS');
       return;
     }
     const id3Sample = Object.assign({}, pes as Required<PES>, {
@@ -757,6 +768,7 @@ function parsePMT(
   typeSupported: TypeSupported,
   isSampleAes: boolean,
   observer: HlsEventEmitter,
+  logger: ILogger,
 ) {
   const result = {
     audioPid: -1,
@@ -779,7 +791,7 @@ function parsePMT(
     switch (data[offset]) {
       case 0xcf: // SAMPLE-AES AAC
         if (!isSampleAes) {
-          logEncryptedSamplesFoundInUnencryptedStream('ADTS AAC');
+          logEncryptedSamplesFoundInUnencryptedStream('ADTS AAC', this.logger);
           break;
         }
       /* falls through */
@@ -802,7 +814,7 @@ function parsePMT(
 
       case 0xdb: // SAMPLE-AES AVC
         if (!isSampleAes) {
-          logEncryptedSamplesFoundInUnencryptedStream('H.264');
+          logEncryptedSamplesFoundInUnencryptedStream('H.264', this.logger);
           break;
         }
       /* falls through */
@@ -830,7 +842,7 @@ function parsePMT(
 
       case 0xc1: // SAMPLE-AES AC3
         if (!isSampleAes) {
-          logEncryptedSamplesFoundInUnencryptedStream('AC-3');
+          logEncryptedSamplesFoundInUnencryptedStream('AC-3', this.logger);
           break;
         }
       /* falls through */
@@ -886,7 +898,12 @@ function parsePMT(
       case 0xc2: // SAMPLE-AES EC3
       /* falls through */
       case 0x87:
-        emitParsingError(observer, new Error('Unsupported EC-3 in M2TS found'));
+        emitParsingError(
+          observer,
+          new Error('Unsupported EC-3 in M2TS found'),
+          undefined,
+          this.logger,
+        );
         return result;
 
       case 0x24: // ITU-T Rec. H.265 and ISO/IEC 23008-2 (HEVC)
@@ -900,6 +917,8 @@ function parsePMT(
           emitParsingError(
             observer,
             new Error('Unsupported HEVC in M2TS found'),
+            undefined,
+            this.logger,
           );
           return result;
         }
@@ -919,7 +938,8 @@ function parsePMT(
 function emitParsingError(
   observer: HlsEventEmitter,
   error: Error,
-  levelRetry?: boolean,
+  levelRetry: boolean | undefined,
+  logger: ILogger,
 ) {
   logger.warn(`parsing error: ${error.message}`);
   observer.emit(Events.ERROR, Events.ERROR, {
@@ -932,11 +952,14 @@ function emitParsingError(
   });
 }
 
-function logEncryptedSamplesFoundInUnencryptedStream(type: string) {
+function logEncryptedSamplesFoundInUnencryptedStream(
+  type: string,
+  logger: ILogger,
+) {
   logger.log(`${type} with AES-128-CBC encryption found in unencrypted stream`);
 }
 
-function parsePES(stream: ElementaryStreamData): PES | null {
+function parsePES(stream: ElementaryStreamData, logger: ILogger): PES | null {
   let i = 0;
   let frag: Uint8Array;
   let pesLen: number;

tests/unit/demuxer/transmuxer.ts
@@ -8,7 +8,6 @@ import Hls from '../../../src/hls';
 import sinon from 'sinon';
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
-import { logger } from '../../../src/utils/logger';
 
 chai.use(sinonChai);
 const expect = chai.expect;
@@ -149,7 +148,7 @@ describe('TransmuxerInterface tests', function () {
     expect(
       firstCall,
       'Demux call 1: ' + JSON.stringify(firstCall, null, 2),
-    ).to.deep.equal({
+    ).to.deep.include({
       cmd: 'demux',
       data,
       decryptdata: currentFrag.decryptdata,
@@ -182,7 +181,7 @@ describe('TransmuxerInterface tests', function () {
     expect(
       secondCall,
       'Demux call 2: ' + JSON.stringify(secondCall, null, 2),
-    ).to.deep.equal({
+    ).to.deep.include({
       cmd: 'demux',
       data,
       decryptdata: newFrag.decryptdata,
@@ -282,6 +281,7 @@ describe('TransmuxerInterface tests', function () {
       data: {
         event: {},
         data: {},
+        instanceNo: transmuxerInterfacePrivates.instanceNo,
       },
     } as any;
 
@@ -304,6 +304,7 @@ describe('TransmuxerInterface tests', function () {
       data: {
         event: 'init',
         data: {},
+        instanceNo: transmuxerInterfacePrivates.instanceNo,
       },
     };
 
@@ -330,10 +331,11 @@ describe('TransmuxerInterface tests', function () {
           logType: 'log',
           message: 'testing logger',
         },
+        instanceNo: transmuxerInterfacePrivates.instanceNo,
       },
     };
 
-    const spy = sinon.spy(logger, 'log');
+    const spy = sinon.spy(hls.logger, 'log');
     transmuxerInterfacePrivates.onWorkerMessage(evt);
     expect(spy).to.have.been.calledWith(evt.data.data.message);
   });

src/controller/base-stream-controller.ts
@@ -365,6 +365,10 @@ export default class BaseStreamController
 
   protected onHandlerDestroying() {
     this.stopLoad();
+    if (this.transmuxer) {
+      this.transmuxer.destroy();
+      this.transmuxer = null;
+    }
     super.onHandlerDestroying();
     // @ts-ignore
     this.hls = this.onMediaSeeking = this.onMediaEnded = null;
@@ -1931,10 +1935,7 @@ export default class BaseStreamController
   }
 
   protected resetTransmuxer() {
-    if (this.transmuxer) {
-      this.transmuxer.destroy();
-      this.transmuxer = null;
-    }
+    this.transmuxer?.reset();
   }
 
   protected recoverWorkerError(data: ErrorData) {

src/demux/audio/aacdemuxer.ts
@@ -4,16 +4,16 @@
 import BaseAudioDemuxer from './base-audio-demuxer';
 import * as ADTS from './adts';
 import * as MpegAudio from './mpegaudio';
-import { logger } from '../../utils/logger';
 import { getId3Data } from '@svta/common-media-library/id3/getId3Data';
 import type { HlsEventEmitter } from '../../events';
 import type { HlsConfig } from '../../config';
+import type { ILogger } from '../../utils/logger';
 
 class AACDemuxer extends BaseAudioDemuxer {
   private readonly observer: HlsEventEmitter;
   private readonly config: HlsConfig;
 
-  constructor(observer, config) {
+  constructor(observer: HlsEventEmitter, config) {
     super();
     this.observer = observer;
     this.config = config;
@@ -42,7 +42,7 @@ class AACDemuxer extends BaseAudioDemuxer {
   }
 
   // Source for probe info - https://wiki.multimedia.cx/index.php?title=ADTS
-  static probe(data: Uint8Array | undefined): boolean {
+  static probe(data: Uint8Array | undefined, logger: ILogger): boolean {
     if (!data) {
       return false;
     }

src/demux/audio/ac3-demuxer.ts
@@ -8,7 +8,7 @@ import type { AudioFrame, DemuxedAudioTrack } from '../../types/demuxer';
 export class AC3Demuxer extends BaseAudioDemuxer {
   private readonly observer: HlsEventEmitter;
 
-  constructor(observer) {
+  constructor(observer: HlsEventEmitter) {
     super();
     this.observer = observer;
   }

src/demux/inject-worker.ts
@@ -1,6 +1,9 @@
 // ensure the worker ends up in the bundle
 // If the worker should not be included this gets aliased to empty.js
 import './transmuxer-worker';
+import { version } from '../version';
+
+const workerStore: Record<string, WorkerContext> = {};
 
 export function hasUMDWorker(): boolean {
   return typeof __HLS_WORKER_BUNDLE__ === 'function';
@@ -10,9 +13,15 @@ export type WorkerContext = {
   worker: Worker;
   objectURL?: string;
   scriptURL?: string;
+  clientCount: number;
 };
 
 export function injectWorker(): WorkerContext {
+  const workerContext = workerStore[version];
+  if (workerContext) {
+    workerContext.clientCount++;
+    return workerContext;
+  }
   const blob = new self.Blob(
     [
       `var exports={};var module={exports:exports};function define(f){f()};define.amd=true;(${__HLS_WORKER_BUNDLE__.toString()})(true);`,
@@ -23,19 +32,44 @@ export function injectWorker(): WorkerContext {
   );
   const objectURL = self.URL.createObjectURL(blob);
   const worker = new self.Worker(objectURL);
-
-  return {
+  const result = {
     worker,
     objectURL,
+    clientCount: 1,
   };
+  workerStore[version] = result;
+  return result;
 }
 
 export function loadWorker(path: string): WorkerContext {
+  const workerContext = workerStore[path];
+  if (workerContext) {
+    workerContext.clientCount++;
+    return workerContext;
+  }
   const scriptURL = new self.URL(path, self.location.href).href;
   const worker = new self.Worker(scriptURL);
-
-  return {
+  const result = {
     worker,
     scriptURL,
+    clientCount: 1,
   };
+  workerStore[path] = result;
+  return result;
+}
+
+export function removeWorkerFromStore(path?: string | null) {
+  const workerContext = workerStore[path || version];
+  if (workerContext) {
+    const clientCount = workerContext.clientCount--;
+    if (clientCount === 1) {
+      const { worker, objectURL } = workerContext;
+      delete workerStore[path || version];
+      if (objectURL) {
+        // revoke the Object URL that was used to create transmuxer worker, so as not to leak it
+        self.URL.revokeObjectURL(objectURL);
+      }
+      worker.terminate();
+    }
+  }
 }

commit 5434baf5eae97d807aeaf169b29f69d03a419ca4

Fix logger reference error in TS demuxer (introduced in #6550 - found testing TS with encrypted samples)

src/demux/transmuxer.ts
@@ -55,7 +55,6 @@ export default class Transmuxer {
   private observer: HlsEventEmitter;
   private typeSupported: TypeSupported;
   private config: HlsConfig;
-  private vendor: string;
   private id: PlaylistLevelType;
   private demuxer?: Demuxer;
   private remuxer?: Remuxer;
@@ -76,7 +75,6 @@ export default class Transmuxer {
     this.observer = observer;
     this.typeSupported = typeSupported;
     this.config = config;
-    this.vendor = vendor;
     this.id = id;
     this.logger = logger;
   }
@@ -447,7 +445,7 @@ export default class Transmuxer {
   }
 
   private configureTransmuxer(data: Uint8Array): void | Error {
-    const { config, observer, typeSupported, vendor } = this;
+    const { config, observer, typeSupported } = this;
     // probe for content type
     let mux;
     for (let i = 0, len = muxConfig.length; i < len; i++) {

src/demux/tsdemuxer.ts
@@ -791,7 +791,7 @@ function parsePMT(
     switch (data[offset]) {
       case 0xcf: // SAMPLE-AES AAC
         if (!isSampleAes) {
-          logEncryptedSamplesFoundInUnencryptedStream('ADTS AAC', this.logger);
+          logEncryptedSamplesFoundInUnencryptedStream('ADTS AAC', logger);
           break;
         }
       /* falls through */
@@ -814,7 +814,7 @@ function parsePMT(
 
       case 0xdb: // SAMPLE-AES AVC
         if (!isSampleAes) {
-          logEncryptedSamplesFoundInUnencryptedStream('H.264', this.logger);
+          logEncryptedSamplesFoundInUnencryptedStream('H.264', logger);
           break;
         }
       /* falls through */
@@ -842,7 +842,7 @@ function parsePMT(
 
       case 0xc1: // SAMPLE-AES AC3
         if (!isSampleAes) {
-          logEncryptedSamplesFoundInUnencryptedStream('AC-3', this.logger);
+          logEncryptedSamplesFoundInUnencryptedStream('AC-3', logger);
           break;
         }
       /* falls through */
@@ -902,7 +902,7 @@ function parsePMT(
           observer,
           new Error('Unsupported EC-3 in M2TS found'),
           undefined,
-          this.logger,
+          logger,
         );
         return result;
 
@@ -918,7 +918,7 @@ function parsePMT(
             observer,
             new Error('Unsupported HEVC in M2TS found'),
             undefined,
-            this.logger,
+            logger,
           );
           return result;
         }