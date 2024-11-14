f53373f586362a8ca2d764d14794ad4e1a4d576e
Stefan Huber <sh@signalwerk.ch>
2024-11-14 20:03:54 +0100

    feat: `maxDevicePixelRatio` limits the browser's value (#6825)

--- src/config.ts
+++ src/config.ts
@@ -272,6 +272,7 @@ export type HlsConfig = {
   enableSoftwareAES: boolean;
   minAutoBitrate: number;
   ignoreDevicePixelRatio: boolean;
+  maxDevicePixelRatio: number;
   preferManagedMediaSource: boolean;
   timelineOffset?: number;
   loader: { new (confg: HlsConfig): Loader<LoaderContext> };
@@ -356,6 +357,7 @@ export const hlsDefaultConfig: HlsConfig = {
   capLevelOnFPSDrop: false, // used by fps-controller
   capLevelToPlayerSize: false, // used by cap-level-controller
   ignoreDevicePixelRatio: false, // used by cap-level-controller
+  maxDevicePixelRatio: Number.POSITIVE_INFINITY, // used by cap-level-controller
   preferManagedMediaSource: true,
   initialLiveManifestSize: 1, // used by stream-controller
   maxBufferLength: 30, // used by stream-controller
