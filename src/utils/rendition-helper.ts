7a0ded1e72115ee24a8122a06d5b8ed0eddfb2ed
Rob Walch <rwalch@apple.com>
2025-01-21 15:34:35 -0800

    Match characteristics in tracks with undefined value #6962

--- src/utils/rendition-helper.ts
+++ src/utils/rendition-helper.ts
@@ -326,14 +326,7 @@ export function matchesOption(
     track: MediaPlaylist,
   ) => boolean,
 ): boolean {
-  const {
-    groupId,
-    name,
-    lang,
-    assocLang,
-    characteristics,
-    default: isDefault,
-  } = option;
+  const { groupId, name, lang, assocLang, default: isDefault } = option;
   const forced = (option as SubtitleSelectionOption).forced;
   return (
     (groupId === undefined || track.groupId === groupId) &&
@@ -342,8 +335,11 @@ export function matchesOption(
     (lang === undefined || track.assocLang === assocLang) &&
     (isDefault === undefined || track.default === isDefault) &&
     (forced === undefined || track.forced === forced) &&
-    (characteristics === undefined ||
-      characteristicsMatch(characteristics, track.characteristics)) &&
+    (!('characteristics' in option) ||
+      characteristicsMatch(
+        option.characteristics || '',
+        track.characteristics,
+      )) &&
     (matchPredicate === undefined || matchPredicate(option, track))
   );
 }
