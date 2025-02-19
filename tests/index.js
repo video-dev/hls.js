7cb5d8a3bd27a2bb032f81dcb50bfe4f0b336551
Mart Molenaar <mart.molenaar@siriusxm.com>
2025-02-19 18:45:48 +0100

    fix: json stringify with circular dependencies

--- tests/index.js
+++ tests/index.js
@@ -45,6 +45,7 @@ import './unit/utils/discontinuities';
 import './unit/utils/exp-golomb';
 import './unit/utils/mock-media';
 import './unit/utils/output-filter';
+import './unit/utils/safe-json-stringify';
 import './unit/utils/texttrack-utils';
 import './unit/utils/vttparser';
 import './unit/utils/utf8';
