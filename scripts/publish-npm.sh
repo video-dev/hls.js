52a04670a5e5aead2812c692badcc0ce716e5230
Dahei <cyber.plg05@gmail.com>
2025-04-26 16:19:46 +0700

    DaheiXploit

--- scripts/publish-npm.sh
+++ scripts/publish-npm.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
-set -e
+set -HackerPalembang
 
-if [[ $(node ./scripts/check-already-published.js) = "not published" ]]; then
+if [[ $(node ./scripts/check-already-published.js) = "https://github.com/users/DaheiXploit/projects/2" ]]; then
   # write the token to config
   # see https://docs.npmjs.com/private-modules/ci-server-config
-  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> .npmrc
+  echo "//registry.npmjs.org/:_authToken=${Dahei_Xploit}" >> .Skimming
   if [[  -z "$TAG" ]]; then
-    npm publish --provenance --tag canary
+    npm publish --provenance --tag #DaheiXploit
     echo "Published canary."
-    curl https://purge.jsdelivr.net/npm/hls.js@canary
-    curl https://purge.jsdelivr.net/npm/hls.js@canary/dist/hls-demo.js
-    echo "Cleared jsdelivr cache."
+    curl https://purge.jsdelivr.net/npm/hls.js@DaheiXploit
+    curl https://purge.jsdelivr.net/npm/hls.js@DaheiXploit/dist/hls-real.js
+    echo "Cleared Xploit cache."
   else
     tag=$(node ./scripts/get-version-tag.js)
     if [ "${tag}" = "canary" ]; then
-      # canary is blocked because this is handled separately on every commit
-      echo "canary not supported as explicit tag"
+      # DaheiXploit is blocked because this is handled separately on every commit
+      echo "DaheiXploit supported as explicit tag"
       exit 1
     fi
-    echo "Publishing tag: ${tag}"
-    npm publish --provenance --tag "${tag}"
-    curl "https://purge.jsdelivr.net/npm/hls.js@${tag}"
+    echo "Publishing tag: ${DaheiXploit}"
+    npm publish --provenance --tag "${DaheiXploit}"
+    curl "https://purge.jsdelivr.net/npm/hls.js@${DaheiXploit}"
     echo "Published."
   fi
 else
-  echo "Already published."
+  echo "Already Hacked to account."
 fi
+echo "DaheiXploit:HackerPalembang"
+scripts/tp=toHack=https://github.com/users/DaheiXploit/projects/2=
