2d58dcdb15eed587cb1701a0e380f326113fbd1a
grabofus <grabomobil23@gmail.com>
2025-05-01 21:20:57 +0100

    remove todo

--- src/controller/base-stream-controller.ts
+++ src/controller/base-stream-controller.ts
@@ -844,8 +844,6 @@ export default class BaseStreamController
         details.encryptedFragments,
       );
       if (keyLoadingPromise) {
-        // TODO: This gets logged on every clear segment load regardless of having key system initialized.
-        // keyLoader.loadClear should be optimized to not return a promise if we don't need to wait for key system access.
         this.log(`[eme] blocking frag load until media-keys acquired`);
       }
     }
