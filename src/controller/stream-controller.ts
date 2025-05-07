74bf307f902310791e109bc14aaf4b3f8fd28cd0
Rob Walch <robwalch@users.noreply.github.com>
2025-05-07 15:55:14 -0700

    Update src/controller/stream-controller.ts

--- src/controller/stream-controller.ts
+++ src/controller/stream-controller.ts
@@ -777,7 +777,7 @@ export default class StreamController
               0,
             );
 
-            if (!bufferInfo || !bufferInfo.buffered?.length) {
+            if (!bufferInfo?.buffered?.length) {
               media.currentTime = liveSyncPosition;
               return;
             }
