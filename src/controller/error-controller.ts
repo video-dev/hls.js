e7ea1372a818e769dcbc1f12165350b427523143
Rob Walch <robwalch@users.noreply.github.com>
2025-07-02 12:38:35 -0700

    Allow content-steering to penalize pathways on buffer append error without interfering with append retries when there is no alternate

--- src/controller/error-controller.ts
+++ src/controller/error-controller.ts
@@ -218,7 +218,7 @@ export default class ErrorController
       case ErrorDetails.REMUX_ALLOC_ERROR:
       case ErrorDetails.BUFFER_APPEND_ERROR:
         // Buffer-controller can set errorAction when append errors can be ignored or resolved locally
-        if (!data.errorAction && hls.levels.length > 1) {
+        if (!data.errorAction) {
           data.errorAction = this.getLevelSwitchAction(
             data,
             data.level ?? hls.loadLevel,
