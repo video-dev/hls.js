f3cec847f05f76ece79813ba16e23a23222d3a8c
Tom Jenkinson <tom@tjenkinson.me>
2026-03-27 11:59:41 +0000

    Use commit hash for nvm instead of version

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,7 +2,8 @@
 set -euo pipefail
 
 echo "Installing nvm..."
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+# v0.40.4
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/62387b8f92aa012d48202747fd75c40850e5e261/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 \. "$NVM_DIR/nvm.sh"
 
