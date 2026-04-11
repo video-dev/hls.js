890c5785bc349e8fa7d41c879580304f0060d084
Rob Walch <rwalch@apple.com>
2026-04-11 10:41:08 -0700

    Fix Interstitials live start with short sliding window (N3 or less / no seek on start)

--- src/controller/interstitials-controller.ts
+++ src/controller/interstitials-controller.ts
@@ -896,6 +896,9 @@ export default class InterstitialsController
       return;
     }
     const backwardSeek = diff <= -0.01;
+    if (this.timelinePos === -1 && !this.effectivePlayingItem) {
+      this.checkStart();
+    }
     this.timelinePos = currentTime;
     this.bufferedPos = currentTime;
 
@@ -986,6 +989,10 @@ export default class InterstitialsController
       return;
     }
 
+    if (this.timelinePos === -1 && !this.effectivePlayingItem) {
+      this.checkStart();
+    }
+
     // Only allow timeupdate to advance primary position, seeking is used for jumping back
     // this prevents primaryPos from being reset to 0 after re-attach
     if (currentTime > this.timelinePos) {
@@ -1034,18 +1041,27 @@ export default class InterstitialsController
     const effectivePlayingItem = this.effectivePlayingItem;
     if (timelinePos === -1) {
       const startPosition = this.hls.startPosition;
-      this.log(timelineMessage('checkStart', startPosition));
       this.timelinePos = startPosition;
       if (interstitialEvents.length && interstitialEvents[0].cue.pre) {
+        this.log(timelineMessage('checkStart (preroll)', startPosition));
         const index = schedule.findEventIndex(interstitialEvents[0].identifier);
         this.setSchedulePosition(index);
       } else if (startPosition >= 0 || !this.primaryLive) {
+        this.log(timelineMessage('checkStart', startPosition));
         const start = (this.timelinePos =
           startPosition > 0 ? startPosition : 0);
         const index = schedule.findItemIndexAtTime(start);
         this.setSchedulePosition(index);
+      } else {
+        this.log('[checkStart] waiting for live start');
       }
     } else if (effectivePlayingItem && !this.playingItem) {
+      this.log(
+        timelineMessage(
+          'checkStart (playing item)',
+          effectivePlayingItem.start,
+        ),
+      );
       const index = schedule.findItemIndex(effectivePlayingItem);
       this.setSchedulePosition(index);
     }
