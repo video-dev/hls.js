video-dev/hls.js

commit 2a1961a8f6b1413b53d433f419f062afd26d6ad6

Workaround Chrome silent coded frame eviction by removing later segments from buffer when PTS overlap is detected Resolves #6777

src/remux/passthrough-remuxer.ts
@@ -390,11 +390,29 @@ class PassThroughRemuxer extends Logger implements Remuxer {
       initSegment.trackId = initPTS.trackId;
     }
 
-    const startTime = decodeTime - initPTS.baseTime / initPTS.timescale;
-    const endTime = startTime + duration;
+    const startDTS = decodeTime - initPTS.baseTime / initPTS.timescale;
+    const endDTS = startDTS + duration;
+    const startPTS =
+      baseOffsetSamples?.ptsMin !== undefined
+        ? baseOffsetSamples.ptsMin / baseOffsetSamples.timescale -
+          initPTS.baseTime / initPTS.timescale
+        : startDTS;
+    const endPTS = baseOffsetSamples?.ptsMax
+      ? baseOffsetSamples.ptsMax / baseOffsetSamples.timescale -
+        initPTS.baseTime / initPTS.timescale
+      : endDTS;
+
+    // For troubleshooting duplicates of https://github.com/video-dev/hls.js/issues/6777
+    // if (videoSampleTimestamps) {
+    //   console.log(
+    //     `#6777 segment ${chunkMeta.sn}: dts: ${videoSampleTimestamps.start}-${videoSampleTimestamps.start + videoSampleTimestamps.duration} pts: ${videoSampleTimestamps.ptsMin}-${
+    //       videoSampleTimestamps.ptsMax
+    //     }`,
+    //   );
+    // }
 
     if (duration > 0) {
-      this.lastEndTime = endTime;
+      this.lastEndTime = endDTS;
     } else {
       this.warn('Duration parsed from mp4 should be greater than zero');
       this.resetNextTimestamp();
@@ -407,10 +425,10 @@ class PassThroughRemuxer extends Logger implements Remuxer {
     const track: RemuxedTrack = {
       data1,
       data2,
-      startPTS: startTime,
-      startDTS: startTime,
-      endPTS: endTime,
-      endDTS: endTime,
+      startPTS,
+      startDTS,
+      endPTS,
+      endDTS,
       type,
       hasAudio,
       hasVideo,
@@ -421,13 +439,14 @@ class PassThroughRemuxer extends Logger implements Remuxer {
 
     result.audio = hasAudio && !hasVideo ? track : undefined;
     result.video = hasVideo ? track : undefined;
+    const isVideoContiguous = this.isVideoContiguous;
     const videoSampleCount = videoSampleTimestamps?.sampleCount;
     if (videoSampleCount) {
       const firstKeyFrame = videoSampleTimestamps.keyFrameIndex;
       const independent = firstKeyFrame !== -1;
       track.nb = videoSampleCount;
       track.dropped =
-        firstKeyFrame === 0 || this.isVideoContiguous
+        firstKeyFrame === 0 || isVideoContiguous
           ? 0
           : independent
             ? firstKeyFrame
@@ -439,7 +458,7 @@ class PassThroughRemuxer extends Logger implements Remuxer {
           (videoSampleTimestamps.keyFrameStart - initPTS.baseTime) /
           initPTS.timescale;
       }
-      if (!this.isVideoContiguous) {
+      if (!isVideoContiguous) {
         result.independent = independent;
       }
       this.isVideoContiguous ||= independent;

src/utils/mp4-tools.ts
@@ -712,10 +712,11 @@ type TrackFragmentRun = {
   samples: TrackFragmentRunSample[];
 };
 export type TrackTimes = {
-  cts?: number;
   duration: number;
   keyFrameIndex?: number;
   keyFrameStart?: number;
+  ptsMin?: number;
+  ptsMax?: number;
   start: number;
   sampleCount: number;
   trun: TrackFragmentRun[];
@@ -852,10 +853,8 @@ export function getSampleData(
           };
           trackTimes.trun.push(fragRun);
         }
+        let size;
         for (let ix = 0; ix < sampleCount; ix++) {
-          const sample: TrackFragmentRunSample = {
-            size: 0,
-          };
           if (sampleDurationPresent) {
             sampleDuration = readUint32(trun, offset);
             offset += 4;
@@ -863,17 +862,18 @@ export function getSampleData(
             sampleDuration = defaultSampleDuration;
           }
           if (sampleSizePresent) {
-            sample.size = readUint32(trun, offset);
+            size = readUint32(trun, offset);
             offset += 4;
           } else {
-            sample.size = defaultSampleSize;
+            size = defaultSampleSize;
           }
-          sampleOffset += sample.size;
+          sampleOffset += size;
           if (sampleOffset <= eof) {
-            samples[ix] = sample;
+            let flags;
+            let cts = 0;
             if (sampleFlagsPresent) {
               const isNonSyncSample = trun[offset + 1] & 0x01;
-              sample.flags = {
+              flags = {
                 isNonSync: isNonSyncSample ? 1 : 0,
                 dependsOn: (trun[offset] & 0x03) === 1 ? 1 : 2,
               };
@@ -887,13 +887,30 @@ export function getSampleData(
             }
             if (sampleCompositionTimeOffsetPresent) {
               const version = trun[0];
-              if (version === 0) {
-                sample.cts = readUint32(trun, offset);
-              } else {
-                sample.cts = readSint32(trun, offset);
-              }
+              cts =
+                version === 0
+                  ? readUint32(trun, offset)
+                  : readSint32(trun, offset);
               offset += 4;
             }
+            const pts = sampleDTS + cts;
+            if (
+              !Number.isFinite(trackTimes.ptsMin) ||
+              pts < trackTimes.ptsMin!
+            ) {
+              trackTimes.ptsMin = pts;
+            }
+            if (
+              !Number.isFinite(trackTimes.ptsMax) ||
+              pts + sampleDuration > trackTimes.ptsMax!
+            ) {
+              trackTimes.ptsMax = pts + sampleDuration;
+            }
+            samples[ix] = {
+              cts,
+              flags,
+              size,
+            };
           }
           sampleDTS += sampleDuration;
           rawDuration += sampleDuration;

src/controller/buffer-controller.ts
@@ -419,6 +419,7 @@ transfer tracks: ${stringify(transferredTracks, (key, value) => (key === 'initSe
                 bufferedTimeRanges,
                 playlistType,
                 null,
+                null,
                 true,
               );
             }

src/controller/fragment-tracker.ts
@@ -5,6 +5,7 @@ import {
   type MediaFragment,
   type Part,
 } from '../loader/fragment';
+import { userAgentChromeVersion } from '../utils/user-agent';
 import type Hls from '../hls';
 import type { SourceBufferName } from '../types/buffer';
 import type { ComponentAPI } from '../types/component-api';
@@ -154,6 +155,7 @@ export class FragmentTracker implements ComponentAPI {
     elementaryStream: SourceBufferName,
     timeRange: TimeRanges,
     playlistType: PlaylistLevelType,
+    appendedFrag?: MediaFragment | null,
     appendedPart?: Part | null,
     removeAppending?: boolean,
   ) {
@@ -165,18 +167,19 @@ export class FragmentTracker implements ComponentAPI {
     const appendedPartSn = appendedPart?.fragment.sn || -1;
     Object.keys(this.fragments).forEach((key) => {
       const fragmentEntity = this.fragments[key];
-      if (!fragmentEntity) {
+      if (!fragmentEntity || !this.hls) {
         return;
       }
-      if (appendedPartSn >= fragmentEntity.body.sn) {
+      const frag = fragmentEntity.body;
+      if (appendedPartSn >= frag.sn) {
         return;
       }
       if (
         !fragmentEntity.buffered &&
         (!fragmentEntity.loaded || removeAppending)
       ) {
-        if (fragmentEntity.body.type === playlistType) {
-          this.removeFragment(fragmentEntity.body);
+        if (frag.type === playlistType) {
+          this.removeFragment(frag);
         }
         return;
       }
@@ -185,7 +188,7 @@ export class FragmentTracker implements ComponentAPI {
         return;
       }
       if (esData.time.length === 0) {
-        this.removeFragment(fragmentEntity.body);
+        this.removeFragment(frag);
         return;
       }
       esData.time.some((time: FragmentTimeRange) => {
@@ -196,10 +199,29 @@ export class FragmentTracker implements ComponentAPI {
         );
         if (isNotBuffered) {
           // Unregister partial fragment as it needs to load again to be reused
-          this.removeFragment(fragmentEntity.body);
+          this.removeFragment(frag);
         }
         return isNotBuffered;
       });
+      // Flush forward buffer in Chrome when PTS overlap detected
+      // Workaround https://github.com/video-dev/hls.js/issues/6777 (https://issues.chromium.org/u/1/issues/336839131)
+      if (appendedFrag && appendedFrag !== frag && userAgentChromeVersion()) {
+        const endPTS = appendedFrag.endPTS;
+        const otherStartPTS = frag.startPTS;
+        if (endPTS && otherStartPTS && fragmentEntity.range.video) {
+          const diff = otherStartPTS - endPTS;
+          // overlap is no more than 1/10s
+          if (diff < 0 && diff > -0.1) {
+            this.removeFragment(frag);
+            this.hls.trigger(Events.BUFFER_FLUSHING, {
+              // pad removal start to avoid accedental removal of appendedFrag
+              startOffset: endPTS + 0.004,
+              endOffset: Infinity,
+              type: 'video',
+            });
+          }
+        }
+      }
     });
   }
 
@@ -236,6 +258,7 @@ export class FragmentTracker implements ComponentAPI {
         elementaryStream,
         timeRange,
         playlistType,
+        frag,
         part,
       );
     });

src/controller/stream-controller.ts
@@ -16,6 +16,10 @@ import {
   removeEventListener,
 } from '../utils/event-listener-helper';
 import { useAlternateAudio } from '../utils/rendition-helper';
+import {
+  userAgentFirefoxVersion,
+  userAgentIsAndroidLike,
+} from '../utils/user-agent';
 import type { FragmentTracker } from './fragment-tracker';
 import type Hls from '../hls';
 import type { Fragment, MediaFragment } from '../loader/fragment';
@@ -1428,7 +1432,6 @@ export default class StreamController
         audioCodec = 'mp4a.40.5';
       }
       // Handle `audioCodecSwitch`
-      const ua = navigator.userAgent.toLowerCase();
       if (this.audioCodecSwitch) {
         if (audioCodec) {
           if (audioCodec.indexOf('mp4a.40.5') !== -1) {
@@ -1445,7 +1448,7 @@ export default class StreamController
           audioMetadata &&
           'channelCount' in audioMetadata &&
           (audioMetadata.channelCount || 1) !== 1 &&
-          ua.indexOf('firefox') === -1
+          !userAgentFirefoxVersion()
         ) {
           audioCodec = 'mp4a.40.5';
         }
@@ -1454,7 +1457,7 @@ export default class StreamController
       if (
         audioCodec &&
         audioCodec.indexOf('mp4a.40.5') !== -1 &&
-        ua.indexOf('android') !== -1 &&
+        userAgentIsAndroidLike() &&
         audio.container !== 'audio/mpeg'
       ) {
         // Exclude mpeg audio

src/demux/audio/mpegaudio.ts
@@ -1,9 +1,8 @@
 /**
  *  MPEG parser helper
  */
+import { userAgentChromeVersion } from '../../utils/user-agent';
 import type { DemuxedAudioTrack } from '../../types/demuxer';
-
-let chromeVersion: number | null = null;
 
 const BitratesMap = [
   32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448, 32, 48, 56,
@@ -115,11 +114,7 @@ export function parseHeader(data: Uint8Array, offset: number) {
       Math.floor((sampleCoefficient * bitRate) / sampleRate + paddingBit) *
       bytesInSlot;
 
-    if (chromeVersion === null) {
-      const userAgent = navigator.userAgent || '';
-      const result = userAgent.match(/Chrome\/(\d+)/i);
-      chromeVersion = result ? parseInt(result[1]) : 0;
-    }
+    const chromeVersion = userAgentChromeVersion();
     const needChromeFix = !!chromeVersion && chromeVersion <= 87;
 
     if (

src/remux/mp4-remuxer.ts
@@ -9,6 +9,10 @@ import {
   timestampToString,
   toMsFromMpegTsClock,
 } from '../utils/timescale-conversion';
+import {
+  userAgentChromeVersion,
+  userAgentSafariVersion,
+} from '../utils/user-agent';
 import type { HlsConfig } from '../config';
 import type { HlsEventEmitter } from '../events';
 import type { ChunkMetadata } from '../hls';
@@ -42,9 +46,6 @@ const AAC_SAMPLES_PER_FRAME = 1024;
 const MPEG_AUDIO_SAMPLE_PER_FRAME = 1152;
 const AC3_SAMPLES_PER_FRAME = 1536;
 const MPEG_TS_PTS_ROLLOVER = 8589934592; // 2^33
-
-let chromeVersion: number | null = null;
-let safariWebkitVersion: number | null = null;
 
 function createMp4Sample(
   isKeyframe: boolean,
@@ -96,16 +97,6 @@ export default class MP4Remuxer extends Logger implements Remuxer {
     this.config = config;
     this.typeSupported = typeSupported;
     this.ISGenerated = false;
-
-    if (chromeVersion === null) {
-      const userAgent = navigator.userAgent || '';
-      const result = userAgent.match(/Chrome\/(\d+)/i);
-      chromeVersion = result ? parseInt(result[1]) : 0;
-    }
-    if (safariWebkitVersion === null) {
-      const result = navigator.userAgent.match(/Safari\/(\d+)/i);
-      safariWebkitVersion = result ? parseInt(result[1]) : 0;
-    }
   }
 
   destroy() {
@@ -563,7 +554,7 @@ export default class MP4Remuxer extends Logger implements Remuxer {
         inputSamples[0].pts -
         normalizePts(inputSamples[0].dts, inputSamples[0].pts);
       if (
-        chromeVersion &&
+        userAgentChromeVersion() &&
         nextVideoTs !== null &&
         Math.abs(pts - cts - (nextVideoTs + initTime)) < 15000
       ) {
@@ -635,7 +626,7 @@ export default class MP4Remuxer extends Logger implements Remuxer {
         if (
           !foundOverlap ||
           nextVideoPts >= inputSamples[0].pts ||
-          chromeVersion
+          userAgentChromeVersion()
         ) {
           firstDTS = nextVideoPts;
           const firstPTS = inputSamples[0].pts - delta;
@@ -832,6 +823,7 @@ export default class MP4Remuxer extends Logger implements Remuxer {
     }
 
     if (outputSamples.length) {
+      const chromeVersion = userAgentChromeVersion();
       if (chromeVersion) {
         if (chromeVersion < 70) {
           // Chrome workaround, mark first sample as being a Random Access Point (keyframe) to avoid sourcebuffer append issue
@@ -840,7 +832,7 @@ export default class MP4Remuxer extends Logger implements Remuxer {
           flags.dependsOn = 2;
           flags.isNonSync = 0;
         }
-      } else if (safariWebkitVersion) {
+      } else if (userAgentSafariVersion()) {
         // Fix for "CNN special report, with CC" in test-streams (Safari browser only)
         // Ignore DTS when frame durations are irregular. Safari MSE does not handle this leading to gaps.
         if (
@@ -913,6 +905,12 @@ export default class MP4Remuxer extends Logger implements Remuxer {
       nb: outputSamples.length,
       dropped: track.dropped,
     };
+    // For troubleshooting duplicates of https://github.com/video-dev/hls.js/issues/6374
+    // console.log(
+    //   `#6374 segment ${chunkMeta.sn} timeOffset: ${timeOffset} dts: ${firstDTS}-${endDTS} pts: ${minPTS}-${maxPTS} cts[0]=${
+    //     outputSamples[0].cts
+    //   } cts[${outputSamples.length - 1}]=${outputSamples[outputSamples.length - 1].cts} sample-duration: ${mp4SampleDuration} timescale: ${timeScale}`,
+    // );
     track.samples = [];
     track.dropped = 0;
     return data;

commit fdaedde413cb9c1c9e592428edd5652a69228b11

Fix regression in fmp4 endPTS parsing introduced in #7807 Fix handling of audio fragment parsing error prior to appending init-segment

src/controller/audio-stream-controller.ts
@@ -905,6 +905,9 @@ class AudioStreamController
     // If we are, subsequently check if the currently loading fragment (fragCurrent) has changed.
     if (this.fragContextChanged(frag) || !details) {
       this.fragmentTracker.removeFragment(frag);
+      if (initSegment?.tracks) {
+        this.resetTransmuxer();
+      }
       return;
     }
 

src/controller/base-stream-controller.ts
@@ -1663,7 +1663,7 @@ export default class BaseStreamController
       if (nextPart > -1 && targetBufferTime < part.start) {
         break;
       }
-      const loaded = part.loaded;
+      const loaded = part.loaded || part.gap;
       if (loaded) {
         nextPart = -1;
       } else if (
@@ -1672,7 +1672,7 @@ export default class BaseStreamController
       ) {
         nextPart = i;
       }
-      contiguous = loaded;
+      contiguous = loaded && !part.gap;
     }
     const part = partList[nextPart];
     if (part && part.fragment !== frag) {
@@ -2055,7 +2055,6 @@ export default class BaseStreamController
       this.resetFragmentErrors(filterType);
       if (part) {
         part.gap = true;
-        frag.clearElementaryStreamInfo();
       } else {
         this.treatAsGap(frag);
       }

src/remux/passthrough-remuxer.ts
@@ -3,13 +3,12 @@ import {
   flushTextTrackMetadataCueSamples,
   flushTextTrackUserdataCueSamples,
 } from './mp4-remuxer';
-import { ErrorDetails, ErrorTypes } from '../errors';
-import { Events, type HlsEventEmitter } from '../events';
 import { ElementaryStreamTypes } from '../loader/fragment';
 import { getCodecCompatibleName } from '../utils/codecs';
 import { type ILogger, Logger } from '../utils/logger';
 import { patchEncyptionData, writeUint32 } from '../utils/mp4-tools';
 import { getSampleData, parseInitSegment } from '../utils/mp4-tools';
+import type { HlsEventEmitter } from '../events';
 import type { TrackFragmentSample } from './mp4-generator';
 import type { HlsConfig } from '../config';
 import type { DecryptData } from '../loader/level-key';
@@ -402,14 +401,15 @@ class PassThroughRemuxer extends Logger implements Remuxer {
     const startDTS = decodeTime - initPTS.baseTime / initPTS.timescale;
     const endDTS = startDTS + duration;
     const startPTS =
-      baseOffsetSamples?.ptsMin !== undefined
+      hasVideo && baseOffsetSamples?.ptsMin !== undefined
         ? baseOffsetSamples.ptsMin / baseOffsetSamples.timescale -
           initPTS.baseTime / initPTS.timescale
         : startDTS;
-    const endPTS = baseOffsetSamples?.ptsMax
-      ? baseOffsetSamples.ptsMax / baseOffsetSamples.timescale -
-        initPTS.baseTime / initPTS.timescale
-      : endDTS;
+    const endPTS =
+      hasVideo && baseOffsetSamples?.ptsMax
+        ? baseOffsetSamples.ptsMax / baseOffsetSamples.timescale -
+          initPTS.baseTime / initPTS.timescale
+        : endDTS;
 
     // For troubleshooting duplicates of https://github.com/video-dev/hls.js/issues/6777
     // if (videoSampleTimestamps) {

src/utils/discontinuities.ts
@@ -32,9 +32,7 @@ export function shouldAlignOnDiscontinuities(
 
 function adjustFragmentStart(frag: Fragment, sliding: number) {
   const start = frag.start + sliding;
-  frag.startPTS = start;
   frag.setStart(start);
-  frag.endPTS = start + frag.duration;
 }
 
 export function adjustSlidingStart(sliding: number, details: LevelDetails) {
@@ -148,7 +146,9 @@ export function alignMediaPlaylistByPDT(
     frag = findFirstFragWithCC(fragments, targetCC);
   }
   if (!refFrag || !frag) {
-    refFrag = refFragments[Math.floor(refFragments.length / 2)];
+    refFrag =
+      refFragments[Math.floor(refFragments.length / 2)] ||
+      refFragments.filter((f) => !!f)[0];
     frag =
       findFirstFragWithCC(fragments, refFrag.cc) ||
       fragments[Math.floor(fragments.length / 2)];

src/utils/mp4-tools.ts
@@ -759,11 +759,12 @@ export function getSampleData(
         type: track.type,
       });
       // get start DTS
+      let baseTime: number | undefined;
       const tfdt = findBox(traf, ['tfdt'])[0];
 
       if (tfdt as any) {
         const version = tfdt[0];
-        let baseTime = readUint32(tfdt, 4);
+        baseTime = readUint32(tfdt, 4);
         if (version === 1) {
           // If value is too large, assume signed 64-bit. Negative track fragment decode times are invalid, but they exist in the wild.
           // This prevents large values from being used for initPTS, which can cause playlist sync issues.
@@ -815,7 +816,7 @@ export function getSampleData(
       }
 
       const truns = findBox(traf, ['trun']);
-      let sampleDTS = trackTimes.start || 0;
+      let sampleDTS = baseTime || 0;
       let rawDuration = 0;
       let sampleDuration = defaultSampleDuration;
       for (let j = 0; j < truns.length; j++) {

tests/unit/utils/discontinuities.ts
@@ -18,8 +18,6 @@ use(sinonChai);
 
 const mockReferenceFrag = objToFragment({
   start: 20,
-  startPTS: 20,
-  endPTS: 24,
   duration: 4,
   cc: 0,
 });
@@ -27,22 +25,16 @@ const mockReferenceFrag = objToFragment({
 const mockFrags = [
   {
     start: 0,
-    startPTS: 0,
-    endPTS: 4,
     duration: 4,
     cc: 0,
   },
   {
     start: 4,
-    startPTS: 4,
-    endPTS: 8,
     duration: 4,
     cc: 1,
   },
   {
     start: 8,
-    startPTS: 8,
-    endPTS: 16,
     duration: 8,
     cc: 1,
   },
@@ -58,29 +50,23 @@ describe('discontinuities', function () {
     const expected = [
       {
         start: 20,
-        startPTS: 20,
-        endPTS: 24,
         duration: 4,
         cc: 0,
       },
       {
         start: 24,
-        startPTS: 24,
-        endPTS: 28,
         duration: 4,
         cc: 1,
       },
       {
         start: 28,
-        startPTS: 28,
-        endPTS: 36,
         duration: 8,
         cc: 1,
       },
     ].map(objToFragment);
 
     adjustSlidingStart(mockReferenceFrag.start, details);
-    expect(expected).to.deep.equal(details.fragments);
+    expect(details.fragments).to.deep.equal(expected);
     expect(details.alignedSliding).to.be.true;
   });
 
@@ -94,30 +80,22 @@ describe('discontinuities', function () {
         fragments: [
           {
             start: 18,
-            startPTS: undefined,
-            endPTS: undefined,
             duration: 2,
             programDateTime: 1629821766107,
           },
           {
             start: 20,
-            startPTS: undefined,
-            endPTS: 22,
             duration: 2,
             programDateTime: 1629821768107,
           },
           {
             start: 22,
-            startPTS: 22,
-            endPTS: 30,
             duration: 8,
             programDateTime: 1629821770107,
           },
         ].map(objToFragment),
         fragmentHint: objToFragment({
           start: 30,
-          startPTS: 30,
-          endPTS: 32,
           duration: 2,
           programDateTime: 1629821778107,
         }),
@@ -128,8 +106,6 @@ describe('discontinuities', function () {
       fragments: [
         {
           start: 18,
-          startPTS: undefined,
-          endPTS: undefined,
           duration: 2,
           programDateTime: 1629821768107,
         },
@@ -144,30 +120,22 @@ describe('discontinuities', function () {
       fragments: [
         {
           start: 16,
-          startPTS: 16,
-          endPTS: 18,
           duration: 2,
           programDateTime: 1629821766107,
         },
         {
           start: 18,
-          startPTS: 18,
-          endPTS: 20,
           duration: 2,
           programDateTime: 1629821768107,
         },
         {
           start: 20,
-          startPTS: 20,
-          endPTS: 28,
           duration: 8,
           programDateTime: 1629821770107,
         },
       ].map(objToFragment),
       fragmentHint: objToFragment({
         start: 28,
-        startPTS: 28,
-        endPTS: 30,
         duration: 2,
         programDateTime: 1629821778107,
       }),
@@ -197,32 +165,24 @@ describe('discontinuities', function () {
         fragments: [
           {
             start: 20,
-            startPTS: 20,
-            endPTS: 24,
             duration: 4,
             cc: 1,
             programDateTime: 1503892800000,
           },
           {
             start: 24,
-            startPTS: 24,
-            endPTS: 28,
             duration: 4,
             cc: 2,
             programDateTime: 1503892850000,
           },
           {
             start: 28,
-            startPTS: 28,
-            endPTS: 36,
             duration: 8,
             cc: 3,
             programDateTime: 1501111110000,
           },
           {
             start: 28,
-            startPTS: 28,
-            endPTS: 36,
             duration: 8,
             cc: 3,
             programDateTime: 1501111118000,
@@ -235,32 +195,24 @@ describe('discontinuities', function () {
       fragments: [
         {
           start: 0,
-          startPTS: 0,
-          endPTS: 4,
           duration: 4,
           cc: 2,
           programDateTime: 1503892850000,
         },
         {
           start: 4,
-          startPTS: 4,
-          endPTS: 8,
           duration: 4,
           cc: 3,
           programDateTime: 1501111110000,
         },
         {
           start: 8,
-          startPTS: 8,
-          endPTS: 12,
           duration: 4,
           cc: 3,
           programDateTime: 1501111114000,
         },
         {
           start: 12,
-          startPTS: 12,
-          endPTS: 16,
           duration: 4,
           cc: 4,
           programDateTime: 1503892854000,
@@ -276,32 +228,24 @@ describe('discontinuities', function () {
       fragments: [
         {
           start: 24,
-          startPTS: 24,
-          endPTS: 28,
           duration: 4,
           cc: 2,
           programDateTime: 1503892850000,
         },
         {
           start: 28,
-          startPTS: 28,
-          endPTS: 32,
           duration: 4,
           cc: 3,
           programDateTime: 1501111110000,
         },
         {
           start: 32,
-          startPTS: 32,
-          endPTS: 36,
           duration: 4,
           cc: 3,
           programDateTime: 1501111114000,
         },
         {
           start: 36,
-          startPTS: 36,
-          endPTS: 40,
           duration: 4,
           cc: 4,
           programDateTime: 1503892854000,
@@ -326,24 +270,18 @@ describe('discontinuities', function () {
         fragments: [
           {
             start: 20,
-            startPTS: 20,
-            endPTS: 24,
             duration: 4,
             cc: 0,
             programDateTime: 1503892800000,
           },
           {
             start: 24,
-            startPTS: 24,
-            endPTS: 28,
             duration: 4,
             cc: 1,
             programDateTime: 1503892804000,
           },
           {
             start: 28,
-            startPTS: 28,
-            endPTS: 36,
             duration: 8,
             cc: 1,
             programDateTime: 1503892808000,
@@ -356,24 +294,18 @@ describe('discontinuities', function () {
       fragments: [
         {
           start: 0,
-          startPTS: 0,
-          endPTS: 4,
           duration: 4,
           cc: 2,
           programDateTime: 1503892850000,
         },
         {
           start: 4,
-          startPTS: 4,
-          endPTS: 8,
           duration: 4,
           cc: 2,
           programDateTime: 1503892854000,
         },
         {
           start: 8,
-          startPTS: 8,
-          endPTS: 16,
           duration: 8,
           cc: 3,
           programDateTime: 1503892858000,
@@ -389,8 +321,6 @@ describe('discontinuities', function () {
       fragments: [
         {
           start: 70,
-          startPTS: 70,
-          endPTS: 74,
           duration: 4,
           cc: 2,
           programDateTime: 1503892850000,
@@ -398,8 +328,6 @@ describe('discontinuities', function () {
         },
         {
           start: 74,
-          startPTS: 74,
-          endPTS: 78,
           duration: 4,
           cc: 2,
           programDateTime: 1503892854000,
@@ -407,8 +335,6 @@ describe('discontinuities', function () {
         },
         {
           start: 78,
-          startPTS: 78,
-          endPTS: 86,
           duration: 8,
           cc: 3,
           programDateTime: 1503892858000,
@@ -431,10 +357,9 @@ describe('discontinuities', function () {
   describe('alignDiscontinuities', function () {
     it('aligns playlists (LevelDetails fragments starts) based on overlapping discontinuity sequence change', function () {
       const prevDetails = objToLevelDetails({
-        fragments: [
-          mockReferenceFrag,
-          { start: 24, startPTS: 24, endPTS: 28, duration: 4, cc: 1 },
-        ].map(objToFragment),
+        fragments: [mockReferenceFrag, { start: 24, duration: 4, cc: 1 }].map(
+          objToFragment,
+        ),
       });
       const curDetails = objToLevelDetails({
         fragments: mockFrags.map(objToFragment),
@@ -448,8 +373,8 @@ describe('discontinuities', function () {
       const prevDetails = objToLevelDetails({
         fragments: [
           mockReferenceFrag,
-          { start: 24, startPTS: 24, endPTS: 28, duration: 4, cc: 1 },
-          { start: 28, startPTS: 28, endPTS: 32, duration: 4, cc: 2 },
+          { start: 24, duration: 4, cc: 1 },
+          { start: 28, duration: 4, cc: 2 },
         ].map(objToFragment),
       });
       const curDetails = objToLevelDetails({
@@ -464,15 +389,15 @@ describe('discontinuities', function () {
       const prevDetails = objToLevelDetails({
         fragments: [
           mockReferenceFrag,
-          { start: 24, startPTS: 24, endPTS: 28, duration: 4.5, cc: 1 },
-          { start: 28.5, startPTS: 28, endPTS: 32, duration: 4, cc: 2 },
+          { start: 24, duration: 4.5, cc: 1 },
+          { start: 28.5, duration: 4, cc: 2 },
         ].map(objToFragment),
       });
       const curDetails = objToLevelDetails({
         fragments: [
-          { start: 0, startPTS: 0, endPTS: 4, duration: 4.5, cc: 1 },
-          { start: 4.5, startPTS: 4.5, endPTS: 8.5, duration: 4, cc: 2 },
-          { start: 8.5, startPTS: 4.5, endPTS: 12.5, duration: 4, cc: 3 },
+          { start: 0, duration: 4.5, cc: 1 },
+          { start: 4.5, duration: 4, cc: 2 },
+          { start: 8.5, duration: 4, cc: 3 },
         ].map(objToFragment),
       });
       alignDiscontinuities(curDetails, prevDetails, logger);