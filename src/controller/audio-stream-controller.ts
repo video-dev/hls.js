47a75b49c6f3b7d6371b426e76ac77d2b6f589c3
Rob Walch <rwalch@apple.com>
2026-04-29 13:42:03 -0700

    Fix api-extractor output for dependency update in #7824

--- src/controller/audio-stream-controller.ts
+++ src/controller/audio-stream-controller.ts
@@ -139,17 +139,8 @@ class AudioStreamController
   }
 
   // INIT_PTS_FOUND is triggered when the video track parsed in the stream-controller has a new PTS value
-  onInitPtsFound(
-    event: Events.INIT_PTS_FOUND,
-    {
-      frag,
-      id,
-      initPTS,
-      timescale,
-      trackId,
-      timestampOffsets,
-    }: InitPTSFoundData,
-  ) {
+  onInitPtsFound(event: Events.INIT_PTS_FOUND, data: InitPTSFoundData) {
+    const { frag, id, initPTS, timescale, trackId, timestampOffsets } = data;
     // Always update the new INIT PTS
     // Can change due level switch
     if (id === PlaylistLevelType.MAIN) {
