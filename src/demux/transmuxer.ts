video-dev/hls.js

commit 55355d18643d344620945653033b59d8543cc5f8

Add support for #EXT-X-I-FRAME-STREAM-INF and #EXT-X-I-FRAMES-ONLY (#7757)

src/demux/transmuxer.ts
@@ -95,7 +95,7 @@ export default class Transmuxer {
     const stats = chunkMeta.transmuxing;
     stats.executeStart = now();
 
-    let uintData: Uint8Array<ArrayBuffer> = new Uint8Array(data);
+    const uintData: Uint8Array<ArrayBuffer> = new Uint8Array(data);
     const { currentTransmuxState, transmuxConfig } = this;
     if (state) {
       this.currentTransmuxState = state;
@@ -122,65 +122,59 @@ export default class Transmuxer {
       const decrypter = this.getDecrypter();
       const aesMode = getAesModeFromFullSegmentMethod(keyData.method);
 
-      // Software decryption is synchronous; webCrypto is not
-      if (decrypter.isSync()) {
-        // Software decryption is progressive. Progressive decryption may not return a result on each call. Any cached
-        // data is handled in the flush() call
-        let decryptedData = decrypter.softwareDecrypt(
+      const plainTextLength =
+        chunkMeta.size !== data.byteLength && chunkMeta.iframe
+          ? chunkMeta.size
+          : 0;
+
+      this.asyncResult = true;
+      this.decryptionPromise = decrypter
+        .decrypt(
           uintData,
           keyData.key.buffer,
           keyData.iv.buffer,
           aesMode,
-        );
-        // For Low-Latency HLS Parts, decrypt in place, since part parsing is expected on push progress
-        const loadingParts = chunkMeta.part > -1;
-        if (loadingParts) {
-          const data = decrypter.flush();
-          decryptedData = data ? data.buffer : data;
-        }
-        if (!decryptedData) {
-          stats.executeEnd = now();
-          return emptyResult(chunkMeta);
-        }
-        uintData = new Uint8Array(decryptedData);
-      } else {
-        this.asyncResult = true;
-        this.decryptionPromise = decrypter
-          .webCryptoDecrypt(
-            uintData,
-            keyData.key.buffer,
-            keyData.iv.buffer,
-            aesMode,
-          )
-          .then((decryptedData): TransmuxerResult => {
-            // Calling push here is important; if flush() is called while this is still resolving, this ensures that
-            // the decrypted data has been transmuxed
-            const result = this.push(
-              decryptedData,
-              null,
-              chunkMeta,
-            ) as TransmuxerResult;
-            this.decryptionPromise = null;
-            return result;
-          });
-        return this.decryptionPromise;
-      }
+          plainTextLength,
+        )
+        .then((decryptedData): TransmuxerResult => {
+          // Calling push here is important; if flush() is called while this is still resolving, this ensures that
+          // the decrypted data has been transmuxed
+          const result = this.push(
+            decryptedData,
+            null,
+            chunkMeta,
+          ) as TransmuxerResult;
+          this.decryptionPromise = null;
+          return result;
+        });
+      return this.decryptionPromise;
     }
 
     const resetMuxers = this.needsProbing(discontinuity, trackSwitch);
+
     if (resetMuxers) {
-      const error = this.configureTransmuxer(uintData);
-      if (error) {
-        this.logger.warn(`[transmuxer] ${error.message}`);
-        this.observer.emit(Events.ERROR, Events.ERROR, {
-          type: ErrorTypes.MEDIA_ERROR,
-          details: ErrorDetails.FRAG_PARSING_ERROR,
-          fatal: false,
-          error,
-          reason: error.message,
-        });
-        stats.executeEnd = now();
-        return emptyResult(chunkMeta);
+      if (!this.demuxer) {
+        // Configure the demuxer using an init segment when I-FRAME MPEG2-TS (I-FRAME media segments have no PMT).
+        // MP4 probing only works on media segments (which have moof data).
+        const segmentFormatData =
+          chunkMeta.iframe &&
+          initSegmentData &&
+          TSDemuxer.probe(initSegmentData, this.logger)
+            ? initSegmentData
+            : uintData;
+        const error = this.configureTransmuxer(segmentFormatData);
+        if (error) {
+          this.logger.warn(`[transmuxer] ${error.message}`);
+          this.observer.emit(Events.ERROR, Events.ERROR, {
+            type: ErrorTypes.MEDIA_ERROR,
+            details: ErrorDetails.FRAG_PARSING_ERROR,
+            fatal: false,
+            error,
+            reason: error.message,
+          });
+          stats.executeEnd = now();
+          return emptyResult(chunkMeta);
+        }
       }
     }
 
@@ -191,6 +185,7 @@ export default class Transmuxer {
         videoCodec,
         duration,
         decryptdata,
+        chunkMeta,
       );
     }
 
@@ -265,7 +260,7 @@ export default class Transmuxer {
       return emptyResults;
     }
 
-    const demuxResultOrPromise = demuxer.flush(timeOffset);
+    const demuxResultOrPromise = demuxer.flush(timeOffset, chunkMeta);
     if (isPromise(demuxResultOrPromise)) {
       this.asyncResult = true;
       // Decrypt final SAMPLE-AES samples
@@ -303,6 +298,7 @@ export default class Transmuxer {
       accurateTimeOffset,
       true,
       this.id,
+      chunkMeta,
     );
     transmuxResults.push({
       remuxResult,
@@ -336,6 +332,7 @@ export default class Transmuxer {
     videoCodec: string | undefined,
     trackDuration: number,
     decryptdata: DecryptData | null,
+    chunkMeta: ChunkMetadata,
   ) {
     const { demuxer, remuxer } = this;
     if (!demuxer || !remuxer) {
@@ -346,6 +343,8 @@ export default class Transmuxer {
       audioCodec,
       videoCodec,
       trackDuration,
+      decryptdata,
+      chunkMeta,
     );
     remuxer.resetInitSegment(
       initSegmentData,
@@ -401,7 +400,7 @@ export default class Transmuxer {
   ): TransmuxerResult {
     const { audioTrack, videoTrack, id3Track, textTrack } = (
       this.demuxer as Demuxer
-    ).demux(data, timeOffset, false, !this.config.progressive);
+    ).demux(data, timeOffset, chunkMeta, false, !this.config.progressive);
     const remuxResult = this.remuxer!.remux(
       audioTrack,
       videoTrack,
@@ -411,6 +410,7 @@ export default class Transmuxer {
       accurateTimeOffset,
       false,
       this.id,
+      chunkMeta,
     );
     return {
       remuxResult,
@@ -426,7 +426,7 @@ export default class Transmuxer {
     chunkMeta: ChunkMetadata,
   ): Promise<TransmuxerResult> {
     return (this.demuxer as Demuxer)
-      .demuxSampleAes(data, decryptData, timeOffset)
+      .demuxSampleAes(data, decryptData, timeOffset, chunkMeta)
       .then((demuxResult) => {
         const remuxResult = this.remuxer!.remux(
           demuxResult.audioTrack,
@@ -437,6 +437,7 @@ export default class Transmuxer {
           accurateTimeOffset,
           false,
           this.id,
+          chunkMeta,
         );
         return {
           remuxResult,
@@ -445,10 +446,10 @@ export default class Transmuxer {
       });
   }
 
-  private configureTransmuxer(data: Uint8Array): void | Error {
+  private configureTransmuxer(data: Uint8Array): undefined | Error {
     const { config, observer, typeSupported } = this;
     // probe for content type
-    let mux;
+    let mux: MuxConfig | undefined;
     for (let i = 0, len = muxConfig.length; i < len; i++) {
       if (muxConfig[i].demux?.probe(data, this.logger)) {
         mux = muxConfig[i];
@@ -461,14 +462,16 @@ export default class Transmuxer {
     // so let's check that current remuxer and demuxer are still valid
     const demuxer = this.demuxer;
     const remuxer = this.remuxer;
-    const Remuxer: MuxConfig['remux'] = mux.remux;
     const Demuxer: MuxConfig['demux'] = mux.demux;
-    if (!remuxer || !(remuxer instanceof Remuxer)) {
-      this.remuxer = new Remuxer(observer, config, typeSupported, this.logger);
-    }
+    const Remuxer: MuxConfig['remux'] = mux.remux;
     if (!demuxer || !(demuxer instanceof Demuxer)) {
+      this.logger.log(`Using ${Demuxer.name}`);
       this.demuxer = new Demuxer(observer, config, typeSupported, this.logger);
       this.probe = Demuxer.probe;
+    }
+    if (!remuxer || !(remuxer instanceof Remuxer)) {
+      this.logger.log(`Using ${Remuxer.name}`);
+      this.remuxer = new Remuxer(observer, config, typeSupported, this.logger);
     }
   }
 

src/types/transmuxer.ts
@@ -14,6 +14,8 @@ export class ChunkMetadata {
   public readonly id: number;
   public readonly size: number;
   public readonly partial: boolean;
+  public readonly iframe: boolean;
+  public readonly duration: number;
   public readonly transmuxing: HlsChunkPerformanceTiming =
     getNewPerformanceTiming();
   public readonly buffering: {
@@ -31,6 +33,8 @@ export class ChunkMetadata {
     size = 0,
     part = -1,
     partial = false,
+    duration?: number,
+    iframe?: boolean,
   ) {
     this.level = level;
     this.sn = sn;
@@ -38,6 +42,8 @@ export class ChunkMetadata {
     this.size = size;
     this.part = part;
     this.partial = partial;
+    this.duration = duration || 0;
+    this.iframe = iframe || false;
   }
 }
 

.eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
     __USE_M2TS_ADVANCED_CODECS__: true,
     __USE_MEDIA_CAPABILITIES__: true,
     __USE_INTERSTITIALS__: true,
+    __USE_IFRAMES__: true,
   },
   // see https://github.com/standard/eslint-config-standard
   // 'prettier' (https://github.com/prettier/eslint-config-prettier) must be last

build-config.js
@@ -62,12 +62,8 @@ const flags = {
   variableSubstitution: readFeatureFlag('VARIABLE_SUBSTITUTION'),
   m2tsAdvancedCodecs: readFeatureFlag('M2TS_ADVANCED_CODECS'),
   mediaCapabilities: readFeatureFlag('MEDIA_CAPABILITIES'),
-  interstitials: readFeatureFlag(
-    'INTERSTITIALS',
-    // Backward-compatible support for legacy misspelled env vars
-    'USE_INTERSTITALS',
-    'INTERSTITALS',
-  ),
+  interstitials: readFeatureFlag('INTERSTITIALS'),
+  iframes: readFeatureFlag('IFRAMES'),
 };
 
 function getFeatureSupport(type) {
@@ -81,6 +77,7 @@ function getFeatureSupport(type) {
     m2tsAdvancedCodecs: isFeatureEnabled(type, flags.m2tsAdvancedCodecs),
     mediaCapabilities: isFeatureEnabled(type, flags.mediaCapabilities),
     interstitials: isFeatureEnabled(type, flags.interstitials),
+    iframes: isFeatureEnabled(type, flags.iframes),
   };
 }
 
@@ -105,6 +102,7 @@ const buildConstants = (
     __USE_M2TS_ADVANCED_CODECS__: JSON.stringify(features.m2tsAdvancedCodecs),
     __USE_MEDIA_CAPABILITIES__: JSON.stringify(features.mediaCapabilities),
     __USE_INTERSTITIALS__: JSON.stringify(features.interstitials),
+    __USE_IFRAMES__: JSON.stringify(features.iframes),
 
     ...additional,
   },
@@ -316,6 +314,13 @@ function getAliasesForDist(format, features) {
       '../controller/interstitials-schedule': `../${emptyFile}`,
       '../loader/interstitial-event': `../${emptyFile}`,
       '../loader/interstitial-asset-list': `../${emptyFile}`,
+    };
+  }
+
+  if (!features.iframes) {
+    aliases = {
+      ...aliases,
+      './controller/iframe-controller': `./${emptyFile}`,
     };
   }
 

src/config.ts
@@ -8,7 +8,12 @@ import ContentSteeringController from './controller/content-steering-controller'
 import EMEController from './controller/eme-controller';
 import ErrorController from './controller/error-controller';
 import FPSController from './controller/fps-controller';
+import GapController from './controller/gap-controller';
+import ID3TrackController from './controller/id3-track-controller';
+import { IFrameController } from './controller/iframe-controller';
 import InterstitialsController from './controller/interstitials-controller';
+import LatencyController from './controller/latency-controller';
+import StreamController from './controller/stream-controller';
 import { SubtitleStreamController } from './controller/subtitle-stream-controller';
 import SubtitleTrackController from './controller/subtitle-track-controller';
 import { TimelineController } from './controller/timeline-controller';
@@ -333,6 +338,14 @@ export type HlsConfig = {
   cmcdController?: typeof CMCDController;
   // Content Steering
   contentSteeringController?: typeof ContentSteeringController;
+  // IFrame Controller (setting to null disables I-Frame instance support)
+  iframeController?: typeof IFrameController;
+  // ID3 Track Controller
+  id3TrackController?: typeof ID3TrackController;
+  // Gap Controller
+  gapController?: typeof GapController;
+  // Latency Controller
+  latencyController?: typeof LatencyController;
   // Interstitial Controller (setting to null disables Interstitials parsing and playback)
   interstitialsController?: typeof InterstitialsController;
   // Option to disable internal playback handling of Interstitials (set to false to disable Interstitials playback without disabling parsing and schedule events)
@@ -342,15 +355,18 @@ export type HlsConfig = {
   // How many seconds past the end of a live playlist to preload Interstitial assets
   interstitialLiveLookAhead: number;
   // An optional `Hls` instance ID prefixed to debug logs
+  loggerId?: string;
+  // Identifies the `Hls` instance as an Interstitial asset player (also fills in for `loggerId`)
   assetPlayerId?: string;
   // MediaCapabilies API for level, track, and switch filtering
   useMediaCapabilities: boolean;
 
+  streamController: typeof StreamController;
   abrController: typeof AbrController;
   bufferController: typeof BufferController;
   capLevelController: typeof CapLevelController;
   errorController: typeof ErrorController;
-  fpsController: typeof FPSController;
+  fpsController?: typeof FPSController;
   progressive: boolean;
   lowLatencyMode: boolean;
   primarySessionId?: string;
@@ -443,11 +459,6 @@ export const hlsDefaultConfig: HlsConfig = {
   xhrSetup: undefined, // used by xhr-loader
   licenseXhrSetup: undefined, // used by eme-controller
   licenseResponseCallback: undefined, // used by eme-controller
-  abrController: AbrController,
-  bufferController: BufferController,
-  capLevelController: CapLevelController,
-  errorController: ErrorController,
-  fpsController: FPSController,
   stretchShortVideoTrack: false, // used by mp4-remuxer
   maxAudioFramesDrift: 1, // used by mp4-remuxer
   forceKeyFrameOnDiscontinuity: true, // used by ts-demuxer
@@ -610,6 +621,16 @@ export const hlsDefaultConfig: HlsConfig = {
   fragLoadingRetryDelay: 1000,
   fragLoadingMaxRetryTimeout: 64000,
 
+  streamController: StreamController,
+  abrController: AbrController,
+  bufferController: BufferController,
+  capLevelController: CapLevelController,
+  errorController: ErrorController,
+  fpsController: FPSController,
+  id3TrackController: ID3TrackController,
+  gapController: GapController,
+  latencyController: LatencyController,
+
   // Dynamic Modules
   ...timelineConfig(),
   subtitleStreamController: __USE_SUBTITLES__
@@ -626,6 +647,7 @@ export const hlsDefaultConfig: HlsConfig = {
   contentSteeringController: __USE_CONTENT_STEERING__
     ? ContentSteeringController
     : undefined,
+  iframeController: __USE_IFRAMES__ ? IFrameController : undefined,
   interstitialsController: __USE_INTERSTITIALS__
     ? InterstitialsController
     : undefined,

src/controller/audio-stream-controller.ts
@@ -141,14 +141,21 @@ class AudioStreamController
   // INIT_PTS_FOUND is triggered when the video track parsed in the stream-controller has a new PTS value
   onInitPtsFound(
     event: Events.INIT_PTS_FOUND,
-    { frag, id, initPTS, timescale, trackId }: InitPTSFoundData,
+    {
+      frag,
+      id,
+      initPTS,
+      timescale,
+      trackId,
+      timestampOffsets,
+    }: InitPTSFoundData,
   ) {
     // Always update the new INIT PTS
     // Can change due level switch
     if (id === PlaylistLevelType.MAIN) {
       const cc = frag.cc;
       const inFlightFrag = this.fragCurrent;
-      this.initPTS[cc] = { baseTime: initPTS, timescale, trackId };
+      this.initPTS = timestampOffsets;
       this.log(
         `InitPTS for cc: ${cc} found from main: ${initPTS / timescale} (${initPTS}/${timescale}) trackId: ${trackId}`,
       );
@@ -663,6 +670,7 @@ class AudioStreamController
         payload.byteLength,
         partIndex,
         partial,
+        frag.duration,
       );
       transmuxer.push(
         payload,

commit 028471bdec0ea14541a24ef1c68c603764ecb651

Fix demuxer reset on discontinuity (regression introduced in dev with #7757)

src/demux/transmuxer.ts
@@ -153,28 +153,26 @@ export default class Transmuxer {
     const resetMuxers = this.needsProbing(discontinuity, trackSwitch);
 
     if (resetMuxers) {
-      if (!this.demuxer) {
-        // Configure the demuxer using an init segment when I-FRAME MPEG2-TS (I-FRAME media segments have no PMT).
-        // MP4 probing only works on media segments (which have moof data).
-        const segmentFormatData =
-          chunkMeta.iframe &&
-          initSegmentData &&
-          TSDemuxer.probe(initSegmentData, this.logger)
-            ? initSegmentData
-            : uintData;
-        const error = this.configureTransmuxer(segmentFormatData);
-        if (error) {
-          this.logger.warn(`[transmuxer] ${error.message}`);
-          this.observer.emit(Events.ERROR, Events.ERROR, {
-            type: ErrorTypes.MEDIA_ERROR,
-            details: ErrorDetails.FRAG_PARSING_ERROR,
-            fatal: false,
-            error,
-            reason: error.message,
-          });
-          stats.executeEnd = now();
-          return emptyResult(chunkMeta);
-        }
+      // Configure the demuxer using an init segment when I-FRAME MPEG2-TS (I-FRAME media segments have no PMT).
+      // MP4 probing only works on media segments (which have moof data).
+      const segmentFormatData =
+        chunkMeta.iframe &&
+        initSegmentData &&
+        TSDemuxer.probe(initSegmentData, this.logger)
+          ? initSegmentData
+          : uintData;
+      const error = this.configureTransmuxer(segmentFormatData);
+      if (error) {
+        this.logger.warn(`[transmuxer] ${error.message}`);
+        this.observer.emit(Events.ERROR, Events.ERROR, {
+          type: ErrorTypes.MEDIA_ERROR,
+          details: ErrorDetails.FRAG_PARSING_ERROR,
+          fatal: false,
+          error,
+          reason: error.message,
+        });
+        stats.executeEnd = now();
+        return emptyResult(chunkMeta);
       }
     }
 